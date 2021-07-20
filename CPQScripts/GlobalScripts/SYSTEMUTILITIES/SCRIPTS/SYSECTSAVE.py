bc8c36be8aebca80b818327770555d0bfc99a347
selvirajapriyak <78843935+selvirajapriyak@users.noreply.github.com>
2021-07-20 18:57:11 +0530

    Update SYSECTSAVE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
@@ -561,6 +561,19 @@ def MaterialSave(ObjectName, RECORD, warning_msg, SectionRecId=None):
                     tableInfo.AddRow(tablerow)
                     
                     Sql.Upsert(tableInfo)
+        
+        ##calling qtpost script
+        try:
+            if TableName == 'SAQTMT' and 'QUOTE_STATUS' in RECORD.keys():
+                Trace.Write('QUOTE_STATUS -- inside')
+                if RECORD.get("QUOTE_STATUS") ==  'APPROVED':
+                    Trace.Write('inside---')
+                    crm_result = ScriptExecutor.ExecuteGlobal('QTPOSTQCRM',{'QUOTE_ID':"'"+str(Quote.GetGlobal("contract_quote_record_id"))+"'",'Fun_type':'cpq_to_crm'})
+                    Trace.Write("ends--"+str(crm_result))
+        except Exception,e:
+            Trace.Write("except---"+str(e))
+
+
 
     return "", warning_msg, str(ErrorequiredDict), ErrorequiredDictMSg, SecRecId, ErrorequiredtabDictMSg,notification,notificationinterval
 
