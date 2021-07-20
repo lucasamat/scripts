7c4fd136ba6bd302a97d48250fc51c6ce19f9216
selvirajapriyak <78843935+selvirajapriyak@users.noreply.github.com>
2021-07-20 19:15:30 +0530

    Update SYSECTSAVE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
@@ -568,8 +568,8 @@ def MaterialSave(ObjectName, RECORD, warning_msg, SectionRecId=None):
                 Trace.Write('QUOTE_STATUS -- inside')
                 if RECORD.get("QUOTE_STATUS") ==  'APPROVED':
                     #quote_id
-                    Trace.Write('inside---'+str('QTPOSTQCRM',{'QUOTE_ID':"'"+str(Quote.GetGlobal("contract_quote_record_id"))+"'",'Fun_type':'cpq_to_crm'}))
-                    crm_result = ScriptExecutor.ExecuteGlobal('QTPOSTQCRM',{'QUOTE_ID':"'"+str(Quote.GetGlobal("contract_quote_record_id"))+"'",'Fun_type':'cpq_to_crm'})
+                    #Trace.Write('inside---'+str('QTPOSTQCRM',{'QUOTE_ID':"'"+str(Quote.GetGlobal("contract_quote_record_id"))+"'",'Fun_type':'cpq_to_crm'}))
+                    crm_result = ScriptExecutor.ExecuteGlobal('QTPOSTQCRM',{'QUOTE_ID':str(Quote.GetGlobal("contract_quote_record_id")),'Fun_type':'cpq_to_crm'})
                     Trace.Write("ends--"+str(crm_result))
         except Exception,e:
             Trace.Write("except---"+str(e))
