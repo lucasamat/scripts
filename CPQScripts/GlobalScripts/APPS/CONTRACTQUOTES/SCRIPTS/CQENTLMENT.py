21bf8e096225f6b9123ee158c63e7b6a483ee52b
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-07-21 13:09:26 +0530

    Update CQENTLMENT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
@@ -1074,7 +1074,7 @@ class Entitlements:
 			
 		factcurreny = ""
 		dataent = ""
-		quoteid = str(Quote.GetGlobal("contract_quote_record_id"))
+		#quoteid = str(Quote.GetGlobal("contract_quote_record_id"))
 		getedit_calc = Sql.GetFirst("SELECT PRICE_METHOD,DATA_TYPE as DT FROM PRENVL (NOLOCK) where ENTITLEMENT_NAME = 'ADDL_PERF_GUARANTEE_91_1' AND ENTITLEMENT_VALUE_CODE = 'MANUAL INPUT' ")
 		if getedit_calc:
 			if getedit_calc.PRICE_METHOD == "MANUAL PRICE":				
