e884f98f9d7bb6cc657cd152998cc7e3e0fc4dbb
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-07-22 18:38:33 +0530

    Update CQPREVWDTL.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
@@ -312,7 +312,7 @@ def constructquoteinformation(Qt_rec_id, Quote, MODE):
 					# if sefl_api != "REGION":
 					Trace.Write('At line 289-->'+str(sefl_api))
 					sec_str += (
-						"<div class='col-md-3 pad-0'> <input type='text' title = '"+  str(eval("col_name." + str(sefl_api)))+"' value = '"
+						"<div class='col-md-3 pad-0'> <input type='text' id ='"+str(sefl_api)+"' title = '"+  str(eval("col_name." + str(sefl_api)))+"' value = '"
 						+ str(eval("col_name." + str(sefl_api)))
 						+ "' 'title':userInput}, incrementalTabIndex, enable: isEnabled' class='form-control' style='height: 28px;border-top: 0 !important;border-bottom: 0 !important;' id='' title='' tabindex='' disabled=''> </div>"
 					)
