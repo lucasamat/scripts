bf47ea3603b04e287368eb859fb5698ed97a5340
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-07-22 14:47:36 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -1089,7 +1089,7 @@ def Related_Sub_Banner(
 						FourthValue = get_val.EQUIPMENT_ID
 						FifthLable = "Serial Number"
 						FifthValue = get_val.SERIAL_NUMBER 
-					elif subTabName == "Details" and str(ObjName) == "SAQSGB"P
+					elif subTabName == "Details" and str(ObjName) == "SAQSGB":
 						PrimaryLable = "Product Offering ID"
 						PrimaryValue = str(TreeSuperParentParam)
 						SecondLable = "Fab Location ID"
