55c3657f716c216db7b2e75d9ab9d429613752e8
Viknesh <Vignesh_Vijayakumar@contractor.amat>
2021-07-26 17:59:29 +0530

    Update SYCTLKPPUP.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYCTLKPPUP.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYCTLKPPUP.py
@@ -434,6 +434,7 @@ def GSCONTLOOKUPPOPUP(
                 VAL_Obj = Sql.GetList(VAL_Str)
                 count_query=SqlHelper.GetList("SELECT COUNT(*) as cnt FROM cpq_permissions where permission_type ='0'")
             else:
+                Trace.Write('At 437')
                 if str(where).strip() != "":
                     where = " where " + str(where)
                     VAL_Str = "SELECT top 10 " + str(API_NAME_str) + " FROM " + str(TABLEID) + " " + str(where)
