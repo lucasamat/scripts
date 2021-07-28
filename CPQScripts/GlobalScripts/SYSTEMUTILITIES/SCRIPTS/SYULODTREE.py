d82e1b3463fb87aa6a53a642d575231de29ff207
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-07-28 16:45:51 +0530

    Update SYULODTREE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
@@ -1344,7 +1344,7 @@ class TreeView:
 							global g_total
 							g_total += 1
 							
-							if g_total < 25:
+							if g_total < 23:
 								Trace.Write("=============>>> Profile Tab 1111 "+str(DynamicQuery))
 								childQuery = Sql.GetList("" + str(DynamicQuery) + "")   
 							else:
