fcceed15761a20daf821ce099616b18027f33b63
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-07-30 13:30:21 +0530

    Update CQUPDLNITM.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQUPDLNITM.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQUPDLNITM.py
@@ -23,11 +23,12 @@ def getData():
         Queryload = SqlHelper.GetList("select ownerId,cartId,CONDITION_COUNTER,CONDITION_DATA_TYPE,CONDITION_RATE,CONDITION_TYPE,CONDITIONTYPE_NAME,CONDITIONTYPE_RECORD_ID,UOM,CONDITION_VALUE,UOM_RECORD_ID,LINE,QUOTE_ID,QTEITM_RECORD_ID,QUOTE_NAME,SERVICE_DESCRIPTION,SERVICE_ID,STEP_NUMBER,SERVICE_RECORD_ID,QUOTE_RECORD_ID from QT__SAQICD where ownerId = '"+Ownrid+"' and cartId = '"+CrtId+"'  and SERVICE_ID= '"+str(item.PartNumber)+"' ")
         if Queryload and counter == 0:
             for row in Queryload:
+                rolled_up_id ="dyn_" + "1"
                 if str(item.PartNumber) == str(row.SERVICE_ID) :
-                    rolled_up_id ="dyn_" + "1"
+                    
                     rate += "<td class='numberalign'>"+str(row.CONDITION_RATE).strip()+"</td>"
                     data_info += "<tr class='"+tr_class+"' id='1' ><td class='textalign' id=' "+str(row.SERVICE_ID)+"'>"+str(row.STEP_NUMBER)+"</td><td class='textalign'>"+str(row.CONDITION_TYPE)+"</td><td class='textalign'>"+str(row.CONDITIONTYPE_NAME).replace("`","'")+"</td>"+rate+"</td><td class='textalign'>'USD'</td><td class='numberalign'>"+str(row.UOM)+"</td><td class='numberalign'>"+str(row.CONDITION_VALUE)+"</td><td class='textalign'>"+str(row.UOM)+"</td><td class='textalign'>'STATIC'</td><td class='textalign'>'C'</td><td class='numberalign'>'1'</td><td class='textalign'>'INACTIVE'</td><td class='textalign'>'YO66'</td><td class='centeralign'>"+factor_id+"</td></tr>"
-            data.append({rolled_up_id:data_info})
+                data.append({rolled_up_id:data_info})
     Trace.Write(str(data))
     return data
 LoadAction = Param.LoadAction
