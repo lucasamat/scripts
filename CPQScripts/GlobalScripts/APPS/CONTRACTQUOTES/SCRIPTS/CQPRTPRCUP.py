60fbb9d223e02ae4bd117c7624147f8f78c879f1
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-07-30 13:54:13 +0530

    Update CQPRTPRCUP.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPRTPRCUP.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPRTPRCUP.py
@@ -31,7 +31,7 @@ Sql = SQL()
 QUOTE = Param.CPQ_Columns['Entries']
 script_start_time = time.time()
 Log.Info("QUOTE ID---> "+str(QUOTE)+"CPS Price Script Started")
-Log.Info("---->CPI Hitting  2021")
+Log.Info("------->CPI Hitting  2021")
 webclient = System.Net.WebClient()
 webclient.Headers[System.Net.HttpRequestHeader.ContentType] = "application/json"
 webclient.Headers[System.Net.HttpRequestHeader.Authorization] = "Basic c2ItYzQwYThiMWYtYzU5NS00ZWJjLTkyYzYtYzM4ODg4ODFmMTY0IWIyNTAzfGNwc2VydmljZXMtc2VjdXJlZCFiMzkxOm9zRzgvSC9hOGtkcHVHNzl1L2JVYTJ0V0FiMD0=";
