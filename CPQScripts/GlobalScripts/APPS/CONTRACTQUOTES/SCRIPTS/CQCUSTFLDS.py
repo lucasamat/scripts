ea10403254bb28be9e01e894843ec2ac3690c4a7
Namrata <namratasivakumar.contractor@amat.com>
2021-08-02 12:29:15 +0530

    New script
    New script

---
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCUSTFLDS.py
@@ -0,0 +1,23 @@
+# =========================================================================================================================================
+#   __script_name : CQCUSTFLDS.PY
+#   __script_description : THIS SCRIPT IS USED TO ASSIGNT THE CUSTOM FIELD VALUE TO THE CORRESPONDING CUSTOMG FIELDS.
+#   __primary_author__ :GAYATHRI AMARESAN
+#   __create_date :
+#   © BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
+# ==========================================================================================================================================
+
+def custfieldsupdated(saleprice):
+	Quote.GetCustomField('SALE_PRICE').Content = str(saleprice)
+
+
+try:
+	customfield = Param.customfield
+except Exception:
+	customfield = ""
+try:
+	saleprice = Param.saleprice
+except Exception:
+	saleprice = ""
+
+if customfield == "Yes":
+	ApiResponse = ApiResponseFactory.JsonResponse(custfieldsupdated(saleprice))
