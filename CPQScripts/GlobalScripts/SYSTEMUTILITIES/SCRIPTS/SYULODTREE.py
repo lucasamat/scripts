4538129105714cf5e7b143e52f0ccfc3b23bffc9
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-08-02 12:19:42 +0530

    Update SYULODTREE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
@@ -1558,8 +1558,6 @@ class TreeView:
 										ChildDict["objname"] = "ACAPTX"
 										
 										# ChildList.append(ChildDict)
-							elif NodeName == 'SECTION_FIELD_ID' and TabName == 'Profile':
-								childQueryObj = childdata
 							elif NodeName.find(",") == -1 and NodeName.find("-") == -1:                               
 								if str(NodeName) == "OBJECT_NAME" and TabName == 'Profile': 
 									NodeText = str(eval("childdata." + str(NodeName)))									
