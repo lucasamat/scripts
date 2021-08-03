ab71b083ae6fc236d1c58594eb463f1cdb093cdb
Gayathri Amaresan <Gayathri_Amaresan@contractor.amat.com>
2021-08-03 18:32:11 +0530

    Sales Price Update
    Sales Price Update

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCUSTFLDS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCUSTFLDS.py
@@ -3,22 +3,56 @@
 #   __script_description : THIS SCRIPT IS USED TO ASSIGNT THE CUSTOM FIELD VALUE TO THE CORRESPONDING CUSTOMG FIELDS.
 #   __primary_author__ :GAYATHRI AMARESAN
 #   __create_date :
-#   © BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
+#   Â© BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
 # ==========================================================================================================================================
-
-def custfieldsupdated(saleprice):
-	Quote.GetCustomField('SALE_PRICE').Content = str(saleprice)
+from SYDATABASE import SQL
+Sql = SQL()
+def custfieldsupdated(saleprice,service_id):
+    Trace.Write("saleprice"+str(saleprice))
+    quoteitem_covered_obj = SqlHelper.GetFirst("select count(CpqTableEntryId) as cnt from SAQICO where SERVICE_ID = '{service_id}'and QUOTE_RECORD_ID = '{quote_record_id}'".format(service_id = service_id,quote_record_id = Quote.GetGlobal("contract_quote_record_id")))
+    count = quoteitem_covered_obj.cnt
+    Trace.Write("count of SAQICO "+str(count))
+    covered_obj_sale_price = int(saleprice)/count
+    Trace.Write("covered_obj_sale_price "+str(covered_obj_sale_price))
+    update_sales_price = "UPDATE SAQICO SET SALES_PRICE = {covered_obj_sale_price} WHERE SAQICO.SERVICE_ID = '{service_id}' and QUOTE_RECORD_ID = '{QuoteRecordId}' ".format(
+    covered_obj_sale_price=covered_obj_sale_price,			
+    service_id=service_id,
+    QuoteRecordId=Quote.GetGlobal("contract_quote_record_id"),
+    )
+    Sql.RunQuery(update_sales_price)
+    update_quote_item_obj = "UPDATE SAQITM SET SALES_PRICE = {saleprice} WHERE SAQICO.SERVICE_ID = '{service_id}' and QUOTE_RECORD_ID = '{QuoteRecordId}' ".format(
+    saleprice=saleprice,			
+    service_id=service_id,
+    QuoteRecordId=Quote.GetGlobal("contract_quote_record_id"),
+    )
+    Sql.RunQuery(update_quote_item_obj)
+    Quote.GetCustomField('SALE_PRICE').Content = saleprice
     return saleprice
-
+def salepriceedit(service_id):
+    editable = "false"
+    editablity_obj = SqlHelper.GetFirst("select SERVICE_TYPE from MAMTRL where SAP_PART_NUMBER like '%"+str(service_id)+"'")
+    if editablity_obj is not None:
+        servicetype = editablity_obj.SERVICE_TYPE
+        if servicetype == "NON TOOL BASED":
+            editable = "true"
+            return service_id,editable
 
 try:
-	customfield = Param.customfield
+    customfield = Param.customfield
 except Exception:
-	customfield = ""
+    customfield = ""
 try:
-	saleprice = Param.saleprice
+    saleprice = Param.saleprice
+    Trace.Write("try sale"+str(saleprice))
+except Exception,e:
+    saleprice = ""
+    Trace.Write("342"+str(e))
+try:
+    service_id = Param.service_id
 except Exception:
-	saleprice = ""
+    service_id = ""  
 
 if customfield == "Yes":
-	ApiResponse = ApiResponseFactory.JsonResponse(custfieldsupdated(saleprice))
+    ApiResponse = ApiResponseFactory.JsonResponse(custfieldsupdated(saleprice,service_id))
+elif service_id != "":
+    ApiResponse = ApiResponseFactory.JsonResponse(salepriceedit(service_id))
