dae191ad34df6b20b17692254603d0f7d4b4d84b
Gayathri Amaresan <Gayathri_Amaresan@contractor.amat.com>
2021-08-03 20:34:52 +0530

    Update CQCUSTFLDS.py
    indent

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCUSTFLDS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCUSTFLDS.py
@@ -27,47 +27,47 @@ def custfieldsupdated(saleprice,service_id):
     )
     Sql.RunQuery(update_quote_item_obj)
     a = Sql.GetFirst("SELECT ISNULL(SALES_DISCOUNT_PRICE,0) AS  SALES_DISCOUNT_PRICE, SERVICE_ID,QUOTE_RECORD_ID,GREENBOOK,YEAR_OVER_YEAR,CONTRACT_VALID_FROM,CONTRACT_VALID_TO  FROM SAQICO (NOLOCK) WHERE SERVICE_ID = '{service_id}' and QUOTE_RECORD_ID = '{QuoteRecordId}'".format(service_id=service_id ,QuoteRecordId=Quote.GetGlobal("contract_quote_record_id")))
-				
-	if float(a.SALES_DISCOUNT_PRICE) != 0.0 or float(a.SALES_DISCOUNT_PRICE) != 0.00:
-		discount =(float(saleprice)/float(a.SALES_DISCOUNT_PRICE))*100.00
-	else:
-		discount = 0.00
+                
+    if float(a.SALES_DISCOUNT_PRICE) != 0.0 or float(a.SALES_DISCOUNT_PRICE) != 0.00:
+        discount =(float(saleprice)/float(a.SALES_DISCOUNT_PRICE))*100.00
+    else:
+        discount = 0.00
 
-	getdates = Sql.GetFirst("SELECT CONTRACT_VALID_FROM,CONTRACT_VALID_TO FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID = '{}'".format(a.QUOTE_RECORD_ID))
-
-
-	import datetime as dt
-	fmt = '%m/%d/%Y'
-	d1 = dt.datetime.strptime(str(getdates.CONTRACT_VALID_FROM).split(" ")[0], fmt)
-	d2 = dt.datetime.strptime(str(getdates.CONTRACT_VALID_TO).split(" ")[0], fmt)
-	days = (d2 - d1).days
-	Trace.Write("number of days---------------->"+str((d2 - d1).days))
+    getdates = Sql.GetFirst("SELECT CONTRACT_VALID_FROM,CONTRACT_VALID_TO FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID = '{}'".format(a.QUOTE_RECORD_ID))
 
 
-	yoy = float(a.YEAR_OVER_YEAR)
+    import datetime as dt
+    fmt = '%m/%d/%Y'
+    d1 = dt.datetime.strptime(str(getdates.CONTRACT_VALID_FROM).split(" ")[0], fmt)
+    d2 = dt.datetime.strptime(str(getdates.CONTRACT_VALID_TO).split(" ")[0], fmt)
+    days = (d2 - d1).days
+    Trace.Write("number of days---------------->"+str((d2 - d1).days))
 
-	year1 = float(saleprice)
-	year2 = 0.00
-	year3 = 0.00
-	year4 = 0.00
-	year5 = 0.00
-	dec1 = (float(saleprice)*yoy)/100
 
-	if days > 365:
-		year2 = float(saleprice) - dec1
-		dec2 = (float(saleprice)*dec1)/100
-	if days > 730:
-		year3 = float(saleprice) - dec2
-		dec3 = (float(saleprice)*dec2)/100
-	if days > 1095:
-		year4 = float(saleprice) - dec3
-		dec4 = (float(saleprice)*dec3)/100
-	if days > 1460:
-		year5 = float(saleprice) - dec4
+    yoy = float(a.YEAR_OVER_YEAR)
 
-	ext_price = year1 + year2 + year3 + year4 + year5
+    year1 = float(saleprice)
+    year2 = 0.00
+    year3 = 0.00
+    year4 = 0.00
+    year5 = 0.00
+    dec1 = (float(saleprice)*yoy)/100
 
-	Sql.RunQuery("UPDATE SAQICO SET SALES_PRICE = '{saleprice}', DISCOUNT = '{discount}',YEAR_1 = {y1},YEAR_2 = {y2},YEAR_3={y3},YEAR_4={y4},YEAR_5 = {y5},EXTENDED_PRICE = {ext} WHERE SERVICE_ID = '{service_id}' and QUOTE_RECORD_ID = '{QuoteRecordId}'".format(saleprice=saleprice,service_id=service_id ,QuoteRecordId=Quote.GetGlobal("contract_quote_record_id"),discount=discount,y1=year1,y2=year2,y3=year3,y4=year4,y5=year5,ext=ext_price))
+    if days > 365:
+        year2 = float(saleprice) - dec1
+        dec2 = (float(saleprice)*dec1)/100
+    if days > 730:
+        year3 = float(saleprice) - dec2
+        dec3 = (float(saleprice)*dec2)/100
+    if days > 1095:
+        year4 = float(saleprice) - dec3
+        dec4 = (float(saleprice)*dec3)/100
+    if days > 1460:
+        year5 = float(saleprice) - dec4
+
+    ext_price = year1 + year2 + year3 + year4 + year5
+
+    Sql.RunQuery("UPDATE SAQICO SET SALES_PRICE = '{saleprice}', DISCOUNT = '{discount}',YEAR_1 = {y1},YEAR_2 = {y2},YEAR_3={y3},YEAR_4={y4},YEAR_5 = {y5},EXTENDED_PRICE = {ext} WHERE SERVICE_ID = '{service_id}' and QUOTE_RECORD_ID = '{QuoteRecordId}'".format(saleprice=saleprice,service_id=service_id ,QuoteRecordId=Quote.GetGlobal("contract_quote_record_id"),discount=discount,y1=year1,y2=year2,y3=year3,y4=year4,y5=year5,ext=ext_price))
     return saleprice
 def salepriceedit(service_id):
     editable = "false"
