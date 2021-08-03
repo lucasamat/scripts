6b64bd1a1160e40186a3bb096985a4f77f88e5ff
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-08-03 11:57:42 +0530

    Revert "Update CQNESTGRID.py"
    This reverts commit 1595f8d4edfe23a8fec58521a62c6359784be789.

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
@@ -7695,10 +7695,11 @@ def GetCovObjChild(recid, PerPage, PageInform, A_Keys, A_Values):
             )
         elif TreeParentParam == "Receiving Equipment":
             child_obj_recid = Sql.GetList(
-                "select top "+str(PerPage)+" * from (select ROW_NUMBER() OVER( ORDER BY QUOTE_SERVICE_COVERED_OBJECT_ASSEMBLIES_RECORD_ID) AS ROW, QUOTE_SERVICE_COVERED_OBJECT_ASSEMBLIES_RECORD_ID,EQUIPMENT_ID,ASSEMBLY_ID,ASSEMBLY_DESCRIPTION,EQUIPMENTTYPE_ID,GOT_CODE,EQUIPMENT_DESCRIPTION,MNT_PLANT_ID,FABLOCATION_ID,WARRANTY_START_DATE,WARRANTY_END_DATE from SAQSCA (NOLOCK) where EQUIPMENT_ID = '{Parent_Equipmentid}' and QUOTE_RECORD_ID = '{ContractRecordId}' and SERVICE_ID = '{treeparam}' ) m where m.ROW BETWEEN ".format(
+                "select top "+str(PerPage)+" * from (select ROW_NUMBER() OVER( ORDER BY QUOTE_SERVICE_COVERED_OBJECT_ASSEMBLIES_RECORD_ID) AS ROW, QUOTE_SERVICE_COVERED_OBJECT_ASSEMBLIES_RECORD_ID,EQUIPMENT_ID,ASSEMBLY_ID,ASSEMBLY_DESCRIPTION,EQUIPMENTTYPE_ID,GOT_CODE,EQUIPMENT_DESCRIPTION,MNT_PLANT_ID,FABLOCATION_ID,WARRANTY_START_DATE,WARRANTY_END_DATE from SAQSCA (NOLOCK) where EQUIPMENT_ID = '{Parent_Equipmentid}' and QUOTE_RECORD_ID = '{ContractRecordId}' and SERVICE_ID = '{treeparam}' AND FABLOCATION_ID = '{fab}') m where m.ROW BETWEEN ".format(
                     ContractRecordId=Quote.GetGlobal("contract_quote_record_id"),
                     Parent_Equipmentid=Parent_Equipmentid,
-                    treeparam=TreeSuperParentParam                    
+                    treeparam=TreeSuperParentParam,
+                    fab=TreeParam
                 )
                 + str(Page_start)
                 + " and "
@@ -7712,14 +7713,17 @@ def GetCovObjChild(recid, PerPage, PageInform, A_Keys, A_Values):
                 + str(Parent_Equipmentid)
                 + "'and SERVICE_ID ='"
                 + str(TreeSuperParentParam)
-                + "' "
+                + "' AND FABLOCATION_ID = '"
+                + str(TreeParam)
+                + "'"
             ) 
         elif TreeSuperParentParam == "Receiving Equipment":
             child_obj_recid = Sql.GetList(
-                "select top "+str(PerPage)+" * from (select ROW_NUMBER() OVER( ORDER BY QUOTE_SERVICE_COVERED_OBJECT_ASSEMBLIES_RECORD_ID) AS ROW, QUOTE_SERVICE_COVERED_OBJECT_ASSEMBLIES_RECORD_ID,EQUIPMENT_ID,ASSEMBLY_ID,ASSEMBLY_DESCRIPTION,EQUIPMENTTYPE_ID,GOT_CODE,EQUIPMENT_DESCRIPTION,MNT_PLANT_ID,FABLOCATION_ID,WARRANTY_START_DATE,WARRANTY_END_DATE from SAQSCA (NOLOCK) where EQUIPMENT_ID = '{Parent_Equipmentid}' and QUOTE_RECORD_ID = '{ContractRecordId}' and SERVICE_ID = '{treeparam}' ) m where m.ROW BETWEEN ".format(
+                "select top "+str(PerPage)+" * from (select ROW_NUMBER() OVER( ORDER BY QUOTE_SERVICE_COVERED_OBJECT_ASSEMBLIES_RECORD_ID) AS ROW, QUOTE_SERVICE_COVERED_OBJECT_ASSEMBLIES_RECORD_ID,EQUIPMENT_ID,ASSEMBLY_ID,ASSEMBLY_DESCRIPTION,EQUIPMENTTYPE_ID,GOT_CODE,EQUIPMENT_DESCRIPTION,MNT_PLANT_ID,FABLOCATION_ID,WARRANTY_START_DATE,WARRANTY_END_DATE from SAQSCA (NOLOCK) where EQUIPMENT_ID = '{Parent_Equipmentid}' and QUOTE_RECORD_ID = '{ContractRecordId}' and SERVICE_ID = '{treeparam}' AND FABLOCATION_ID = '{fab}') m where m.ROW BETWEEN ".format(
                     ContractRecordId=Quote.GetGlobal("contract_quote_record_id"),
                     Parent_Equipmentid=Parent_Equipmentid,
-                    treeparam=TreeTopSuperParentParam                    
+                    treeparam=TreeTopSuperParentParam,
+                    fab=TreeParentParam
                 )
                 + str(Page_start)
                 + " and "
@@ -7733,7 +7737,9 @@ def GetCovObjChild(recid, PerPage, PageInform, A_Keys, A_Values):
                 + str(Parent_Equipmentid)
                 + "'and SERVICE_ID ='"
                 + str(TreeTopSuperParentParam)
-                + "' "
+                + "' AND FABLOCATION_ID = '"
+                + str(TreeParentParam)
+                + "'"
             )        
         if QueryCountObj is not None:
             QueryCount = QueryCountObj.cnt
