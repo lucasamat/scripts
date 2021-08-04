b738d358bb41d65304ebcd8c44d287a659c03738
Joe Ebenezer --CNTR <Joe_Ebenezer@contractor.amat.com>
2021-08-04 12:53:55 +0530

    Update CQENTLMENT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
@@ -204,7 +204,7 @@ class Entitlements:
 
 							ent_total_val.append(val.STANDARD_ATTRIBUTE_VALUE)
 
-							if NewValue != 'select'::
+							if NewValue != 'select':
 								ent_non_selec_value.append(val.STANDARD_ATTRIBUTE_VALUE)
 								Trace.Write("ent_total_val "+str(ent_total_val))
 								Trace.Write("ent_non_selec_value "+str(ent_non_selec_value))
