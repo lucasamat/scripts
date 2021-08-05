e242c6f6e964d1c24534875e6b17c61c965e96e4
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-08-05 19:56:17 +0530

    Update CQENTLMENT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
@@ -93,7 +93,7 @@ class Entitlements:
 				ent_temp = "ENT_SAVE_BKP_"+str(get_c4c_quote_id.C4C_QUOTE_ID)
 				ent_temp_drop = Sql.GetFirst("sp_executesql @T=N'IF EXISTS (SELECT ''X'' FROM SYS.OBJECTS WHERE NAME= ''"+str(ent_temp)+"'' ) BEGIN DROP TABLE "+str(ent_temp)+" END  ' ")
 				where_cond = where.replace("'","''")
-				Sql.GetFirst("sp_executesql @T=N'declare @H int; Declare @val Varchar(MAX);DECLARE @XML XML; SELECT @val =  replace(replace(STUFF((SELECT ''''+FINAL from(select  REPLACE(entitlement_xml,''<QUOTE_ITEM_ENTITLEMENT>'',sml) AS FINAL FROM (select ''  <QUOTE_ITEM_ENTITLEMENT><QUOTE_ID>''+quote_id+''</QUOTE_ID><QUOTE_RECORD_ID>''+QUOTE_RECORD_ID+''</QUOTE_RECORD_ID><SERVICE_ID>''+service_id+''</SERVICE_ID>'' AS sml,replace(entitlement_xml,''&'','';#38'')  as entitlement_xml from "+str(tableName)+"(nolock) WHERE "+str(where_cond)+" )A )a FOR XML PATH ('''')), 1, 1, ''''),''&lt;'',''<''),''&gt;'',''>'')  SELECT @XML = CONVERT(XML,''<ROOT>''+@VAL+''</ROOT>'') exec sys.sp_xml_preparedocument @H output,@XML; select QUOTE_ID,QUOTE_RECORD_ID,SERVICE_ID,ENTITLEMENT_NAME,ENTITLEMENT_COST_IMPACT,ENTITLEMENT_TYPE,ENTITLEMENT_VALUE_CODE INTO "+str(ent_temp)+"  from openxml(@H, ''ROOT/QUOTE_ITEM_ENTITLEMENT'', 0) with (QUOTE_ID VARCHAR(100) ''QUOTE_ID'',QUOTE_RECORD_ID VARCHAR(100) ''QUOTE_RECORD_ID'',ENTITLEMENT_NAME VARCHAR(100) ''ENTITLEMENT_NAME'',SERVICE_ID VARCHAR(100) ''SERVICE_ID'',ENTITLEMENT_COST_IMPACT VARCHAR(100) ''ENTITLEMENT_COST_IMPACT'',ENTITLEMENT_TYPE VARCHAR(100) ''ENTITLEMENT_TYPE'',ENTITLEMENT_VALUE_CODE VARCHAR(100) ''ENTITLEMENT_VALUE_CODE'') ; exec sys.sp_xml_removedocument @H; '")
+				Sql.GetFirst("sp_executesql @T=N'declare @H int; Declare @val Varchar(MAX);DECLARE @XML XML; SELECT @val =  replace(replace(STUFF((SELECT ''''+FINAL from(select  REPLACE(entitlement_xml,''<QUOTE_ITEM_ENTITLEMENT>'',sml) AS FINAL FROM (select ''  <QUOTE_ITEM_ENTITLEMENT><QUOTE_ID>''+quote_id+''</QUOTE_ID><QUOTE_RECORD_ID>''+QUOTE_RECORD_ID+''</QUOTE_RECORD_ID><SERVICE_ID>''+service_id+''</SERVICE_ID>'' AS sml,replace(entitlement_xml,''&'','';#38'')  as entitlement_xml from "+str(tableName)+"(nolock) WHERE "+str(where_cond)+" )A )a FOR XML PATH ('''')), 1, 1, ''''),''&lt;'',''<''),''&gt;'',''>'')  SELECT @XML = CONVERT(XML,''<ROOT>''+@VAL+''</ROOT>'') exec sys.sp_xml_preparedocument @H output,@XML; select QUOTE_ID,QUOTE_RECORD_ID,SERVICE_ID,ENTITLEMENT_NAME,ENTITLEMENT_COST_IMPACT,ENTITLEMENT_TYPE,ENTITLEMENT_VALUE_CODE,ENTITLEMENT_DISPLAY_VALUE INTO "+str(ent_temp)+"  from openxml(@H, ''ROOT/QUOTE_ITEM_ENTITLEMENT'', 0) with (QUOTE_ID VARCHAR(100) ''QUOTE_ID'',QUOTE_RECORD_ID VARCHAR(100) ''QUOTE_RECORD_ID'',ENTITLEMENT_NAME VARCHAR(100) ''ENTITLEMENT_NAME'',SERVICE_ID VARCHAR(100) ''SERVICE_ID'',ENTITLEMENT_COST_IMPACT VARCHAR(100) ''ENTITLEMENT_COST_IMPACT'',ENTITLEMENT_TYPE VARCHAR(100) ''ENTITLEMENT_TYPE'',ENTITLEMENT_VALUE_CODE VARCHAR(100) ''ENTITLEMENT_VALUE_CODE'',ENTITLEMENT_DISPLAY_VALUE VARCHAR(100) ''ENTITLEMENT_DISPLAY_VALUE'') ; exec sys.sp_xml_removedocument @H; '")
 				Parentgetdata=Sql.GetList("SELECT * FROM {} WHERE {}".format(ent_temp,where))
 				Trace.Write("where------ "+str(where))
 				if Parentgetdata:					
@@ -117,27 +117,28 @@ class Entitlements:
 							
 						#webclient.Headers.Add("If-Match", "111")
 						webclient.Headers.Add("If-Match", "1"+str(cpsmatchID))	
-						Trace.Write('row--'+str(row.ENTITLEMENT_NAME))					
-						try:
-							requestdata = '{"characteristics":['
-							
-							requestdata +='{"id":"'+ str(row.ENTITLEMENT_NAME) + '","values":[' 
-							if row.ENTITLEMENT_TYPE in ('Check Box','CheckBox'):
-								for code in eval(row.ENTITLEMENT_VALUE_CODE):
-									requestdata += '{"value":"' + code + '","selected":true}'
-									requestdata +=','
-								requestdata +=']},'	
-							else:
-								requestdata+= '{"value":"' +str(row.ENTITLEMENT_VALUE_CODE) + '","selected":true}]},'
-							requestdata += ']}'
-							requestdata = requestdata.replace('},]','}]')
-							Trace.Write("requestdata--child-- " + str(requestdata))
-							response1 = webclient.UploadString(Request_URL, "PATCH", str(requestdata))
-							cpsmatchID = cpsmatchID + 10			
-							
-						except Exception:
-							Trace.Write("Patch Error-1-"+str(sys.exc_info()[1]))
-							cpsmatchID = cpsmatchID
+						Trace.Write('row--'+str(row.ENTITLEMENT_NAME))	
+						if 	row.ENTITLEMENT_VALUE_CODE and 	val.ENTITLEMENT_NAME !='undefined' and val.ENTITLEMENT_DISPLAY_VALUE !='select':
+							try:
+								requestdata = '{"characteristics":['
+								
+								requestdata +='{"id":"'+ str(row.ENTITLEMENT_NAME) + '","values":[' 
+								if row.ENTITLEMENT_TYPE in ('Check Box','CheckBox'):
+									for code in eval(row.ENTITLEMENT_VALUE_CODE):
+										requestdata += '{"value":"' + code + '","selected":true}'
+										requestdata +=','
+									requestdata +=']},'	
+								else:
+									requestdata+= '{"value":"' +str(row.ENTITLEMENT_VALUE_CODE) + '","selected":true}]},'
+								requestdata += ']}'
+								requestdata = requestdata.replace('},]','}]')
+								Trace.Write("requestdata--child-- " + str(requestdata))
+								response1 = webclient.UploadString(Request_URL, "PATCH", str(requestdata))
+								cpsmatchID = cpsmatchID + 10			
+								
+							except Exception:
+								Trace.Write("Patch Error-1-"+str(sys.exc_info()[1]))
+								cpsmatchID = cpsmatchID
 
 			getdata=Sql.GetList("SELECT * FROM {} WHERE {}".format(tableName,where))
 			cpsmatc_incr = cpsmatchID + 10
@@ -237,7 +238,10 @@ class Entitlements:
 			cpsmatc_incr = cpsmatchID
 		Request_URL = "https://cpservices-product-configuration.cfapps.us10.hana.ondemand.com/api/v2/configurations/"+str(cpsConfigID)
 		webclient.Headers[System.Net.HttpRequestHeader.Authorization] = "Bearer " + str(response["access_token"])
-		requestdata_split = requestdata.split('"')[11]
+		try:
+			requestdata_split = requestdata.split('"')[11]
+		except:
+			requestdata_split =""	
 		Product.SetGlobal("pre_ent_val",str(requestdata_split))
 		Trace.Write("ELSE = "+str(Product.GetGlobal("pre_ent_val")))
 		Trace.Write("requestdata---180---" + str(requestdata))
