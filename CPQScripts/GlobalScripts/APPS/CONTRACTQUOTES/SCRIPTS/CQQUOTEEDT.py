116944a950b970238c435acb5ba457920accedc1
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2021-08-06 20:00:28 +0530

    Update CQQUOTEEDT.py
    Sleep added after refresh action

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQUOTEEDT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQUOTEEDT.py
@@ -11,6 +11,7 @@ Param = Param
 Log = Log
 ApiResponseFactory = ApiResponseFactory
 import re
+import time
 from SYDATABASE import SQL
 Sql = SQL()
 
@@ -42,6 +43,7 @@ def bannerdetails(Quoteid,active_tab_name):
             Log.Info("MatchObj:" + qid)
             Quote = QuoteHelper.Edit(qid)
             Quote.RefreshActions()
+            time.sleep(5)
             ##getting contarct rec id as global
             if contract_record_id:
                 Quote.SetGlobal("contract_record_id",contract_record_id)
