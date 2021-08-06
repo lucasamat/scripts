de758a64ee2e0df744bf4729bf5a2aedae10c67b
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2021-08-06 17:29:13 +0530

    Update CQQUOTEEDT.py
    Indentation error resolved

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQUOTEEDT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQUOTEEDT.py
@@ -18,38 +18,38 @@ Sql = SQL()
 def bannerdetails(Quoteid,active_tab_name):
     Log.Info(Quoteid)
     Log.Info(active_tab_name)
-	contract_record_id = ""
-	matchObj = re.match( r'.*>\s*[A-Z]{1,2}(\d+)[A-Z]{1,2}[^>]*?\-', Quoteid)
-	Trace.Write('Quoteid'+str(Quoteid))
-	if active_tab_name == "Contracts":
-		reObj = re.match( r'.*>\s*(\d+)*?<', Quoteid)
-		Trace.Write('reObj'+str(reObj))
-		SQLObj = Sql.GetFirst("SELECT QUOTE_ID FROM SAQTMT (NOLOCK) WHERE CRM_CONTRACT_ID='" + str(reObj.group(1)) + "'")
-		##assigning contract rec id globally starts
-		get_contract_rec_id = Sql.GetFirst("SELECT CONTRACT_RECORD_ID FROM CTCNRT (NOLOCK) WHERE CONTRACT_ID='" + str(reObj.group(1)) + "'")
-		if get_contract_rec_id:
-			contract_record_id = str(get_contract_rec_id.CONTRACT_RECORD_ID)
-		Trace.Write("contract_record_id"+str(contract_record_id))
-		###ends
-		Quoteid = SQLObj.QUOTE_ID
-		matchObj = re.match( r'^\s*[A-Z]{1,2}(\d+)[A-Z]{1,2}[^>]*?\-', Quoteid)
-		
-	if Quoteid is not None and str(Quoteid) !='':
-		if matchObj:
-			Quote = QuoteHelper.Edit(str(matchObj.group(1)))
-			Quote.RefreshActions()
-			##getting contarct rec id as global
-			if contract_record_id:
-				Quote.SetGlobal("contract_record_id",contract_record_id)
-				test = Quote.GetGlobal("contract_record_id")
-				Trace.Write("test"+str(test))
-				Log.Info(str(test))
-			##ends
-			return Quote.CompositeNumber
+    contract_record_id = ""
+    matchObj = re.match( r'.*>\s*[A-Z]{1,2}(\d+)[A-Z]{1,2}[^>]*?\-', Quoteid)
+    Trace.Write('Quoteid'+str(Quoteid))
+    if active_tab_name == "Contracts":
+        reObj = re.match( r'.*>\s*(\d+)*?<', Quoteid)
+        Trace.Write('reObj'+str(reObj))
+        SQLObj = Sql.GetFirst("SELECT QUOTE_ID FROM SAQTMT (NOLOCK) WHERE CRM_CONTRACT_ID='" + str(reObj.group(1)) + "'")
+        ##assigning contract rec id globally starts
+        get_contract_rec_id = Sql.GetFirst("SELECT CONTRACT_RECORD_ID FROM CTCNRT (NOLOCK) WHERE CONTRACT_ID='" + str(reObj.group(1)) + "'")
+        if get_contract_rec_id:
+            contract_record_id = str(get_contract_rec_id.CONTRACT_RECORD_ID)
+        Trace.Write("contract_record_id"+str(contract_record_id))
+        ###ends
+        Quoteid = SQLObj.QUOTE_ID
+        matchObj = re.match( r'^\s*[A-Z]{1,2}(\d+)[A-Z]{1,2}[^>]*?\-', Quoteid)
+        
+    if Quoteid is not None and str(Quoteid) !='':
+        if matchObj:
+            Quote = QuoteHelper.Edit(str(matchObj.group(1)))
+            Quote.RefreshActions()
+            ##getting contarct rec id as global
+            if contract_record_id:
+                Quote.SetGlobal("contract_record_id",contract_record_id)
+                test = Quote.GetGlobal("contract_record_id")
+                Trace.Write("test"+str(test))
+                Log.Info(str(test))
+            ##ends
+            return Quote.CompositeNumber
 
 try:
-	Quoteid = Param.Quoteid
-	active_tab_name = Param.CurrentTab
+    Quoteid = Param.Quoteid
+    active_tab_name = Param.CurrentTab
 except:
-	Quoteid=''
+    Quoteid=''
 ApiResponse = ApiResponseFactory.JsonResponse(bannerdetails(Quoteid,active_tab_name))
