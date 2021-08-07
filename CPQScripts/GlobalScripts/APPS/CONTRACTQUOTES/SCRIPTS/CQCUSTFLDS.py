1e061ed2774d657780ae2f7bfdaad2ca820053f1
Gayathri Amaresan <Gayathri_Amaresan@contractor.amat.com>
2021-08-07 12:16:55 +0530

    Update CQCUSTFLDS.py
    Updated the code for the sales price calculation

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCUSTFLDS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCUSTFLDS.py
@@ -3,97 +3,149 @@
 #   __script_description : THIS SCRIPT IS USED TO ASSIGNT THE CUSTOM FIELD VALUE TO THE CORRESPONDING CUSTOMG FIELDS.
 #   __primary_author__ :GAYATHRI AMARESAN
 #   __create_date :
-#   Â© BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
+#   Ã‚Â© BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
 # ==========================================================================================================================================
 from SYDATABASE import SQL
 Sql = SQL()
-def custfieldsupdated(saleprice,service_id):
-    Trace.Write("saleprice"+str(saleprice))
-    quoteitem_covered_obj = SqlHelper.GetFirst("select count(CpqTableEntryId) as cnt from SAQICO where SERVICE_ID = '{service_id}'and QUOTE_RECORD_ID = '{quote_record_id}'".format(service_id = service_id,quote_record_id = Quote.GetGlobal("contract_quote_record_id")))
-    count = quoteitem_covered_obj.cnt
-    Trace.Write("count of SAQICO "+str(count))
-    covered_obj_sale_price = int(saleprice)/count
-    Trace.Write("covered_obj_sale_price "+str(covered_obj_sale_price))
-    update_sales_price = "UPDATE SAQICO SET SALES_PRICE = {covered_obj_sale_price} WHERE SAQICO.SERVICE_ID = '{service_id}' and QUOTE_RECORD_ID = '{QuoteRecordId}' ".format(
-    covered_obj_sale_price=covered_obj_sale_price,			
-    service_id=service_id,
-    QuoteRecordId=Quote.GetGlobal("contract_quote_record_id"),
-    )
-    Sql.RunQuery(update_sales_price)
-    update_quote_item_obj = "UPDATE SAQITM SET SALES_PRICE = {saleprice} WHERE SAQICO.SERVICE_ID = '{service_id}' and QUOTE_RECORD_ID = '{QuoteRecordId}' ".format(
-    saleprice=saleprice,			
-    service_id=service_id,
-    QuoteRecordId=Quote.GetGlobal("contract_quote_record_id"),
-    )
-    Sql.RunQuery(update_quote_item_obj)
-    a = Sql.GetFirst("SELECT ISNULL(SALES_DISCOUNT_PRICE,0) AS  SALES_DISCOUNT_PRICE, SERVICE_ID,QUOTE_RECORD_ID,GREENBOOK,YEAR_OVER_YEAR,CONTRACT_VALID_FROM,CONTRACT_VALID_TO  FROM SAQICO (NOLOCK) WHERE SERVICE_ID = '{service_id}' and QUOTE_RECORD_ID = '{QuoteRecordId}'".format(service_id=service_id ,QuoteRecordId=Quote.GetGlobal("contract_quote_record_id")))
-                
-    if float(a.SALES_DISCOUNT_PRICE) != 0.0 or float(a.SALES_DISCOUNT_PRICE) != 0.00:
-        discount =(float(saleprice)/float(a.SALES_DISCOUNT_PRICE))*100.00
-    else:
-        discount = 0.00
+def custfieldsupdated(saleprice,service_id,lineitemid,discount):
+	saleprice = float(saleprice)
+	lineitemid = float(lineitemid)
+	a = Sql.GetFirst("SELECT ISNULL(SALES_DISCOUNT_PRICE,0) AS  SALES_DISCOUNT_PRICE, SERVICE_ID,QUOTE_RECORD_ID,ISNULL(YEAR_OVER_YEAR,0) AS YEAR_OVER_YEAR  FROM SAQITM (NOLOCK) WHERE SERVICE_ID like '%{service_id}%' and QUOTE_RECORD_ID = '{QuoteRecordId}' and LINE_ITEM_ID = {lineitemid} ".format(service_id=service_id ,QuoteRecordId=Quote.GetGlobal("contract_quote_record_id"),lineitemid = lineitemid))
+	if float(a.SALES_DISCOUNT_PRICE) != 0.0 or float(a.SALES_DISCOUNT_PRICE) != 0.00:
+		discount =((float(a.SALES_DISCOUNT_PRICE)-float(saleprice))/a.SALES_DISCOUNT_PRICE)*100.00
+		Trace.Write("discount"+str(discount))
+	else:
+		discount = 0.00
 
-    getdates = Sql.GetFirst("SELECT CONTRACT_VALID_FROM,CONTRACT_VALID_TO FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID = '{}'".format(a.QUOTE_RECORD_ID))
+	getdates = Sql.GetFirst("SELECT CONTRACT_VALID_FROM,CONTRACT_VALID_TO FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID = '{}'".format(a.QUOTE_RECORD_ID))
 
 
-    import datetime as dt
-    fmt = '%m/%d/%Y'
-    d1 = dt.datetime.strptime(str(getdates.CONTRACT_VALID_FROM).split(" ")[0], fmt)
-    d2 = dt.datetime.strptime(str(getdates.CONTRACT_VALID_TO).split(" ")[0], fmt)
-    days = (d2 - d1).days
-    Trace.Write("number of days---------------->"+str((d2 - d1).days))
+	import datetime as dt
+	fmt = '%m/%d/%Y'
+	d1 = dt.datetime.strptime(str(getdates.CONTRACT_VALID_FROM).split(" ")[0], fmt)
+	d2 = dt.datetime.strptime(str(getdates.CONTRACT_VALID_TO).split(" ")[0], fmt)
+	days = (d2 - d1).days
+	yoy = float(a.YEAR_OVER_YEAR)
 
+	year1 = float(saleprice)
+	year2 = 0.00
+	year3 = 0.00
+	year4 = 0.00
+	year5 = 0.00
+	dec1 = (float(saleprice)*yoy)/100
 
-    yoy = float(a.YEAR_OVER_YEAR)
+	if days > 365:
+		year2 = float(saleprice) - dec1
+		dec2 = (float(saleprice)*dec1)/100
+	if days > 730:
+		year3 = float(saleprice) - dec2
+		dec3 = (float(saleprice)*dec2)/100
+	if days > 1095:
+		year4 = float(saleprice) - dec3
+		dec4 = (float(saleprice)*dec3)/100
+	if days > 1460:
+		year5 = float(saleprice) - dec4
 
-    year1 = float(saleprice)
-    year2 = 0.00
-    year3 = 0.00
-    year4 = 0.00
-    year5 = 0.00
-    dec1 = (float(saleprice)*yoy)/100
+	ext_price = year1 + year2 + year3 + year4 + year5
 
-    if days > 365:
-        year2 = float(saleprice) - dec1
-        dec2 = (float(saleprice)*dec1)/100
-    if days > 730:
-        year3 = float(saleprice) - dec2
-        dec3 = (float(saleprice)*dec2)/100
-    if days > 1095:
-        year4 = float(saleprice) - dec3
-        dec4 = (float(saleprice)*dec3)/100
-    if days > 1460:
-        year5 = float(saleprice) - dec4
-
-    ext_price = year1 + year2 + year3 + year4 + year5
-
-    Sql.RunQuery("UPDATE SAQICO SET SALES_PRICE = '{saleprice}', DISCOUNT = '{discount}',YEAR_1 = {y1},YEAR_2 = {y2},YEAR_3={y3},YEAR_4={y4},YEAR_5 = {y5},EXTENDED_PRICE = {ext} WHERE SERVICE_ID = '{service_id}' and QUOTE_RECORD_ID = '{QuoteRecordId}'".format(saleprice=saleprice,service_id=service_id ,QuoteRecordId=Quote.GetGlobal("contract_quote_record_id"),discount=discount,y1=year1,y2=year2,y3=year3,y4=year4,y5=year5,ext=ext_price))
-    return saleprice
+	Sql.RunQuery("UPDATE SAQITM SET SALES_PRICE = '{saleprice}', DISCOUNT = '{discount}',YEAR_1 = {y1},YEAR_2 = {y2},YEAR_3={y3},YEAR_4={y4},YEAR_5 = {y5},EXTENDED_PRICE = {ext} WHERE SERVICE_ID like '%{service_id}%' and QUOTE_RECORD_ID = '{QuoteRecordId}' and LINE_ITEM_ID = {lineitemid} ".format(saleprice=saleprice,service_id=service_id ,QuoteRecordId=Quote.GetGlobal("contract_quote_record_id"),discount=discount,y1=year1,y2=year2,y3=year3,y4=year4,y5=year5,ext=ext_price,lineitemid = lineitemid))
+	for item in Quote.MainItems:
+		Trace.Write("Quote Quote Quote")
+		item_number = int(item.RolledUpQuoteItem)
+		service_id = item.PartNumber
+		if  service_id == item.PartNumber:
+			Trace.Write("service_id")
+			item.YEAR_OVER_YEAR.Value = yoy
+			item.SALES_PRICE.Value = saleprice
+			item.DISCOUNT.Value = discount
+			item.YEAR_1.Value = year1
+			item.YEAR_2.Value = year2
+			item.YEAR_3.Value = year3
+			item.YEAR_4.Value = year4
+			item.YEAR_5.Value = year5
+			item.EXTENDED_PRICE.Value = ext_price
+	##SAQICO update based on the header values....        
+	quoteitem_covered_obj = SqlHelper.GetFirst("select count(CpqTableEntryId) as cnt from SAQICO where SERVICE_ID = '{service_id}'and QUOTE_RECORD_ID = '{quote_record_id}' and LINE_ITEM_ID = {lineitemid} ".format(service_id = service_id,quote_record_id = Quote.GetGlobal("contract_quote_record_id"),lineitemid = lineitemid))
+	count = quoteitem_covered_obj.cnt
+	Trace.Write("count of SAQICO "+str(count))
+	covered_obj_sale_price = saleprice/count
+	yr1 = year1/count
+	yr2 = year2/count
+	yr3 = year3/count
+	yr4 = year4/count
+	yr5 = year5/count
+	yearoveryear = yoy
+	sales_discount_price = float(a.SALES_DISCOUNT_PRICE)/count
+	extended_price = ext_price/count
+	Trace.Write("covered_obj_sale_price "+str(covered_obj_sale_price))
+	update_sales_price = "UPDATE SAQICO SET SALES_PRICE = {covered_obj_sale_price},DISCOUNT = '{discount}',YEAR_1 = {y1},YEAR_2 = {y2},YEAR_3={y3},YEAR_4={y4},YEAR_5 = {y5},EXTENDED_PRICE = {ext},SALES_DISCOUNT_PRICE = {sales_discount_price},YEAR_OVER_YEAR = {yearoveryear}  WHERE SERVICE_ID like '%{service_id}%' and QUOTE_RECORD_ID = '{QuoteRecordId}' and LINE_ITEM_ID = {lineitemid} ".format(
+	covered_obj_sale_price=covered_obj_sale_price,service_id=service_id ,QuoteRecordId=Quote.GetGlobal("contract_quote_record_id"),discount=discount,y1=yr1,y2=yr2,y3=yr3,y4=yr4,y5=yr5,ext=extended_price,sales_discount_price = sales_discount_price,lineitemid =lineitemid,yearoveryear= yearoveryear
+	)
+	Sql.RunQuery(update_sales_price)
+	Sql.RunQuery("""UPDATE SAQIGB
+	SET
+	YEAR_1 = IQ.YEAR_1,
+	YEAR_2 = IQ.YEAR_2,
+	YEAR_3 = IQ.YEAR_3,
+	YEAR_4 = IQ.YEAR_4,
+	YEAR_5 = IQ.YEAR_5,
+	EXTENDED_PRICE = IQ.EXTENDED_PRICE,
+	SALES_DISCOUNT_PRICE = IQ.SALES_DISCOUNT_PRICE,
+	DISCOUNT = IQ.DISCOUNT
+	FROM SAQIGB (NOLOCK)
+	INNER JOIN (SELECT SAQICO.QUOTE_RECORD_ID,
+	SAQICO.GREENBOOK_RECORD_ID,
+	SUM(ISNULL(SAQICO.EXTENDED_PRICE, 0)) as EXTENDED_PRICE,
+	SUM(ISNULL(SAQICO.SALES_DISCOUNT_PRICE, 0)) as SALES_DISCOUNT_PRICE,
+	SUM(ISNULL(SAQICO.YEAR_1, 0)) as YEAR_1,
+	SUM(ISNULL(SAQICO.YEAR_2, 0)) as YEAR_2,
+	SUM(ISNULL(SAQICO.YEAR_3, 0)) as YEAR_3,
+	SUM(ISNULL(SAQICO.YEAR_4, 0)) as YEAR_4,
+	SUM(ISNULL(SAQICO.YEAR_5, 0)) as YEAR_5
+	FROM SAQICO (NOLOCK)
+	WHERE SAQICO.QUOTE_RECORD_ID = '{QuoteRecordId}'
+	GROUP BY SAQICO.LINE_ITEM_ID, SAQICO.QUOTE_RECORD_ID,SAQICO.GREENBOOK_RECORD_ID,SAQICO.DISCOUNT)IQ
+	ON SAQIGB.QUOTE_RECORD_ID = IQ.QUOTE_RECORD_ID AND SAQIGB.GREENBOOK_RECORD_ID = IQ.GREENBOOK_RECORD_ID
+	WHERE SAQIGB.QUOTE_RECORD_ID = '{QuoteRecordId}'""".format(QuoteRecordId=Quote.GetGlobal("contract_quote_record_id")))
+	pricefactor_obj = Sql.GetFirst("SELECT FACTOR_PCTVAR FROM PRCFVA (NOLOCK) WHERE FACTOR_VARIABLE_ID = '{}'".format(service_id))
+	if float(pricefactor_obj.FACTOR_PCTVAR) < discount:
+	   Sql.RunQuery("UPDATE SAQITM SET PRICING_STATUS = 'APPROVAL REQUIRED' WHERE QUOTE_RECORD_ID = '{}' AND SERVICE_ID LIKE '%{}%'".format(Quote.GetGlobal("contract_quote_record_id"),a.SERVICE_ID))
+	Sql.RunQuery("""UPDATE SAQICO SET
+PRICING_STATUS = 'APPROVAL REQUIRED' FROM SAQICO INNER JOIN SAQITM ON SAQICO.QUOTE_RECORD_ID = SAQITM.QUOTE_RECORD_ID AND SAQICO.LINE_ITEM_ID = SAQITM.LINE_ITEM_ID WHERE SAQITM.PRICING_STATUS = 'APPROVAL REQUIRED' AND SAQITM.SERVICE_ID like '%{}%'""".format(service_id))
+	return saleprice
 def salepriceedit(service_id):
-    editable = "false"
-    editablity_obj = SqlHelper.GetFirst("select SERVICE_TYPE from MAMTRL where SAP_PART_NUMBER like '%"+str(service_id)+"'")
-    if editablity_obj is not None:
-        servicetype = editablity_obj.SERVICE_TYPE
-        if servicetype == "NON TOOL BASED":
-            editable = "true"
-            return service_id,editable
+	editable = "false"
+	editablity_obj = SqlHelper.GetFirst("select SERVICE_TYPE from MAMTRL where SAP_PART_NUMBER like '%"+str(service_id)+"'")
+	if editablity_obj is not None:
+		servicetype = editablity_obj.SERVICE_TYPE
+		if servicetype == "NON TOOL BASED":
+			editable = "true"
+	return service_id,editable
 
 try:
-    customfield = Param.customfield
+	customfield = Param.customfield
 except Exception:
-    customfield = ""
+	customfield = ""
 try:
-    saleprice = Param.saleprice
-    Trace.Write("try sale"+str(saleprice))
+	saleprice = Param.saleprice
+	Trace.Write("try sale"+str(saleprice))
 except Exception,e:
-    saleprice = ""
-    Trace.Write("342"+str(e))
+	saleprice = ""
+	#Trace.Write("342"+str(e))
 try:
-    service_id = Param.service_id
+	service_id = Param.service_id
 except Exception:
-    service_id = ""  
+	service_id = ""
+try:
+	lineitemid = Param.lineitemid
+except Exception:
+	lineitemid = ""
+try:
+	discount = Param.discount
+except Exception:
+	discount = ""
 
 if customfield == "Yes":
-    ApiResponse = ApiResponseFactory.JsonResponse(custfieldsupdated(saleprice,service_id))
+	ApiResponse = ApiResponseFactory.JsonResponse(custfieldsupdated(saleprice,service_id,lineitemid,discount))
 elif service_id != "":
-    ApiResponse = ApiResponseFactory.JsonResponse(salepriceedit(service_id))
+	ApiResponse = ApiResponseFactory.JsonResponse(salepriceedit(service_id))
