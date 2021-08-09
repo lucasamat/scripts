3ffd69dd26fdaf02bfbb9423893c60696bfba018
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-08-09 12:48:17 +0530

    Update SYDRPCONST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYDRPCONST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYDRPCONST.py
@@ -199,10 +199,10 @@ class DropConstraint:
                 for loop in query_result:
                     Trace.Write('not null messgae---199----')
                     FK_CONSTRAINT = Sql.GetFirst(
-                        "SELECT Result=COUNT(1) FROM SYOBJC CON INNER JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE SCHCON ON SCHCON.TABLE_NAME = CON.OBJECT_APINAME AND "
-                        + " SCHCON.COLUMN_NAME = CON.OBJECTFIELD_APINAME AND CON.CONSTRAINT_NAME LIKE '%FK_%'  WHERE CON.OBJECT_APINAME='"
+                        "SELECT Result=COUNT(1) FROM SYOBJC CON INNER JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE SCHCON ON SCHCON.TABLE_NAME = CON.OBJECT_APINAME  "
+                        + "    WHERE CON.OBJECT_APINAME='"
                         + str(self.ObjectName)
-                        + "' AND CON.OBJECTFIELD_APINAME = '"
+                        + "' AND CON.CONSTRAINT_TYPE LIKE '%FOREIGN%' AND CON.OBJECTFIELD_APINAME = '"
                         + str(objectApiName)
                         + "'"
                     )
