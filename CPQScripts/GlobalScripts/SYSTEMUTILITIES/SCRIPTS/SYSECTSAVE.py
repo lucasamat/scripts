1b8e8ea7da3e8a9cadbe804980e7abe94702d169
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-08-10 16:10:11 +0530

    Update SYSECTSAVE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
@@ -821,6 +821,7 @@ def MaterialSave(ObjectName, RECORD, warning_msg, SectionRecId=None):
                                     for Productattribute, Productvalue in rootvalue.items():
                                         if Productattribute=="characteristics":
                                             for prdvalue in Productvalue:
+                                                multi_value = ""
                                                 #overallattributeslist.append(prdvalue['id'])
                                                 if prdvalue['visible'] =='false':
                                                     attributesdisallowedlst.append(prdvalue['id'])
@@ -880,6 +881,7 @@ def MaterialSave(ObjectName, RECORD, warning_msg, SectionRecId=None):
                                         get_display_val = Sql.GetFirst("SELECT STANDARD_ATTRIBUTE_DISPLAY_VAL  from STANDARD_ATTRIBUTE_VALUES S INNER JOIN ATTRIBUTE_DEFN (NOLOCK) A ON A.STANDARD_ATTRIBUTE_CODE=S.STANDARD_ATTRIBUTE_CODE WHERE S.STANDARD_ATTRIBUTE_CODE = '{}' AND A.SYSTEM_ID = '{}' AND S.STANDARD_ATTRIBUTE_VALUE = '{}' ".format(STANDARD_ATTRIBUTE_VALUES.STANDARD_ATTRIBUTE_CODE,attrs,  attributevalues[attrs] ) )
                                         ent_disp_val = get_display_val.STANDARD_ATTRIBUTE_DISPLAY_VAL 
                                     elif PRODUCT_ATTRIBUTES.ATT_DISPLAY_DESC in ('Check Box') and ent_disp_val and ent_val_code:
+                                        Trace.Write('multi_value---'+str(multi_value))
                                         if multi_value == 'true':
                                             ent_val = str(tuple(ent_val_code)).replace(',)',')')
                                             get_display_val = Sql.GetList("SELECT STANDARD_ATTRIBUTE_DISPLAY_VAL  from STANDARD_ATTRIBUTE_VALUES S INNER JOIN ATTRIBUTE_DEFN (NOLOCK) A ON A.STANDARD_ATTRIBUTE_CODE=S.STANDARD_ATTRIBUTE_CODE WHERE S.STANDARD_ATTRIBUTE_CODE = '{}' AND A.SYSTEM_ID = '{}' AND S.STANDARD_ATTRIBUTE_VALUE in {} ".format(STANDARD_ATTRIBUTE_VALUES.STANDARD_ATTRIBUTE_CODE,attrs,  ent_val ) )
