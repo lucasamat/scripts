431cd91c37905f6f53c6050994b9ca404e6e95bb
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-08-10 16:40:12 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -2702,6 +2702,7 @@ class SYLDRTLIST:
                     Trace.Write("12111111===3=====>"+str(Action_permission.get("Edit")).upper())
                     if str(Action_permission.get("Edit")).upper() == "TRUE":
                         if related_list_edit_permission:
+                            Trace.Write('other_tab111===')
                             if primary_view != "":
                                 Trace.Write('other_tab==='+str(other_tab))
                                 if other_tab == "1":
