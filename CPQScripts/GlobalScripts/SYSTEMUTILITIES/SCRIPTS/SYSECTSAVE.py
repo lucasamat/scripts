11e31088b6e29d2e3343a1cc0fe0d5d84c858958
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-08-10 15:10:57 +0530

    Update SYSECTSAVE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
@@ -909,9 +909,9 @@ def MaterialSave(ObjectName, RECORD, warning_msg, SectionRecId=None):
                                     </QUOTE_ITEM_ENTITLEMENT>""".format(ent_name = str(attrs),ent_val_code = ent_val_code,ent_type = DTypeset[PRODUCT_ATTRIBUTES.ATT_DISPLAY_DESC] if PRODUCT_ATTRIBUTES else  '',ent_desc = ATTRIBUTE_DEFN.STANDARD_ATTRIBUTE_NAME,ent_disp_val = ent_disp_val if HasDefaultvalue==True else '',ct = '',pi = '',is_default = '1',pm = '',cf = '')
                                     cpsmatc_incr = int(cpsmatchID) + 10
                                     Trace.Write('cpsmatc_incr'+str(cpsmatc_incr))
-                                    Updatecps = "UPDATE {} SET CPS_MATCH_ID ={},CPS_CONFIGURATION_ID = '{}',ENTITLEMENT_XML='{}' WHERE {} ".format('SAQTSE', cpsmatc_incr,cpsConfigID,insertservice, whereReq)
-                                    Trace.Write('cpsmatc_incr'+str(cpsmatc_incr))
-                                    Sql.RunQuery(Updatecps)
+                                Updatecps = "UPDATE {} SET CPS_MATCH_ID ={},CPS_CONFIGURATION_ID = '{}',ENTITLEMENT_XML='{}' WHERE {} ".format('SAQTSE', cpsmatc_incr,cpsConfigID,insertservice, whereReq)
+                                Trace.Write('cpsmatc_incr'+str(cpsmatc_incr))
+                                Sql.RunQuery(Updatecps)
                                 
 
         except Exception,e:
