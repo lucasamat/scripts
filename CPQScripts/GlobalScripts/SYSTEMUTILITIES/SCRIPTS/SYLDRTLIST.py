96bfd3c5ec69df26ac4eadabd081c59f7d64f827
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-08-10 16:57:38 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -2675,6 +2675,8 @@ class SYLDRTLIST:
                             )
                         elif str(current_tab).upper() == "PROFILE" and (ObjectName == "SYPROF"):
                             Action_str += '<li><a class="dropdown-item" href="#" onclick="profileObjSet(this)" data-target="#viewProfileRelatedList" data-toggle="modal">VIEW<a><li>'
+                        elif ObjectName == "SAQTRV":
+                                Action_str += '<li><a id = '' class="dropdown-item" href="#" onclick="">EDIT DESC</a></li>'    
 
                     else:
                         if str(current_tab).upper() == "PROFILE" and (ObjectName == "SYPROF"):
