0d13d935c56678c547985698792d3a270fb7ab61
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-08-11 12:20:08 +0530

    Update CQNESTGRID.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
@@ -6596,7 +6596,7 @@ def GetCovObjMaster(PerPage, PageInform, A_Keys, A_Values):
         data_dict["WARRANTY_END_DATE"] = ('<abbr id ="" title="' + str(par.WARRANTY_END_DATE) + '">' + str(par.WARRANTY_END_DATE) + "</abbr>") 
         data_dict["WARRANTY_END_DATE_ALERT"] = ('<abbr id ="" title="' + str(par.WARRANTY_END_DATE_ALERT) + '">' + str(par.WARRANTY_END_DATE_ALERT) + "</abbr>") 
         data_list.append(data_dict)
-
+    Trace.Write('data_list--'+str(data_list))
     hyper_link = ["QUOTE_SERVICE_COVERED_OBJECTS_RECORD_ID"]
     table_header += "<tr>"
     table_header += (
