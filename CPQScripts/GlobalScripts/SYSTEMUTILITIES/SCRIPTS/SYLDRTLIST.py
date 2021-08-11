deb5528bc17543686493f4c4e9220514e8f37daa
Joe Ebenezer --CNTR <Joe_Ebenezer@contractor.amat.com>
2021-08-11 12:38:59 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -7509,6 +7509,9 @@ class SYLDRTLIST:
                         elif str(RECORD_ID) == "SYOBJR-95981" or str(RECORD_ID)=="SYOBJR-98459":
                             RecAttValue = Product.Attributes.GetByName("QSTN_SYSEFL_SY_01110").GetValue()
                             Qustr +=" where "+str(Wh_API_NAME) + " = '"+str(RecAttValue)+"'"
+                        elif str(RECORD_ID) == "SYOBJR-95980":
+                            RecAttValue = Product.Attributes.GetByName("QSTN_SYSEFL_SY_01110").GetValue()
+                            Qustr = " where "+str(ATTRIBUTE_VALUE_STR)+" "+ str(Wh_API_NAME) + " = '" + str(RecAttValue) + "'"
                         else:
                             Qustr = " where " + str(Wh_API_NAME) + " = '" + str(RecAttValue) + "'"
                             Trace.Write("7100"+str(Qustr))
