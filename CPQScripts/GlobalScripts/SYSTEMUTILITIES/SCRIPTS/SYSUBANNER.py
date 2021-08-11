6065d9612e990600efce0bd90cf5c6d326dd1c90
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-08-11 13:48:19 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -228,7 +228,7 @@ def Related_Sub_Banner(
             ThirdQuery = Sql.GetFirst(
                 "select * from SYOBJD (nolock) where OBJECT_NAME = '" + str(ObjName) + "' AND IS_KEY = 'True' "
             )
-            rev_quote = Sql.GetFirsst(" SELECT * FROM SAQTRV (NOLOCK) WHERE QUOTE_RECORD_ID = '{contract_quote_record_id}' AND ACTIVE = 'TRUE' ".format(contract_quote_record_id = Quote.GetGlobal("contract_quote_record_id")))
+            rev_quote = Sql.GetFirst(" SELECT * FROM SAQTRV (NOLOCK) WHERE QUOTE_RECORD_ID = '{contract_quote_record_id}' AND ACTIVE = 'TRUE' ".format(contract_quote_record_id = Quote.GetGlobal("contract_quote_record_id")))
             if rev_quote: 
                 PrimaryLable = 'Acitve Revision'
                 PrimaryValue = rev_quote.QTEREV_ID
