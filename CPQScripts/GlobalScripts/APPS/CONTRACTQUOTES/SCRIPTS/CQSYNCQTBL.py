d66f004cb29f7d3737846067a8b8ed4e4dd4e1a2
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-08-17 16:47:12 +0530

    Update CQSYNCQTBL.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
@@ -232,16 +232,14 @@ class SyncQuoteAndCustomTables:
                             except:
                                 #Log.Info('except-----')
                                 ent_disp_val = ent_disp_val 
-                        if str(attrs) == 'AGS_POA_PROD_TYPE' and ent_disp_val != '':
-                            Log.Info("ENTERED POA----------->")
-                            Sql.RunQuery("UPDATE SAQTSV SET SERVICE_TYPE = '{}' WHERE QUOTE_RECORD_ID = '{}'".format(ent_disp_val,quote_record_id))    
+                           
                                     
                         else:
                             ent_disp_val = ent_disp_val
 
-                        '''if str(attrs) == 'AGS_POA_PROD_TYPE' and ent_disp_val != '':
+                        if str(attrs) == 'AGS_POA_PROD_TYPE' and ent_disp_val != '':
                             Log.Info("ENTERED POA----------->")
-                            Sql.RunQuery("UPDATE SAQTSV SET SERVICE_TYPE = '{}' WHERE QUOTE_RECORD_ID = '{}'".format(ent_disp_val,quote_record_id))'''
+                            Sql.RunQuery("UPDATE SAQTSV SET SERVICE_TYPE = '{}' WHERE QUOTE_RECORD_ID = '{}'".format(ent_disp_val,quote_record_id))
                                                
                         DTypeset={"Drop Down":"DropDown","Free Input, no Matching":"FreeInputNoMatching","Check Box":"CheckBox"}
                         #Trace.Write(str(attrs)+'--------'+str(HasDefaultvalue)+'----ent_disp_val----ent_disp_val-HasDefaultvalue=True--'+str(ent_disp_val))
