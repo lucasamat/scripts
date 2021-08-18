c6b5a068b5db3d367ad03cda088a329deb871d0e
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-08-18 20:12:14 +0530

    Update SYATABSAVE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYATABSAVE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYATABSAVE.py
@@ -672,7 +672,7 @@ for tab in Product.Tabs:
                                                 # Product.Attributes.GetByName("SEC_N_TAB_PAGE_ALERT").HintFormula = """<div class='col-md-12' id='PageAlert' style='display':'block'; ><div class='row modulesecbnr brdr' data-toggle='collapse' data-target='#Alert13' aria-expanded='true' >NOTIFICATIONS<i class='pull-right fa fa-chevron-down '></i><i class='pull-right fa fa-chevron-up'></i></div><div  id='Alert13' class='col-md-12  alert-notification  brdr collapse in' ><div  class='col-md-12 alert-danger'><label ><img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/stopicon1.svg" alt="Error">  ERROR : '{}' is a required field </label></div></div></div>""".format(sectalert)
                                                 # sectalert = ", ".join(Field_Labels)
                                                 # product = Product.Attributes.GetByName("SEC_N_TAB_PAGE_ALERT").HintFormula
-                                                Trace.Write("prod999==="+str(product))
+                                                #Trace.Write("prod999==="+str(product))
                                                 if Field_Labels is None or Field_Labels == '':
                                                     Trace.Write('Field_Labels000======')
                                                     Product.Attributes.GetByName(
