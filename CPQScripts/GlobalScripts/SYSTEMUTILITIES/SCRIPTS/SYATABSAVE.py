7516612d2f074bec984d7773bfce18e0d1d96ecd
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-08-18 12:28:17 +0530

    Update SYATABSAVE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYATABSAVE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYATABSAVE.py
@@ -669,16 +669,23 @@ for tab in Product.Tabs:
                                                 Product.Attributes.GetByName("SEC_N_TAB_PAGE_ALERT").Allowed = True
                                                 Trace.Write("88866")
 
-                                                Product.Attributes.GetByName("SEC_N_TAB_PAGE_ALERT").HintFormula = """<div class='col-md-12' id='PageAlert' style='display':'block'; ><div class='row modulesecbnr brdr' data-toggle='collapse' data-target='#Alert13' aria-expanded='true' >NOTIFICATIONS<i class='pull-right fa fa-chevron-down '></i><i class='pull-right fa fa-chevron-up'></i></div><div  id='Alert13' class='col-md-12  alert-notification  brdr collapse in' ><div  class='col-md-12 alert-danger'><label ><img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/stopicon1.svg" alt="Error">  ERROR : '{}' is a required field </label></div></div></div>""".format(sectalert)
+                                                # Product.Attributes.GetByName("SEC_N_TAB_PAGE_ALERT").HintFormula = """<div class='col-md-12' id='PageAlert' style='display':'block'; ><div class='row modulesecbnr brdr' data-toggle='collapse' data-target='#Alert13' aria-expanded='true' >NOTIFICATIONS<i class='pull-right fa fa-chevron-down '></i><i class='pull-right fa fa-chevron-up'></i></div><div  id='Alert13' class='col-md-12  alert-notification  brdr collapse in' ><div  class='col-md-12 alert-danger'><label ><img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/stopicon1.svg" alt="Error">  ERROR : '{}' is a required field </label></div></div></div>""".format(sectalert)
                                                 sectalert = ", ".join(Field_Labels)
                                                 product = Product.Attributes.GetByName("SEC_N_TAB_PAGE_ALERT").HintFormula
                                                 Trace.Write("prod999==="+str(product))
+                                                if Field_Labels is None or Field_Labels == '':
+                                                    Trace.Write('Field_Labels000======')
+                                                    Product.Attributes.GetByName(
+                                                        "SEC_N_TAB_PAGE_ALERT"
+                                                    ).HintFormula = "<div class='col-md-12' id='PageAlert' style='display':'block';  ><div class='row modulesecbnr brdr' data-toggle='collapse' data-target='#Alert13' aria-expanded='true' >NOTIFICATIONS<i class='pull-right fa fa-chevron-down '></i><i class='pull-right fa fa-chevron-up'></i></div><div  id='Alert13' class='col-md-12  alert-notification  brdr collapse in' ><div  class='col-md-12 alert-danger'><label ><img src='/mt/APPLIEDMATERIALS_TST/Additionalfiles/stopicon1.svg' alt='Error'>  ERROR : '{}' are required fields </label></div></div></div>".format(
+                                                        sectalert
+                                                    )
                                                 
                                                 if len(Field_Labels) > 1:
                                                     Trace.Write('Field_Labels======')
                                                     Product.Attributes.GetByName(
                                                         "SEC_N_TAB_PAGE_ALERT"
-                                                    ).HintFormula = "<div class='col-md-12' id='PageAlert' style='display':'block';  ><div class='row modulesecbnr brdr' data-toggle='collapse' data-target='#Alert13' aria-expanded='true' >NOTIFICATIONS<i class='pull-right fa fa-chevron-down '></i><i class='pull-right fa fa-chevron-up'></i></div><div  id='Alert13' class='col-md-12  alert-notification  brdr collapse in' ><div  class='col-md-12 alert-danger'><label ><img src='/mt/APPLIEDMATERIALS_TST/Additionalfiles/stopicon1.svg' alt='Error'>  ERROR : '{0}' are required fields </label></div></div></div>".format(
+                                                    ).HintFormula = "<div class='col-md-12' id='PageAlert' style='display':'block';  ><div class='row modulesecbnr brdr' data-toggle='collapse' data-target='#Alert13' aria-expanded='true' >NOTIFICATIONS<i class='pull-right fa fa-chevron-down '></i><i class='pull-right fa fa-chevron-up'></i></div><div  id='Alert13' class='col-md-12  alert-notification  brdr collapse in' ><div  class='col-md-12 alert-danger'><label ><img src='/mt/APPLIEDMATERIALS_TST/Additionalfiles/stopicon1.svg' alt='Error'>  ERROR : '{}' are required fields </label></div></div></div>".format(
                                                         sectalert
                                                     )
                                                 if Product.Attributes.GetByName("QSTN_SYSEFL_AC_00006"):
