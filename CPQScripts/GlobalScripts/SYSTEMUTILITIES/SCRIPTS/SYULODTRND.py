62e6f0352925abac3ae01300807c668d5994cd1b
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-08-18 14:00:50 +0530

    Update SYULODTRND.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
@@ -3592,6 +3592,8 @@ def EntitlementTreeViewHTMLDetail(
 								STDVALUES =  Sql.GetFirst("SELECT STANDARD_ATTRIBUTE_VALUE from STANDARD_ATTRIBUTE_VALUES  where  SYSTEM_ID like '%{sys_id}%' ".format(sys_id = str(attrSysId))  )							
 								sec_str1 = ""
 								Trace.Write(str(attrSysId)+'--attrValue---3594---'+str(attrValue))
+								if attrValue == "DefaultValue":
+									attrValue = ''
 								sec_str1 += (
 									'<input class="form-control '+str(disable_edit)+'" style ="'+str(add_style)+'"  id = "'
 									+ str(attrSysId)
