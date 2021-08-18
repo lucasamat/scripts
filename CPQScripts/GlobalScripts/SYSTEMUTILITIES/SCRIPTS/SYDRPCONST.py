73e5c7f5c4cd689b951c7dcc8e45e7f0a1915801
Joe Ebenezer --CNTR <Joe_Ebenezer@contractor.amat.com>
2021-08-18 16:02:33 +0530

    Update SYDRPCONST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYDRPCONST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYDRPCONST.py
@@ -189,9 +189,7 @@ class DropConstraint:
                 # DROP NOT NULL
                 Trace.Write('not null messgae---')
                 query_result = Sql.GetList(
-                    "SELECT OBJECT_APINAME, OBJECTFIELD_APINAME FROM SYOBJC(NOLOCK) WHERE CONSTRAINT_TYPE = 'NOT NULL' AND OBJECT_APINAME = '"
-                    + str(self.ObjectName)
-                    + "' "
+                    "SELECT OBJECT_APINAME, OBJECTFIELD_APINAME FROM SYOBJC(NOLOCK) WHERE CONSTRAINT_TYPE = 'NOT NULL' AND OBJECT_APINAME = 'QTQTBP' "
                     + " AND OBJECTFIELD_APINAME ='"
                     + str(objectApiName)
                     + "' "
