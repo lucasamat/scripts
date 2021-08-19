15d1bddf973724414f221390e953ecf208c335f7
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-08-19 11:16:57 +0530

    Update SYLDRLADBN.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRLADBN.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRLADBN.py
@@ -483,7 +483,11 @@ QuestionRecId, val, TabName, TABLE_RECORDID = "", "Tab list", "", ""
 # GET CURRENT ACTION
 value = productAttributesGetByName("MA_MTR_TAB_ACTION").GetValue()
 Trace.Write('')
-
+SYACTI_OBJNAME = Sql.GetList(
+                            "SELECT PAGEACTION_RECORD_ID,SAPCPQ_ATTRIBUTE_NAME,ACTION_NAME, TAB_NAME FROM SYPGAC (NOLOCK) WHERE TAB_RECORD_ID='"
+                            + TAB_RECID
+                            + "' "
+                        )
 if myAttribute is not None:
     Trace.Write("check inside else")
     if (str(TestProduct.CurrentTab) == 'Approval Chain' and str(TestProduct.Name) == 'APPROVAL CENTER'):
