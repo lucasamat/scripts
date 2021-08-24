b8a0f74f000d9a12dc4f4879fb0ea257a996c600
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-08-25 00:10:25 +0530

    A055S000P01-4218

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQUDQTSMRY.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQUDQTSMRY.py
@@ -39,8 +39,8 @@ class ContractQuoteSummaryUpdate:
     def _quote_item_lines_update(self):
         decimal_discount = int(self.discount) / 100
         Sql.RunQuery("""UPDATE SAQICO SET 
-                                        SALES_PRICE = ISNULL(SALES_PRICE,0) - (ISNULL(SALES_PRICE,0) * {Discount}),
-                                        YEAR_1 = ISNULL(SALES_PRICE,0) - (ISNULL(SALES_PRICE,0) * {Discount})
+                                        NET_PRICE = ISNULL(NET_PRICE,0) - (ISNULL(NET_PRICE,0) * {Discount}),
+                                        YEAR_1 = ISNULL(NET_PRICE,0) - (ISNULL(NET_PRICE,0) * {Discount})
                                         DISCOUNT = {Discount}
                                     FROM SAQICO (NOLOCK)                                     
                                     WHERE QUOTE_RECORD_ID = '{QuoteRecordId}'""".format(
@@ -51,7 +51,7 @@ class ContractQuoteSummaryUpdate:
         self._update_year()
         # Update Year2 to Year5 - End
         Sql.RunQuery("""UPDATE SAQICO SET 
-                                        EXTENDED_PRICE = ISNULL(YEAR_1,0) + ISNULL(YEAR_2,0) + ISNULL(YEAR_3,0) + ISNULL(YEAR_4,0) + ISNULL(YEAR_5,0)
+                                        NET_VALUE = ISNULL(YEAR_1,0) + ISNULL(YEAR_2,0) + ISNULL(YEAR_3,0) + ISNULL(YEAR_4,0) + ISNULL(YEAR_5,0)
                                     FROM SAQICO (NOLOCK)                                     
                                     WHERE QUOTE_RECORD_ID = '{QuoteRecordId}'""".format(
                                         QuoteRecordId=self.contract_quote_record_id 
@@ -59,28 +59,60 @@ class ContractQuoteSummaryUpdate:
                     )
     def _quote_item_update(self):
         Sql.RunQuery("""UPDATE SAQITM
-							SET 
-							TARGET_PRICE = IQ.TARGET_PRICE,
-							YEAR_1 = IQ.YEAR_1,
-							YEAR_2 = IQ.YEAR_2							
-							FROM SAQITM (NOLOCK)
-							INNER JOIN (SELECT SAQITM.CpqTableEntryId,
-										CAST(ROUND(ISNULL(SUM(ISNULL(SAQICO.TARGET_PRICE, 0)), 0), 0) as decimal(18,2)) as TARGET_PRICE,
-										CAST(ROUND(ISNULL(SUM(ISNULL(SAQICO.YEAR_1, 0)), 0), 0) as decimal(18,2)) as YEAR_1,
-										CAST(ROUND(ISNULL(SUM(ISNULL(SAQICO.YEAR_2, 0)), 0), 0) as decimal(18,2)) as YEAR_2
-										FROM SAQITM (NOLOCK) 
-										JOIN SAQICO (NOLOCK) ON SAQICO.QUOTE_RECORD_ID = SAQITM.QUOTE_RECORD_ID AND SAQICO.LINE_ITEM_ID = SAQITM.LINE_ITEM_ID
-										WHERE SAQITM.QUOTE_RECORD_ID = '{QuoteRecordId}' 
-										GROUP BY SAQITM.LINE_ITEM_ID, SAQITM.QUOTE_RECORD_ID, SAQITM.CpqTableEntryId)IQ
-							ON SAQITM.CpqTableEntryId = IQ.CpqTableEntryId 
-							WHERE SAQITM.QUOTE_RECORD_ID = '{QuoteRecordId}' """.format(QuoteRecordId=self.contract_quote_record_id))
+                            SET 
+                            NET_VALUE = IQ.NET_VALUE,
+                            NET_PRICE = IQ.NET_PRICE,
+                            YEAR_1 = IQ.YEAR_1,
+                            YEAR_2 = IQ.YEAR_2							
+                            FROM SAQITM (NOLOCK)
+                            INNER JOIN (SELECT SAQITM.CpqTableEntryId,
+                                        CAST(ROUND(ISNULL(SUM(ISNULL(SAQICO.NET_VALUE, 0)), 0), 0) as decimal(18,2)) as NET_VALUE,
+                                        CAST(ROUND(ISNULL(SUM(ISNULL(SAQICO.NET_PRICE, 0)), 0), 0) as decimal(18,2)) as NET_PRICE,
+                                        CAST(ROUND(ISNULL(SUM(ISNULL(SAQICO.YEAR_1, 0)), 0), 0) as decimal(18,2)) as YEAR_1,
+                                        CAST(ROUND(ISNULL(SUM(ISNULL(SAQICO.YEAR_2, 0)), 0), 0) as decimal(18,2)) as YEAR_2
+                                        FROM SAQITM (NOLOCK) 
+                                        JOIN SAQICO (NOLOCK) ON SAQICO.QUOTE_RECORD_ID = SAQITM.QUOTE_RECORD_ID AND SAQICO.LINE_ITEM_ID = SAQITM.LINE_ITEM_ID
+                                        WHERE SAQITM.QUOTE_RECORD_ID = '{QuoteRecordId}' 
+                                        GROUP BY SAQITM.LINE_ITEM_ID, SAQITM.QUOTE_RECORD_ID, SAQITM.CpqTableEntryId)IQ
+                            ON SAQITM.CpqTableEntryId = IQ.CpqTableEntryId 
+                            WHERE SAQITM.QUOTE_RECORD_ID = '{QuoteRecordId}' """.format(QuoteRecordId=self.contract_quote_record_id))
     
+    def _update_quote_summary(self):
+        quote_currency = str(Quote.GetCustomField('Currency').Content)		
+        total_net_price = 0.00		
+        total_year_1 = 0.00
+        total_year_2 = 0.00        
+        total_net_value = 0.00
+        items_data = {}
+        
+        items_obj = Sql.GetList("SELECT SERVICE_ID, LINE_ITEM_ID, ISNULL(YEAR_1, 0) as YEAR_1 ,ISNULL(YEAR_2, 0) as YEAR_2 , ISNULL(NET_VALUE,0) AS NET_VALUE, ISNULL(NET_PRICE, 0) as NET_PRICE FROM SAQITM (NOLOCK) WHERE QUOTE_RECORD_ID = '{}'".format(self.contract_quote_record_id))
+        if items_obj:
+            for item_obj in items_obj:
+                items_data[int(float(item_obj.LINE_ITEM_ID))] = {'NET_VALUE':item_obj.TARGETNET_VALUE_PRICE, 'SERVICE_ID':(item_obj.SERVICE_ID.replace('- BASE', '')).strip(), 'YEAR_1':item_obj.YEAR_1, 'YEAR_2':item_obj.YEAR_2, 'NET_PRICE':item_obj.NET_PRICE}
+        for item in Quote.MainItems:
+            item_number = int(item.RolledUpQuoteItem)
+            if item_number in items_data.keys():
+                if items_data.get(item_number).get('SERVICE_ID') == item.PartNumber:
+                    item_data = items_data.get(item_number)
+                    item.TOTAL_COST.NET_PRICE = float(item_data.get('NET_PRICE'))
+                    total_net_price += item.NET_PRICE.Value
+                    item.NET_VALUE.Value = item_data.get('NET_VALUE')
+                    total_net_value += item.NET_VALUE.Value	
+                    item.YEAR_1.Value = item_data.get('YEAR_1')
+                    total_year_1 += item.YEAR_1.Value
+                    item.YEAR_2.Value = item_data.get('YEAR_2')
+                    total_year_2 += item.YEAR_2.Value        
+                    item.DISCOUNT.Value = str(self.discount)            
+        
+        Quote.GetCustomField('DISCOUNT').Content = str(self.discount)
+        Quote.GetCustomField('TOTAL_NET_PRICE').Content =str(total_net_price) + " " + quote_currency
+        Quote.GetCustomField('YEAR_1').Content = str(total_year_1) + " " + quote_currency
+        Quote.GetCustomField('YEAR_2').Content = str(total_year_2) + " " + quote_currency        
+        Quote.GetCustomField('TOTAL_NET_VALUE').Content = str(total_net_value) + " " + quote_currency
+        Quote.Save()
+
     def update_summary(self):
         if self.contract_quote_record_id:
-            Quote.GetCustomField('DISCOUNT').Content = str(self.discount)
-            for item in Quote.MainItems:
-                item.DISCOUNT.Value = str(self.discount)
-            Quote.Save()
             self._quote_item_lines_update()
             self._quote_item_update()
 discount = Param.Discount
