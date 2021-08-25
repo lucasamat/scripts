dd186b6c7edb6da994e210e6ff1b5dfded450c10
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-08-25 14:02:23 +0530

    Update CQNESTGRID.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
@@ -3926,9 +3926,9 @@ def QuoteAssemblyPreventiveMaintainenceParent(PerPage, PageInform, A_Keys, A_Val
         Trace.Write("inside---")
         cls = "eq(2)"
         dbl_clk_function = (
-            'var checkedRows=[]; localStorage.setItem("multiedit_checkbox_clicked", []); $("'
+            'var checkedRows=[]; debugger;localStorage.setItem("multiedit_checkbox_clicked", []); $("'
             + str(table_ids)
-            + '").on("check.bs.table", function (e, row, $element) { console.log("checked00009==");checkedRows.push($element.closest("tr").find("td:'
+            + '").on("click-row.bs.table", function (e, row, $element) { console.log("checked00009==");checkedRows.push($element.closest("tr").find("td:'
             + str(cls)
             + '").text()); localStorage.setItem("multiedit_checkbox_clicked", checkedRows); }); $("'
             + str(table_ids)
