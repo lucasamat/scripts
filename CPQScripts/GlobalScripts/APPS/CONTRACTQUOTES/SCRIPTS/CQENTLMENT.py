d80471d0de459fb3526768e631a00c06d57680af
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-08-26 14:03:53 +0530

    Update CQENTLMENT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
@@ -1080,28 +1080,33 @@ class Entitlements:
 							get_cost_impact = value.ENTITLEMENT_COST_IMPACT
 							get_price_impact = value.ENTITLEMENT_PRICE_IMPACT
 							get_curr = value.PRICE_METHOD
-							if 'AGS_SFM_DEI_PAC' in get_name and 'Included' in get_value:
-								#get_cost_impact = "{0:.2f}".format(next(float(x.split("_")[0]) for x in list1 if str(e.EQUIPMENT_ID) in x))
-								get_cost_impact = "{0:.2f}".format(float(list1[eq]))
-								#get_cost_impact = "{0:.2f}".format(float(list1[0]))
-								get_curr = curr
-								Trace.Write("ENTCOST1 = "+str(get_cost_impact))
-							elif 'AGS_RFM_INS_T1' in get_name and 'Included' in get_value:
-								#get_cost_impact = "{0:.2f}".format(next(float(x.split("_")[0]) for x in list2 if str(e.EQUIPMENT_ID) in x))
-								#Trace.Write("list2="+str(list2))
-								get_cost_impact = "{0:.2f}".format(float(list2[eq]))
-								get_curr = curr
-								Trace.Write("ENTCOST2 = "+str(float(list2[e.EQUIPMENT_ID])))
-								Trace.Write("ENTCOST2 = "+str(get_cost_impact))
-							elif 'AGS_RFM_INS_T2' in get_name and 'Included' in get_value:
-								#get_cost_impact = "{0:.2f}".format(next(float(x.split("_")[0]) for x in list3 if str(e.EQUIPMENT_ID) in x))
-								get_cost_impact = "{0:.2f}".format(float(list3[eq]))
-								get_curr = curr
-								Trace.Write("ENTCOST3 = "+str(get_cost_impact))
-							elif 'AGS_RFM_INS_T3' in get_name and 'Included' in get_value:
-								#get_cost_impact = "{0:.2f}".format(next(float(x.split("_")[0]) for x in list1 if str(e.EQUIPMENT_ID) in x))
-								get_cost_impact = "{0:.2f}".format(float(list4[eq]))
-								get_curr = curr
+							try:
+								if 'AGS_SFM_DEI_PAC' in get_name and 'Included' in get_value:
+									#get_cost_impact = "{0:.2f}".format(next(float(x.split("_")[0]) for x in list1 if str(e.EQUIPMENT_ID) in x))
+									get_cost_impact = "{0:.2f}".format(float(list1[eq]))
+									#get_cost_impact = "{0:.2f}".format(float(list1[0]))
+									get_curr = curr
+									Trace.Write("ENTCOST1 = "+str(get_cost_impact))
+								elif 'AGS_RFM_INS_T1' in get_name and 'Included' in get_value:
+									#get_cost_impact = "{0:.2f}".format(next(float(x.split("_")[0]) for x in list2 if str(e.EQUIPMENT_ID) in x))
+									#Trace.Write("list2="+str(list2))
+									if list2:
+										get_cost_impact = "{0:.2f}".format(float(list2[eq]))
+										get_curr = curr
+									#Trace.Write("ENTCOST2 = "+str(float(list2[e.EQUIPMENT_ID])))
+									#Trace.Write("ENTCOST2 = "+str(get_cost_impact))
+								elif 'AGS_RFM_INS_T2' in get_name and 'Included' in get_value:
+									#get_cost_impact = "{0:.2f}".format(next(float(x.split("_")[0]) for x in list3 if str(e.EQUIPMENT_ID) in x))
+									get_cost_impact = "{0:.2f}".format(float(list3[eq]))
+									get_curr = curr
+									Trace.Write("ENTCOST3 = "+str(get_cost_impact))
+								elif 'AGS_RFM_INS_T3' in get_name and 'Included' in get_value:
+									#get_cost_impact = "{0:.2f}".format(next(float(x.split("_")[0]) for x in list1 if str(e.EQUIPMENT_ID) in x))
+									get_cost_impact = "{0:.2f}".format(float(list4[eq]))
+									get_curr = curr
+							except:
+								get_curr = ''
+								pass
 								#Trace.Write("ENTCOST4 = "+str(get_cost_impact))
 							
 							updatexml  = """<QUOTE_ITEM_ENTITLEMENT>
