c6a836d5bb7c5a6c9ef51522559809da59d4d746
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-08-27 09:48:43 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -2435,13 +2435,13 @@ def Related_Sub_Banner(
 										get_SAQFDV = Sql.GetFirst("SELECT COUNT(DISTINCT VALUEDRIVER_ID) as VALUEDRIVER_ID  FROM SAQFDV(NOLOCK) WHERE QUOTE_RECORD_ID = '"+str(get_quote)+"'")
 
 										if GetPRVLDR and get_SAQVDV and get_SAQFDV:
-											if GetPRVLDR.VALUE_DRIVER_ID == get_SAQVDV.VALUEDRIVER_ID = get_SAQFDV.VALUEDRIVER_ID:
+											if GetPRVLDR.VALUE_DRIVER_ID == get_SAQVDV.VALUEDRIVER_ID == get_SAQFDV.VALUEDRIVER_ID:
 												get_quote = Quote.GetGlobal("contract_quote_record_id")
 												getPRGBVD = SqlHelper.GetList("SELECT COUNT(DISTINCT VALUEDRIVER_ID) as VALUEDRIVER_ID FROM PRGBVD(NOLOCK) WHERE  GBLVALDRV_RECORD_ID != '' AND VALUEDRIVER_TYPE ='FAB BASED SURVEY' and EDITABLE  = 'True'")
 												get_SAQFGV = SqlHelper.GetFirst("SELECT COUNT(DISTINCT VALUEDRIVER_ID) as VALUEDRIVER_ID  FROM SAQFGV(NOLOCK) WHERE QUOTE_RECORD_ID = '"+str(get_quote)+"'")
 												get_SAQEDV = SqlHelper.GetFirst("SELECT COUNT(DISTINCT VALUEDRIVER_ID) as VALUEDRIVER_ID  FROM SAQFGV(NOLOCK) WHERE QUOTE_RECORD_ID = '"+str(get_quote)+"'")
 												if getPRGBVD and get_SAQFGV and get_SAQEDVD:
-													if getPRGBVD.VALUEDRIVER_ID and get_SAQFGV.VALUEDRIVER_ID and get_SAQEDVD.VALUEDRIVER_ID:
+													if getPRGBVD.VALUEDRIVER_ID == get_SAQFGV.VALUEDRIVER_ID == get_SAQEDVD.VALUEDRIVER_ID:
 														Trace.Write('button found')
 														buttonvisibility = "Show_button"
 											else:
