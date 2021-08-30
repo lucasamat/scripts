c058e8d5ddbe4c5efa8f2f6d42cba9b53f7d5414
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-08-30 19:57:22 +0530

    Update CQREVISION.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQREVISION.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQREVISION.py
@@ -21,26 +21,32 @@ Sql = SQL()
 ScriptExecutor = ScriptExecutor
 quote_contract_recordId = Quote.GetGlobal("contract_quote_record_id")
 Trace.Write('23----')
-#create new revision start
+
 def create_new_revision(Opertion):
 	if Quote is not None:
-		
+		#create new revision start
 		edit_new_rev_quote = QuoteHelper.Edit(Quote.CompositeNumber)
 		create_new_rev = Quote.CreateNewRevision(True)
 		composite_number = Quote.CompositeNumber
 		current_revison = Quote.RevisionNumber
+		#craete new revision ends
 
+		#create new revision -SAQTRV - update-start
 		quote_revision_table_info = Sql.GetTable("SAQTRV")
 		quote_revision_id = str(Guid.NewGuid()).upper()
 		#Quote.SetGlobal("quote_revision_record_id",str(quote_revision_id))
 		quote_rev_data = {"QUOTE_REVISION_RECORD_ID": str(quote_revision_id),"QUOTE_ID": composite_number,"QUOTE_NAME": '',"QUOTE_RECORD_ID": quote_contract_recordId,"ACTIVE":0,"REV_CREATE_DATE":'',"REV_EXPIRE_DATE":'',"REVISION_STATUS":"IN-PROGRESS","QTEREV_ID":current_revison,"REV_APPROVE_DATE":''}
 		quote_revision_table_info.AddRow(quote_rev_data)
-		#Quote.GetCustomField('QUOTE_REVISION_ID').Content = quote_revision_id
-		Log.Info('quote_revision_table_info---443--quote_rev_data--'+str(quote_rev_data))
 		Sql.Upsert(quote_revision_table_info)
+		#create new revision -SAQTRV - update-end
+
+		#get quote data for update in SAQTMT start
+		
+		#get quote data for update in SAQTMT end
+
 
 	return True
-#craete new revision ends
+
 
 Opertion = Param.Opertion
 #Trace.Write(str(totalyear)+"--GET_DICT--------------"+str(GET_DICT))
