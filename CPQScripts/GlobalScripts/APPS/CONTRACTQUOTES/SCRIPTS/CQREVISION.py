d28ceb6bfb5fc9562089baa5d4d3e2a7ca787233
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-08-30 18:05:05 +0530

    Update CQREVISION.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQREVISION.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQREVISION.py
@@ -22,22 +22,22 @@ ScriptExecutor = ScriptExecutor
 ContractRecordId = Quote.GetGlobal("contract_quote_record_id")
 Trace.Write('23----')
 def CreateNewRevision(Opertion):
-    if Quote is not None:
-        QInfo= Quote.CreateNewRevision(True)
-        NRev = QuoteHelper.Edit(Quote.CompositeNumber)
-        compositeNUmber = Quote.CompositeNumber
-        currentrevison = Quote.RevisionNumber
+	if Quote is not None:
+		QInfo= Quote.CreateNewRevision(True)
+		NRev = QuoteHelper.Edit(Quote.CompositeNumber)
+		compositeNUmber = Quote.CompositeNumber
+		currentrevison = Quote.RevisionNumber
 
-        quote_revision_table_info = Sql.GetTable("SAQTRV")
-        quote_revision_id = str(Guid.NewGuid()).upper()
-        #Quote.SetGlobal("quote_revision_record_id",str(quote_revision_id))
-        quote_rev_data = {"QUOTE_REVISION_RECORD_ID": str(quote_revision_id),"QUOTE_ID": compositeNUmber,"QUOTE_NAME": contract_quote_data.get("contract_quote_data"),"QUOTE_RECORD_ID": ContractRecordId,"ACTIVE":0,"REV_CREATE_DATE":'',"REV_EXPIRE_DATE":'',"REVISION_STATUS":"IN-PROGRESS","QTEREV_ID":currentrevison,"REV_APPROVE_DATE":''}
-        quote_revision_table_info.AddRow(quote_rev_data)
-        #Quote.GetCustomField('QUOTE_REVISION_ID').Content = quote_revision_id
-        Log.Info('quote_revision_table_info---443--quote_rev_data--'+str(quote_rev_data))
-        Sql.Upsert(quote_revision_table_info)
+		quote_revision_table_info = Sql.GetTable("SAQTRV")
+		quote_revision_id = str(Guid.NewGuid()).upper()
+		#Quote.SetGlobal("quote_revision_record_id",str(quote_revision_id))
+		quote_rev_data = {"QUOTE_REVISION_RECORD_ID": str(quote_revision_id),"QUOTE_ID": compositeNUmber,"QUOTE_NAME": contract_quote_data.get("contract_quote_data"),"QUOTE_RECORD_ID": ContractRecordId,"ACTIVE":0,"REV_CREATE_DATE":'',"REV_EXPIRE_DATE":'',"REVISION_STATUS":"IN-PROGRESS","QTEREV_ID":currentrevison,"REV_APPROVE_DATE":''}
+		quote_revision_table_info.AddRow(quote_rev_data)
+		#Quote.GetCustomField('QUOTE_REVISION_ID').Content = quote_revision_id
+		Log.Info('quote_revision_table_info---443--quote_rev_data--'+str(quote_rev_data))
+		Sql.Upsert(quote_revision_table_info)
 
-    return True
+	return True
 
 
 Opertion = Param.Opertion
