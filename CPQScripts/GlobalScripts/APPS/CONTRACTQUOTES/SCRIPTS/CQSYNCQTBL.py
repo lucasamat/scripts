edcf200d9e1e3300b5b1cf9b7fd82a6b76b0605f
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-08-30 17:07:59 +0530

    Update CQSYNCQTBL.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
@@ -560,7 +560,7 @@ class SyncQuoteAndCustomTables:
 					quote_revision_table_info = Sql.GetTable("SAQTRV")
 					quote_revision_id = str(Guid.NewGuid()).upper()
 					Quote.SetGlobal("quote_revision_record_id",str(quote_revision_id))
-					quote_rev_data = {"QUOTE_REVISION_RECORD_ID": str(quote_revision_id),"QUOTE_ID": quote_id,"QUOTE_NAME": contract_quote_data.get("contract_quote_data"),"QUOTE_RECORD_ID": contract_quote_data.get("MASTER_TABLE_QUOTE_RECORD_ID"),"ACTIVE":1,"REV_CREATE_DATE":str(created_date),"REV_EXPIRE_DATE":str(expired_date),"REVISION_STATUS":"IN-PROGRESS","QTEREV_ID":0"REV_APPROVE_DATE":''}
+					quote_rev_data = {"QUOTE_REVISION_RECORD_ID": str(quote_revision_id),"QUOTE_ID": quote_id,"QUOTE_NAME": contract_quote_data.get("contract_quote_data"),"QUOTE_RECORD_ID": contract_quote_data.get("MASTER_TABLE_QUOTE_RECORD_ID"),"ACTIVE":1,"REV_CREATE_DATE":str(created_date),"REV_EXPIRE_DATE":str(expired_date),"REVISION_STATUS":"IN-PROGRESS","QTEREV_ID":0",REV_APPROVE_DATE":''}
 					quote_salesorg_table_info.AddRow(salesorg_data)
 					Quote.GetCustomField('QuoteStartDate').Content = 
 					Log.Info('quote_revision_table_info---443--quote_rev_data--'+str(quote_rev_data))
