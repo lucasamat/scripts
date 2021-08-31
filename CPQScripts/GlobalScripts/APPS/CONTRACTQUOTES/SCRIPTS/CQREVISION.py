3524c7b4b16a6ea2e7597a4124b6530e247c72e2
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-08-31 12:47:16 +0530

    Update CQREVISION.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQREVISION.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQREVISION.py
@@ -38,7 +38,7 @@ def create_new_revision(Opertion):
 		#Quote.SetGlobal("quote_revision_record_id",str(quote_revision_id))
 		get_current_rev = Sql.GetFirst("select MAX(QTEREV_ID) as rev_id from SAQTRV where QUOTE_RECORD_ID = '"+str(quote_contract_recordId)+"'")
 		newrev_inc = int(get_current_rev.rev_id)+1
-		quote_rev_data = {"QUOTE_REVISION_RECORD_ID": str(quote_revision_id),"QUOTE_ID": get_quote_info_details.QUOTE_ID,"QUOTE_NAME": '',"QUOTE_RECORD_ID": quote_contract_recordId,"ACTIVE":0,"REV_CREATE_DATE":'',"REV_EXPIRE_DATE":'',"REVISION_STATUS":"IN-PROGRESS","QTEREV_ID":newrev_inc,"REV_APPROVE_DATE":''}
+		quote_rev_data = {"QUOTE_REVISION_RECORD_ID": str(quote_revision_id),"QUOTE_ID": get_quote_info_details.QUOTE_ID,"QUOTE_NAME": get_quote_info_details.QUOTE_NAME,"QUOTE_RECORD_ID": quote_contract_recordId,"ACTIVE":0,"REV_CREATE_DATE":'',"REV_EXPIRE_DATE":'',"REVISION_STATUS":"IN-PROGRESS","QTEREV_ID":newrev_inc,"REV_APPROVE_DATE":''}
 		quote_revision_table_info.AddRow(quote_rev_data)
 		Sql.Upsert(quote_revision_table_info)
 		#create new revision -SAQTRV - update-end
