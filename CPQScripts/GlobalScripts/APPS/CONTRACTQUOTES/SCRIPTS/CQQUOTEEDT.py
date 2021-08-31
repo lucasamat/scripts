1b5380ad730c6e6b9181ca20dca1681d9d4e0fee
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-08-31 15:56:17 +0530

    Update CQQUOTEEDT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQUOTEEDT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQUOTEEDT.py
@@ -31,9 +31,9 @@ def bannerdetails(Quoteid,active_tab_name):
 		Quoteid = SQLObj.QUOTE_ID
 		matchObj = re.match( r'^\s*[A-Z]{1,2}(\d+)[A-Z]{1,2}[^>]*?\-', Quoteid)
 	if active_tab_name == "Quotes":
-		Trace.Write('34-----')
-		reObj = re.match( r'.*>\s*(\d+)*?<', Quoteid)
-		SQLObj = Sql.GetFirst("SELECT QUOTE_ID FROM SAQTMT (NOLOCK) WHERE CRM_CONTRACT_ID='" + str(reObj.group(1)) + "'")
+		Trace.Write('34-----'+str(Quoteid))
+		#reObj = re.match( r'.*>\s*(\d+)*?<', Quoteid)
+		#SQLObj = Sql.GetFirst("SELECT QUOTE_ID FROM SAQTMT (NOLOCK) WHERE CRM_CONTRACT_ID='" + str(reObj.group(1)) + "'")
 	if Quoteid is not None and str(Quoteid) !='':
 		if matchObj:
 			qid=str(matchObj.group(1))
