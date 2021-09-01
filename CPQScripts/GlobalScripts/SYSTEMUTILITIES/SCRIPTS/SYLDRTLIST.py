4438d22219a05ab9d1ba0164eab92d48b727fc19
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-09-01 15:45:40 +0530

    removed condition

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -1196,7 +1196,7 @@ class SYLDRTLIST:
 						Trace.Write('1196---contract_quote_record_id-------'+str(contract_quote_record_id))
 						RecAttValue = contract_quote_record_id
 						#Trace.Write('1196---RecAttValue--RecAttValue-----'+str(RecAttValue))
-						Qustr =  " where QUOTE_RECORD_ID = '" + str(contract_quote_record_id) + "' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id)+"' "                  
+						Qustr =  " where QUOTE_RECORD_ID = '" + str(contract_quote_record_id) + "'"           
 					elif RECORD_ID == "SYOBJR-93130":
 						CommonTreeSuperParentParam = Product.GetGlobal("CommonTreeSuperParentParam")
 						CommonTreeParentParam = Product.GetGlobal("CommonTreeParentParam")
@@ -7590,7 +7590,7 @@ class SYLDRTLIST:
 							Trace.Write('1196---contract_quote_record_id-------'+str(contract_quote_record_id))
 							RecAttValue = contract_quote_record_id
 							#Trace.Write('1196---RecAttValue--RecAttValue-----'+str(RecAttValue))
-							Qustr =  " where QUOTE_RECORD_ID = '" + str(contract_quote_record_id) + "' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id)+"'" 
+							Qustr =  " where QUOTE_RECORD_ID = '" + str(contract_quote_record_id) + "'" 
 						elif str(RECORD_ID) == "SYOBJR-95981" or str(RECORD_ID)=="SYOBJR-98459":
 							RecAttValue = Product.Attributes.GetByName("QSTN_SYSEFL_SY_01110").GetValue()
 							Qustr +=" where "+str(Wh_API_NAME) + " = '"+str(RecAttValue)+"'"
