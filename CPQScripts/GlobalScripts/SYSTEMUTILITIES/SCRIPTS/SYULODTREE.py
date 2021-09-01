09d2b89b42abbefe7ca4366fa7e62e59e8de2f55
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-09-01 19:58:03 +0530

    Update SYULODTREE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
@@ -452,7 +452,7 @@ class TreeView:
 			if GetActiveRevision:
 				Quote.SetGlobal("quote_revision_record_id",GetActiveRevision.QUOTE_REVISION_RECORD_ID)
 				Quote.SetGlobal("quote_rev_id",str(GetActiveRevision.QTEREV_ID))
-			getQuote = Sql.GetFirst("SELECT MASTER_TABLE_QUOTE_RECORD_ID,QTEREV_RECORD_ID,QTEREV_ID FROM SAQTMT(NOLOCK) WHERE QUOTE_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.CompositeNumber,quote_revision_record_id))
+			getQuote = Sql.GetFirst("SELECT MASTER_TABLE_QUOTE_RECORD_ID,QTEREV_RECORD_ID,QTEREV_ID FROM SAQTMT(NOLOCK) WHERE QUOTE_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.CompositeNumber,GetActiveRevision.QUOTE_REVISION_RECORD_ID))
 			Quote.SetGlobal("contract_quote_record_id",getQuote.MASTER_TABLE_QUOTE_RECORD_ID)
 			#GetActiveRevision = Sql.GetFirst("SELECT QUOTE_REVISION_RECORD_ID FROM SAQTRV (NOLOCK) WHERE QUOTE_ID LIKE '%{}%' AND ACTIVE = 1".format(Quote.CompositeNumber))
 			#quote_revision_record_id = Quote.GetCustomField('QUOTE_REVISION_ID').Content
