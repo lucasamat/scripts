32680a7d4d6aeb271704f5df29474ecc71de85f7
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-09-06 16:29:24 +0530

    A055S000P01-8778

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -4585,7 +4585,7 @@ class ContractQuoteCoveredObjModel(ContractQuoteCrudOpertion):
 				self._insert_quote_service_preventive_maintenance_kit_parts(batch_group_record_id=batch_group_record_id)
 				#COVERED OBJ PRE DEFINED LOGIC
 				try:						
-					CQTVLDRIFW.iflow_valuedriver_rolldown(self.contract_quote_record_id,"PREDEFINED WAFER DRIVER",self.tree_param, self.tree_parent_level_0, self.tree_parent_level_1, self.tree_parent_level_2,self.user_id,self.user_name,self.quote_revision_record_id)
+					CQTVLDRIFW.ValueDriverPreDefined(self.contract_quote_record_id,"PREDEFINED WAFER DRIVER",self.tree_param, self.tree_parent_level_0, self.tree_parent_level_1, self.tree_parent_level_2,self.user_id,self.user_name,self.quote_revision_record_id)
 				except:
 					Trace.Write("EXCEPT----PREDEFINED WAFER DRIVER IFLOW")
 				#COVERED OBJ DRIVER ROLL DOWN
