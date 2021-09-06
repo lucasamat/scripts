5cb98b295a016c4a0760e252d00405c0aa905b1a
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-09-06 14:15:22 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -4484,7 +4484,7 @@ class SYLDRTLIST:
                     Trace.Write('SubTab--ww--'+str(SubTab))
                     end = int(SubTab.split(' ')[-1]) * 12
                     start = end - 12 + 1
-                    if TreeParentParam == "Billing":
+                    if TreeParam == "Billing":
                         item_billing_plans_obj = Sql.GetList("""SELECT FORMAT(BILLING_DATE, 'MM-dd-yyyy') as BILLING_DATE FROM (SELECT ROW_NUMBER() OVER(ORDER BY BILLING_DATE)
                                     AS ROW, * FROM (SELECT DISTINCT BILLING_DATE
                                                         FROM SAQIBP (NOLOCK) WHERE QUOTE_RECORD_ID = '{}'  AND QTEREV_RECORD_ID='{}'
