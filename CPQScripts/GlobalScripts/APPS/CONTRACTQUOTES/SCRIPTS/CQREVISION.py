b684b60607c50e64cbd4f878ab01dec5a1f28ec0
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-09-06 15:04:36 +0530

    Update CQREVISION.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQREVISION.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQREVISION.py
@@ -189,7 +189,8 @@ def create_new_revision(Opertion,cartrev):
 			#quote_table_info.AddRow(quote_detials)
 			#Sql.Upsert(quote_table_info)
 		#get quote data for update in SAQTMT end
-
+		Quote = QuoteHelper.Edit(str(get_quote_id))				
+		Quote.RefreshActions()
 
 	return True
 
