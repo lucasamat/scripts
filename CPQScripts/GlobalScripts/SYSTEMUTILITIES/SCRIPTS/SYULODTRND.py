cec90c942a3c664de8bb0898947b5858b2afad46
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-09-06 13:41:04 +0530

    Update SYULODTRND.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
@@ -3144,7 +3144,7 @@ def EntitlementTreeViewHTMLDetail(
 							edit_pencil_icon = '<a href="#" class="editclick"><i title="Double Click to Edit" class="fa fa-lock"  aria-hidden="true"></i></a>'
 						attrValueSysId = attributevalues.get(attrSysId)
 						##info tooltip adding in entitlement grid starts..
-						info_column = '''<a  title="{attrSysId}" data-placement="auto top" data-trigger="focus"  class="bgcccwth10"><i class="fa fa-info-circle fltlt"></i></a>'''.format(attrSysId= attrSysId)
+						info_column = '''<a   data-placement="auto top" data-trigger="focus"  class="bgcccwth10"><i title="{attrValueSysId}" class="fa fa-info-circle fltlt"></i></a>'''.format(attrSysId= attrSysId)
 						##info tooltip adding in entitlement grid ends..
 						disp_val = ""
 						userselectedvalue = []
