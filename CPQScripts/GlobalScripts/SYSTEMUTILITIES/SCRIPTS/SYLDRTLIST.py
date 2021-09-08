9ef729840f4a7c87a794f816d58083e2e36d91da
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-09-08 20:06:39 +0530

    Update SYLDRTLIST.py
    syntax issue fixed

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -7683,34 +7683,33 @@ class SYLDRTLIST:
 					Trace.Write("7105")
 					Trace.Write("select_obj_str+++BILLING"+str(select_obj_str))    
 					select_obj_str = select_obj_str.replace("DEFAULT","[DEFAULT]") 
-					if str(RECORD_ID) == "SYOBJR-00007":#billing matrix pagination
-					Trace.Write("Pagination ifffff")
-					pivot_columns = ",".join(['[{}]'.format(billing_date) for billing_date in billing_date_column])
-					if Qustr:
+					if str(RECORD_ID) == "SYOBJR-00007":
+						Trace.Write("Pagination ifffff")
+						pivot_columns = ",".join(['[{}]'.format(billing_date) for billing_date in billing_date_column])
+						if Qustr:
 							if str(TreeParentParam)== "Billing":
 								Qustr += " AND SERVICE_ID = '{}' AND BILLING_DATE BETWEEN '{}' AND '{}'".format(TreeParam,billing_date_column[0], billing_date_column[-1])
 							else:
 								Qustr += " AND BILLING_DATE BETWEEN '{}' AND '{}'".format(billing_date_column[0], billing_date_column[-1])
-					pivot_query_str = """
-									SELECT ROW_NUMBER() OVER(ORDER BY EQUIPMENT_ID)
-									AS ROW, *
-										FROM (
-											SELECT 
-												{Columns}                                           
-											FROM {ObjectName}
-											{WhereString}
-										) AS IQ
-										PIVOT
-										(
-											SUM(BILLING_AMOUNT)
-											FOR BILLING_DATE IN ({PivotColumns})
-										)AS PVT
-									""".format(OrderByColumn=Wh_API_NAMEs, Columns=column_before_pivot_change, ObjectName=ObjectName,
-												WhereString=Qustr, PivotColumns=pivot_columns)                        
-					Qury_str = """
-									SELECT DISTINCT TOP {PerPage} * FROM ( SELECT * FROM ({InnerQuery}) OQ WHERE ROW BETWEEN {Start} AND {End} ) AS FQ ORDER BY EQUIPMENT_ID
-									""".format(PerPage=PerPage, OrderByColumn=Wh_API_NAMEs, InnerQuery=pivot_query_str, Start=Page_start, End=Page_End)
-					QuryCount_str = "SELECT COUNT(*) AS cnt FROM ({InnerQuery}) OQ ".format(InnerQuery=pivot_query_str) 
+						pivot_query_str = """
+										SELECT ROW_NUMBER() OVER(ORDER BY EQUIPMENT_ID)
+										AS ROW, *
+											FROM (
+												SELECT 
+													{Columns}                                           
+												FROM {ObjectName}
+												{WhereString}
+											) AS IQ
+											PIVOT
+											(
+												SUM(BILLING_AMOUNT)
+												FOR BILLING_DATE IN ({PivotColumns})
+											)AS PVT
+										""".format(OrderByColumn=Wh_API_NAMEs, Columns=column_before_pivot_change, ObjectName=ObjectName,
+													WhereString=Qustr, PivotColumns=pivot_columns)                        
+						Qury_str = """SELECT DISTINCT TOP {PerPage} * FROM ( SELECT * FROM ({InnerQuery}) OQ WHERE ROW BETWEEN {Start} AND {End} ) AS FQ ORDER BY EQUIPMENT_ID
+										""".format(PerPage=PerPage, OrderByColumn=Wh_API_NAMEs, InnerQuery=pivot_query_str, Start=Page_start, End=Page_End)
+						QuryCount_str = "SELECT COUNT(*) AS cnt FROM ({InnerQuery}) OQ ".format(InnerQuery=pivot_query_str) 
 					else:
 						Qury_str = (
 							"select top "
