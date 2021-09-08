b95389595acece18842a53e6c34d0cd96d7e609e
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-09-08 16:30:52 +0530

    Update SYMCTABSGD.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYMCTABSGD.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYMCTABSGD.py
@@ -971,6 +971,16 @@ class CONTAINER:
                                                 + str(Page_End)
                                                 + " "
                                             )
+                                            elif flag == 1 and (str(x_tabs) == 'Quotes' or str(x_tabs) == 'Contracts'):
+                                                Trace.Write("flag11====")
+                                                where += " AND CPQTABLEENTRYADDEDBY = '{}' ".format(User.UserName)
+                                                QueryStr = (" select TOP 1000 RV.SALESORG_ID,RV.REVISION_STATUS,RV.REVISION_DESCRIPTION,QT.ACCOUNT_ID,QT.ACCOUNT_NAME,QT.CONTRACT_VALID_FROM,QT.CONTRACT_VALID_TO,QT.OWNER_ID,QT.MASTER_TABLE_QUOTE_RECORD_ID,QT.QTEREV_ID,QT.QUOTE_ID,OP.OPPORTUNITY_NAME FROM SAQTMT (NOLOCK) QT JOIN SAQTRV(NOLOCK) RV ON RV.QUOTE_ID = QT.QUOTE_ID and RV.QUOTE_REVISION_RECORD_ID = QT.QTEREV_RECORD_ID JOIN SAOPPR(NOLOCK) OP on OP.SALESORG_ID = RV.SALESORG_ID "
+                                                + str(where)
+                                                + ") m where m.ROW BETWEEN "
+                                                + str(Page_start)
+                                                + " and "
+                                                + str(Page_End)
+                                                + "")
                                             elif flag == 0 and (str(x_tabs) == 'My Approvals Queue'):
                                                 where += " AND APPROVALSTATUS = 'REQUESTED' AND ARCHIVED = 0 "
                                                 QueryStr = (
