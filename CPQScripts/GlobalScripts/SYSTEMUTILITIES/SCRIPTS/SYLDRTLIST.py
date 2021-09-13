a88bc9aec233d6aa2ad9acebccf8616598609d97
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-09-13 19:31:08 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -3559,7 +3559,7 @@ class SYLDRTLIST:
 									+ "</th>"
 								)    
 								
-				elif RECORD_ID == 'SYOBJR-00009' and invs in ('PRICE_BENCHMARK_TYPE','TOOL_CONFIGURATION','ANNUAL_BENCHMARK_BOOKING_PRICE','CONTRACT_ID','CONTRACT_VALID_FROM','CONTRACT_VALID_TO','BENCHMARKING_THRESHOLD','NET_VALUE_INGL_CURR'):
+				elif RECORD_ID == 'SYOBJR-00009' and invs in ('PRICE_BENCHMARK_TYPE','TOOL_CONFIGURATION','ANNUAL_BENCHMARK_BOOKING_PRICE','CONTRACT_ID','CONTRACT_VALID_FROM','CONTRACT_VALID_TO','BENCHMARKING_THRESHOLD'):
 					if invs in ('PRICE_BENCHMARK_TYPE','TOOL_CONFIGURATION','ANNUAL_BENCHMARK_BOOKING_PRICE','CONTRACT_ID','CONTRACT_VALID_FROM','CONTRACT_VALID_TO','BENCHMARKING_THRESHOLD'):
 						if pricing_picklist_value == 'Pricing':
 							rowspan_level1 = 'rowspan="2"'
@@ -3568,15 +3568,13 @@ class SYLDRTLIST:
 						align = ''
 
 						if not table_group_columns :
-							table_header += '<th colspan="7" rowspan =2 data-align="center"><div><button style="border:none;" class="glyphicon glyphicon-minus-sign" id="price-benchmark-column-toggle" onclick="price_benchmark_column_toggle(this)"></button>PRICE BENCHMARKING</div></th>'
+							table_header += '<th colspan="7" '+rowspan_level1+' data-align="center"><div><button style="border:none;" class="glyphicon glyphicon-minus-sign" id="price-benchmark-column-toggle" onclick="price_benchmark_column_toggle(this)"></button>PRICE BENCHMARKING</div></th>'
 						if str(invs) in right_align_list:
 							align = 'right'
 						elif str(invs) in center_align_list:
 							align = 'center'
 						table_group_columns += (
-									'<th '
-									+ rowspan_level1
-									+ ' data-toggle="bootstrap-table" data-field="'
+									'<th data-toggle="bootstrap-table" data-field="'
 									+ str(invs)
 									+ '" data-filter-control="input" data-align="'
 									+ str(align)
