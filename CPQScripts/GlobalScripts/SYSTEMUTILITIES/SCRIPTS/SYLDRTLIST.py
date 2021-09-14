2c1d097b4a239467ae4c34d7b11e7ea4bda76714
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-09-14 20:48:52 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -3627,7 +3627,7 @@ class SYLDRTLIST:
 					else:
 						rowspan_level1 = ""
 					if not table_group_columns2:
-						table_header += '<th colspan="2" '+rowspan_level1+'  data-align="center"><div><button style="border:none;" class="glyphicon glyphicon-minus-sign" id="cost-column-toggle" onclick="entitlement_category_toggle(this)"></button>COST</div></th>'
+						table_header += '<th colspan="2" '+rowspan_level1+'  data-align="center"><div><button style="border:none;" class="glyphicon glyphicon-minus-sign" id="cost-column-toggle" onclick="line_item_column_toggle(this)"></button>COST</div></th>'
 					if str(invs) in right_align_list:
 						align = 'right'
 					elif str(invs) in center_align_list:
@@ -3654,7 +3654,7 @@ class SYLDRTLIST:
 					else:
 						rowspan_level1 = ""
 					if not table_group_columns3:
-						table_header += '<th colspan="2" '+rowspan_level1+'  data-align="center"><div><button style="border:none;" class="glyphicon glyphicon-minus-sign" id="price-column-toggle" onclick="entitlement_category_toggle(this)"></button>PRICE</div></th>'
+						table_header += '<th colspan="2" '+rowspan_level1+'  data-align="center"><div><button style="border:none;" class="glyphicon glyphicon-minus-sign" id="price-column-toggle" onclick="line_item_column_toggle(this)"></button>PRICE</div></th>'
 					if str(invs) in right_align_list:
 						align = 'right'
 					elif str(invs) in center_align_list:
@@ -3681,7 +3681,7 @@ class SYLDRTLIST:
 					else:
 						rowspan_level1 = ""
 					if not table_group_columns4:
-						table_header += '<th colspan="2" '+rowspan_level1+'  data-align="center"><div><button style="border:none;" class="glyphicon glyphicon-minus-sign" id="linesummary-column-toggle" onclick="entitlement_category_toggle(this)"></button>PRICE</div></th>'
+						table_header += '<th colspan="2" '+rowspan_level1+'  data-align="center"><div><button style="border:none;" class="glyphicon glyphicon-minus-sign" id="linesummary-column-toggle" onclick="line_item_column_toggle(this)"></button>PRICE</div></th>'
 					if str(invs) in right_align_list:
 						align = 'right'
 					elif str(invs) in center_align_list:
