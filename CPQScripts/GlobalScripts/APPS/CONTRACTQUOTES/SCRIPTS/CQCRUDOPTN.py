a4b8ec8379533689a493398e4662d0223e46eaba
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2021-09-14 16:34:33 +0530

    Update CQCRUDOPTN.py
    syntax error fixed.

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -4318,7 +4318,7 @@ class ContractQuoteCoveredObjModel(ContractQuoteCrudOpertion):
 		if getdate:
 			#update_contract_date_greenbook_level = "UPDATE SAQSGB SET CONTRACT_VALID_FROM = 'CONVERT(VARCHAR(10),{},101)', CONTRACT_VALID_TO = 'CONVERT(VARCHAR(10),{},101)' QTEREV_ID='{}' WHERE QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID = '{}'".format(getdate.CONTRACT_VALID_FROM, getdate.CONTRACT_VALID_TO, getdate.QTEREV_ID, self.contract_quote_record_id,self.quote_revision_record_id)
 			update_contract_date_greenbook_level = "UPDATE SAQSGB SET QTEREV_ID='{}' WHERE QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID = '{}'".format(getdate.QTEREV_ID, self.contract_quote_record_id,self.quote_revision_record_id)
-   			Sql.RunQuery(update_contract_date_greenbook_level)
+			Sql.RunQuery(update_contract_date_greenbook_level)
 					
 	
 	def _insert_quote_service_preventive_maintenance_kit_parts(self, **kwargs):
