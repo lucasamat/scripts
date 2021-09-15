484a20310d69957328b95d5e374e328d0eae666a
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-09-15 15:07:32 +0530

    Revert "Update SYULODTRND.py"
    This reverts commit 2994f1ae2ddb13419b340018dce8e142458da641.

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
@@ -2800,9 +2800,9 @@ def EntitlementTreeViewHTMLDetail(
 										selected_option = ' title="'+str(value.STANDARD_ATTRIBUTE_DISPLAY_VAL)+'" '
 									VAR1 += (
 										'<option '+disallow_style+' id="'+value.SYSTEM_ID+'"  value = "'
-										+ value.STANDARD_ATTRIBUTE_DISPLAY_VAL 
-										+ '"'+select_option+'>'
-										+ value.STANDARD_ATTRIBUTE_DISPLAY_VAL
+										+ str(value.STANDARD_ATTRIBUTE_DISPLAY_VAL) 
+										+ '"'+str(select_option)+'>'
+										+ str(value.STANDARD_ATTRIBUTE_DISPLAY_VAL)
 										+ "</option>"
 									)
 							sec_str1 += (
