446cf7b0cf48b1e701f8ad6f774a20dbd232ca67
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-09-16 10:59:19 +0530

    Update CQSYNCQTBL.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
@@ -479,7 +479,7 @@ class SyncQuoteAndCustomTables:
 					)
 				)
 				##To Set the Quote Revision Record while refreshing the page..
-				Quote.SetGlobal("quote_revision_record_id",str(quote_obj.QTEREV_RECORD_ID))
+				#Quote.SetGlobal("quote_revision_record_id",str(quote_obj.QTEREV_RECORD_ID))
 				##To Set the Quote Revision Record while refreshing the page..
 				payid =""
 				paydesc = ""
