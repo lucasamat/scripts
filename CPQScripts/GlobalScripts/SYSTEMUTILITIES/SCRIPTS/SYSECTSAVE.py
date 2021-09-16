1309253331583ff58b4d9361d4a55c4aa2b47aed
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2021-09-16 20:47:28 +0530

    Update SYSECTSAVE.py
    Updating the Revision Approved Date while changing the status to Approved

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
@@ -475,6 +475,7 @@ def MaterialSave(ObjectName, RECORD, warning_msg, SectionRecId=None):
 						get_record_val =  newdict.get("QUOTE_REVISION_RECORD_ID")
 						get_rev_val =  newdict.get("QTEREV_ID")
 						get_status = newdict.get("REVISION_STATUS")
+						get_approved_date = newdict.get("REV_APPROVE_DATE")
 						if getactive == 'false':
 							getactive = 0
 						else:
@@ -488,6 +489,11 @@ def MaterialSave(ObjectName, RECORD, warning_msg, SectionRecId=None):
 						Quote.SetGlobal("contract_quote_record_id",get_quote_info_details.MASTER_TABLE_QUOTE_RECORD_ID)
 						Quote.SetGlobal("quote_revision_record_id",str(get_quote_info_details.QTEREV_RECORD_ID))
 						if get_status.upper() == "APPROVED":
+							##Updating the Revision Approved Date while changing the status to Approved...
+							if get_approved_date == "":
+								RevisionApprovedDate = datetime.now().date()
+								Sql.RunQuery("UPDATE SAQTRV SET REV_APPROVE_DATE = '{RevisionApprovedDate}' WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' and QTEREV_RECORD_ID = '{RevisionRecordId}' ".format(RevisionApprovedDate = RevisionApprovedDate,QuoteRecordId = Quote.GetGlobal("contract_quote_record_id"),RevisionRecordId = Quote.GetGlobal("quote_revision_record_id")))
+							##Updating the Revision Approved Date while changing the status to Approved...
 							crm_result = ScriptExecutor.ExecuteGlobal('QTPOSTACRM',{'QUOTE_ID':str(newdict.get("QUOTE_ID")),'REVISION_ID':str(get_rev_val),'Fun_type':'cpq_to_crm'})
 					#A055S000P01-4288 end
 					else:
