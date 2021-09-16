20c54019449049d116f70a36c80dda8ac67f98a5
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2021-09-16 16:15:21 +0530

    Update CQREVISION.py
    Commented for QI generated in Revision.

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQREVISION.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQREVISION.py
@@ -226,10 +226,10 @@ def create_new_revision(Opertion,cartrev):
 		#get quote data for update in SAQTMT end
 		NRev = QuoteHelper.Edit(get_quote_info_details.QUOTE_ID)
 		Quote.RefreshActions()
-		for item in Quote.MainItems:
-			item.Delete()
+		#for item in Quote.MainItems:
+		#	item.Delete()
 		Quote.Save()
-		Quote.RefreshActions()
+		#Quote.RefreshActions()
 		current_revison1 = Quote.RevisionNumber
 		Trace.Write("============>>1111 "+str(current_revison1))
 		get_quote_info_details = Sql.GetFirst("select * from SAQTMT where QUOTE_ID = '"+str(Quote.CompositeNumber)+"'")
