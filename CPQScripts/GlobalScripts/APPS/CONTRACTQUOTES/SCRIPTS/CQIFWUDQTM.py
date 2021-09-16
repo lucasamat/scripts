60c5e1dd6eb71a2f8bb25b7ea2622a518a0e851b
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-09-16 18:38:17 +0530

    Update CQIFWUDQTM.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQIFWUDQTM.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQIFWUDQTM.py
@@ -18,7 +18,7 @@ ScriptExecutor = ScriptExecutor
 #Log.Info('quote_revision_record_id- '+str(quote_revision_record_id))
 def quoteiteminsert(Qt_id):
     quote_number = Qt_id[2:12]
-    #Log.Info('quote_revision_record_id- '+str(quote_revision_record_id)+'--'+str(Qt_id))
+    Log.Info('quote_revision_record_id- ')
     quote_Edit = QuoteHelper.Edit(quote_number)
     get_curr = str(Quote.GetCustomField('Currency').Content)
     total_cost = 0.00
