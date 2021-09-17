37253e64b6f1b8b3d376413ff68e95b3c98e2019
Viknesh <Vignesh_Vijayakumar@contractor.amat>
2021-09-17 12:41:02 +0530

    Added Header panel for customer value drivers in fab level of fab locations

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTFABVIEW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTFABVIEW.py
@@ -456,7 +456,10 @@ def servicefabview(ACTION,CurrentRecordId):
 	date_field = []
 	
 	GetPRVLDR = Sql.GetList("SELECT DISTINCT VALUE_DRIVER_ID,VALUE_DRIVER_RECORD_ID FROM PRVLDR(NOLOCK) WHERE VALUE_DRIVER_TYPE LIKE '%QUOTE BASED%'")
-	sec_str += ('<table id="' + str(table_id)+ '" data-escape="true" data-html="true"    data-show-header="true" > <thead><tr>')
+	sec_str += ('<div id = "fabnotify">')
+	sec_str += ('''<div class="dyn_main_head master_manufac glyphicon pointer   glyphicon-chevron-down fabvaldrives-toggle" onclick="dyn_main_sec_collapse_arrow(this)" data-target=".fabvaldrives" data-toggle="collapse"><label class="onlytext"><label class="onlytext"><div> <div id="ctr_drop" class="btn-group dropdown"><div class="dropdown"><i data-toggle="dropdown" class="fa fa-sort-desc dropdown-toggle"></i><ul class="dropdown-menu left" aria-labelledby="dropdownMenuButton"><li class="edit_list"> <a id="customer_value_driver_info" class="dropdown-item" href="#" onclick="customer_value_EDIT(this)">EDIT</a></li></ul></div></div> CUSTOMER VALUE DRIVERS INFORMATION</div></label></label></div>''')
+	sec_str += ('<div class = "fabvaldrives collapse in">')
+	sec_str += ('<table id="' + str(table_id)+ '" data-escape="true" data-html="true"  data-locale = "en-US" data-show-header="true"> <thead><tr>')
 	for key, invs in enumerate(list(desc_list)):
 		invs = str(invs).strip()
 		qstring = attr_dict.get(str(invs)) or ""
