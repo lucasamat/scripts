1dc14dfc83ec4c723e2a43ef708612d6f7a7b74a
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-09-20 19:49:06 +0530

    Update SYATABSAVE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYATABSAVE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYATABSAVE.py
@@ -437,7 +437,7 @@ for tab in Product.Tabs:
                                         )
                                         Trace.Write("99999888"+str(flag))    
                                         if TABLE_NAME == "ACAPCH" and len(row['APRCHN_ID']) < 8:
-                                            flag = 'false'
+                                            flag = 'False'
                                         if iskey is not None and flag == "True": 
                                             Trace.Write("99999")                                           
                                             col_name = (iskey.API_NAME).strip()                                                                                                                                    
