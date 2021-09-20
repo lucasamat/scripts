fb9ca7bb340311d0843fcf25a3b330f25b850ba2
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-09-20 18:34:04 +0530

    Update SYATABSAVE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYATABSAVE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYATABSAVE.py
@@ -578,11 +578,7 @@ for tab in Product.Tabs:
                                                         ##CPQ Attribute name ends
                                                         tableInfo = Sql.GetTable(TABLE_NAME)                                                        
                                                         tableInfo.AddRow(row)
-                                                        Trace.Write("OBJ_J "+str(TABLE_NAME))
-                                                        if TABLE_NAME == "ACAPCH" and len(row['APRCHN_ID']) < 8:
-                                                            pass
-                                                        else:
-                                                            Sql.Upsert(tableInfo)
+                                                        Sql.Upsert(tableInfo)
                                                         
                                                         
                                                     Product.Attributes.GetByName(str(RECORD_ID)).Allowed = True
