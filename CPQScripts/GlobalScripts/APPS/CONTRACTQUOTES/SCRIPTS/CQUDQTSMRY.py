d86e5062d12fcd1e578f32b30b5d01384edecf82
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-09-21 12:11:16 +0530

    Update CQUDQTSMRY.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQUDQTSMRY.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQUDQTSMRY.py
@@ -188,7 +188,7 @@ class ContractQuoteSummaryUpdate:
                         FROM SAQICO (NOLOCK) 
                         WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{RevisionRecordId}'
                         GROUP BY FABLOCATION_ID, QUOTE_RECORD_ID,QTEREV_RECORD_ID,LINE_ITEM_ID,SERVICE_ID)IQ
-            ON SAQICO.FABLOCATION_ID = IQ.FABLOCATION_ID AND SAQICO.QUOTE_RECORD_ID = IQ.QUOTE_RECORD_ID AND SAQICO.SERVICE_ID = IQ.SERVICE_ID AND SAQICO.QTEREV_RECORD_ID = IQ.QTEREV_RECORD_ID 
+            ON SAQICO.QUOTE_RECORD_ID = IQ.QUOTE_RECORD_ID AND SAQICO.SERVICE_ID = IQ.SERVICE_ID AND SAQICO.QTEREV_RECORD_ID = IQ.QTEREV_RECORD_ID 
             WHERE SAQICO.QUOTE_RECORD_ID = '{QuoteRecordId}' AND SAQICO.QTEREV_RECORD_ID = '{RevisionRecordId}' """.format(QuoteRecordId=self.contract_quote_record_id,RevisionRecordId=self.quote_revision_record_id,
             Discount=self.discount))
         Sql.RunQuery("""UPDATE SAQITM
