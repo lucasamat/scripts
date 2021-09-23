17af773380b57470fbc3d7dfd9ad00a9166d4a1f
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-09-23 12:41:39 +0530

    Update SYCONUPDAL.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYCONUPDAL.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYCONUPDAL.py
@@ -223,7 +223,7 @@ class ConfigUpdateScript:
 					labels.append(objd_record.FIELD_LABEL)
 				##A055S000P01-9370 , A055S000P01-4191 code starts...
 				if self.current_tab_name == "Quote":
-					field_lables = "Key,Quote ID,Active Revision ID,Account ID,Account Name,Contract Valid From,Contract Valid To,Revision status,Sales Org ID,Quote owner"
+					field_lables = "Key,Quote ID,Active Revision ID,Account ID,Account Name,Contract Valid From,Contract Valid To,Revision status,Sales Org ID,Quote owner,POES"
 				else:
 					field_lables = ",".join(labels)
 				##A055S000P01-9370, A055S000P01-4191 code ends..
