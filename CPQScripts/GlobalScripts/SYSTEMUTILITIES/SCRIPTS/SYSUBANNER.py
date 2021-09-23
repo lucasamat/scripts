0509b4266125cb71863799fa90560444f4fb0e2b
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-09-23 18:44:29 +0530

    Update SYSUBANNER.py
    A055S000P01-9398

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -1252,7 +1252,12 @@ def Related_Sub_Banner(
                     ThirdLable = "Product Offering Type"
                     ThirdValue = TreeParentParam
                     ##adding configuration status in offering subtab
-                    status_image = 'config_status_icon.png'
+                    where_cond = "WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' AND SERVICE_ID ='{}'".format(Quote.GetGlobal(contract_quote_record_id, quote_revision_record_id, TreeParam) )
+                    get_status = ScriptExecutor.ExecuteGlobal("CQENTLNVAL", {'action':'GET_STATUS','partnumber':TreeParam,'where_cond':where_cond,'ent_level_table':'SAQTSE'})
+                    if get_status == 'true':
+                        status_image = 'config_status_icon.png'
+                    else:
+                        status_image = 'config_pend_status_icon.png'
                     FourthLable = "Configuration Status"
                     FourthValue = '<img class="treeinsideicon" src="/mt/appliedmaterials_tst/Additionalfiles/AMAT/Quoteimages/{image}"/>'.format(image = status_image)
                     FifthLable = ""
