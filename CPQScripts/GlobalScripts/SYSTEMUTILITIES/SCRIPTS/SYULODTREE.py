7d02e8b8887ab56888c34c26f8829d36da55d73b
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-09-23 17:47:03 +0530

    Update SYULODTREE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
@@ -1942,8 +1942,9 @@ class TreeView:
 								if str(ObjectName).strip() == 'ACAPCH' and str(NodeName) == 'APRCHNSTP_NAME' and str(ProductName).upper() == "APPROVAL CENTER":
 									NodeText = "Step "+str(childdata.APRCHNSTP_NUMBER)+ " : " +str(NodeText)
 								##showing config status along with offering	
+								Trace.Write('NodeText--child-'+str(ObjectName))
 								if str(ObjectName).strip() == 'SAQTSV' and str(NodeName) == 'SERVICE_ID':
-									Trace.Write('NodeText--child-'+str(NodeText))
+									Trace.Write('NodeText--inside-'+str(NodeText))
 									image_url = 'config_status_icon.png'
 									image_url = '<img class="leftside-bar-status_icon" src="/mt/appliedmaterials_tst/Additionalfiles/AMAT/Quoteimages/{image_url}"/>'.format(image_url = image_url)
 									NodeText = image_url+NodeText
