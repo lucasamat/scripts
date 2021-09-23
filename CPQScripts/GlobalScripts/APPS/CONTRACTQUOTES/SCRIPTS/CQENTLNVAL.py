3b192bc774b049b574bb52eb1e36f216eab4cb5e
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-09-23 11:51:55 +0530

    Create CQENTLNVAL.py
    script created for getting entitlement value

---
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLNVAL.py
@@ -0,0 +1,19 @@
+# =========================================================================================================================================
+#   __script_name : CQENTLNVAL.py
+#   __script_description : THIS SCRIPT IS USED TO GET ENTITLEMENT VALUES.
+#   __primary_author__ : 
+#   __create_date :8/23/2021
+#   © BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
+# ==========================================================================================================================================
+import Webcom
+from datetime import datetime
+import Webcom.Configurator.Scripting.Test.TestProduct
+import time
+from SYDATABASE import SQL
+Sql = SQL()
+
+import System.Net
+import sys
+
+
+Trace.Write('script called')
