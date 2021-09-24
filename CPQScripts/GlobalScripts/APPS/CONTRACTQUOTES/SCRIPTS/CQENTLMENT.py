103e785506c3dcd0fa85935099b4532facedf328
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-09-24 13:34:55 +0530

    Update CQENTLMENT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
@@ -653,6 +653,15 @@ class Entitlements:
 								display_value_arr = [i.STANDARD_ATTRIBUTE_DISPLAY_VAL for i in STANDARD_ATTRIBUTE_VALUES]
 								
 								multi_select_attr_list[str(attrSysId)] = display_value_arr
+							if ent_val_code:
+								display_value_arr =[]
+								ent_chkbox_code = str(tuple(eval(ent_chkbox_code))).replace(',)',')')
+								STANDARD_ATTRIBUTE_VALUES=SqlHelper.GetList("SELECT V.STANDARD_ATTRIBUTE_DISPLAY_VAL, V.STANDARD_ATTRIBUTE_VALUE FROM PRODUCT_ATTRIBUTES PA INNER JOIN ATTRIBUTES A ON PA.PA_ID=A.PA_ID INNER JOIN STANDARD_ATTRIBUTE_VALUES V ON A.STANDARD_ATTRIBUTE_VALUE_CD = V.STANDARD_ATTRIBUTE_VALUE_CD INNER JOIN ATTRIBUTE_DEFN (NOLOCK) AD ON AD.STANDARD_ATTRIBUTE_CODE=V.STANDARD_ATTRIBUTE_CODE WHERE AD.SYSTEM_ID = '{sys_id}' AND  PA.PRODUCT_ID ='{pid}' AND V.STANDARD_ATTRIBUTE_VALUE in {ent_chkbox_code}".format(sys_id = str(attrSysId),pid =product_obj.PRD_ID), ent_chkbox_code= ent_chkbox_code)
+								if STANDARD_ATTRIBUTE_VALUES :
+									
+									display_value_arr = [i.STANDARD_ATTRIBUTE_DISPLAY_VAL for i in STANDARD_ATTRIBUTE_VALUES]
+									
+							multi_select_attr_list[str(key)] = display_value_arr
 						if attributevalues.get(attrSysId) is None:
 							ent_disp_val = ''
 						else:
