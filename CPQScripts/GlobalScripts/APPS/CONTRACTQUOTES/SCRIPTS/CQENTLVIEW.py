056b5aac4ebaa516053a7e984b874f666b32a1d0
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-09-28 21:36:16 +0530

    Update CQENTLVIEW.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
@@ -481,9 +481,9 @@ class EntitlementView():
                                 attributes_disallowed_list.append(attrSysId)
                             else:
                                 add_style = ""
-                            Trace.Write(str(attrSysId)+'--attrLabel-2602---attrValue--'+str(add_style))
+                            #Trace.Write(str(attrSysId)+'--attrLabel-2602---attrValue--'+str(add_style))
                             if attrSysId in attributedefaultvalue:
-                                Trace.Write("add_style----3077----- "+str(attrSysId))
+                                #Trace.Write("add_style----3077----- "+str(attrSysId))
                                 add_style = "color:#1B78D2"
                             # if attrSysId in attributedefaultvalue:
                             # 	add_style_color = ";color: red"
