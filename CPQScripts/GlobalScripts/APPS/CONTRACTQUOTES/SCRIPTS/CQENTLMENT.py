4c05c87703857efb357fedd39cc307f1f2e87983
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-09-28 16:55:56 +0530

    Update CQENTLMENT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
@@ -542,9 +542,10 @@ class Entitlements:
 								if prdvalue["visible"] == "false":
 									try:
 										getrec = Sql.GetFirst("select RECORD_ID from SYSECT where PARENT_SECTION_TEXT = '"+str(prdvalue["id"])+"'")
+										attr_tab_list_disallow.append(getrec.RECORD_ID)
 									except:
 										getrec = Sql.GetFirst("select RECORD_ID from SYSECT where PARENT_SECTION_TEXT = '"+str(prdvalue["id"])+"'")
-									attr_tab_list_disallow.append(getrec.RECORD_ID)
+									
 						if Productattribute == "characteristics":
 							for prdvalue in Productvalue:
 								#dropdownallowlist = [] 
