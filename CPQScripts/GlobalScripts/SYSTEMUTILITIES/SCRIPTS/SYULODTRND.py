34d1634bee491d9ba0fe191e0ae576f2d5e92b7a
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-09-29 17:59:27 +0530

    Update SYULODTRND.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
@@ -1437,7 +1437,7 @@ and GREENBOOK = '{}' AND FABLOCATION_ID = '{}'""".format(quote_record_id,quote_r
 					and formula_data_type != "CHECKBOX"
 					and formula_data_type != "CURRENCY"
 				):	
-					Trace.Write(str(readonly)+'current_obj_api_name--1440---'+str(current_obj_api_name))				
+					Trace.Write(str(lookup_val)+'--lookup_val---'+str(readonly)+'current_obj_api_name--1440---'+str(current_obj_api_name))				
 					if current_obj_api_name in lookup_val and str(readonly) != "readonly":
 						Trace.Write(str(readonly)+'current_obj_api_name--1443---'+str(current_obj_api_name))	
 						sec_str += (
