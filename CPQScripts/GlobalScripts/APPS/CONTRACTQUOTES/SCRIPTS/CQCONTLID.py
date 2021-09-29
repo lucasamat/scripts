556dd94124e3ee28f7048f3ecb07f4f9d4c42ee7
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-09-29 11:58:07 +0530

    Update CQCONTLID.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCONTLID.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCONTLID.py
@@ -60,6 +60,7 @@ class tool_idle:
             lookup_list = {ins.LOOKUP_API_NAME: ins.API_NAME for ins in Objd_Obj}
 
             current_obj_value = ""
+            onchange = ""
             current_obj_api_name = attr.API_NAME
 
         #sec_str = '<div id="contract_cust_fields_div">'     
