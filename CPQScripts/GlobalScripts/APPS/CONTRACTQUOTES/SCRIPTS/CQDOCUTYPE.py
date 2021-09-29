05d9f07fe171f65239ebcda330677c516d4630f2
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2021-09-29 16:11:00 +0530

    Update CQDOCUTYPE.py
    Commit

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQDOCUTYPE.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQDOCUTYPE.py
@@ -15,10 +15,11 @@ Sql = SQL()
 
 def update_document_type(QuoteRecordId,RevisionRecordId):
     document_type_list = []
-    service_obj  = Sql.GetList("select SERVICE_ID from SAQTSV where QUOTE_RECORD_ID = '{QuoteRecordId}' and QTEREV_RECORD_ID = '{RevisionRecordId}'".format(QuoteRecordId = Quote.GetGlobal("contract_quote_record_id"),RevisionRecordId = Quote.GetGlobal("quote_revision_record_id")))
+    service_obj  = Sql.GetList("select SERVICE_ID from SAQTSV(NOLOCK) where QUOTE_RECORD_ID = '{QuoteRecordId}' and QTEREV_RECORD_ID = '{RevisionRecordId}'".format(QuoteRecordId = Quote.GetGlobal("contract_quote_record_id"),RevisionRecordId = Quote.GetGlobal("quote_revision_record_id")))
+    Quote_obj = Sql.GetFirst("SELECT POES FROM  SAQTMT(NOLOCK) WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' and QTEREV_RECORD_ID = '{RevisionRecordId}'".format(QuoteRecordId = Quote.GetGlobal("contract_quote_record_id"),RevisionRecordId = Quote.GetGlobal("quote_revision_record_id")))
     for service in service_obj:
         #Log.Info("service_obj--------------->"+str(service.SERVICE_ID))
-        document_type_obj = Sql.GetFirst("select DOCTYP_ID from MAMADT where SAP_PART_NUMBER = '{}'".format(service.SERVICE_ID))
+        document_type_obj = Sql.GetFirst("select DOCTYP_ID from MAMADT(NOLOCK) where SAP_PART_NUMBER = '{}' AND POES ='{}'".format(service.SERVICE_ID,Quote_obj.POES))
         #Log.Info("document_type_obj--------------->"+str(document_type_obj.DOCTYP_ID))
         if document_type_obj is not None:
             document_type_list.append(document_type_obj.DOCTYP_ID)
@@ -29,7 +30,7 @@ def update_document_type(QuoteRecordId,RevisionRecordId):
     else:
         document_type = document_type_obj.DOCTYP_ID
     #Log.Info(document_type_list)
-    document_type_obj = Sql.GetFirst("select DOCTYP_ID,DOCTYP_RECORD_ID from MAMADT where DOCTYP_ID = '{}'".format(document_type))
+    document_type_obj = Sql.GetFirst("select DOCTYP_ID,DOCTYP_RECORD_ID from MAMADT(NOLOCK) where DOCTYP_ID = '{}' AND POES ='{}' ".format(document_type,Quote_obj.POES))
     #Log.Info("UPDATE SAQTRV SET DOCTYP_ID = '{DocumentType}',DOCTYP_RECORD_ID = '{DocumentTypeRecordId}' WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' and QTEREV_RECORD_ID = '{RevisionRecordId}' ".format(DocumentType = document_type,DocumentTypeRecordId = document_type_obj.DOCTYP_RECORD_ID,QuoteRecordId = Quote.GetGlobal("contract_quote_record_id"),RevisionRecordId = Quote.GetGlobal("quote_revision_record_id")))
     Sql.RunQuery("UPDATE SAQTRV SET DOCTYP_ID = '{DocumentType}',DOCTYP_RECORD_ID = '{DocumentTypeRecordId}' WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' and QTEREV_RECORD_ID = '{RevisionRecordId}' ".format(DocumentType = document_type,DocumentTypeRecordId = document_type_obj.DOCTYP_RECORD_ID,QuoteRecordId = Quote.GetGlobal("contract_quote_record_id"),RevisionRecordId = Quote.GetGlobal("quote_revision_record_id")))
 
