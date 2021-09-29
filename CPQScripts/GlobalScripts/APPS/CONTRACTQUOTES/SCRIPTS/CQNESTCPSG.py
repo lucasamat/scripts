86e4fc3f36f49f45b916a452eb332450944b1729
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2021-09-29 17:55:28 +0530

    Update CQNESTCPSG.py
    changed hyperlink

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTCPSG.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTCPSG.py
@@ -198,7 +198,7 @@ def getsparepartslist(PerPage, PageInform, A_Keys, A_Values):
             table_header += (
                 '<th data-field="'
                 + str(invs)
-                + '" data-filter-control="input" data-title-tooltip="'+str(qstring)+'" data-formatter="EquipHyperLinkTreeLink" data-sortable="true"'+ str(qstring)+'"><abbr title="'
+                + '" data-filter-control="input" data-title-tooltip="'+str(qstring)+'" data-formatter="SparePartsLineItemHyperLink" data-sortable="true"'+ str(qstring)+'"><abbr title="'
                 + str(qstring)
                 + '">'
                 + str(qstring)
