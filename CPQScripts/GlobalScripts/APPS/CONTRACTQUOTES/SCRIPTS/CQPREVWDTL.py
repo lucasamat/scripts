65b7495a1199cc72fea1dc88345a7b2c83a3118a
Aravind <Aravind_Muthiah@contractor.amat.com>
2021-09-30 13:50:05 +0530

    Update CQPREVWDTL.py
    Adding POES

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
@@ -113,6 +113,16 @@ def constructopportunity(Qt_rec_id, Quote, MODE):
 						+ str(current_user)
 						+ "' 'title':userInput}, incrementalTabIndex, enable: isEnabled' class='form-control' style='height: 28px;border-top: 0 !important;border-bottom: 0 !important;' id='' title='' tabindex='' disabled=''> </div>"
 					)
+				elif sefl_api=="POES":
+					act_status = (eval("col_name." + str(sefl_api)))
+					sec_str += (
+						'<td><input id="'
+						+ str(sefl_api)
+						+ '" type="CHECKBOX" value="'
+						+ str(act_status)
+						+ '" class="custom" '
+						+ 'disable checked><span class="lbl"></span></td>'
+					)			
 				else:
 					sec_str += (
 						"<div class='col-md-3 pad-0'> <input type='text' value = '"
