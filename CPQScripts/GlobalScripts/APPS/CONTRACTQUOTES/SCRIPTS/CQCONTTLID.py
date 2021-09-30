31a9388cb97fdfd1f04f61bfd655ee1eaf4c84b3
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-09-30 17:04:43 +0530

    Update CQCONTTLID.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCONTTLID.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCONTTLID.py
@@ -93,6 +93,7 @@ class tool_idle:
             )
             
         sec_str += '</tr></thead><tbody class ="tool_idle" >'
+        current_obj_value = ""
         Objd_Obj = Sql.GetList(
             "select FIELD_LABEL,API_NAME,LOOKUP_OBJECT,LOOKUP_API_NAME,DATA_TYPE,FORMULA_DATA_TYPE,PICKLIST_VALUES,PERMISSION,REQUIRED from SYOBJD (NOLOCK)where OBJECT_NAME = '"
             + str(ObjectName)
