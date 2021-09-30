899002c13a2ecbb6e61c9449d2dcdb1f10d67cb0
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-09-30 17:45:02 +0530

    Update CQCONTTLID.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCONTTLID.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCONTTLID.py
@@ -44,31 +44,7 @@ class tool_idle:
             "REQUIRED",
             "VALUES",
             "",    
-        ]
-        # Objd_Obj = Sql.GetList(
-        #     "select FIELD_LABEL,API_NAME,LOOKUP_OBJECT,LOOKUP_API_NAME,DATA_TYPE,FORMULA_DATA_TYPE from SYOBJD (NOLOCK)where OBJECT_NAME = '"
-        #     + str(ObjectName)
-        #     + "'"
-        # )
-        # lookup_disply_list = []
-        # if Objd_Obj is not None:
-        #     attr_list = {}
-        #     api_names = [inn.API_NAME for inn in Objd_Obj]
-        #     for attr in Objd_Obj:
-        #         attr_list[str(attr.API_NAME)] = str(attr.FIELD_LABEL)
-        #         if str(attr.LOOKUP_API_NAME) != "" and str(attr.LOOKUP_API_NAME) is not None:
-        #             lookup_disply_list.append(str(attr.API_NAME))
-        #     checkbox_list = [
-        #         inn.API_NAME for inn in Objd_Obj if (inn.DATA_TYPE == "CHECKBOX" or inn.FORMULA_DATA_TYPE == "CHECKBOX")
-        #     ]
-        #     lookup_list = {ins.LOOKUP_API_NAME: ins.API_NAME for ins in Objd_Obj}
-
-            # current_obj_value = ""
-            # onchange = ""
-            # disable = ""            
-            # current_obj_api_name = attr.API_NAME
-
-        #sec_str = '<div id="contract_cust_fields_div">'     
+        ]            
 
         sec_str = '<div id="container" class="g4 pad-10 brdr except_sec">'
         sec_str += (
@@ -279,21 +255,7 @@ class tool_idle:
             values_lists += "var " + str(invsk) + ' = $("' + str(filter_class) + '").val(); '
             values_lists += " ATTRIBUTE_VALUEList.push(" + str(invsk) + "); "
             a_test.append(invsk)
-            dbl_clk_function = (	 
-            '$("'	
-            + str(table_ids)	
-            + '").on("dbl-click-cell.bs.table", onClickCell); $("'	
-            + str(table_ids)	
-            + '").on("all.bs.table", function (e, name, args) { $(".bs-checkbox input").addClass("custom"); $(".bs-checkbox input").after("<span class=\'lbl\'></span>"); }); $("'	
-            + str(table_ids)	
-            + '\ th.bs-checkbox div.th-inner").before(""); $(".bs-checkbox input").addClass("custom"); $(".bs-checkbox input").after("<span class=\'lbl\'></span>"); function onClickCell(event, field, value, row, $element) { var reco_id="";console.log("reco_id2--",reco_id);reco_id=reco_id; reco_id=reco_id.split(","); localStorage.setItem("multiedit_save_date", reco_id);  localStorage.setItem("table_id_RL_edit", "SYOBJR_00011_1E92CAAD_4EE9_4E5C_AA11_80F20D295A63");console.log("value--",field);edit_index = $("'+str(table_ids)+'").find("[data-field="+ field +"]").index()+1;localStorage.setItem("edit_index",edit_index); cpq.server.executeScript("SYBLKETRLG", {"TITLE":field, "VALUE":value, "CLICKEDID":"SYOBJR_00011_1E92CAAD_4EE9_4E5C_AA11_80F20D295A63", "RECORDID":reco_id, "ELEMENT":"RELATEDEDIT"}, function(data) { debugger; data1=data[0]; data2=data[1]; data3 = data[2];if(data1 != "NO"){ if(document.getElementById("RL_EDIT_DIV_ID") ) { localStorage.setItem("saqico_title", field); inp = "#"+data3;localStorage.setItem("value_tag", "'+ str(table_id)+' "+inp);$("'+str(table_ids)+' "+inp).closest("tr").find("td:nth-child("+edit_index+")").attr("contenteditable", true); var buttonlen = $("#seginnerbnr").find("button#saveButton"); if (buttonlen.length == 0){	$("#seginnerbnr").append("<button class=\'btnconfig\' onclick=\'PreventiveMaintainenceTreeTable();\' type=\'button\' value=\'Cancel\' id=\'cancelButton\'>CANCEL</button><button class=\'btnconfig\' type=\'button\' value=\'Save\' onclick=\'multiedit_save_RL()\' id=\'saveButton\'>SAVE</button>");}else{$("#cancelButton").css("display", "block");$("#saveButton").css("display", "block");}$("'+str(table_ids)+' " +inp).closest("tr").find("td:nth-child("+edit_index+")").addClass("light_yellow"); document.getElementById("cont_multiEditModalSection").style.display = "none";  var divHeight = $("#cont_multiEditModalSection").height(); $("#cont_multiEditModalSection .modal-backdrop").css("min-height", divHeight+"px"); $("#cont_multiEditModalSection .modal-dialog").css("width","550px"); $(".modal-dialog").css("margin-top","100px"); } if (data2.length !== 0){ $.each( data2, function( key, values ) { onclick_datepicker(values) }); } } }); }                   $("'	
-            + str(table_ids)	
-            + "\").on('sort.bs.table', function (e, name, order) {  currenttab = $(\"ul#carttabs_head .active\").text().trim(); localStorage.setItem('"	
-            + str(table_id)	
-            + "_SortColumn', name); localStorage.setItem('"	
-            + str(table_id)	
-            + "_SortColumnOrder', order); }); "	
-        )
+            
                             
         return (
             sec_str,
