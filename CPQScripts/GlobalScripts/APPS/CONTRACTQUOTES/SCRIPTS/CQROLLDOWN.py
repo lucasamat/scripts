da8a64ce66fbae2c3674575b2ae415c7cf8a0bdc
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-09-30 15:41:23 +0530

    Update CQROLLDOWN.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQROLLDOWN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQROLLDOWN.py
@@ -296,7 +296,8 @@ def covobjrenewal():
 				DTypeset={"Drop Down":"DropDown","Free Input, no Matching":"FreeInputNoMatching","Check Box":"CheckBox"}
 				
 				insertservice += """<QUOTE_ITEM_ENTITLEMENT>
-				<ENTITLEMENT_NAME>{ent_name}</ENTITLEMENT_NAME>
+				<ENTITLEMENT_ID>{ent_name}</ENTITLEMENT_ID>
+				<ENTITLEMENT_NAME>{ent_desc}</ENTITLEMENT_NAME>
 				<ENTITLEMENT_VALUE_CODE>{ent_val_code}</ENTITLEMENT_VALUE_CODE>
 				<ENTITLEMENT_TYPE>{ent_type}</ENTITLEMENT_TYPE>				
 				<ENTITLEMENT_DISPLAY_VALUE>{ent_disp_val}</ENTITLEMENT_DISPLAY_VALUE>
