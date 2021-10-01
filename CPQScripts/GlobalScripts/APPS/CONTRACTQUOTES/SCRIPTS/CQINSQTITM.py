1db6204f26e06dbc18a8f79f62ac05339646f009
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-10-01 12:17:22 +0530

    Create CQINSQTITM.py

---
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQINSQTITM.py
@@ -0,0 +1,13 @@
+# =========================================================================================================================================
+#   __script_name : CQINSQTITM.PY
+#   __script_description : THIS SCRIPT IS USED TO INSERT QUOTE ITEMS AND ITS RELATED TABLES
+#   __primary_author__ : AYYAPPAN SUBRAMANIYAN
+#   __create_date :30-09-2021
+#   © BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
+# ==========================================================================================================================================
+
+import Webcom.Configurator.Scripting.Test.TestProduct
+import System.Net
+from SYDATABASE import SQL
+
+Sql = SQL()
