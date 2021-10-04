2987b5eef3118bed9f8b75a41312196b566e5aff
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-10-04 10:08:34 +0530

    Update CQCRUDOPTN.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -1062,11 +1062,11 @@ class ContractQuoteOfferingsModel(ContractQuoteCrudOpertion):
 			except:
 				pass
 			##calling pre-logic valuedriver script
-			try:
-				Trace.Write("PREDEFINED WAFER DRIVER IFLOW")
-				CQTVLDRIFW.valuedriver_predefined(self.contract_quote_record_id,"SERVICE_LEVEL",OfferingRow_detail.get("SERVICE_ID"),self.user_id,self.quote_revision_record_id)
-			except:
-				Trace.Write("EXCEPT----PREDEFINED DRIVER IFLOW")
+			# try:
+			# 	Trace.Write("PREDEFINED WAFER DRIVER IFLOW")
+			# 	CQTVLDRIFW.valuedriver_predefined(self.contract_quote_record_id,"SERVICE_LEVEL",OfferingRow_detail.get("SERVICE_ID"),self.user_id,self.quote_revision_record_id)
+			# except:
+			# 	Trace.Write("EXCEPT----PREDEFINED DRIVER IFLOW")
 
 class PartsListModel(ContractQuoteCrudOpertion):
 	Trace.Write('-----Parts List CRUD-----')
