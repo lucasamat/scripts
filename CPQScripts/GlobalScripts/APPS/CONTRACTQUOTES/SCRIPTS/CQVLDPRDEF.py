c9fbfd1f7f97552a5e4f3898feae895f9618bad7
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-10-04 13:09:40 +0530

    Update CQVLDPRDEF.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQVLDPRDEF.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQVLDPRDEF.py
@@ -50,12 +50,10 @@ def devicetype_predefinedlogic(entitlement_string, equipment_record_id):
 	return entitlement_string
 
 def equipment_predefined():
-	getallequip_recid = Sql.GetList(""" SELECT EQUIPMENT_RECORD_ID FROM SAQSCE WHERE QUOTE_RECORD_ID='{}' AND QTEREV_RECORD_ID='{}' """.format(str(quote_record_id), str(quote_revision_record_id)))
-
-	for equip_id in getallequip_recid:
-		getxml_equip_recid = Sql.GetList(""" SELECT ENTITLEMENT_XML FROM SAQSCE WHERE EQUIPMENT_RECORD_ID='{}' QUOTE_RECORD_ID='{}' AND QTEREV_RECORD_ID='{}' """ .format(str(equip_id.EQUIPMENT_RECORD_ID), str(quote_record_id), str(quote_revision_record_id)))
+	getall_recid = Sql.GetList(""" SELECT EQUIPMENT_RECORD_ID,ENTITLEMENT_XML FROM SAQSCE {}""".format(str(where_condition) ))
+	for rec in getall_recid:
+		input_xml = rec.ENTITLEMENT_XML 
 		
-		input_xml = getxml_equip_recid.ENTITLEMENT_XML
 
 		#ref_dict = {'AGS_Z0091_VAL_WAFNOD': wafernode_predefinedlogic,'AGS_Z0091_VAL_DEVTYP': devicetype_predefinedlogic}
 
@@ -69,37 +67,67 @@ def equipment_predefined():
 			x=re.findall(pattern_name,sub_string)
 			entxmldict[x[0]]=sub_string
 
-		for key in ref_dict:
-			if entxmldict[key]:
-				entxmldict[key] = ref_dict[key](entxmldict[key],equip_id.EQUIPMENT_RECORD_ID)
-				final_xml += entxmldict[key]
-			else:
-				final_xml += entxmldict[key]
 
 		Sql.RunQuery( "UPDATE SAQSCE SET ENTITLEMENT_XML = ''{}'' WHERE QUOTE_RECORD_ID = '{}' AND EQUIPMENT_RECORD_ID = '{}' AND QTEREV_RECORD_ID='{}'".format(final_xml, quote_record_id, equip_id.EQUIPMENT_RECORD_ID, quote_revision_record_id) )
 
-##service level
-def service_level_predefined(updateentXML): 
+def greenbook_predefined():
+	getxml_query = Sql.GetList(""" SELECT GREENBOOK,ENTITLEMENT_XML,GREENBOOK_RECORD_ID,FABLOCATION_RECORD_ID FROM SAQSGE {}""".format(str(where_condition) ))
 	
-	#updateentXML = getxml_query.ENTITLEMENT_XML
-	get_valuedriver_ids = Sql.GetList("SELECT PRENTL.ENTITLEMENT_ID,PRENTL.ENTITLEMENT_DESCRIPTION from PRENTL (NOLOCK) INNER JOIN PRENLI (NOLOCK) ON PRENTL.ENTITLEMENT_ID = PRENLI.ENTITLEMENT_ID WHERE SERVICE_ID = 'Z0091' AND VISIBLE_IN_CONFIG = 1 AND ENTITLEMENT_TYPE ='VALUE DRIVER' AND PRENTL.ENTITLEMENT_ID not in ('AGS_Z0091_VAL_CSTSEG','AGS_Z0091_VAL_SVCCMP','AGS_Z0091_VAL_QLYREQ') AND PRENLI.ENTITLEMENTLEVEL_NAME = 'OFFERING LEVEL' ")
+	get_valuedriver_ids = Sql.GetList("SELECT PRENTL.ENTITLEMENT_ID,PRENTL.ENTITLEMENT_DESCRIPTION from PRENTL (NOLOCK) INNER JOIN PRENLI (NOLOCK) ON PRENTL.ENTITLEMENT_ID = PRENLI.ENTITLEMENT_ID WHERE SERVICE_ID = 'Z0091' AND VISIBLE_IN_CONFIG = 1 AND ENTITLEMENT_TYPE ='VALUE DRIVER' AND PRENLI.ENTITLEMENTLEVEL_NAME = 'OFFERING FAB GREENBOOK LEVEL' AND PRENTL.ENTITLEMENT_ID NOT IN (SELECT ENTITLEMENT_ID from PRENLI (NOLOCK) WHERE ENTITLEMENTLEVEL_NAME IN ('OFFERING FAB LEVEL','OFFERING LEVEL')) ".format(TreeParam) )
+	for rec in getxml_query:
+		entxmldict = {}
+		pattern_tag = re.compile(r'(<QUOTE_ITEM_ENTITLEMENT>[\w\W]*?</QUOTE_ITEM_ENTITLEMENT>)')
+		pattern_name = re.compile(r'<ENTITLEMENT_ID>([^>]*?)</ENTITLEMENT_ID>')
+		updateentXML = rec.ENTITLEMENT_XML
+		for m in re.finditer(pattern_tag, updateentXML):
+			sub_string = m.group(1)
+			x=re.findall(pattern_name,sub_string)
+			entxmldict[x[0]]=sub_string
+		for val in get_valuedriver_ids:
+			if 'GREENBOOK' in val.ENTITLEMENT_DESCRIPTION.upper():
+				ent_value = rec.GREENBOOK
+				updateentXML = updating_xml(entxmldict,updateentXML,val.ENTITLEMENT_ID,ent_value)
+		
+		Sql.RunQuery( "UPDATE SAQSGE SET ENTITLEMENT_XML = '{}' {} AND FABLOCATION_RECORD_ID = '{}' AND GREENBOOK_RECORD_ID ='{}'".format(updateentXML.replace("'","''") ,where_condition,rec.FABLOCATION_RECORD_ID, rec.GREENBOOK_RECORD_ID   ) )
+			
+		
+
+
+def fab_predefined():
+	pass
+##service level
+def service_level_predefined(): 
+	getxml_query = Sql.GetFirst(""" SELECT ENTITLEMENT_XML FROM SAQTSE {} """.format(str(where_condition)))
+	entxmldict = {}
+	pattern_tag = re.compile(r'(<QUOTE_ITEM_ENTITLEMENT>[\w\W]*?</QUOTE_ITEM_ENTITLEMENT>)')
+	pattern_name = re.compile(r'<ENTITLEMENT_ID>([^>]*?)</ENTITLEMENT_ID>')
+	updateentXML = getxml_query.ENTITLEMENT_XML
+	for m in re.finditer(pattern_tag, updateentXML):
+		sub_string = m.group(1)
+		x=re.findall(pattern_name,sub_string)
+		entxmldict[x[0]]=sub_string
+
+	get_valuedriver_ids = Sql.GetList("SELECT PRENTL.ENTITLEMENT_ID,PRENTL.ENTITLEMENT_DESCRIPTION from PRENTL (NOLOCK) INNER JOIN PRENLI (NOLOCK) ON PRENTL.ENTITLEMENT_ID = PRENLI.ENTITLEMENT_ID WHERE SERVICE_ID = 'Z0091' AND VISIBLE_IN_CONFIG = 1 AND ENTITLEMENT_TYPE ='VALUE DRIVER' AND PRENLI.ENTITLEMENTLEVEL_NAME = 'OFFERING LEVEL' ")
 
 	for val in get_valuedriver_ids:
 		if 'PRODUCT OFFERING' in val.ENTITLEMENT_DESCRIPTION.upper() or 'INTERCEPT' in val.ENTITLEMENT_DESCRIPTION.upper():
-			updateentXML = updating_xml(updateentXML,val.ENTITLEMENT_ID )
-	Product.SetGlobal("updateentXML",updateentXML)
+			ent_value = ""
+			updateentXML = updating_xml(entxmldict,updateentXML,val.ENTITLEMENT_ID,ent_value )
+	#Product.SetGlobal("updateentXML",updateentXML)
 	Sql.RunQuery( "UPDATE SAQTSE SET ENTITLEMENT_XML = '{}' WHERE QUOTE_RECORD_ID = '{}' AND SERVICE_ID = '{}' AND QTEREV_RECORD_ID='{}'".format(updateentXML.replace("'","''") , quote_record_id,TreeParam, quote_revision_record_id) )
-
-
-
-		
 	
-def updating_xml(input_xml, ent_id):
+	
+def updating_xml(entxmldict, input_xml, ent_id, ent_value):
 	where =""
-	Trace.Write("entxmldict--"+str(entxmldict))	
-	if LEVEL != 'SERVICE_LEVEL':
-		#call other level
-		pass
+	#Trace.Write("entxmldict--"+str(entxmldict))	
+	
+	if ent_value:
+		get_value_code = Sql.GetFirst("SELECT ENTITLEMENT_VALUE_CODE FROM PRENVL WHERE ENTITLEMENT_ID ='{}' AND SERVICE_ID = '{}' AND ENTITLEMENT_DISPLAY_VALUE = '{}'".format(ent_id, TreeParam, ent_value) )
+		entitlement_string = entxmldict[ent_id]
+		entitlement_string = re.sub('<ENTITLEMENT_DISPLAY_VALUE>[^>]*?</ENTITLEMENT_DISPLAY_VALUE>','<ENTITLEMENT_DISPLAY_VALUE>'+str(ent_value)+'</ENTITLEMENT_DISPLAY_VALUE>',entitlement_string)
+
+		entitlement_string = re.sub('<ENTITLEMENT_VALUE_CODE>[^>]*?</ENTITLEMENT_VALUE_CODE>','<ENTITLEMENT_VALUE_CODE>'+str(get_value_code.ENTITLEMENT_VALUE_CODE)+'</ENTITLEMENT_VALUE_CODE>',entitlement_string)
+		where = " AND PRENVL.ENTITLEMENT_DISPLAY_VALUE = '{}'".format(ent_value)
 
 	get_coefficient_val = Sql.GetFirst("SELECT ENTITLEMENT_COEFFICIENT, PRENTL.ENTITLEMENT_ID FROM PRENVL (NOLOCK) INNER JOIN PRENTL (NOLOCK) ON PAR_ENPAR_ENTITLEMETITLEMENT_ID = PRENVL.ENTITLEMENT_ID AND PRENVL.SERVICE_ID = PRENTL.SERVICE_ID WHERE PRENVL.ENTITLEMENT_ID = '{}' AND PRENVL.SERVICE_ID = '{}' {}".format(ent_id, TreeParam, where))
 	if get_coefficient_val.ENTITLEMENT_ID in entxmldict.keys():
@@ -113,30 +141,16 @@ def updating_xml(input_xml, ent_id):
 
 
 
-
-	# assign_xml = """<ENTITLEMENT_ID>{ent_name}</ENTITLEMENT_ID>
-	# 					<ENTITLEMENT_DESCRIPTION>{tool_desc}</ENTITLEMENT_DESCRIPTION>
-	# 					<ENTITLEMENT_VALUE_CODE>{ent_val_code}</ENTITLEMENT_VALUE_CODE>
-	# 					<ENTITLEMENT_DISPLAY_VALUE>{ent_disp_val}</ENTITLEMENT_DISPLAY_VALUE>"""
-	# get_xml = re.sub(r'<ENTITLEMENT_ID>'+str(xml_val)+'<[\w\W]*?</CALCULATION_FACTOR>', assign_xml, updateentXML )
-
-			
-			
-
-	
-
-##fn call
-
 if LEVEL == 'SERVICE_LEVEL':
-	getxml_query = Sql.GetFirst(""" SELECT ENTITLEMENT_XML FROM SAQTSE {} """.format(str(where_condition)))
-	entxmldict = {}
-	pattern_tag = re.compile(r'(<QUOTE_ITEM_ENTITLEMENT>[\w\W]*?</QUOTE_ITEM_ENTITLEMENT>)')
-	pattern_name = re.compile(r'<ENTITLEMENT_ID>([^>]*?)</ENTITLEMENT_ID>')
-	
-	for m in re.finditer(pattern_tag, getxml_query.ENTITLEMENT_XML):
-		sub_string = m.group(1)
-		x=re.findall(pattern_name,sub_string)
-		entxmldict[x[0]]=sub_string
-	
-	service_level_predefined(getxml_query.ENTITLEMENT_XML)
+	service_level_predefined()
+else:
+	obj_list = ["SAQSFE,SAQSGE,SAQSCE"]
+	for obj in obj_list:
+		if obj == "SAQSFE":
+			fab_predefined()
+		elif obj == "SAQSGE":
+			greenbook_predefined()
+		elif obj == "SAQSCE":
+			equipment_predefined()
+
 #predefined_logic()
