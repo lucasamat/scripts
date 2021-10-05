a12ec546647e8198b891114e06ef1b0014637f37
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-10-05 18:59:30 +0530

    Update CQSYNCQTBL.py
    9608

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
@@ -1760,6 +1760,7 @@ class SyncQuoteAndCustomTables:
 	##A055S000P01-8690 starts..
 sync_obj = SyncQuoteAndCustomTables(Quote)
 sync_obj.create_custom_table_record()
+#A055S000P01-9608 START
 quote_record_id = Quote.GetGlobal("contract_quote_record_id")
 quote_revision_id = Quote.GetGlobal("quote_revision_record_id")
 getService = Sql.GetFirst("SELECT CpqTableEntryId FROM SAQTSV WHERE WHERE QUOTE_RECORD_ID = '{}' AND QUOTE_REVISION_RECORD_ID = '{}'".format(quote_record_id,quote_revision_id))
@@ -1768,5 +1769,6 @@ if getService is None:
 
 	if getRevision is None:
 		ScriptExecutor.ExecuteGlobal('CQDOCUTYPE',{'QUOTE_RECORD_ID':quote_record_id,'QTEREV_RECORD_ID':quote_revision_id,'SERVICE_ID':""})
+#A055S000P01-9608 END
 
 
