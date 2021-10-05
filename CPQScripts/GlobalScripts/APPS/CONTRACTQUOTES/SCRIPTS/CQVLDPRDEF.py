eec0930bc0b7f3f8ecd86201c352b11693c19916
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-10-05 13:30:26 +0530

    Update CQVLDPRDEF.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQVLDPRDEF.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQVLDPRDEF.py
@@ -15,7 +15,10 @@ from System.Net import CookieContainer, NetworkCredential, Mail
 from System.Net.Mail import SmtpClient, MailAddress, Attachment, MailMessage
 from SYDATABASE import SQL
 Sql = SQL()
-
+try:
+	AttributeList= Param.AttributeList
+except:
+	AttributeList = ''
 try:
 	quote_record_id = Param.quote_rec_id
 except:
@@ -37,7 +40,6 @@ except:
 	where_condition = ""
 	serviceId =''
 
-Log.Info('predefined script started')	
 
 def equipment_predefined():
 	get_valuedriver_ids = Sql.GetList("SELECT PRENTL.ENTITLEMENT_ID,PRENTL.ENTITLEMENT_DESCRIPTION from PRENTL (NOLOCK) INNER JOIN PRENLI (NOLOCK) ON PRENTL.ENTITLEMENT_ID = PRENLI.ENTITLEMENT_ID WHERE SERVICE_ID = '{}' AND VISIBLE_IN_CONFIG = 1 AND ENTITLEMENT_TYPE ='VALUE DRIVER' AND PRENLI.ENTITLEMENTLEVEL_NAME = 'OFFERING FAB GREENBOOK TOOL LEVEL' AND PRENTL.ENTITLEMENT_ID NOT IN (SELECT ENTITLEMENT_ID from PRENLI (NOLOCK) WHERE ENTITLEMENTLEVEL_NAME IN ('OFFERING FAB LEVEL','OFFERING LEVEL','OFFERING FAB GREENBOOK LEVEL')) ".format(TreeParam) )
@@ -187,7 +189,7 @@ def updating_xml(entxmldict, input_xml, ent_id, ent_value):
 
 def valuedriver_onchage():
 	entxmldict = {}
-	getxml_query = Sql.GetList(""" SELECT ENTITLEMENT_XML FROM SAQTSE {}""".format(str(responsive_where)))
+	getxml_query = Sql.GetList(""" SELECT ENTITLEMENT_XML FROM '{objname}' '{where}'""".format(objname=TreeParam,where=str(where_condition)))
 	get_coefficient_val = Sql.GetFirst("SELECT ENTITLEMENT_COEFFICIENT, PRENTL.ENTITLEMENT_ID FROM PRENVL (NOLOCK) INNER JOIN PRENTL (NOLOCK) ON PAR_ENPAR_ENTITLEMETITLEMENT_ID = PRENVL.ENTITLEMENT_ID AND PRENVL.SERVICE_ID = PRENTL.SERVICE_ID WHERE PRENVL.ENTITLEMENT_ID = '{}' AND PRENVL.SERVICE_ID = '{}' ".format(AttributeList, serviceId))
 	
 	for rec in getxml_query:
