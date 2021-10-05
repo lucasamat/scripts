1d98527642ea571e382f1cca148f2fef25139051
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-10-05 18:53:23 +0530

    Update CQVLDPRDEF.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQVLDPRDEF.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQVLDPRDEF.py
@@ -22,7 +22,7 @@ except:
 	quote_record_id = ""
 	
 try:
-	get_selected_value = eval(Param.get_selected_dict)
+	get_selected_value = eval(Param.get_selected_value)
 except:
 	get_selected_value = ""
 try:
