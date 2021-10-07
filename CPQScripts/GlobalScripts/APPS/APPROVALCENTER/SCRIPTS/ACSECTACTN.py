62d3bb2ea5759fd6800ba6b38d9233dc6b242f38
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-10-07 15:49:35 +0530

    Update ACSECTACTN.py

--- CPQScripts/GlobalScripts/APPS/APPROVALCENTER/SCRIPTS/ACSECTACTN.py
+++ CPQScripts/GlobalScripts/APPS/APPROVALCENTER/SCRIPTS/ACSECTACTN.py
@@ -2699,7 +2699,7 @@ class approvalCenter:
 					approval_queue_obj = Sql.GetFirst("select APPROVAL_RECORD_ID from ACAPMA (NOLOCK) INNER JOIN ACAPCH (NOLOCK) ON ACAPMA.APRCHN_ID = ACAPCH.APRCHN_ID where ACAPMA.APRTRXOBJ_RECORD_ID = '{quote_record_id}' AND ACAPCH.APPROVAL_METHOD = 'PARALLEL STEP APPROVAL'".format(quote_record_id = quote_record_id))
 					approval_record_id = approval_queue_obj.APPROVAL_RECORD_ID
 				else:
-					approval_queue_obj = Sql.GetFirst("select APPROVAL_RECORD_ID from ACAPMA where APRTRXOBJ_RECORD_ID = '{quote_record_id}'".format(quote_record_id = quote_record_id))
+					approval_queue_obj = Sql.GetFirst("select APPROVAL_RECORD_ID from ACAPMA where APRTRXOBJ_RECORD_ID = '{quote_record_id}'".format(quote_record_id = self.quote_revision_record_id))
 					approval_record_id = approval_queue_obj.APPROVAL_RECORD_ID  
 		else:
 			Quoteid = Sql.GetFirst("Select APRTRXOBJ_RECORD_ID FROM ACAPMA WHERE APPROVAL_RECORD_ID = '"+str(self.QuoteNumber)+"'")
