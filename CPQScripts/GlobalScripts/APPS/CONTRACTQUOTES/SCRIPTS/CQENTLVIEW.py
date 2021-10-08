d0c468d8f9c7965907289d76ca6b869ca6818ef7
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-10-08 18:23:15 +0530

    Update CQENTLVIEW.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
@@ -1305,7 +1305,7 @@ class EntitlementView():
 									#Trace.Write('imgstr--'+str(imgstr)+'---'+str(attrSysId))
 									new_value_dicta["APPROVAL"] = imgstr
 									new_value_dicta["ENTITLEMENT"] = str("<abbr title='"+str(attrName)+"'>"+str(attrName)+"</abbr>")	
-									new_value_dicta["DESCRIPTION"] = str("<abbr title='"+str(get_tooltip)+"'>"+get_tooltip+"</abbr>")
+									new_value_dicta["DESCRIPTION"] = str("<abbr title='"+str(get_tooltip)+"'>"+str(get_tooltip)+"</abbr>")
 									new_value_dicta["REQUIRED"] = get_requiredicon
 									if DType in( "Drop Down", "Check Box", "Free Input, no Matching"):
 										new_value_dicta["VALUE"] = sec_str1 									
