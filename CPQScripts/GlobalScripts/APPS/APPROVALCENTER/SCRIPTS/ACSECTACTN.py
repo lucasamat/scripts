a50157dab556757f38c38869db504cd1fa39376b
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-10-11 15:22:48 +0530

    Update ACSECTACTN.py

--- CPQScripts/GlobalScripts/APPS/APPROVALCENTER/SCRIPTS/ACSECTACTN.py
+++ CPQScripts/GlobalScripts/APPS/APPROVALCENTER/SCRIPTS/ACSECTACTN.py
@@ -1618,7 +1618,7 @@ class approvalCenter:
 								"""select max(ACAPTX.APRCHNSTP_ID) as MaxStep, max(ACAPTX.APPROVAL_ROUND) as appround,ACAPTX.APRCHN_ID,ACAPTX.REQUESTOR_COMMENTS 
 									from ACAPTX (nolock)
 									inner join ACAPMA (nolock) on ACAPMA.APPROVAL_RECORD_ID = ACAPTX.APPROVAL_RECORD_ID
-									where ACAPMA.APRTRXOBJ_RECORD_ID = '{revision_rec_id}' AND ACAPMA.APRCHN_RECORD_ID = '{chain_rec_id}' GROUP BY ACAPTX.APRCHN_ID""".format(
+									where ACAPMA.APRTRXOBJ_RECORD_ID = '{revision_rec_id}' AND ACAPMA.APRCHN_RECORD_ID = '{chain_rec_id}' GROUP BY ACAPTX.APRCHN_ID,ACAPTX.REQUESTOR_COMMENTS""".format(
 									revision_rec_id=  self.quote_revision_record_id,chain_rec_id = GetMaxStep.APRCHN_RECORD_ID
 								)
 							)
