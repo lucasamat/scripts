dd0629a4cc894d209de67ffddef28d7c94484d7b
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2021-10-13 16:30:06 +0530

    Update CPQTOC4CWB.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CPQTOC4CWB.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CPQTOC4CWB.py
@@ -18,7 +18,7 @@ Sql = SQL()
 def quote_header_details(contract_quote_record_id,quote_revision_record_id):
     Log.Info("inside the CPQTOC4CWB11111111111")
     revision_obj = Sql.GetFirst("select SALESORG_ID,DOCTYP_ID,DISTRIBUTIONCHANNEL_ID,DIVISION_ID,QTEREV_ID,REVISION_DESCRIPTION,REVISION_STATUS,CONTRACT_VALID_FROM,CONTRACT_VALID_TO FROM SAQTRV WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
-    quote_obj = Sql.GetFirst("select NET_VALUE,OWNER_NAME,ACCOUNT_ID FROM SAQTMT WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
+    quote_obj = Sql.GetFirst("select NET_VALUE,OWNER_NAME,ACCOUNT_ID FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
     Log.Info("inside the CPQTOC4CWB22222222222")
     quote_header_data = {
         "ProcessingTypeCode": revision_obj.DOCTYP_ID,
