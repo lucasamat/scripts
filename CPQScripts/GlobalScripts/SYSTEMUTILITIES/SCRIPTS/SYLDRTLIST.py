3fb0f6e130bbd2700a21ccb66e9f3ed363a47dcd
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-10-13 16:28:32 +0530

    Revert "Update SYLDRTLIST.py"
    This reverts commit 85b2811c18c6e10e9179ba5be3894ff01c60d974.

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -2063,7 +2063,7 @@ class SYLDRTLIST:
                         elif str(RECORD_ID) == "SYOBJR-98870":
                             Qustr += " AND SERVICE_ID = '"+str(TreeParam)+"'"
                         Trace.Write('In 1958---'+str(Qustr))
-                        if str(RECORD_ID) not in("SYOBJR-98869","SYOBJR-00643","SYOBJR-00013","SYOBJR-98825"):
+                        if str(RECORD_ID) not in("SYOBJR-98869","SYOBJR-00643","SYOBJR-00013"):
                         	Qustr += "AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id)+"'"
                         Qury_str = (
                             "select DISTINCT top "
