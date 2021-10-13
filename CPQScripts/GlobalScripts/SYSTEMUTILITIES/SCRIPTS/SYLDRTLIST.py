d5bb37dc5ff4818a02123ad20fe787f80f8fad9a
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-10-13 16:50:03 +0530

    Update SYLDRTLIST.py
    added condition

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -2062,9 +2062,11 @@ class SYLDRTLIST:
                             Qustr += "AND EQUIPMENT_ID = '"+str(equipment_id)+"'"
                         elif str(RECORD_ID) == "SYOBJR-98870":
                             Qustr += " AND SERVICE_ID = '"+str(TreeParam)+"'"
+                        elif str(RECORD_ID) == "SYOBJR-98825":
+                            Qustr += ""
                         Trace.Write('In 1958---'+str(Qustr))
                         if str(RECORD_ID) not in("SYOBJR-98869","SYOBJR-00643","SYOBJR-00013"):
-                            Qustr += "AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id)+"'"
+                            Qustr += " AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id)+"'"
                         Qury_str = (
                             "select DISTINCT top "
                             + str(PerPage)
