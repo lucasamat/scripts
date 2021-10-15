e2237251c294c7d9c49dcd380746158e00bc1c7f
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-10-15 16:20:10 +0530

    Update SYULODTRND.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
@@ -134,7 +134,7 @@ def CommonTreeViewHTMLDetail(
 			if objh_obj is not None:
 				ObjectName = str(objh_obj.OBJECT_NAME)
 				
-	if str(ObjectName) in ["ACAPCH","SYPRAP", "SAQIBP","SAQTBP","SASORG","PREXRT","SYTABS","ACACSS","ACACST","ACACSA","cpq_permissions","SAQITM","SYOBJD","SYPRTB","SYPSAC","SYPRSN","SYAPPS","SYOBJC","SYSECT","USERS","SYSEFL","SYPROH","SAQTMT","PRCURR","SYROMA","SYPGAC","SAQTIP","SYOBJX","SYPRSF","SYROUS","SYOBFD","SYPRAC","SAQSCO","SAQTRV"]:
+	if str(ObjectName) in ["ACAPCH","SYPRAP", "SAQIBP","SAQTBP","SASORG","PREXRT","SYTABS","ACACSS","ACACST","ACACSA","cpq_permissions","SAQITM","SYOBJD","SYPRTB","SYPSAC","SYPRSN","SYAPPS","SYOBJC","SYSECT","USERS","SYSEFL","SYPROH","SAQTMT","PRCURR","SYROMA","SYPGAC","SAQTIP","SYOBJX","SYPRSF","SYROUS","SYOBFD","SYPRAC","SAQSCO","SAQTRV","SAQTSV"]:
 			canedit = "TRUE"
 	if Product.GetGlobal("TreeParentLevel0") == "Billing":
 		ObjectName = "SAQTBP"
