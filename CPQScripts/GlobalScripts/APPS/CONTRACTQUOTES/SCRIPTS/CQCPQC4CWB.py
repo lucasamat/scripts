4e4e0a6ee98dc49763728276829e9285edb4dc6e
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2021-10-15 19:42:28 +0530

    Update CQCPQC4CWB.py
    Changed the record id variable

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCPQC4CWB.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCPQC4CWB.py
@@ -19,7 +19,7 @@ from System import Convert
 from SYDATABASE import SQL
 
 Sql = SQL()
-def writeback_to_c4c(writeback,quote_record_id,quote_revision_record_id):
+def writeback_to_c4c(writeback,contract_quote_record_id,quote_revision_record_id):
     if writeback == "quote_header":
         revision_obj = Sql.GetFirst("select SALESORG_ID,DOCTYP_ID,DISTRIBUTIONCHANNEL_ID,DIVISION_ID,QTEREV_ID,REVISION_DESCRIPTION,REVISION_STATUS,CONTRACT_VALID_FROM,CONTRACT_VALID_TO FROM SAQTRV WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
         quote_obj = Sql.GetFirst("select NET_VALUE,OWNER_NAME,ACCOUNT_ID FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
@@ -29,9 +29,9 @@ def writeback_to_c4c(writeback,quote_record_id,quote_revision_record_id):
         requestdata = (
             '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body><CPQ_Columns><writeback>'
             + str(writeback)
-            + "</writeback><quote_record_id>"
-            +str(quote_record_id)
-            +"</quote_record_id><quote_revision_record_id>"
+            + "</writeback><contract_quote_record_id>"
+            +str(contract_quote_record_id)
+            +"</contract_quote_record_id><quote_revision_record_id>"
             + str(quote_revision_record_id)
             +"</quote_revision_record_id><quote_header_data>"
             + str(quote_header_data)
@@ -48,9 +48,9 @@ def writeback_to_c4c(writeback,quote_record_id,quote_revision_record_id):
         requestdata = (
             '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body><CPQ_Columns><writeback>'
             + str(writeback)
-            + "</writeback><quote_record_id>"
-            +str(quote_record_id)
-            +"</quote_record_id><quote_revision_record_id>"
+            + "</writeback><contract_quote_record_id>"
+            +str(contract_quote_record_id)
+            +"</contract_quote_record_id><quote_revision_record_id>"
             + str(quote_revision_record_id)
             +"</quote_revision_record_id><opportunity_header_data>"
             + str(opportunity_header_data)
