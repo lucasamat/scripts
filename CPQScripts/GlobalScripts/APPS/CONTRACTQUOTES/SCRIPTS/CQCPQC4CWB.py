cdb70f3fb7bfad2b96d294160ad811e2c15536f6
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2021-10-18 19:13:48 +0530

    Update CQCPQC4CWB.py
    US 10833

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCPQC4CWB.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCPQC4CWB.py
@@ -22,7 +22,12 @@ Sql = SQL()
 def writeback_to_c4c(writeback,contract_quote_record_id,quote_revision_record_id):
     if writeback == "quote_header":
         revision_obj = Sql.GetFirst("select SALESORG_ID,DOCTYP_ID,DISTRIBUTIONCHANNEL_ID,DIVISION_ID,QTEREV_ID,REVISION_DESCRIPTION,REVISION_STATUS,CONTRACT_VALID_FROM,CONTRACT_VALID_TO FROM SAQTRV WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
+        
         quote_obj = Sql.GetFirst("select NET_VALUE,OWNER_NAME,ACCOUNT_ID FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
+        
+        opportunity_obj = Sql.GetFirst("select C4C_QTEOBJ_ID SAOPQT WHERE MASTER_TABLE_QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
+        c4c_quote_object_id = opportunity_obj.C4C_QTEOBJ_ID
+        
         ##quote header write back details starts...
         quote_header_data = '{\"ProcessingTypeCode\":'+str(revision_obj.DOCTYP_ID)+', \"BuyerPartyID\":'+str(quote_obj.ACCOUNT_ID)+', \"EmployeeResponsiblePartyID\":'+str(quote_obj.OWNER_NAME)+', \"SalesUnitPartyID\":'+str(revision_obj.SALESORG_ID)+', \"DistributionChannelCode\":'+str(revision_obj.DISTRIBUTIONCHANNEL_ID)+', \"DivisionCode\":'+str(revision_obj.DIVISION_ID)+', \"ZWB_ContractValidFrom_KUT\":'+str(revision_obj.CONTRACT_VALID_FROM)+', \"ZWB_ContractValidTo_KUT\":'+str(revision_obj.CONTRACT_VALID_TO)+', \"ZWB_QuoteRevisionID_KUT\":'+str(revision_obj.QTEREV_ID)+', \"ZWB_RevisionDescription_KUT\":'+str(revision_obj.REVISION_DESCRIPTION)+', \"ZWB_RevisionStatus_KUT\":'+str(revision_obj.REVISION_STATUS)+', \"ZWB_TotalQuoteContent_KUT\":'+str(quote_obj.NET_VALUE)+', \"ZWB_TotalQuotecurrencyCode_KUT\":"USD"}'
         ##quote header write back details ends...
@@ -35,7 +40,9 @@ def writeback_to_c4c(writeback,contract_quote_record_id,quote_revision_record_id
             + str(quote_revision_record_id)
             +"</quote_revision_record_id><quote_header_data>"
             + str(quote_header_data)
-            +"</quote_header_data></CPQ_Columns></soapenv:Body></soapenv:Envelope>"
+            +"</quote_header_data><c4c_quote_object_id>"
+            + str(c4c_quote_object_id)
+            +"</c4c_quote_object_id></CPQ_Columns></soapenv:Body></soapenv:Envelope>"
         )
     elif writeback == "opportunity_header":
         ##To Fetch the values from revision table....
