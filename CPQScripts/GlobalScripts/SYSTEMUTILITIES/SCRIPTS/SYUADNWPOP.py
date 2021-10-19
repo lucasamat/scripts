e0d0b25e4d7f91e14e07e64cc7249fab9012125b
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2021-10-19 14:09:07 +0530

    Update SYUADNWPOP.py
    Commit

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
@@ -1765,7 +1765,7 @@ def POPUPLISTVALUEADDNEW(
 				where_string += " AND"
 				Trace.Write("soureceequipments "+str(where_string))
 			if TreeParam == "Customer Information":
-				where_string += """ MAEQUP.GREENBOOK_RECORD_ID != '' AND MAEQUP.GREENBOOK_RECORD_ID is not null AND MAEQUP.EQUIPMENT_ID  NOT IN (SELECT EQUIPMENT_ID FROM SAQSTE (NOLOCK) where QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}')""".format(contract_quote_record_id,quote_revision_record_id)
+				where_string += """ SAACNT.ACCOUNT_ID  NOT IN (SELECT PARTY_ID FROM SAQTIP (NOLOCK) where QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' and PARTY_ROLE ='{}')""".format(contract_quote_record_id,quote_revision_record_id,ROLE)
 			
 			# table_data = Sql.GetList(
 			# 	"select {} from MAEQUP (NOLOCK) inner join SAQSCF (NOLOCK) on MAEQUP.FABLOCATION_RECORD_ID = SAQSCF.SRCFBL_RECORD_ID and MAEQUP.ACCOUNT_RECORD_ID = SAQSCF.SRCACC_RECORD_ID and MAEQUP.FABLOCATION_ID = SAQSCF.SRCFBL_ID inner join  MAFBLC (nolock) on MAFBLC.FAB_LOCATION_ID = SAQSCF.SRCFBL_ID AND MAFBLC.ACCOUNT_ID = SAQSCF.SRCACC_ID AND MAEQUP.PAR_EQUIPMENT_ID = '' AND SAQSCF.QUOTE_RECORD_ID = '{}' {} {} {}".format(", ".join(ordered_keys),contract_quote_record_id,"WHERE " +where_string if where_string else "",order_by,
@@ -1774,8 +1774,8 @@ def POPUPLISTVALUEADDNEW(
 			# )
 			table_data = Sql.GetList("Select {} FROM SAACNT".format(", ".join(ordered_keys)))
 			QueryCountObj = Sql.GetFirst(
-				"select count(*) as cnt from SAACNT (NOLOCK)".format(
-				contract_quote_record_id, "WHERE " +where_string if where_string else ""
+				"select count(*) as cnt from SAACNT (NOLOCK) {}".format(
+				"WHERE " +where_string if where_string else ""
 				)
 			)
 			if QueryCountObj is not None:
