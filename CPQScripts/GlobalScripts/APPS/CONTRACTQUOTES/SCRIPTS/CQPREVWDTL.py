33a732defded22e9684e69e421262ea3d81a2070
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-10-19 19:29:15 +0530

    Update CQPREVWDTL.py
    UI issue in CBC

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
@@ -452,8 +452,8 @@ def constructCBC(Qt_rec_id, Quote, MODE):
 		
 	sec_str += '</tr></thead><tbody class ="cleanbook_chklst" >'
 	checklist_vals = Sql.GetList("select TOP 1000 CHECKLIST_ID,CHECKLIST_DESCRIPTION,SERVICE_CONTRACT,SPECIALIST_REVIEW,COMMENT FROM SAQCBC(NOLOCK) WHERE QUOTE_RECORD_ID = '{quote_recid}' AND QTEREV_RECORD_ID = '{quote_revision_recid}' ORDER BY CpqTableEntryId ASC".format(quote_recid=Quote,quote_revision_recid=quote_revision_record_id))
-	sec_str +='<tr>'
 	for value in checklist_vals:
+		sec_str +='<tr>'
 		sec_str += ('<td><input id="CHECKLIST_ID" type="text" value="'+str(value.CHECKLIST_ID)+'" title="'+str(value.CHECKLIST_ID)+'" class="form-control related_popup_css fltlt" disabled></td>')
 		sec_str += ('<td><input id="CHECKLIST_DESCRIPTION" type="text" value="'+str(value.CHECKLIST_DESCRIPTION)+'" title="'+str(value.CHECKLIST_DESCRIPTION)+'" class="form-control related_popup_css fltlt" disabled></td>')
 		if str(value.SERVICE_CONTRACT).upper() == "TRUE" or str(value.SERVICE_CONTRACT) =="1":
@@ -465,11 +465,10 @@ def constructCBC(Qt_rec_id, Quote, MODE):
 		else:
 			sec_str += ('<td><input id="SPECIALIST_REVIEW" type="checkbox" value="'+str(value.SPECIALIST_REVIEW)+'" title="'+str(value.SPECIALIST_REVIEW)+'" class="custom" disabled><span class="lbl"></span></td>')
 		sec_str += ('<td><input id="COMMENT" type="text" value="'+str(value.COMMENT)+'" title="'+str(value.COMMENT)+'" class="form-control related_popup_css fltlt" disabled></td>')
-	
-	sec_str += (
-			'<td class="float_r_bor_bot"><div class="col-md-12 editiconright"><a href="#" onclick="editclick_row(this)" class="editclick"><i class="fa fa-pencil" aria-hidden="true"></i></a></div></td>'
-		)           
-	sec_str += '</tr>'
+		sec_str += (
+				'<td class="float_r_bor_bot"><div class="col-md-12 editiconright"><a href="#" onclick="editclick_row(this)" class="editclick"><i class="fa fa-pencil" aria-hidden="true"></i></a></div></td>'
+			)           
+		sec_str += '</tr>'
 
 	sec_str += '</tbody></table>'
 	values_lists = ""
