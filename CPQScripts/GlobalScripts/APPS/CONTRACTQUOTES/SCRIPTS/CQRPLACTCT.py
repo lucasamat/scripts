d7df5f96925194ad67cf1b73d56235f3ac894492
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2021-10-19 16:22:55 +0530

    Create CQRPLACTCT.py
    New script

---
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQRPLACTCT.py
@@ -0,0 +1,21 @@
+# =========================================================================================================================================
+#   __script_name : CQRPLACTCT.PY
+#   __script_description : THIS SCRIPT IS USED TO REPLACE ACCOUNT AND CONTACT WHEN USER CLICKS ON REPLACE BUTTON ON A RELATED LIST RECORD.
+#   __primary_author__ : WASIM ABDUL
+#   __create_date : 19/10/2021
+#   © BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
+# ==========================================================================================================================================
+import datetime
+import Webcom.Configurator.Scripting.Test.TestProduct
+import sys
+import re
+import System.Net
+import SYCNGEGUID as CPQID
+from SYDATABASE import SQL
+#from datetime import datetime
+#from datetime import datetime
+#import time
+Sql = SQL()
+ScriptExecutor = ScriptExecutor
+
+
