b2a7abcfe8b881b7410d668f36b4a3d93d7f7d84
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2021-10-19 14:00:01 +0530

    Update CQCPQC4CWB.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCPQC4CWB.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCPQC4CWB.py
@@ -25,11 +25,11 @@ def writeback_to_c4c(writeback,contract_quote_record_id,quote_revision_record_id
         
         quote_obj = Sql.GetFirst("select NET_VALUE,OWNER_NAME,ACCOUNT_ID FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
         
-        opportunity_obj = Sql.GetFirst("select C4C_QTEOBJ_ID FROM SAOPQT WHERE QUOTE_RECORD_ID = '{}'".format(contract_quote_record_id))
+        opportunity_obj = Sql.GetFirst("select ISNULL(C4C_QTEOBJ_ID,0) AS C4C_QTEOBJ_ID FROM SAOPQT WHERE QUOTE_RECORD_ID = '{}'".format(contract_quote_record_id))
         c4c_quote_object_id = opportunity_obj.C4C_QTEOBJ_ID
         
         ##quote header write back details starts...
-        quote_header_data = '{\"ProcessingTypeCode\":'+str(revision_obj.DOCTYP_ID)+', \"BuyerPartyID\":'+str(quote_obj.ACCOUNT_ID)+', \"EmployeeResponsiblePartyID\":'+str(quote_obj.OWNER_NAME)+', \"SalesUnitPartyID\":'+str(revision_obj.SALESORG_ID)+', \"DistributionChannelCode\":'+str(revision_obj.DISTRIBUTIONCHANNEL_ID)+', \"DivisionCode\":'+str(revision_obj.DIVISION_ID)+', \"ZWB_ContractValidFrom_KUT\":'+str(revision_obj.CONTRACT_VALID_FROM)+', \"ZWB_ContractValidTo_KUT\":'+str(revision_obj.CONTRACT_VALID_TO)+', \"ZWB_QuoteRevisionID_KUT\":'+str(revision_obj.QTEREV_ID)+', \"ZWB_RevisionDescription_KUT\":'+str(revision_obj.REVISION_DESCRIPTION)+', \"ZWB_RevisionStatus_KUT\":'+str(revision_obj.REVISION_STATUS)+', \"ZWB_TotalQuoteContent_KUT\":'+str(quote_obj.NET_VALUE)+', \"ZWB_TotalQuotecurrencyCode_KUT\":USD}'
+        quote_header_data = '{\"ProcessingTypeCode\":'+str(revision_obj.DOCTYP_ID)+', \"BuyerPartyID\":'+str(quote_obj.ACCOUNT_ID)+', \"EmployeeResponsiblePartyID\":'+str(quote_obj.OWNER_NAME)+', \"SalesUnitPartyID\":'+str(revision_obj.SALESORG_ID)+', \"DistributionChannelCode\":'+str(revision_obj.DISTRIBUTIONCHANNEL_ID)+', \"DivisionCode\":'+str(revision_obj.DIVISION_ID)+', \"ZWB_ContractValidFrom_KUT\":'+str(revision_obj.CONTRACT_VALID_FROM)+', \"ZWB_ContractValidTo_KUT\":'+str(revision_obj.CONTRACT_VALID_TO)+', \"ZWB_QuoteRevisionID_KUT\":'+str(revision_obj.QTEREV_ID)+', \"ZWB_RevisionDescription_KUT\":'+str(revision_obj.REVISION_DESCRIPTION)+', \"ZQuoteRevisionStatus\":'+str(revision_obj.REVISION_STATUS)+', \"ZWB_TotalQuoteContent_KUT\":'+str(quote_obj.NET_VALUE)+', \"ZWB_TotalQuotecurrencyCode_KUT\":USD}'
         ##quote header write back details ends...
         requestdata = (
             '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body><CPQ_Columns><writeback>'
@@ -46,10 +46,11 @@ def writeback_to_c4c(writeback,contract_quote_record_id,quote_revision_record_id
         )
     elif writeback == "opportunity_header":
         ##To Fetch the values from revision table....
-        revision_obj = Sql.GetFirst("select DOC_CURRENCY,CONTRACT_VALID_FROM,CONTRACT_VALID_TO,ISNULL(NET_VALUE,0) AS NET_VALUE FROM SAQTRV WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
-        
+        revision_obj = Sql.GetFirst("select REVISION_STATUS,DOC_CURRENCY,CONTRACT_VALID_FROM,CONTRACT_VALID_TO,ISNULL(NET_VALUE,0) AS NET_VALUE FROM SAQTRV WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
+        opportunity_object = SqlHelper.GetFirst("select ISNULL(SAOPPR.C4C_OPPOBJ_ID,0) AS C4C_OPPOBJ_ID FROM SAOPPR(NOLOCK) INNER JOIN SAOPQT ON  SAOPPR.OPPORTUNITY_ID = SAOPQT.OPPORTUNITY_ID AND SAOPPR.ACCOUNT_ID = SAOPQT.ACCOUNT_ID WHERE QUOTE_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id")))
+        opportunity_object_id = opportunity_object.C4C_OPPOBJ_ID
         ##opportunity header write back details starts...
-        opportunity_header_data = '{\"ExpectedRevenueAmount\":'+str(revision_obj.NET_VALUE)+', \"ExpectedRevenueAmountCurrencyCode\":USD, \"ExpectedProcessingStartDate\":"", \"ExpectedRevenueStartDate\":'+str(revision_obj.CONTRACT_VALID_FROM)+', \"ExpectedRevenueEndDate\":'+str(revision_obj.CONTRACT_VALID_TO)+'}'
+        opportunity_header_data = '{\"ExpectedRevenueAmount\":'+str(revision_obj.NET_VALUE)+', \"ExpectedRevenueAmountCurrencyCode\":USD, \"ExpectedProcessingStartDate\":"", \"ExpectedRevenueStartDate\":'+str(revision_obj.CONTRACT_VALID_FROM)+', \"ExpectedRevenueEndDate\":'+str(revision_obj.CONTRACT_VALID_TO)+', \"ZQuoteRevisionStatus\":'+str(revision_obj.REVISION_STATUS)+'}'
         ##opportunity header write back details ends...
 
         requestdata = (
@@ -61,7 +62,9 @@ def writeback_to_c4c(writeback,contract_quote_record_id,quote_revision_record_id
             + str(quote_revision_record_id)
             +"</quote_revision_record_id><opportunity_header_data>"
             + str(opportunity_header_data)
-            +"</opportunity_header_data></CPQ_Columns></soapenv:Body></soapenv:Envelope>"
+            +"</opportunity_header_data><opportunity_object_id>"
+            + str(opportunity_object_id)
+            +"</opportunity_object_id></CPQ_Columns></soapenv:Body></soapenv:Envelope>"
         )
     LOGIN_CREDENTIALS = SqlHelper.GetFirst("SELECT URL FROM SYCONF where External_Table_Name='CPQ_TO_C4C_WRITEBACK'")
     LOGIN_QUERY = SqlHelper.GetFirst("SELECT User_name as Username,Password,Domain,URL FROM SYCONF where Domain='AMAT_TST'")
