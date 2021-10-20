a7b8358a18c9e30e02f9ddd01d182e3754adb4c8
Aravind <Aravind_Muthiah@contractor.amat.com>
2021-10-20 10:20:50 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -2805,7 +2805,8 @@ class SYLDRTLIST:
                                 else:
                                     Action_str += ('<li><a class="dropdown-item" href="#" id="deletebtn" onclick="'+ str(onclick)+ '" data-target="#cont_CommonModalDelete" data-toggle="modal">DELETE ACCOUNT</a></li>')
                             elif str(ObjectName)=="SAQICT":
-                                    Action_str += ('<li><a class="dropdown-item" href="#" id="deletebtn" onclick="'+ str(onclick)+ '" style="display: none;" data-target="#cont_CommonModalDelete" data-toggle="modal">DELETE CONTACT</a></li>')  
+                                Trace.write("SAQICT DEL")
+                                Action_str += ('<li><a class="dropdown-item" href="#" id="deletebtn" onclick="'+ str(onclick)+ '" style="display: none;" data-target="#cont_CommonModalDelete" data-toggle="modal">DELETE CONTACT</a></li>')  
                                 
                             else:
                                 Action_str += (
