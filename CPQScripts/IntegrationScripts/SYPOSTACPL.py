ce85049d6ea31c4ca47f0f8a695bddffb1cc8db0
BBabax0116946 <baji_baba@contractor.amat.com>
2021-10-20 18:38:53 +0530

    Update SYPOSTACPL.py
    customer person

--- CPQScripts/IntegrationScripts/SYPOSTACPL.py
+++ CPQScripts/IntegrationScripts/SYPOSTACPL.py
@@ -12,30 +12,24 @@ try:
 	if 'Param' in globals(): 	
 		if hasattr(Param, 'CPQ_Columns'): 
 			rebuilt_data = ''
-			SAACCT_data = ''
 			tbl_name = ''
-			primaryQueryItems = SqlHelper.GetFirst("SELECT NEWID() AS A")
 			for table_dict in Param.CPQ_Columns: 				
 				tbl = str(table_dict.Key)
 				tbl_name = tbl				
-				Single_Record = {}
-				SAACCT_Single_Record = {}
-				Multiple_Record  = []
-				SAACCT_Multiple_Record  = []
+				Single_Record = {}				
+				Multiple_Record  = []				
 				for record_dict in table_dict.Value:
 					tyty = str(type(record_dict))
 					if str(tyty) == "<type 'KeyValuePair[str, object]'>":
 						Single_Record[str(record_dict.Key)] = (record_dict.Value).replace('"','\\"')
-						if str(tbl_name).upper() == 'SAACCT':
-							SAACCT_Single_Record[str(record_dict.Key)] = (record_dict.Value).replace('"','\\"')
+						
 						
 					elif str(tyty) == "<type 'Dictionary[str, object]'>":
 						colu_Info1 = {}
 						for j in record_dict:												
 							colu_Info1[str(j.Key)] = (j.Value).replace('"','\\"')         
 						Multiple_Record.append(colu_Info1)	
-						if str(tbl_name).upper() == 'SAACCT':
-							SAACCT_Multiple_Record.append(colu_Info1)
+						
 							
 						
 				if len(Single_Record) !=  0: 
@@ -44,32 +38,22 @@ try:
 						
 				if len(Multiple_Record) !=  0: 
 					tbl = '"'+tbl+'"'
-					rebuilt_data = rebuilt_data+(str(tbl)+':'+str(Multiple_Record))+','
-					
-				if len(SAACCT_Single_Record) !=  0: 
-					tbl = '"SAACCT"'
-					SAACCT_data = SAACCT_data+(str(tbl)+":"+str(SAACCT_Single_Record))+','
-						
-				if len(SAACCT_Multiple_Record) !=  0: 
-					tbl = '"SAACCT"'
-					SAACCT_data = SAACCT_data+(str(tbl)+':'+str(SAACCT_Multiple_Record))+','
+					rebuilt_data = rebuilt_data+(str(tbl)+':'+str(Multiple_Record))+','				
+				
 					
 			rebuilt_data = str(rebuilt_data)[:-1]
-			SAACCT_data  = SAACCT_data[:-1]
-			#Final_data = "{ \"Param\" :''''{ \"CPQ_Columns\": {" + str(rebuilt_data).replace("'",'"') +"}}	''''}"
+			
 			Final_data = "{ \"CPQ_Columns\": {" + str(rebuilt_data).replace("'",'"') +"}}"
 			sessionid = SqlHelper.GetFirst("SELECT NEWID() AS A")
 			timestamp_sessionid = "'" + str(sessionid.A) + "'"
-
-			primaryQueryItems = SqlHelper.GetFirst( "sp_executesql @statement = N'insert SYINPL (INTEGRATION_PAYLOAD,SESSION_ID,INTEGRATION_NAME,CPQTABLEENTRYDATEADDED)  select ''"+str(Final_data)+ "'','"+ str(timestamp_sessionid)+ "',''DEBMAS'',GetDate()' ")	
-
-			SAACCT_Final_data = "{ \"CPQ_Columns\": {" + str(SAACCT_data).replace("'",'"') +"}}"
 			
-			sessionid = SqlHelper.GetFirst("SELECT NEWID() AS A")
-			timestamp_sessionid = "'" + str(sessionid.A) + "'"
-
-			primaryQueryItems = SqlHelper.GetFirst( "sp_executesql @statement = N'insert SYINPL (INTEGRATION_PAYLOAD,SESSION_ID,INTEGRATION_NAME,CPQTABLEENTRYDATEADDED)  select ''"+str(SAACCT_Final_data)+ "'','"+ str(timestamp_sessionid)+ "',''CONTACT_PERSON'',GetDate()' ")	
-		
+			
+			if str(tbl_name).upper() == 'SAACCT':
+				primaryQueryItems = SqlHelper.GetFirst( "sp_executesql @statement = N'insert SYINPL (INTEGRATION_PAYLOAD,SESSION_ID,INTEGRATION_NAME,CPQTABLEENTRYDATEADDED)  select ''"+str(Final_data)+ "'','"+ str(timestamp_sessionid)+ "',''CONTACT_PERSON'',GetDate()' ")	
+			
+			
+			else:
+				primaryQueryItems = SqlHelper.GetFirst( "sp_executesql @statement = N'insert SYINPL (INTEGRATION_PAYLOAD,SESSION_ID,INTEGRATION_NAME,CPQTABLEENTRYDATEADDED)  select ''"+str(Final_data)+ "'','"+ str(timestamp_sessionid)+ "',''DEBMAS'',GetDate()' ")		
 		
 		
 		
