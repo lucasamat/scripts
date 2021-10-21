26b8c4bca0f39974abb078bd541092d1ca3b5894
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-10-21 13:19:45 +0530

    Update CQPREVWDTL.py
    HTML Structure modified for CBC

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
@@ -449,7 +449,7 @@ def constructCBC(Qt_rec_id, Quote, MODE):
 			+ str(qstring)
 			+ "</th>"
 		)
-		
+	sec_str +=('<th> </th>')	
 	sec_str += '</tr></thead><tbody class ="cleanbook_chklst" >'
 	checklist_vals = Sql.GetList("select TOP 1000 CHECKLIST_ID,CHECKLIST_DESCRIPTION,SERVICE_CONTRACT,SPECIALIST_REVIEW,COMMENT FROM SAQCBC(NOLOCK) WHERE QUOTE_RECORD_ID = '{quote_recid}' AND QTEREV_RECORD_ID = '{quote_revision_recid}' ORDER BY CpqTableEntryId ASC".format(quote_recid=Quote,quote_revision_recid=quote_revision_record_id))
 	for value in checklist_vals:
@@ -464,7 +464,8 @@ def constructCBC(Qt_rec_id, Quote, MODE):
 			sec_str += ('<td class="wid_90"><input id="SPECIALIST_REVIEW" type="checkbox" value="'+str(value.SPECIALIST_REVIEW)+'" title="'+str(value.SPECIALIST_REVIEW)+'" class="custom" disabled checked><span class="lbl"></span></td>')
 		else:
 			sec_str += ('<td class="wid_90"><input id="SPECIALIST_REVIEW" type="checkbox" value="'+str(value.SPECIALIST_REVIEW)+'" title="'+str(value.SPECIALIST_REVIEW)+'" class="custom" disabled><span class="lbl"></span></td>')
-		sec_str += ('<td class="wid_90"><input id="COMMENT" type="text" value="'+str(value.COMMENT)+'" title="'+str(value.COMMENT)+'" class="form-control related_popup_css fltlt" disabled><div class="col-md-12 editiconright"><a href="#" onclick="cbcEDIT(this)" class="editclick"><i class="fa fa-pencil" aria-hidden="true"></i></a></div></td>')
+		sec_str += ('<td class="wid_90"><input id="COMMENT" type="text" value="'+str(value.COMMENT)+'" title="'+str(value.COMMENT)+'" class="form-control related_popup_css fltlt" disabled></td>')
+		sec_str+=('<td class="wid_90"><div class="col-md-12 editiconright"><a href="#" onclick="cbcEDIT(this)" class="editclick"><i class="fa fa-pencil" aria-hidden="true"></i></a></div></td>')
 		sec_str += '</tr>'
 
 	sec_str += '</tbody></table></div>'
