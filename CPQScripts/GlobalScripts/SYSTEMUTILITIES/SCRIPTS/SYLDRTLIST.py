3c23a84009e9b331ddabfbe561e0460d467134c3
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2021-10-22 16:52:45 +0530

    Update SYLDRTLIST.py
    Commented the code for the icon

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -2155,7 +2155,8 @@ class SYLDRTLIST:
                 elif RECORD_ID == 'SYOBJR-00010':
                     imgstr = '<img title="Acquired" src=/mt/APPLIEDMATERIALS_TST/Additionalfiles/Green_Tick.svg>'
                     acquiring_img_str = '<img title="Acquiring" src=/mt/APPLIEDMATERIALS_TST/Additionalfiles/Cloud_Icon.svg>'
-                    cps_pricing_img ="<a href=''#'' onclick=''cps_pricing_call(this)''><img src=''/mt/APPLIEDMATERIALS_TST/Additionalfiles/info.png''  style=''height: 15px; width: 15px; background-size: 20px 20px;''> </a>"
+                    cps_pricing_img = ""
+                    #cps_pricing_img ="<a href=''#'' onclick=''cps_pricing_call(this)''><img src=''/mt/APPLIEDMATERIALS_TST/Additionalfiles/info.png''  style=''height: 15px; width: 15px; background-size: 20px 20px;''> </a>"
                     Qury_str = (
                         "SELECT DISTINCT TOP "
                         + str(PerPage)
@@ -6091,8 +6092,9 @@ class SYLDRTLIST:
                         )
                     elif RECORD_ID == 'SYOBJR-00010':
                         imgstr = '<img title="Acquired" src=/mt/APPLIEDMATERIALS_TST/Additionalfiles/Green_Tick.svg>'
-                        acquiring_img_str = '<img title="Acquiring" src=/mt/APPLIEDMATERIALS_TST/Additionalfiles/Cloud_Icon.svg>'                     
-                        cps_pricing_img ='<a href="#" onclick=""><img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/info.png"></a>'
+                        acquiring_img_str = '<img title="Acquiring" src=/mt/APPLIEDMATERIALS_TST/Additionalfiles/Cloud_Icon.svg>'
+                        cps_pricing_img = ""                     
+                        #cps_pricing_img ='<a href="#" onclick=""><img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/info.png"></a>'
                         Qury_str = (
                             "SELECT DISTINCT TOP "
                             + str(PerPage)
@@ -7487,7 +7489,8 @@ class SYLDRTLIST:
                         elif RECORD_ID == 'SYOBJR-00010':    
                             imgstr = '<img title="Acquired" src=/mt/APPLIEDMATERIALS_TST/Additionalfiles/Green_Tick.svg>'
                             acquiring_img_str = '<img title="Acquiring" src=/mt/APPLIEDMATERIALS_TST/Additionalfiles/Cloud_Icon.svg>'
-                            cps_pricing_img ='<a href="#" onclick=""><img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/info.png"></a>'
+                            cps_pricing_img = ""
+                            #cps_pricing_img ='<a href="#" onclick=""><img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/info.png"></a>'
                             Qury_str = (
                                 "SELECT DISTINCT TOP "
                                 + str(PerPage)
