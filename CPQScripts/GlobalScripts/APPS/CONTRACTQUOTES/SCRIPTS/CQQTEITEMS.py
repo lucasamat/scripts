7b806a0d7f3168e37b8161cab41a5b098e559cb7
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-10-22 11:10:19 +0530

    Update CQQTEITEMS.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQTEITEMS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQTEITEMS.py
@@ -201,6 +201,7 @@ def SaveToolIdling(VALUES):
             GETDATE() AS CPQTABLEENTRYDATEADDED
             FROM PRTIAV (NOLOCK) WHERE TOOLIDLING_VALUE_CODE = '{}' AND TOOLIDLING_ID = '{}'
             """.format(QuoteId,QuoteRecordId,QuoteRevisionId,QuoteRevisionRecordId,User.UserName,y,x.replace("_"," ")))
+    LoadSummary()
 
 SubtabName = Param.SUBTAB
 
