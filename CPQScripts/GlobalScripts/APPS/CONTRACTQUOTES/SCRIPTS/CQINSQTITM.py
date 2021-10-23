a3a75e1bbaf9703fb5950dea3db51dfcd1fe105b
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-10-23 21:43:27 +0530

    Update CQINSQTITM.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQINSQTITM.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQINSQTITM.py
@@ -1453,10 +1453,10 @@ class ContractQuoteItem:
 	def _do_opertion(self):
 		if self.action_type == "INSERT_LINE_ITEMS":
 			spare_parts_count_object = Sql.GetFirst("SELECT COUNT(PART_NUMBER) AS COUNT FROM SAQSPT (NOLOCK) WHERE QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID='{}'".format(self.contract_quote_record_id,self.contract_quote_revision_record_id))
-			get_simple_offering = Sql.GetFirst("SELECT * MAMTRL WHERE SAP_PART_NUMBER = '{}' AND MATERIALCONFIG_TYPE = 'SIMPLE MATERIAL'".format(self.service_id))
+			get_simple_offering = Sql.GetFirst("SELECT COUNT(SAP_PART_NUMBER) AS COUNT MAMTRL WHERE SAP_PART_NUMBER = '{}' AND MATERIALCONFIG_TYPE = 'SIMPLE MATERIAL'".format(self.service_id))
 			if spare_parts_count_object.COUNT > 0: ##User story 4432 starts..				
 				self._insert_quote_item_forecast_parts() ##User story 4432 ends..
-			elif get_simple_offering:
+			elif get_simple_offering.COUNT > 0:
 				self._simple_quote_items_insert()
 				self._simple_insert_quote_item_fab_location()
 				self._simple_insert_quote_item_greenbook()	
