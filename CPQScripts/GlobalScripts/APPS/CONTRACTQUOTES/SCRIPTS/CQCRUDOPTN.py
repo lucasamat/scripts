29f5bde3ee83493ae87843b7e83e8ead5883be94
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-10-26 12:52:47 +0530

    Update CQCRUDOPTN.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -4369,7 +4369,7 @@ class ContractQuoteCoveredObjModel(ContractQuoteCrudOpertion):
 						#quote_ent_roll = self.contract_quote_record_id+"=="+str(ancillary_dict)
 						if self.tree_param != 'Receiving Equipment':
 							level = "COV OBJ ENTITLEMENT,"+str(self.tree_param)+","+str(self.tree_parent_level_0)+","+str(self.user_id)+","+str(self.quote_revision_record_id)
-							CQVLDRIFLW.iflow_valuedriver_rolldown(self.contract_quote_record_id,level)
+							CQVLDRIFLW.iflow_valuedriver_rolldown(self.contract_quote_record_id,level,ancillary_dict)
 							#CQVLDRIFLW.iflow_valuedriver_rolldown(quote_ent_roll,level,str(ancillary_dict))						
 					except:
 						Trace.Write("EXCEPT----COV OBJ ENTITLEMENT IFLOW")
