fc599afbb7d885f510fa183214bc99dbf86b5c7b
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-10-26 17:14:17 +0530

    Update CQENTLVIEW.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
@@ -996,15 +996,11 @@ class EntitlementView():
 
 											Trace.Write(str(attrSysId)+'--attrSysId----'+str(val.ENTITLEMENT_ID)+'-----982------>'+str(val.ENTITLEMENT_DISPLAY_VALUE))
 											
-											# if str(attrName) == "Fab Location":
-											# 	if getquote_sales_val in get_il_sales_list:
-											# 		VAR1 += '<option value="select" ' +str(default)+'> </option>'
-											# 	else:
-											# else:
+											
 											VAR1 += '<option value="select" ' +str(default)+' style= "display:none;"> </option>'
 											for value in standard_attr_values:
 												if value.SYSTEM_ID in dropdowndisallowlist:
-													#Trace.Write(str(attrName)+'--dhurga--3179--1001---'+str(value.SYSTEM_ID)+'-'+str(attribute_code))
+													
 													disallow_style = "style = 'display:none'"
 												else:	
 													disallow_style = ""
