90e82f4d0172eed7b0bbe1c871e3c514d836a09a
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-10-26 16:26:12 +0530

    Update CQENTLVIEW.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
@@ -1040,10 +1040,11 @@ class EntitlementView():
 															+ "</option>"
 														)
 												except:
-													Trace.Write(str(default)+'----except dropdown ---durga--'+str(attrName))
+													Trace.Write(str(default)+'----except dropdown ----'+str(attrName))
 													VAR1 = '<option value="select" ' +str(default)+'  style="display;none;"> </option>'
 													if val.ENTITLEMENT_DISPLAY_VALUE == value.STANDARD_ATTRIBUTE_DISPLAY_VAL:
 														selected_option = val.ENTITLEMENT_DISPLAY_VALUE
+														Trace.Write(str(selected_option)+'---selected_option---except dropdown ----'+str(attrName))
 														approval_status = SqlHelper.GetFirst("SELECT APPROVAL_REQUIRED FROM PRENVL WHERE ENTITLEMENT_ID = '{}' AND ENTITLEMENT_DISPLAY_VALUE = '{}'".format(str(attrSysId),str(val.ENTITLEMENT_DISPLAY_VALUE)) )
 														if approval_status:
 															if approval_status.APPROVAL_REQUIRED == True:
@@ -1051,6 +1052,7 @@ class EntitlementView():
 														VAR1 += (
 															'<option  id="'+str(value.SYSTEM_ID)+'" value = "{value}" selected>{value}</option>'.format(value= value.STANDARD_ATTRIBUTE_DISPLAY_VAL)
 														)
+														Trace.Write(str(selected_option)+'---selected_option---except dropdown ----'+str(attrName))
 													else:
 														VAR1 += (
 															'<option '
