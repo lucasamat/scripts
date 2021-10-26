126449d699f0c204f6a3cc6e5f7b5bc2ed4a636f
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2021-10-26 13:36:58 +0530

    Update SYULODTREE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
@@ -2073,7 +2073,7 @@ class TreeView:
 									elif subTabName == 'Spare Parts':
 										Trace.Write("NodeText spare parts"+str(NodeText))
 										subTabName = ""
-										service_id = NodeText.split('-')[1]
+										service_id = NodeText.split('-')[1].strip()
 										spare_parts_object = Sql.GetFirst("SELECT count(CpqTableEntryId) as cnt FROM SAQIFP (NOLOCK) WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' and SERVICE_ID = '{}'".format(Product.GetGlobal("contract_quote_record_id"),quote_revision_record_id,service_id))
 										if spare_parts_object is not None:
 											if spare_parts_object.cnt > 0:
