b75d36f74a2789e4ffead2a55f76c15ad0a5c8f5
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-10-27 12:19:23 +0530

    Update CQINSQTITM.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQINSQTITM.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQINSQTITM.py
@@ -1124,9 +1124,9 @@ class ContractQuoteItem:
 		else:
 			Log.Info("In Z0091 OR 46")
 			self._quote_item_delete_process()
-			self._quote_items_insert()				
-			self._insert_quote_item_fab_location()
-			self._insert_quote_item_greenbook()	
+			# self._quote_items_insert()				
+			# self._insert_quote_item_fab_location()
+			# self._insert_quote_item_greenbook()	
 		#Log.Info("update completed")
 		return True
 	
