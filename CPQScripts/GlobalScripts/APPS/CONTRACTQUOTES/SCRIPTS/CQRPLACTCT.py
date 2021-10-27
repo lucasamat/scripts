bb48bebac2fdf045186009fafa2811d183e524b5
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2021-10-27 18:57:07 +0530

    Update CQRPLACTCT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQRPLACTCT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQRPLACTCT.py
@@ -17,24 +17,22 @@ Sql = SQL()
 ScriptExecutor = ScriptExecutor
 contract_quote_record_id = Quote.GetGlobal("contract_quote_record_id")
 quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")
-def replace_account(repalce_values,acct_rec_id,table_name):
+def replace_contact(repalce_values,cont_rec_id,table_name):
     Trace.Write("repalce_values===="+str(repalce_values))
-    Trace.Write("acct_rec_id===="+str(acct_rec_id))
-    Trace.Write("table_name===="+str(table_name))
-    
-    # acc_data_chk = Sql.GetFirst("Select * from SAQTIP(NOLOCK) WHERE QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID = '{}' AND QUOTE_INVOLVED_PARTY_RECORD_ID ='{}'".format(contract_quote_record_id,quote_revision_record_id,acct_rec_id))
-    # rpl_data_chk =Sql.GetFirst("Select * FROM SAACNT(NOLOCK) WHERE ACCOUNT_RECORD_ID = '{}'".format(repalce_values))
-    # if acc_data_chk:
-    #     Sql.RunQuery("UPDATE SAQTIP SET PARTY_ID = '{party_id}',PARTY_NAME = '{party_name}',ADDRESS ='{address}',EMAIL ='{email}',PHONE = '{phone}',PARTY_RECORD_ID = '{part_rec_id}' WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' and QTEREV_RECORD_ID = '{RevisionRecordId}' and QUOTE_INVOLVED_PARTY_RECORD_ID ='{acct_rec_id}' ".format(party_id = rpl_data_chk.ACCOUNT_ID,party_name = rpl_data_chk.ACCOUNT_NAME,address = rpl_data_chk.ADDRESS_1,email=rpl_data_chk.EMAIL,phone= rpl_data_chk.PHONE,part_rec_id =rpl_data_chk.ACCOUNT_RECORD_ID,QuoteRecordId = contract_quote_record_id,RevisionRecordId = quote_revision_record_id,acct_rec_id = acct_rec_id)) 
-
+    Trace.Write("cont_rec_id===="+str(cont_rec_id))
+    Trace.Write("table_name===="+str(table_name)) 
+    con_data_chk = Sql.GetFirst("Select * from SAQICT(NOLOCK) WHERE QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID = '{}' AND QUOTE_REV_INVOLVED_PARTY_CONTACT_ID ='{}'".format(contract_quote_record_id,quote_revision_record_id,cont_rec_id))
+    rpl_con_data_chk =Sql.GetFirst("Select * FROM SACONT(NOLOCK) WHERE CONTACT_RECORD_ID = '{}'".format(repalce_values))
+    if con_data_chk:
+        Sql.RunQuery("UPDATE SAQICT SET CITY = '{city}',CONTACT_ID = '{contact_id}',CONTACT_NAME = '{contact_name}',CONTACT_RECORD_ID = '{contact_rec_id}',COUNTRY ='{country}',COUNTRY_RECORD_ID ='{country_rec_id}',EMAIL = '{email}',PHONE = '{phone}',POSTAL_CODE ='{postalcode}' WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' and QTEREV_RECORD_ID = '{RevisionRecordId}' and QUOTE_REV_INVOLVED_PARTY_CONTACT_ID ='{cont_rec_id}' ".format(city = rpl_con_data_chk.CITY,contact_id = rpl_con_data_chk.CONTACT_ID,contact_name = rpl_con_data_chk.CONTACT_NAME,contact_rec_id = rpl_con_data_chk.CONTACT_RECORD_ID,,country =rpl_con_data_chk.COUNTRY,,country_rec_id =rpl_con_data_chk.COUNTRY_RECORD_ID,email=rpl_con_data_chk.EMAIL,phone= rpl_con_data_chk.PHONE,QuoteRecordId = contract_quote_record_id,RevisionRecordId = quote_revision_record_id,cont_rec_id = cont_rec_id))
 try:
     repalce_values = Param.repalce_values
-    acct_rec_id = Param.acct_rec_id
+    cont_rec_id = Param.cont_rec_id
     table_name = Param.table_name
 
 except:
     repalce_values ='' 
-    acct_rec_id = '' 
+    cont_rec_id = '' 
     table_name = '' 
-replace_account(repalce_values,acct_rec_id,table_name)
+replace_contact(repalce_values,cont_rec_id,table_name)
 
