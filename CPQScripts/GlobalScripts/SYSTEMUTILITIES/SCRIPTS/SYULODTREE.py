3100629c14a7fcab4e2d687cded8e310e6fdc171
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-10-28 14:49:21 +0530

    Update SYULODTREE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
@@ -2084,11 +2084,12 @@ class TreeView:
 									elif subTabName == 'Spare Parts':
 										Trace.Write("NodeText spare parts"+str(NodeText))
 										subTabName = ""
-										service_id = NodeText.split('-')[1].strip()
-										spare_parts_object = Sql.GetFirst("SELECT count(CpqTableEntryId) as cnt FROM SAQIFP (NOLOCK) WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' and SERVICE_ID = '{}'".format(Product.GetGlobal("contract_quote_record_id"),quote_revision_record_id,service_id))
-										if spare_parts_object is not None:
-											if spare_parts_object.cnt > 0:
-												subTabName = str(getRightView.SUBTAB_NAME)
+										if NodeText:
+											service_id = NodeText.split('-')[1].strip()
+											spare_parts_object = Sql.GetFirst("SELECT count(CpqTableEntryId) as cnt FROM SAQIFP (NOLOCK) WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' and SERVICE_ID = '{}'".format(Product.GetGlobal("contract_quote_record_id"),quote_revision_record_id,service_id))
+											if spare_parts_object is not None:
+												if spare_parts_object.cnt > 0:
+													subTabName = str(getRightView.SUBTAB_NAME)
 									elif subTabName == 'Equipment'and str(ObjName).strip() == 'SAQITM' and 'BASE' in NodeText:
 										Trace.Write("NodeText spare parts"+str(NodeText))
 										subTabName = ""
