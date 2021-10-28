eae0cac1b7f7a8b82213155a6c7fab9af9170c5f
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-10-28 18:35:01 +0530

    Update CQNESTGRID.py
    removed condition

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
@@ -4059,7 +4059,7 @@ def QuoteAssemblyPreventiveMaintainenceParent(PerPage, PageInform, A_Keys, A_Val
 	### editablity in Grid 
 	Trace.Write("TopSuperParentParam---"+str(TopSuperParentParam))
 	Trace.Write("TreeParentParam---"+str(TreeParentParam))
-	if TopSuperParentParam in ('Comprehensive Services','Complementary Products') or TreeParentParam in ('Comprehensive Services','Complementary Products'): 
+	if TopSuperParentParam in ('Comprehensive Services','Complementary Products'): 
 		Trace.Write("inside---")  
 		cls = "eq(2)"
 		# dbl_clk_function = ( 
