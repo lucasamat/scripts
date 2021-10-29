a9af71d5fc39c930cd83beb35cc087e9f757a5d3
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-10-29 16:04:08 +0530

    Update CQENANCOPR.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENANCOPR.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENANCOPR.py
@@ -583,6 +583,9 @@ class AncillaryProductOperation:
 					if str(ATTRIBUTE_DEFN.STANDARD_ATTRIBUTE_NAME).upper() == "BONUS & PENALTY TIED TO KPI":
 						AttributeID_Pass = attrs
 						NewValue = 'Yes'
+					if str(ATTRIBUTE_DEFN.STANDARD_ATTRIBUTE_NAME).upper() == "Bonus Threshold value":
+						AttributeID_Pass = attrs
+						NewValue = ent_disp_val
 
 					insertservice += """<QUOTE_ITEM_ENTITLEMENT>
 					<ENTITLEMENT_ID>{ent_name}</ENTITLEMENT_ID>
