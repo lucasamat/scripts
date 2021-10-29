d234f77e79d37f3fe65639bda45a935f4c3adf44
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-10-29 18:25:20 +0530

    Update CQNESTGRID.py
    trace

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
@@ -4102,7 +4102,7 @@ def QuoteAssemblyPreventiveMaintainenceParent(PerPage, PageInform, A_Keys, A_Val
 	else:
 		Trace.Write("else---")
 		if TreeParentParam in ('Comprehensive Services','Complementary Products'):
-			Trace.Write('*At 4105') 
+			Trace.Write('**At 4105') 
 			cls = "eq(2)"
 			dbl_clk_function += (
 				'$("'	
