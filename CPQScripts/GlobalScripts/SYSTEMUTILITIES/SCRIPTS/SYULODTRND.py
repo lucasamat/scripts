f07f6e76d2d2ba115515efb4da7e093314f79139
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-10-29 21:44:08 +0530

    Update SYULODTRND.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
@@ -518,7 +518,7 @@ def CommonTreeViewHTMLDetail(
 				
 				else:
 					cancel_btn = save_btn = ''	
-					sec_html_btn = Sql.GetList("SELECT HTML_CONTENT FROM SYPSAC (NOLOCK) WHERE SECTION_RECORD_ID = '"+str(SECTION_EDIT)+"'")
+					sec_html_btn = Sql.GetList("SELECT HTML_CONTENT FROM SYPSAC (NOLOCK) WHERE SECTION_RECORD_ID = '"+str(sec.RECORD_ID)+"'")
 					if sec_html_btn:
 						for btn in sec_html_btn:											
 							if "EDIT" in btn.HTML_CONTENT and (MODE == 'VIEW' or MODE == 'CANCEL'):
