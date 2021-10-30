9dff7cb4e38ce103c7a84af3318aa6956d1d233b
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-10-30 13:12:07 +0530

    Update SYSECTSAVE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
@@ -399,12 +399,8 @@ def MaterialSave(ObjectName, RECORD, warning_msg, SectionRecId=None,subtab_name=
 			"SELECT API_NAME FROM SYOBJD WHERE DATA_TYPE = 'AUTO NUMBER' AND OBJECT_NAME = '" + str(TableName) + "'"
 		)
 		AutoNumb = TABLE_OBJS.API_NAME
-		try:
-			RECID_OBJ = RECORD[str(AutoNumb)]
-			RECID_OBJ_SLICE = RECID_OBJ[slice(0, 6)]
-		except:
-			Trace.Write("RECID_OBJ_J"+str(RECORD))
-			RECID_OBJ_SLICE = RECID_OBJ
+		RECID_OBJ = RECORD[str(AutoNumb)]
+		RECID_OBJ_SLICE = RECID_OBJ[slice(0, 6)]
 		if RECID_OBJ_SLICE == str(TableName):
 			RECID = CPQID.KeyCPQId.GetKEYId(str(TableName), str(RECID_OBJ))
 		else:
