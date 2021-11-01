cb6e121d695a89e82d927642eb94845e2eddc1b7
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-11-01 17:44:09 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -7961,6 +7961,7 @@ class SYLDRTLIST:
                         data_type_val = cur_api_name.DATA_TYPE
                         formu_data_type_val = cur_api_name.FORMULA_DATA_TYPE
                     # str(cur_api_name) is not None and
+                    Trace.Write("CURRENCY_DATA_TYPE"+str(data_type_val)+"-"+str(formu_data_type_val))
                     if  (
                         str(data_type_val) == "CURRENCY" or str(formu_data_type_val) == "CURRENCY"
                     ):                        
