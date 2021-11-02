831de80d2f29aa34be5e7b9a101a87d65cc42343
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-11-02 13:49:59 +0530

    Update CQENANCOPR.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENANCOPR.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENANCOPR.py
@@ -683,6 +683,19 @@ class AncillaryProductOperation:
 	
 	def _update_entitlement(self):
 		Log.Info('attr--685--ttributeList----'+str(self.attributeList))
+		attr_id =''
+		entitlement_obj = Sql.GetList("select ENTITLEMENT_ID,ENTITLEMENT_VALUE_CODE,ENTITLEMENT_DISPLAY_VALUE from (SELECT distinct e.QUOTE_RECORD_ID,e.QTEREV_RECORD_ID, replace(X.Y.value('(ENTITLEMENT_ID)[1]', 'VARCHAR(128)'),';#38','&') as ENTITLEMENT_ID,replace(X.Y.value('(ENTITLEMENT_VALUE_CODE)[1]', 'VARCHAR(128)'),';#38','&') as ENTITLEMENT_VALUE_CODE,replace(X.Y.value('(ENTITLEMENT_DISPLAY_VALUE)[1]', 'VARCHAR(128)'),';#38','&') as ENTITLEMENT_DISPLAY_VALUE FROM (select QUOTE_RECORD_ID,QTEREV_RECORD_ID,convert(xml,replace(ENTITLEMENT_XML,'&',';#38')) as ENTITLEMENT_XML from {table_name} (nolock) where QUOTE_RECORD_ID = '{contract_quote_record_id}' AND QTEREV_RECORD_ID = '{quote_revision_record_id}' and SERVICE_ID = '{service_id}' ) e OUTER APPLY e.ENTITLEMENT_XML.nodes('QUOTE_ITEM_ENTITLEMENT') as X(Y) ) as m where  ( ENTITLEMENT_ID like '{att_id}')".format(table_name = 'SAQTSE' ,contract_quote_record_id = contract_quote_record_id,quote_revision_record_id = self.contract_quote_revision_record_id,service_id = 'Z0091',att_id = 'AGS_Z0046%'))
+		for val in entitlement_obj:
+			if val.ENTITLEMENT_DISPLAY_VALUE and 'AGS_Z0046' in val.ENTITLEMENT_ID:
+				NewValue = val.ENTITLEMENT_DISPLAY_VALUE
+				attr_id = val.ENTITLEMENT_ID
+				add_where =''
+				ServiceId = 'Z0046'
+				whereReq = "QUOTE_RECORD_ID = '{}' and SERVICE_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,ServiceId,self.contract_quote_revision_record_id)
+				ent_params_list = str(whereReq)+"||"+str(add_where)+"||"+str(attr_id)+"||"+str(NewValue)+"||"+str(ServiceId) + "||" + 'SAQTSE'
+				
+				result = ScriptExecutor.ExecuteGlobal("CQASSMEDIT", {"ACTION": 'UPDATE_ENTITLEMENT', 'ent_params_list':ent_params_list})
+				Log.Info('698--ent_params_list---'+str(ent_params_list))
 		pass
 
 
