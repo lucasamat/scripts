00f5a9fd54c1bc6d1d6a18d3f62b7b81350c5cfe
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-11-08 13:44:47 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -2580,9 +2580,10 @@ def Related_Sub_Banner(
         # item_covered_obj = Sql.GetFirst("SELECT COUNT(STATUS) AS STATUS FROM SAQICO WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' AND STATUS NOT IN ('ACQUIRED')".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id))
         #for status in item_covered_obj:
         price_preview_status = []
-        item_covered_obj = SqlHelper.GetList("SELECT DISTINCT STATUS FROM SAQICO (NOLOCK) WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id))
+        item_covered_obj = Sql.GetList("SELECT DISTINCT STATUS FROM SAQICO (NOLOCK) WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id))
         for status in item_covered_obj:
             price_preview_status.append(status.STATUS)
+        Trace.Write("price_preview_status_CHK"+str(price_preview_status))
         if len(price_preview_status) > 1:
             price_bar = "not_acquired_status"
         elif 'ACQUIRED' in price_preview_status:
