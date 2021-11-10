ea7b80c99e67f866d701afef46237cd40b9e76db
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-11-10 20:52:23 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -607,21 +607,7 @@ def Related_Sub_Banner(
             elif TreeParam =="Quote Documents":
                 PrimaryLable = "Dynamic Document Generator"
                 PrimaryValue = "Use the settings below to control the conditional display of information on your Customer Facing Documents"  
-            elif subTabName == "Object List":
-                Trace.Write("SAQRIT-DETAIL===")
-                item_detail = Sql.GetFirst(" SELECT * FROM SAQRIT (NOLOCK) WHERE QUOTE_REVISION_CONTRACT_ITEM_ID ='"+str(CurrentRecordId)+"'")
-                if item_detail:
-                    PrimaryLable = "Product Offering Id"
-                    PrimaryValue =  item_detail.SERVICE_ID
-                    SecondLable = "Quantity"
-                    SecondValue = item_detail.QUANTITY
-                    ThirdLable = "Total Excluding Tax/VAT"
-                    ThirdValue = item_detail.TAX_AMOUNT_INGL_CURR
-                    FourthLable = "Contract Start Date"
-                    FourthValue = item_detail.CONTRACT_VALID_FROM
-                    FifthLable = "Contract End Date"
-                    FifthValue = item_detail.CONTRACT_VALID_TO
-         
+             
                     
 
             elif subTabName == "Equipment" and (TreeParentParam == "Fab Locations" or TreeSuperParentParam == "Product Offerings" or TreeParentParam == "Add-On Products" and sec_rel_sub_bnr == "") and CurrentTab == 'Quotes':		
