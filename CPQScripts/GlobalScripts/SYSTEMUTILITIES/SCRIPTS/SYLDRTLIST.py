1c0debd7dbe6b919dab7fbc8c5e2fdebac25b01d
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-11-10 10:34:37 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -484,6 +484,7 @@ class SYLDRTLIST:
 						attr_list[str(attr.API_NAME)] = str(attr.FIELD_SHORT_LABEL)
 					else:
 						attr_list[str(attr.API_NAME)] = str(attr.FIELD_LABEL)
+					Trace.Write("attr_list_j"+str(attr_list))
 					if (
 						str(attr.LOOKUP_API_NAME) != ""
 						and str(attr.LOOKUP_API_NAME) is not None
