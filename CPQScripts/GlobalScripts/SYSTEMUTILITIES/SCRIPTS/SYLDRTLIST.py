3891ea84b81c553e73e5f7821287da8766e5537f
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-11-11 15:11:06 +0530

    Update SYLDRTLIST.py
    hyperlink issue fix

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -3890,9 +3890,8 @@ class SYLDRTLIST:
 						visible = ""
 						if RECORD_ID == 'SYOBJR-00007' and str(invs) == 'BILLING_AMOUNT':                            
 							visible = 'data-visible="false"'  
-						if str(RECORD_ID) == "SYOBJR-00029" and str(invs)=="TARGET_QUANTITY":
-    						Trace.Write('@Target Quantity HyperLink')
-							table_header += (
+						if str(RECORD_ID) == "SYOBJR-00029" and str(invs)=="QUANTITY":
+    						table_header += (
 								'<th  data-field="'
 								+ str(invs)
 								+ '" data-filter-control="input" data-align="right" data-title-tooltip="'
