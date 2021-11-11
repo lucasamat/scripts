d6c9ec3ff49a9a972242a414e314ed0ca7a64563
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-11-11 13:01:39 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -2083,19 +2083,20 @@ class SYLDRTLIST:
 						elif str(RECORD_ID) == "SYOBJR-98868":
 							Qustr += "AND EQUIPMENT_ID = '"+str(equipment_id)+"'"
 						elif str(RECORD_ID) == "SYOBJR-00029":
-							#service_id = TreeParam
+							service_id = TreeParam.split('-')[0]
 							quote_rec_id = Product.GetGlobal("contract_quote_record_id")
 							quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")
 							# if SubTab == "Parts List":
 							# 	service_id = 'Z0101'
 							# else:
-							# 	service_object = Sql.GetFirst("select SERVICE_ID FROM SAQTSV where QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID = '{}' AND PAR_SERVICE_ID = '{}'".format(quote_rec_id,quote_revision_record_id,TreeParam))
-							# 	if service_object is not None:
-							# 		service_id = service_object.SERVICE_ID
+							service_object = Sql.GetFirst("select SERVICE_ID FROM SAQTSV where QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID = '{}' AND PAR_SERVICE_ID = '{}'".format(quote_rec_id,quote_revision_record_id,service_id))
+							if service_object is not None:
+								serv_id = service_object.SERVICE_ID
 							# Qustr += " AND SERVICE_ID = '"+str(service_id)+"'"							
 							if TreeSuperParentParam == "Product Offerings":
-								service_id = TreeParam.split('-')[0]
-								Qustr += " AND SERVICE_ID = '"+str(service_id)+"'"
+								#service_id = TreeParam.split('-')[0]
+								Qustr += " AND SERVICE_ID = '"+str(serv_id)+"'"
+							elif	
 						Trace.Write('In 1958---*'+str(Qustr))						
 						if str(RECORD_ID) == "SYOBJR-98874":
 							Qustr += " AND LINE = '"+str(line_item)+"'"
