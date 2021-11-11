5569e9e543d2c9db0a8eaf7852500c4c3fcbd28f
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2021-11-11 16:50:51 +0530

    Update CQSYNCQTBL.py
    Removed Indentation issue.

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
@@ -466,7 +466,7 @@ class SyncQuoteAndCustomTables:
 					created_date = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S %p")
 					expired_date = date.today()+ timedelta(days=365)
 					Log.Info("Expiry_Date_Check "+str(expired_date))
-     				Log.Info("END DATE TO CONTRACT VALID TO "+str(end_date))
+					Log.Info("END DATE TO CONTRACT VALID TO "+str(end_date))
 					#A055S000P01-7866
 					#document_type = {"ZTBC": "SSC", "ZWK1": "APG"}
 					quote_type = {"ZTBC":"ZTBC - TOOL BASED", "ZNBC":"ZNBC - NON TOOL BASED", "ZWK1":"ZWK1 - SPARES", "ZSWC":"ZSWC - SOLD WITH SYSTEM"}
