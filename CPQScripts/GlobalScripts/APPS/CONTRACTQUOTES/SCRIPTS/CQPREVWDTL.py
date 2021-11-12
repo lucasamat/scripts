db381b5b31db13b54cf4d45a72d7772fd5b7c794
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2021-11-12 17:10:47 +0530

    Update CQPREVWDTL.py
    Modified statement.

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
@@ -367,7 +367,7 @@ def constructquoteinformation(Qt_rec_id, Quote, MODE):
 					# 	)
 				else:
 					# if sefl_api != "REGION":
-					Trace.Write('At line 289-->arr2'+str(sefl_api))
+					Trace.Write('At line 289-->arr2'+sefl_api)
 					# if sefl_api == "APPDTE_EXCH_RATE" and str(eval("col_name." + str(sefl_api))) != "":
 					# 	sec_str += (
 					# 		"<div class='col-md-3 pad-0'> <input type='text' id ='"+str(sefl_api)+"' title = '"+  str(eval("col_name." + str(sefl_api)))+"' value = '"
@@ -377,7 +377,7 @@ def constructquoteinformation(Qt_rec_id, Quote, MODE):
 					# else:
 					#sefl_api = sefl_api.encode('ascii', 'ignore').decode('ascii')
 					sec_str += (
-						"<div class='col-md-3 pad-0'> <input type='text' id ='"+sefl_api+"' title = '"+  str(eval("col_name." + sefl_api))+"' value = '"
+						"<div class='col-md-3 pad-0'> <input type='text' id ='"+str(sefl_api)+"' title = '"+  str(eval("col_name." + sefl_api))+"' value = '"
 						+ str(eval("col_name." + sefl_api))
 						+ "' 'title':userInput}, incrementalTabIndex, enable: isEnabled' class='form-control' style='height: 28px;border-top: 0 !important;border-bottom: 0 !important;' id='' title='' tabindex='' disabled=''> </div>"
 					)
