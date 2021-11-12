ffc9a099497bdf23187e54a4e9a2840b0b578f88
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2021-11-12 16:26:23 +0530

    Update CQPREVWDTL.py
    Commit

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
@@ -540,7 +540,7 @@ def EditCBC(Qt_rec_id, Quote, MODE):
 	
 	Sql.RunQuery("UPDATE SAQTRV SET	REVISION_STATUS = 'SUBMITTED FOR BOOKING' WHERE QUOTE_RECORD_ID = '{quote_rec_id}' AND QTEREV_RECORD_ID = '{quote_rev_recid}' AND ACTIVE = '1' ".format(quote_rec_id = Quote,quote_rev_recid = quote_revision_record_id))
 	get_quote_details = Sql.GetFirst("Select QUOTE_ID,QTEREV_ID FROM SAQTRV WHERE QUOTE_RECORD_ID = '{quote_rec_id}' AND QTEREV_RECORD_ID = '{quote_rev_recid}' AND ACTIVE = '1' ".format(quote_rec_id = Quote,quote_rev_recid = quote_revision_record_id))
-	crm_result = ScriptExecutor.ExecuteGlobal('QTPOSTACRM',{'QUOTE_ID':str(get_quote_details.QUOTE_ID),'QTEREV_ID':str(get_quote_details.REVISION_ID),'Fun_type':'cpq_to_crm'})		
+	crm_result = ScriptExecutor.ExecuteGlobal('QTPOSTACRM',{'QUOTE_ID':str(get_quote_details.QUOTE_ID),'REVISION_ID':str(get_quote_details.QTEREV_ID),'Fun_type':'cpq_to_crm'})		
 	return True
 	
 
