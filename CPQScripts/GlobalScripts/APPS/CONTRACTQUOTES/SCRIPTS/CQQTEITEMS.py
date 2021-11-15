25c742c8961cfbfddb3f5c998494cf64d147c299
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-11-15 14:14:56 +0530

    Update CQQTEITEMS.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQTEITEMS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQTEITEMS.py
@@ -71,7 +71,7 @@ def LoadSummary():
                             sec_str += '<option id="AGS_'+val.TOOLIDLING_VALUE_CODE+'" value="'+val.TOOLIDLING_VALUE_CODE+'" >'+val.TOOLIDLING_VALUE_CODE+'</option>'
                 if "Idling type" not in x:
                     sec_str += '</select><a href="#" class="editclick" style=" color:#dcdcdc !important;"><i title="Double Click to Edit" class="fa fa-pencil" aria-hidden="true"></i></a></td></tr>'
-                elif ("Cold Idle Allowed" in x and "Yes" in y) and ("Hot Idle Allowed" in x and "Yes" in y):
+                elif ("Cold Idle Allowed" in x and "Yes" in y) or ("Hot Idle Allowed" in x and "Yes" in y):
                     sec_str += '</select><a href="#" class="editclick" style=" color:#dcdcdc !important;"><i title="" class="fa fa-lock" aria-hidden="true"></i></a></td></tr>'
                 else:
                     sec_str += '</select><a href="#" class="editclick" style=" color:#dcdcdc !important;"><i title="" class="fa fa-lock" aria-hidden="true"></i></a></td></tr>'
