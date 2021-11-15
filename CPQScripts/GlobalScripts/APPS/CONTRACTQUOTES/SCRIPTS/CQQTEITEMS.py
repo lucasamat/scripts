ab8c5ee1006678082a5716221cf77961eac21c24
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-11-15 14:22:21 +0530

    Update CQQTEITEMS.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQTEITEMS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQTEITEMS.py
@@ -69,12 +69,12 @@ def LoadSummary():
                     if getAllValues:
                         for val in getAllValues:
                             sec_str += '<option id="AGS_'+val.TOOLIDLING_VALUE_CODE+'" value="'+val.TOOLIDLING_VALUE_CODE+'" >'+val.TOOLIDLING_VALUE_CODE+'</option>'
-                if "Idling type" not in x:
+
+                if (("Cold Idle Allowed" in x and "Yes" in y) or ("Hot Idle Allowed" in x and "Yes" in y)) and "Idling type" in x:
+                    sec_str += '</select><a href="#" class="editclick" style=" color:#dcdcdc !important;"><i title="" class="fa fa-lock" aria-hidden="true"></i></a></td></tr>'
+                else:
                     sec_str += '</select><a href="#" class="editclick" style=" color:#dcdcdc !important;"><i title="Double Click to Edit" class="fa fa-pencil" aria-hidden="true"></i></a></td></tr>'
-                if ("Cold Idle Allowed" in x and "Yes" in y) or ("Hot Idle Allowed" in x and "Yes" in y):
-                    sec_str += '</select><a href="#" class="editclick" style=" color:#dcdcdc !important;"><i title="" class="fa fa-lock" aria-hidden="true"></i></a></td></tr>'
-                if "Idling type" in x:
-                    sec_str += '</select><a href="#" class="editclick" style=" color:#dcdcdc !important;"><i title="" class="fa fa-lock" aria-hidden="true"></i></a></td></tr>'
+                
         sec_str += "</tbody></table>"
     elif ent_value == "No":
         yes_selected = ""
