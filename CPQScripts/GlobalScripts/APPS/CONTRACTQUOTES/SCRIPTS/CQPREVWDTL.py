8179e5eb4aa3784840afe6490222a3d60bdf9ce2
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2021-11-16 19:32:18 +0530

    Update CQPREVWDTL.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
@@ -574,9 +574,9 @@ def constructCBC(Qt_rec_id, Quote, MODE):
 def EditCBC(Qt_rec_id, Quote, MODE):		
 	Trace.Write('CBC Update')
 	#min = Sql.GetFirst("SELECT MIN(CpqTableEntryId) as id from SAQCBC WHERE QUOTE_RECORD_ID = '{quote_rec_id}' AND QTEREV_RECORD_ID = '{quote_rev_recid}'".format(quote_rec_id = Quote,quote_rev_recid = quote_revision_record_id))
-	Trace.Write("aaaaaaa"+str(values))
+	
 	for val in values:
-		#order = int(val["order"])
+		Trace.Write("aaaaaaa"+str(val))#order = int(val["order"])
 		#order = order + min.id - 1
 		Sql.RunQuery("UPDATE SAQCBC SET SERVICE_CONTRACT = '{service_contract}',SPECIALIST_REVIEW = '{specialist_review}',COMMENT = '{comment}' WHERE CHECKLIST_ID = '{checklist_id}' AND QUOTE_RECORD_ID = '{quote_rec_id}' AND QTEREV_RECORD_ID = '{quote_rev_recid}' ".format(checklist_id = val['CHECKLIST_ID'] if val['CHECKLIST_ID'] !="" else "",service_contract = val['SERVICE_CONTRACT'],specialist_review = val['SPECIALIST_REVIEW'],comment = val['COMMENT'],quote_rec_id = Quote,quote_rev_recid = quote_revision_record_id))
 	popupquery=Sql.GetFirst("SELECT COUNT(*) FROM SAQCBC WHERE QUOTE_RECORD_ID = '{quote_rec_id}' AND QTEREV_RECORD_ID = '{quote_rev_recid}' AND SERVICE_CONTRACT='False' AND SPECIALIST_REVIEW='FALSE' AND CHECKLIST_ID NOT IN('2.1','3.1')".format(quote_rec_id = Quote,quote_rev_recid = quote_revision_record_id))
