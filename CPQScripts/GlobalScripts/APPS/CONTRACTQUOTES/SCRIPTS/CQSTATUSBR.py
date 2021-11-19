7d15da06aa992456a7aacc357e6946ba398268e0
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-11-19 16:42:52 +0530

    Update CQSTATUSBR.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSTATUSBR.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSTATUSBR.py
@@ -20,12 +20,11 @@ except:
 try:
     quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")
 except:
-    quote_revision_record_id = "" 	
+    quote_revision_record_id = ""
 
-# Trace.Write('contract_quote_record_id==='+str(Quote.GetGlobal("contract_quote_record_id")))
 try:
     current_prod = Product.Name
-    #Trace.Write('current_prod==GLOBAL---='+str(Quote.GetGlobal("contract_quote_record_id")))
+    
 except:
     current_prod = "Sales"
 try:
@@ -41,9 +40,7 @@ def Dynamic_Status_Bar():
         getfab_info = Sql.GetFirst("SELECT FABLOCATION_NAME from SAQSFB where QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id))
         get_service_ifo = Sql.GetFirst("SELECT COUNT(DISTINCT SERVICE_ID) as SERVICE_ID from SAQTSV where QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id))
         get_equip_details = Sql.GetFirst("SELECT COUNT(DISTINCT SERVICE_ID) as SERVICE_ID from SAQSCO where QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id))
-
-        # item_covered_obj = Sql.GetFirst("SELECT COUNT(STATUS) AS STATUS FROM SAQICO WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' AND STATUS NOT IN ('ACQUIRED')".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id))
-        #for status in item_covered_obj:
+        
         price_preview_status = []
         item_covered_obj = Sql.GetList("SELECT DISTINCT STATUS FROM SAQICO (NOLOCK) WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id))
         if item_covered_obj:
@@ -59,12 +56,7 @@ def Dynamic_Status_Bar():
         else:
             Trace.Write("NO Quote Items")
             price_bar = "no_quote_items"
-        # if item_covered_obj.STATUS > 0:
-        #     price_bar = "acquired_status"
-        #     Trace.Write("config status==="+str(price_bar))
-        # else:
-        #     price_bar = "not_acquired_status"
-        #     Trace.Write("config status111==="+str(price_bar))                    
+                            
         if getsalesorg_ifo and getfab_info:
             Trace.Write('salesorg--present---')
             if get_service_ifo.SERVICE_ID == get_equip_details.SERVICE_ID:
@@ -75,10 +67,7 @@ def Dynamic_Status_Bar():
                 #     buttonvisibility = "Show_button"   
                 # else:
                 Trace.Write('No button-2454-')
-                buttonvisibility = "Hide_button"
-            # elif TreeParam == "Approvals" and CurrentTabName == 'Quotes':
-            # 	Trace.Write("SUBMIT FOR APP Button")
-            # 	sec_rel_sub_bnr = add_button
+                buttonvisibility = "Hide_button"            
             else:
                 Trace.Write('No button--1')
                 buttonvisibility = "Hide_button"
