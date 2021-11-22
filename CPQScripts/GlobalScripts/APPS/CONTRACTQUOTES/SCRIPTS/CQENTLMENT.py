d05dbbe7c1bd90af37de7a692570393d982ec8a2
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2021-11-22 13:33:33 +0530

    Update CQENTLMENT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
@@ -1105,7 +1105,7 @@ class Entitlements:
 					if str(serviceId) in ("Z0091","Z0004","Z0007","Z0006","Z0092","Z0035") and key in ( "AGS_{}_TSC_CONSUM".format(serviceId), "AGS_{}_TSC_NONCNS".format(serviceId), "AGS_{}_NON_CONSUMABLE".format(serviceId)) and str(tableName) in ('SAQSGE','SAQTSE','SAQSFE'):
 						#ancillary_object = 'Z0101'
 						if tableName == "SAQTSE":
-							QuoteModule.service_level_entitlement('Z0091',1)
+							QuoteModule.service_level_entitlement(str(serviceId),1)
 						Trace.Write("entitlement_value -----"+str(entitlement_value))
 						if (entitlement_value == "Some Exclusions" or entitlement_value == "Some Inclusions"):
 							ancillary_object_dict['Z0101'] = "INSERT"
