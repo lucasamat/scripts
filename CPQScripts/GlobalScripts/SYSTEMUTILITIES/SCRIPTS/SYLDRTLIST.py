8ed02d356c350afb039e564841fb55e77b8ffe91
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-11-23 13:37:51 +0530

    Update SYLDRTLIST.py
    trace

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -3068,7 +3068,8 @@ class SYLDRTLIST:
 								+ "' and SE.SECTION_NAME ='BASIC INFORMATION'"
 							)
 						else:
-							Trace.Write('At line 2805 ---2960----- Else'+str(value123)+" - "+str(value1234))
+							Trace.Write('At line 2805 ---2960----- Else'+str(value123))
+							Trace.Write(' - '+str(value1234))
 							imgValue = ''
 							if value1234.startswith("<img"):
 								# value1234 = value1234.replace('"', "&quot;")
