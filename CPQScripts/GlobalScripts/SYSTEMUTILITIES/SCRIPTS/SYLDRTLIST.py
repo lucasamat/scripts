5ff4b953cc0531ab896ee2a3a934b27142a34c62
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2021-11-23 20:07:11 +0530

    Update SYLDRTLIST.py
    Grand total amount printed wrongly issue has been sorted out in X as well T.

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -8631,7 +8631,7 @@ class SYLDRTLIST:
 				footer_tot = ""
 				if ObjectName == "SAQIBP":
 					ContractRecordId = Product.GetGlobal("contract_quote_record_id")
-					gettotaldateamt = Sql.GetList("SELECT BILLING_AMOUNT=SUM(BILLING_AMOUNT),ANNUAL_BILLING_AMOUNT = SUM(ANNUAL_BILLING_AMOUNT),BILLING_DATE FROM SAQIBP WHERE BILLING_DATE in {billing_date_column} and QUOTE_RECORD_ID ='{cq}' AND QTEREV_RECORD_ID='{revision_rec_id}' group by BILLING_DATE ".format(cq=str(ContractRecordId),revision_rec_id = quote_revision_record_id,billing_date_column=str(tuple(billing_date_column))))
+					gettotaldateamt = Sql.GetList("SELECT BILLING_AMOUNT=SUM(BILLING_AMOUNT),ANNUAL_BILLING_AMOUNT = SUM(ANNUAL_BILLING_AMOUNT),BILLING_DATE FROM SAQIBP WHERE BILLING_DATE in {billing_date_column} and QUOTE_RECORD_ID ='{cq}' AND QTEREV_RECORD_ID='{revision_rec_id}' AND SERVICE_ID = '{service_id}' group by BILLING_DATE ".format(cq=str(ContractRecordId),revision_rec_id = quote_revision_record_id,billing_date_column=str(tuple(billing_date_column)),service_id = TreeParam))
 					if gettotaldateamt:
 						my_format = "{:,." + str(decimal_place) + "f}"
 						for val in gettotaldateamt: 
