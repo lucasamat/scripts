7fc7d6c472ae268c040950132fab4cc99df7217c
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2021-11-23 16:53:06 +0530

    Update CQENTLMENT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
@@ -22,8 +22,6 @@ import sys
 from SYDATABASE import SQL
 import datetime
 import CQENTIFLOW
-from CQQTMODULE import QuoteModule as Quote_Module
-QuoteModule = Quote_Module()
 #import CQTVLDRIFW
 userId = str(User.Id)
 userName = str(User.UserName)
