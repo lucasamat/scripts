14bbcc3b42fe030ba9f218dea578b6349aae698a
Aravind <Aravind_Muthiah@contractor.amat.com>
2021-11-25 16:59:35 +0530

    Update CQQTEITEMS.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQTEITEMS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQTEITEMS.py
@@ -65,7 +65,7 @@ def LoadSummary():
                     # style = ""
                     # if "of Tools" in x or "Fee" in x or "Notice" in x or "Duration" in x or "Exception" in x:
                     #     style = ' style="color:#1B78D2"'
-                    if (x == "Cold Idle Allowed" or x == "Warm / Hot Idle Allowed"):
+                    if (x == "Cold Idle Allowed") or (x == "Warm / Hot Idle Allowed"):
                         sec_str += '<td class="required_symbol" style=""><abbr class="required_symbol" title="'+x+'">*</abbr></td><td style=""><select class="form-control remove_yellow disable_edit" style="" id="'+x.replace(" ","_")+'" type="text"  data-content="'+x.replace(" ","_")+'"  title="'+getDefaultValue.TOOLIDLING_VALUE_CODE+'" disabled = ""><option value="select" style="display:none;"> </option><option id="'+x.replace(" ","_")+'" value="'+getDefaultValue.TOOLIDLING_VALUE_CODE+'" selected = "">'+getDefaultValue.TOOLIDLING_VALUE_CODE+'</option>'
 
                     else:
