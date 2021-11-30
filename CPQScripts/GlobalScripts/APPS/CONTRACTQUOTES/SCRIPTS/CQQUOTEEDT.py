6a746addac34167ccccf07541aa969ce0b5ca332
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-11-30 23:43:30 +0530

    Update CQQUOTEEDT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQUOTEEDT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQQUOTEEDT.py
@@ -33,14 +33,14 @@ def bannerdetails(Quoteid,active_tab_name):
 		Quoteid = SQLObj.QUOTE_ID
 		#matchObj = re.match( r'^\s*[A-Z]{1,2}(\d+)[A-Z]{1,2}[^>]*?\-', Quoteid)
 		#matchObj = re.search(r'\d+', Quoteid).group()
-	# if active_tab_name == "Quotes":
-	# 	Trace.Write('matchObj--'+str(matchObj))
-	# 	Trace.Write('Quoteid--'+str(Quoteid))
-	# 	qid=str(matchObj.group(1))
-	# 	get_rev_info = Sql.GetFirst("SELECT QTEREV_ID,QTEREV_RECORD_ID FROM SAQTMT (NOLOCK) WHERE C4C_QUOTE_ID='" + str(qid) + "'")
-	# 	if get_rev_info:
-	# 		Quote.SetGlobal("quote_revision_record_id",str(get_rev_info.QTEREV_RECORD_ID))
-	# 		Quote.SetGlobal("quote_revision_id",str(get_rev_info.QTEREV_ID))
+	if active_tab_name == "Quotes":
+		Trace.Write('matchObj--'+str(matchObj))
+		Trace.Write('Quoteid--'+str(Quoteid))
+		qid=str(matchObj.group(1))
+		get_rev_info = Sql.GetFirst("SELECT QTEREV_ID,QTEREV_RECORD_ID FROM SAQTMT (NOLOCK) WHERE C4C_QUOTE_ID='" + str(qid) + "'")
+		if get_rev_info:
+			Quote.SetGlobal("quote_revision_record_id",str(get_rev_info.QTEREV_RECORD_ID))
+			Quote.SetGlobal("quote_revision_id",str(get_rev_info.QTEREV_ID))
 	if Quoteid is not None and str(Quoteid) !='':
 		if matchObj:
 			#qid=str(matchObj.group(1))
