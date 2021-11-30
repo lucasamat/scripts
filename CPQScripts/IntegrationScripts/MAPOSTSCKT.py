760b1ffca4986d1dfcbae52be6d3da6436891f21
BBabax0116946 <baji_baba@contractor.amat.com>
2021-11-30 20:12:30 +0530

    Update MAPOSTSCKT.py

--- CPQScripts/IntegrationScripts/MAPOSTSCKT.py
+++ CPQScripts/IntegrationScripts/MAPOSTSCKT.py
@@ -7,97 +7,73 @@
 # ==========================================================================================================================================
 import sys
 import datetime 
+import clr
+import System.Net
+from System.Text.Encoding import UTF8
+from System import Convert
+import System
+from System.Net import HttpWebRequest, NetworkCredential
+from System.Net import *
+from System.Net import CookieContainer
+from System.Net import Cookie
+from System.Net import WebRequest
+from System.Net import HttpWebResponse
+from System import Uri
+from SYDATABASE import SQL
 Log.Info("MAPOSTSCKT ---->Hitting")
 
 try:
-	if 'Param' in globals(): 	
-		if hasattr(Param, 'CPQ_Columns'): 
-			rebuilt_data = ''
-			primaryQueryItems = SqlHelper.GetFirst("SELECT NEWID() AS A")		
-			for table_dict in Param.CPQ_Columns: 
-				tbl = str(table_dict.Key)
-				#Log.Info("6666 tbl---->"+str(tbl))
-				
-				Single_Record = {}
-				Multiple_Record  = []    
-				Dirct_record = {}
-				for record_dict in table_dict.Value:
-					#Log.Info("99999 record_dict ---->"+str(table_dict.Value))
-					tyty = str(type(record_dict))
-					if str(tyty) == "<type 'KeyValuePair[str, object]'>":    
-						
-						Single_Record[str(record_dict.Key)] = record_dict.Value
-					elif str(tyty) == "<type 'Dictionary[str, object]'>":
-						colu_Info1 = {}
-						for j in record_dict:								
-												
-							colu_Info1[str(j.Key)] = j.Value         
-						Multiple_Record.append(colu_Info1)	
-					else:
-						Dirct_record[tbl] = str(table_dict.Value)
-						
-							
-				
-				if len(Dirct_record) > 0:
-					for ins in Dirct_record:
-						rebuilt_data = rebuilt_data+'"'+(str(ins)+'"'+":"+'"'+str(Dirct_record[ins]))+'"'+','
-							
-						
-				if len(Single_Record) !=  0: 
-					tbl = '"'+tbl+'"'
-					rebuilt_data = rebuilt_data+(str(tbl)+":"+str(Single_Record))+','
-					#rebuilt_data.append(sig_dict)
-						
-				if len(Multiple_Record) !=  0: 
-					tbl = '"'+tbl+'"'
-					rebuilt_data = rebuilt_data+(str(tbl)+':'+str(Multiple_Record))+','
-					#rebuilt_data.append(mul_dict)
-					
-			#Log.Info("959595---->"+str(rebuilt_data))
-			rebuilt_data = str(rebuilt_data)[:-1]
-			Final_data = "{ \"Param\" :''''{ \"CPQ_Columns\": {" + str(rebuilt_data).replace("'",'"') +"}}	''''}"
-			sessionid = SqlHelper.GetFirst("SELECT NEWID() AS A")
-			timestamp_sessionid = "'" + str(sessionid.A) + "'"
-
-			Parameter = SqlHelper.GetFirst("SELECT QUERY_CRITERIA_1 FROM SYDBQS (NOLOCK) WHERE QUERY_NAME = 'SELECT' ")
-			
-			primaryQueryItems = SqlHelper.GetFirst( ""+ str(Parameter.QUERY_CRITERIA_1)+ " SYINPL (INTEGRATION_PAYLOAD,SESSION_ID,INTEGRATION_NAME)  select ''"+str(Final_data)+ "'','"+ str(timestamp_sessionid)+ "',''SSCM_TO_CPQ_KITBOM_DATA'' ' ")
-			
-			
-			'''LOGIN_CREDENTIALS = SqlHelper.GetFirst("SELECT USER_NAME as Username,Password,Domain FROM SYCONF where Domain='AMAT_TST'")
-			if LOGIN_CREDENTIALS is not None:
-				Login_Username = str(LOGIN_CREDENTIALS.Username)
-				Login_Password = str(LOGIN_CREDENTIALS.Password)
-				authorization = Login_Username+":"+Login_Password
-				binaryAuthorization = UTF8.GetBytes(authorization)
-				authorization = Convert.ToBase64String(binaryAuthorization)
-				authorization = "Basic " + authorization
-
-
-				webclient = System.Net.WebClient()
-				webclient.Headers[System.Net.HttpRequestHeader.ContentType] = "application/json"
-				webclient.Headers[System.Net.HttpRequestHeader.Authorization] = authorization;
-				
-				result = '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body ></soapenv:Body></soapenv:Envelope>'
-				
-				#LOGIN_CRE = SqlHelper.GetFirst("SELECT URL FROM SYCONF where EXTERNAL_TABLE_NAME ='CPQ_TO_CRM_KITBOM_ASYNC'")
-				Async = webclient.UploadString(str(LOGIN_CRE.URL), str(result))'''
-			
-			
-			ApiResponse = ApiResponseFactory.JsonResponse(
-				{
-					"Response": [
-						{
-							"Status": "200",
-							"Message": "Data Sucessfully Uploaded ."
-						}
-					]
-				}
-			)
+	LOGIN_CRE = SqlHelper.GetFirst("SELECT URL FROM SYCONF (nolock) where EXTERNAL_TABLE_NAME ='KITBOM_GETMETHOD'")
 	
+	webRequest = str(LOGIN_CRE.URL)
 
+	def GetNewRequest(targetUrl, Btoken):
+
+		newRequest = HttpWebRequest.Create(targetUrl)
+		newRequest.AllowAutoRedirect = 0
+		newRequest.Headers.Add("AUTHORIZATION", Btoken)
+		newRequest.Method = 'GET'
+		newRequest.ContentLength = 0
+		newRequest.ContentType = 'application/json'
+
+		return newRequest
+
+	Oauth_info = SqlHelper.GetFirst("SELECT  DOMAIN,URL FROM SYCONF where EXTERNAL_TABLE_NAME ='OAUTH'")
+
+	requestdata =Oauth_info.DOMAIN
+	webclient = System.Net.WebClient()
+	webclient.Headers[System.Net.HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded"
+	response = webclient.UploadString(Oauth_info.URL,str(requestdata))
+
+	response = eval(response)
+	access_token = response['access_token']
+	Btoken = "Bearer " + access_token
+
+	rew = GetNewRequest(webRequest, Btoken)
+	resp = rew.GetResponse()
+
+	streamReader = StreamReader(resp.GetResponseStream())
+	jsonData = streamReader.ReadToEnd()				
+
+	conv_data = jsonData.replace("'",'%%').replace('QTQICO','MAKTPT')
+	
+	Parameter = SqlHelper.GetFirst("SELECT QUERY_CRITERIA_1 FROM SYDBQS (NOLOCK) WHERE QUERY_NAME = 'SELECT' ")
+			
+	primaryQueryItems = SqlHelper.GetFirst( ""+ str(Parameter.QUERY_CRITERIA_1)+ " SYINPL (INTEGRATION_PAYLOAD,INTEGRATION_NAME,CpqTableEntryDateModified)  select ''"+conv_data+ "'',''SSCM_TO_CPQ_KITBOM_DATA'',GETDATE() ' ")
+			
+			
+	ApiResponse = ApiResponseFactory.JsonResponse(
+		{
+			"Response": [
+				{
+					"Status": "200",
+					"Message": "Data Sucessfully Uploaded ."
+				}
+			]
+		}
+	)
 
 except:		
 	Log.Info("MAPOSTSCKT ERROR---->:" + str(sys.exc_info()[1]))
 	Log.Info("MAPOSTSCKT ERROR LINE NO---->:" + str(sys.exc_info()[-1].tb_lineno))
-	ApiResponse = ApiResponseFactory.JsonResponse({"Response": [{"Status": "400", "Message": str(sys.exc_info()[1])}]})	
+	ApiResponse = ApiResponseFactory.JsonResponse({"Response": [{"Status": "400", "Message": str(sys.exc_info()[1])}]})
