731c777ceb4354a98238d5467682be4ce1b0e824
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-12-01 18:52:29 +0530

    Update SYSUBANNER.py
    restricted buttons for approved quotes

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -2939,9 +2939,9 @@ def Related_Sub_Banner(
         elif  (str(TreeSuperParentParam).upper() == "PRODUCT OFFERINGS")  and TabName == "Quotes" and str(subTabName)!="Parts List" and str(subTabName)!= "New Parts Only":     
             sec_rel_sub_bnr += ('<button id="fabcostlocate_save" onclick="fabcostlocatesave(this)" style="display: none;" class="btnconfig hidebtn">SAVE</button><button id="fabcostlocate_cancel" onclick="fabcostlocatecancel(this)" style="display: none;" class="btnconfig hidebtn">CANCEL</button>'  )    
             Trace.Write('### _ Multi_buttons'+str(multi_buttons))
-            if str(subTabName)=="PM Events":
+            if str(subTabName)=="PM Events" and quote_status.QUOTE_STATUS != 'APPROVED'::
                 sec_rel_sub_bnr += str(add_button)
-            elif str(subTabName)=="Spare Parts" and str(TreeParentParam)=="Complementary Products":
+            elif str(subTabName)=="Spare Parts" and str(TreeParentParam)=="Complementary Products" and quote_status.QUOTE_STATUS != 'APPROVED'::
                 if str(multi_buttons) != "":
                     for btn in multi_buttons:
                         sec_rel_sub_bnr += (btn)
