704323bad70a8a33bce627cb42d4fd9a11824e1d
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-12-01 18:23:20 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -3116,7 +3116,7 @@ class SYLDRTLIST:
 								new_dict[value123] = ('<abbr id ="' + key_value + '" title="' + value1234 + '">' + imgValue + "</abbr>")
 							else:
 								Trace.Write('At line 2832-- second else')
-								new_dict[value123] = ('<abbr id ="' + key_value + '" title="' + value1234 + '">' + value1234 + "</abbr>")  
+								new_dict[value123] = ('<abbr id ="' + str(key_value) + '" title="' + str(value1234) + '">' + str(value1234) + "</abbr>")  
 								#new_dict[value123] = value1234                           
 						## addon product hyperlink starts
 						if str(RECORD_ID) == "SYOBJR-98859" and value123 == 'ADNPRD_ID':
