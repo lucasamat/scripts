3dd60aadea93cdf570e6881a3dadbde05e742e2c
Namrata Sivakumar <namrata.sivakumar@bostonharborconsulting.com>
2021-12-02 20:18:24 +0530

    Update CQGNRTDOCS.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQGNRTDOCS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQGNRTDOCS.py
@@ -34,89 +34,71 @@ try:
 	quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")
 except:
 	quote_revision_record_id = ""
-
-quoteid = SqlHelper.GetFirst("SELECT QUOTE_ID, QUOTE_NAME,C4C_QUOTE_ID FROM SAQTMT(NOLOCK) WHERE MASTER_TABLE_QUOTE_RECORD_ID =  '"+str(recid)+"' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id) + "'")
-
-
-audit_fields = SqlHelper.GetFirst("SELECT USERS.USERNAME,SAQDOC.CpqTableEntryDateModified,SAQDOC.CPQTABLEENTRYDATEADDED,SAQDOC.CPQTABLEENTRYADDEDBY,SAQDOC.CpqTableEntryModifiedBy from USERS inner join SAQDOC(NOLOCK)  on SAQDOC.CpqTableEntryModifiedBy = USERS.ID  ")
-
-Log.Info('16-------spare quote---'+str(recid))
-extline_pri = 0.00
-# QuoteproductTotals = Quote.QuoteTables["SAQITM"]
-
-# for i in QuoteproductTotals.Rows:
-# 	Log.Info('38--extline_pri---SAQITM---'+str(extline_pri))
-# 	extline_pri += float(i['EXTENDED_UNIT_PRICE'])
-# 	Quote.SetGlobal('SubtotalLineItems', str(extline_pri))
-Quote.GetCustomField('SubtotalLineItems').Content = str(extline_pri)
-Log.Info('52--extline_pri------'+str(extline_pri))
+try:
+	quoteid = SqlHelper.GetFirst("SELECT QUOTE_ID, QUOTE_NAME,C4C_QUOTE_ID FROM SAQTMT(NOLOCK) WHERE MASTER_TABLE_QUOTE_RECORD_ID =  '"+str(recid)+"' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id) + "'")
 
 
+	audit_fields = SqlHelper.GetFirst("SELECT USERS.USERNAME,SAQDOC.CpqTableEntryDateModified,SAQDOC.CPQTABLEENTRYDATEADDED,SAQDOC.CPQTABLEENTRYADDEDBY,SAQDOC.CpqTableEntryModifiedBy from USERS inner join SAQDOC(NOLOCK)  on SAQDOC.CpqTableEntryModifiedBy = USERS.ID  ")
 
-extitm_price = extd_pr =  taxtotal = 0.00
-'''QuoteproductTotalsTM = Quote.QuoteTables["SAQITM"]
-#Log.Info('50----QuoteproductTotalsTM----'+str(QuoteproductTotalsTM.Rows.Count))
-for i in QuoteproductTotalsTM.Rows:
-	Log.Info('16---spare quote-SAQITM----UNIT_PRICE entry--'+i['EXTENDED_PRICE'])
-	extd_pr += float(i['EXTENDED_UNIT_PRICE'])
-	extitm_price += float(i['EXTENDED_UNIT_PRICE'] +(i['TAX']))
-	Quote.SetGlobal('SubitemTools', str(extitm_price))
-	Quote.SetGlobal('SubitemextTools', str(extd_pr))
-Quote.GetCustomField('SubtotalTools').Content = str(extitm_price)
-#Log.Info('64--vextitm_price-----'+str(extitm_price))'''
-# getdynamicrcords = SqlHelper.GetList("Select EXTENDED_UNIT_PRICE,TAX from QT__SAQITM where QUOTE_RECORD_ID = '"+str(recid)+"' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id) + "'")
-# for i in getdynamicrcords:
-# 	Log.Info('16---spare quote-SAQITM----UNIT_PRICE entry--')
-# 	extd_pr += float(i.EXTENDED_UNIT_PRICE)
-# 	extitm_price += float(i.EXTENDED_UNIT_PRICE)
-# 	taxtotal += float(i.TAX)
-# 	Quote.SetGlobal('SubitemTools', str(extitm_price))
-# 	Quote.SetGlobal('SubitemextTools', str(extd_pr))
-# 	Quote.SetGlobal('taxtotal', str(taxtotal))
-Quote.GetCustomField('SubtotalTools').Content = str(extitm_price)
+	Log.Info('16-------spare quote---'+str(recid))
+	extline_pri = 0.00
+	# QuoteproductTotals = Quote.QuoteTables["SAQITM"]
 
-
-exts_price = 0.00
-QuoteproductTotalsco = Quote.QuoteTables["SAQICO"]
-
-for i in QuoteproductTotalsco.Rows:
-	Log.Info('SAQICO---'+str(i['EXTENDED_PRICE']))
-	#exts_price += float(i['EXTENDED_PRICE'])
-	exts_price += float(i['EXTENDED_PRICE'] +(i['TAX']))
-	Quote.SetGlobal('SubtotalTools', str(exts_price))
-Quote.GetCustomField('SubtotalTools').Content = str(exts_price)
+	# for i in QuoteproductTotals.Rows:
+	# 	Log.Info('38--extline_pri---SAQITM---'+str(extline_pri))
+	# 	extline_pri += float(i['EXTENDED_UNIT_PRICE'])
+	# 	Quote.SetGlobal('SubtotalLineItems', str(extline_pri))
+	Quote.GetCustomField('SubtotalLineItems').Content = str(extline_pri)
+	Log.Info('52--extline_pri------'+str(extline_pri))
 
 
 
-extt_price = 0
-fptotal =  0
-'''QuoteproductTotalsfp = Quote.QuoteTables["SAQIFP"]
-Log.Info('50------QuoteproductTotalsfp----'+str(QuoteproductTotalsfp.Rows.Count))
+	extitm_price = extd_pr =  taxtotal = 0.00
 
-for i in QuoteproductTotalsfp.Rows:'''
-getdynamicrcords = SqlHelper.GetList("Select * from QT__SAQIFP where QUOTE_RECORD_ID = '"+str(recid)+"' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id) + "'")
-for val in getdynamicrcords:
-	Log.Info('16---spare quote--UNIT_PRICE entry--')
-	extt_price += float(val.UNIT_PRICE)
-	fptotal += float(val.EXTENDED_UNIT_PRICE)
-	Quote.SetGlobal('SubtotalspareTools', str(extt_price))
-	Log.Info('16--75---extt_price--UNIT_PRICE--SAQIFP--SUBTOTSL---'+str(extt_price))
-	Quote.SetGlobal('SubtotalTools', str(extt_price))
-	Quote.SetGlobal('totalspareTools', str(fptotal))
-Quote.GetCustomField('SubtotalTools').Content = str(extt_price)
-Log.Info('16---spare quote--extt_price--UNIT_PRICE--SAQIFP--SUBTOTSL---'+str(extt_price))
-Log.Info('16---spare quote---TOTAL_PRICE--SAQIFP--total--'+str(fptotal))
 
-ext_price = 0.00
-QuoteproductTotals = Quote.QuoteTables["SAQICO"]
-Log.Info('150------QuoteproductTotalsfp----'+str(QuoteproductTotals.Rows.Count))
-for i in QuoteproductTotals.Rows:
-	Log.Info('93---'+str(ext_price))
-	#ext_price += float(i['SUBTOTAL'])
-	#ext_price = round(float(ext_price) + float(i['SUBTOTAL']), 2)
-	Quote.GetCustomField('SubtotalTools').Content = str(ext_price)
-Log.Info(str(ext_price))
-tableInfo = SqlHelper.GetTable("SAQDOC")
+	Quote.GetCustomField('SubtotalTools').Content = str(extitm_price)
+
+
+	exts_price = 0.00
+	QuoteproductTotalsco = Quote.QuoteTables["SAQICO"]
+
+	for i in QuoteproductTotalsco.Rows:
+		Log.Info('SAQICO---'+str(i['EXTENDED_PRICE']))
+		#exts_price += float(i['EXTENDED_PRICE'])
+		exts_price += float(i['EXTENDED_PRICE'] +(i['TAX']))
+		Quote.SetGlobal('SubtotalTools', str(exts_price))
+	Quote.GetCustomField('SubtotalTools').Content = str(exts_price)
+
+
+
+	extt_price = 0
+	fptotal =  0
+
+	getdynamicrcords = SqlHelper.GetList("Select * from QT__SAQIFP where QUOTE_RECORD_ID = '"+str(recid)+"' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id) + "'")
+	for val in getdynamicrcords:
+		Log.Info('16---spare quote--UNIT_PRICE entry--')
+		extt_price += float(val.UNIT_PRICE)
+		fptotal += float(val.EXTENDED_UNIT_PRICE)
+		Quote.SetGlobal('SubtotalspareTools', str(extt_price))
+		Log.Info('16--75---extt_price--UNIT_PRICE--SAQIFP--SUBTOTSL---'+str(extt_price))
+		Quote.SetGlobal('SubtotalTools', str(extt_price))
+		Quote.SetGlobal('totalspareTools', str(fptotal))
+	Quote.GetCustomField('SubtotalTools').Content = str(extt_price)
+	Log.Info('16---spare quote--extt_price--UNIT_PRICE--SAQIFP--SUBTOTSL---'+str(extt_price))
+	Log.Info('16---spare quote---TOTAL_PRICE--SAQIFP--total--'+str(fptotal))
+
+	ext_price = 0.00
+	QuoteproductTotals = Quote.QuoteTables["SAQICO"]
+	Log.Info('150------QuoteproductTotalsfp----'+str(QuoteproductTotals.Rows.Count))
+	for i in QuoteproductTotals.Rows:
+		Log.Info('93---'+str(ext_price))
+		#ext_price += float(i['SUBTOTAL'])
+		#ext_price = round(float(ext_price) + float(i['SUBTOTAL']), 2)
+		Quote.GetCustomField('SubtotalTools').Content = str(ext_price)
+	Log.Info(str(ext_price))
+	tableInfo = SqlHelper.GetTable("SAQDOC")
+except:
+	pass
 try:
 	recid = Product.Attr('QSTN_SYSEFL_QT_00001').GetValue()
 except:
@@ -288,10 +270,11 @@ except:
 	quote_revision_record_id = ""
 try:
 	recid = Product.Attr('QSTN_SYSEFL_QT_00001').GetValue()
+	quoteid = SqlHelper.GetFirst("SELECT QUOTE_TYPE FROM SAQTMT(NOLOCK) WHERE MASTER_TABLE_QUOTE_RECORD_ID =  '"+str(recid)+"' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id) + "'")
 except:
 	pass
 
-quoteid = SqlHelper.GetFirst("SELECT QUOTE_TYPE FROM SAQTMT(NOLOCK) WHERE MASTER_TABLE_QUOTE_RECORD_ID =  '"+str(recid)+"' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id) + "'")
+
 
 
 if attach == "YES":
