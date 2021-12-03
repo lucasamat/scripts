71a276d9b526aff74f7b36d914ac42b3d945e599
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-12-03 20:48:09 +0530

    Update CQEXPIMAIL.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/CQEXPIMAIL.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/CQEXPIMAIL.py
@@ -13,17 +13,7 @@ class qt_expiration_mail_trigger:
     def __init__(self, Quote):
         self.quote = Quote
 
-    
-
-    # def _get_custom_fields_detail(self):
-    #     return {
-    #         'QuoteExpirationDate':self.quote.GetCustomField('QuoteExpirationDate').Content,
-    #         'quote_expiration_mail':self.quote.GetCustomField('quote_expiration_mail').Content
-    #     }
-
     def mailtrigger(self,expired_quotes):
-        # custom_fields_detail = self._get_custom_fields_detail()
-        # Trace.Write("custom_fields_detail =====>>>>>> " + str(custom_fields_detail))
         Trace.Write("Mail Sending Function"+str(expired_quotes))
         for quotes in expired_quotes:
             getting_quotes = Sql.GetList("SELECT OWNER_NAME,QUOTE_ID,CONTRACT_VALID_TO,QUOTE_EXPIRE_DATE FROM SAQTMT (NOLOCK) WHERE QUOTE_ID = '"+str(quotes)+"'")
@@ -57,21 +47,10 @@ class qt_expiration_mail_trigger:
                     msg = MailMessage(fromEmail, toEmail)
                     msg.Subject = Subject
                     msg.IsBodyHtml = True
-                    msg.Body = mailBody
-                    # copyEmail1 = MailAddress("mayura.priya@bostonharborconsulting.com")
-                    # msg.CC.Add(copyEmail1)
-                    #copyEmail2 = MailAddress("wasim.abdul@bostonharborconsulting.com")
-                    #msg.CC.Add(copyEmail2)
-                    # copyEmail2 = MailAddress("sathyabama.akhala@bostonharborconsulting.com")
-                    # msg.CC.Add(copyEmail2)
-                    #copyEmail4 = MailAddress("aditya.shivkumar@bostonharborconsulting.com")
-                    #msg.CC.Add(copyEmail4)
-                    #copyEmail5 = MailAddress("namrata.sivakumar@bostonharborconsulting.com")
-                    #msg.CC.Add(copyEmail5)    
+                    msg.Body = mailBody  
                     mailClient.Send(msg)
                     Trace.Write("Mail Sent Successfully")
                     #Quote.GetCustomField("quote_expiration_mail").Content = "FALSE"
-                    # quote_expiration_mail = "FALSE"
                 except Exception:
                     self.exceptMessage = "SYCONUPDAL : mailtrigger : EXCEPTION : UNABLE TO TRIGGER E-EMAIL : EXCEPTION E : "
                     Trace.Write(self.exceptMessage)
@@ -79,23 +58,6 @@ class qt_expiration_mail_trigger:
 
 
 expiration_obj = qt_expiration_mail_trigger(Quote)
-
-# now = datetime.datetime.now()
-# current_date_obj = str(now).split(" ")[0].strip()
-# today_date = datetime.datetime.strptime(str(current_date_obj),"%Y-%m-%d")
-# today_date_string = str(today_date).split(" ")[0].strip()
-# Trace.Write("Today_date "+str(today_date_string))
-# today_date_string = "2023-03-07"
-# quote_expiration_date = Quote.GetCustomField('QuoteExpirationDate').Content
-# quote_expiration_date_obj = datetime.datetime.strptime(str(quote_expiration_date),"%Y-%m-%d")
-# mail_trigger_date = quote_expiration_date_obj - timedelta(days=14)
-# mail_trigger_date = str(mail_trigger_date).split(" ")[0].strip()
-
-
-# quote_expiration_date = Quote.GetCustomField('QuoteExpirationDate').Content
-# quote_expiration_date_obj = datetime.datetime.strptime(str(quote_expiration_date),"%Y-%m-%d").strftime('%m-%d-%Y')
-# quote_expiration_date = str(quote_expiration_date_obj).split(" ")[0].strip()
-# quote_expiration_date = quote_expiration_date.replace("-","/")
 
 now = datetime.datetime.now()
 current_date_obj = str(now).split(" ")[0].strip()
@@ -115,7 +77,6 @@ expired_quotes = []
 if expired_quotes_query is not None:
     for quotes in expired_quotes_query:   
         expire_date = str(quotes.QUOTE_EXPIRE_DATE).split(" ")[0]
-        # Trace.Write("expire_date_CHK_J "+str(target_mail_date)+" - "+str(expire_date) + " - " +  str(quotes.QUOTE_EXPIRE_DATE))
         if str(target_mail_date) == str(expire_date):
             expired_quotes.append(quotes.QUOTE_ID)
         if expired_quotes is not None:
