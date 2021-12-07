6307d44ec8b57232832e44c3b01aa473f47c60cd
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-12-07 17:55:06 +0530

    Update SYUADNWPOP.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
@@ -157,7 +157,7 @@ def POPUPLISTVALUEADDNEW(
 		)
 
 	if TABLEID != "ADDNEW__SYOBJR_93123_SYOBJ_00267" or TABLEID != "ADDNEW__SYOBJR_98788_SYOBJ_00907":
-		if TABLEID != "ADDNEW__SYOBJR_95800_SYOBJ_00458":
+		if TABLEID != "ADDNEW__SYOBJR_95800_SYOBJ_00458" or TABLEID != "ADDNEW__SYOBJR_00643_SYOBJ_0026410":
 			Trace.Write("cmng to tableid===")
 			rec_field = Sql.GetFirst(
 				"SELECT API_NAME,API_FIELD_NAME FROM SYSEFL (NOLOCK) WHERE SAPCPQ_ATTRIBUTE_NAME='" + str(RECORD_FEILD) + "'"
@@ -165,6 +165,18 @@ def POPUPLISTVALUEADDNEW(
 			if rec_field is not None and rec_field != "":
 				record_field = str(eval("rec_field.API_FIELD_NAME"))
 				record_value = str(eval("rec_field.API_NAME"))
+			else:
+				record_field = ""
+				record_value = ""
+		elif TABLEID == "ADDNEW__SYOBJR_00643_SYOBJ_0026410":
+			rec_field = Sql.GetFirst(
+				"SELECT API_NAME,API_FIELD_NAME FROM SYSEFL (NOLOCK) WHERE SAPCPQ_ATTRIBUTE_NAME='" + str(popup_table_id) + "'"
+			)
+			if rec_field is not None and rec_field != "":
+				record_field = str(eval("rec_field.API_FIELD_NAME"))
+				record_value = str(eval("rec_field.API_NAME"))
+				Trace.Write("record_field==="+str(record_field))
+				Trace.Write("record_value==="+str(record_value))
 			else:
 				record_field = ""
 				record_value = ""
