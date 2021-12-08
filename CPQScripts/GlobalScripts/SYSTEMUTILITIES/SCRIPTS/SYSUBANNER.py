a71e3a31c0b28c8ee770278e736e6bcf6bc4f6ba
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-12-08 13:02:21 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -1236,7 +1236,7 @@ def Related_Sub_Banner(
                         # FourthValue = get_val.FABLOCATION_ID
                         # FifthLable = "Equipment"
                         # FifthValue = "ALL" 
-                if (TopSuperParentParam == "Product Offerings") and subTabName == "Parts List":
+                if (TopSuperParentParam == "Product Offerings") and subTabName == "Exclusions":
                     try:
                         getService = Sql.GetFirst("select SERVICE_DESCRIPTION from SAQTSV where SERVICE_ID = '"+str(TreeParentParam)+"'")
                         desc = getService.SERVICE_DESCRIPTION
@@ -2975,7 +2975,7 @@ def Related_Sub_Banner(
         # elif str(TreeParentParam) == "Comprehensive Services" and TabName == "Quotes" and subTabName == "Parts List":
         #     Trace.Write("@@@2473")
         #     sec_rel_sub_bnr += ('<button id="delete_parts" onclick="bulk_del_yes()"  class="btnconfig" disabled = "">DELETE</button>') 
-        elif  (str(TreeSuperParentParam).upper() == "PRODUCT OFFERINGS")  and TabName == "Quotes" and str(subTabName)!="Parts List" and str(subTabName)!= "New Parts Only":     
+        elif  (str(TreeSuperParentParam).upper() == "PRODUCT OFFERINGS")  and TabName == "Quotes" and str(subTabName)!="Exclusions" and str(subTabName)!= "New Parts Only":     
             sec_rel_sub_bnr += ('<button id="fabcostlocate_save" onclick="fabcostlocatesave(this)" style="display: none;" class="btnconfig hidebtn">SAVE</button><button id="fabcostlocate_cancel" onclick="fabcostlocatecancel(this)" style="display: none;" class="btnconfig hidebtn">CANCEL</button>'  )    
             Trace.Write('### _ Multi_buttons'+str(multi_buttons))
             if str(subTabName)=="PM Events" and revision_status.REVISION_STATUS != 'APPROVED':
@@ -2986,15 +2986,15 @@ def Related_Sub_Banner(
                         sec_rel_sub_bnr += (btn)
                 else:
                     sec_rel_sub_bnr += str(add_button)
-        elif  (str(TreeSuperParentParam).upper() == "COMPREHENSIVE SERVICES")  and TabName == "Quotes" and str(subTabName)!="Parts List" :
+        elif  (str(TreeSuperParentParam).upper() == "COMPREHENSIVE SERVICES")  and TabName == "Quotes" and str(subTabName)!="Exclusions" :
             sec_rel_sub_bnr += ('<button id="fabcostlocate_save" onclick="fabcostlocatesave(this)" style="display: none;" class="btnconfig">SAVE</button><button id="fabcostlocate_cancel" onclick="fabcostlocatecancel(this)" style="display: none;" class="btnconfig">CANCEL</button>'  )
             if str(subTabName)=="PM Events" and revision_status.REVISION_STATUS != 'APPROVED':
                 sec_rel_sub_bnr += str(add_button)
-        elif  (str(TreeTopSuperParentParam).upper() == "COMPREHENSIVE SERVICES")  and TabName == "Quotes" and (subTabName)!="Parts List":
+        elif  (str(TreeTopSuperParentParam).upper() == "COMPREHENSIVE SERVICES")  and TabName == "Quotes" and (subTabName)!="Exclusions":
             if str(subTabName)=="PM Events":
                 sec_rel_sub_bnr += ('<button id="ADDNEW__SYOBJR_00011_SYOBJ_00974" onclick="PM_FrequencyInlineEdit()" class="btnconfig" >INLINE EDIT</button>')
             else:
-                if str(subTabName)!="Parts List":
+                if str(subTabName)!="Exclusions":
                     sec_rel_sub_bnr += ('<button id="fabcostlocate_save" onclick="fabcostlocatesave(this)" style="display: none;" class="btnconfig">SAVE</button><button id="fabcostlocate_cancel" onclick="fabcostlocatecancel(this)" style="display: none;" class="btnconfig">CANCEL</button>'  )
         # elif str(TreeParam) == "Quote Items" and TabName == "Quotes" and subTabName == "Summary":
         #     sec_rel_sub_bnr =''
