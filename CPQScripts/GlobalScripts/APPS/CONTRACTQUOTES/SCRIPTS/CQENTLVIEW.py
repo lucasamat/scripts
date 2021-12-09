c0fd29951ba7263fe7c0ea531a134f9b6dcae948
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-12-09 17:40:04 +0530

    Update CQENTLVIEW.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
@@ -806,7 +806,7 @@ class EntitlementView():
 			for val in GetXMLsecField:
 				inserted_value_dict[val.ENTITLEMENT_ID] = val.ENTITLEMENT_VALUE_CODE
 			inserted_value_list = [val.ENTITLEMENT_ID for val in GetXMLsecField if GetXMLsecField]
-			Trace.Write('766---ObjectName-----'+str(overallattributeslist_visible))
+			Trace.Write(str(ProductPartnumber)+'----766---ObjectName-----'+str(overallattributeslist_visible))
 			if ProductPartnumber == 'Z0046':
 				inserted_value_list.append('AGS_Z0046_PQB_APPLCN')
 			Trace.Write(str(inserted_value_list)+'----802-----overallattributeslist_visible----'+str(overallattributeslist_visible))
