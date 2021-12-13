45c4a5d25698c89ffae88c42871535061cf74daa
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2021-12-13 18:50:06 +0530

    Update CQWBFRMC4C.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQWBFRMC4C.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQWBFRMC4C.py
@@ -13,21 +13,20 @@ import sys
 if 'Param' in globals():
     Log.Info("CQWBFRMC4C called.......")	
     if hasattr(Param, "CPQ_Columns"):
-        Log.Info("CQWBFRMC4C called.......inside if")
-        for values in Param.CPQ_Columns:
-            Log.Info("CQWBFRMC4C called....... inside for")
-            Key = str(values.Key)
-            Log.Info("CQWBFRMC4C called......."+str(values.Key))
-            Log.Info("CQWBFRMC4C called......."+str(values.Value))
-            if str(Key).upper() == "OPPORTUNITY_ID":
-                opportunity_id = str(values.Value)
-            if str(Key).upper() == "OPPORTUNITY_STATUS":
-                opportunity_status = str(values.Value).upper()
-            ##To Fetch the Picklist value based on the value from C4C...
-            opportunity_status_dictionary = {"LOST":"OPPORTUNITY LOST", "CANCELLED":"OPPORTUNITY CANCELLED"}
-            
-            ##To update the revision status in cpq based on the status from c4c value....
-            Sql.RunQuery("UPDATE SAQTRV SET REVISION_STATUS = '{}' FROM SAQTRV JOIN SAOPQT ON SAQTRV.QUOTE_RECORD_ID = SAOPQT.QUOTE_RECORD_ID  WHERE OPPORTUNITY_ID = '{}'".format(opportunity_status_dictionary.get(str(opportunity_status)),opportunity_id))
+        Log.Info("CQWBFRMC4C called.......inside if")    
+         
+        opportunity_data = [str(param_result.Value) for param_result in Param.CPQ_Columns]
+        opportunity_id = opportunity_data[0]
+        opportunity_status = opportunity_data[-1]
+        
+        Log.Info("opportunity_id--"+str(opportunity_id))
+        Log.Info("opportunity_status--"+str(opportunity_status))
+        
+        ##To Fetch the Picklist value based on the value from C4C...
+        opportunity_status_dictionary = {"LOST":"OPPORTUNITY LOST", "CANCELLED":"OPPORTUNITY CANCELLED"}
+        
+        ##To update the revision status in cpq based on the status from c4c value....
+        Sql.RunQuery("UPDATE SAQTRV SET REVISION_STATUS = '{}' FROM SAQTRV JOIN SAOPQT ON SAQTRV.QUOTE_RECORD_ID = SAOPQT.QUOTE_RECORD_ID  WHERE OPPORTUNITY_ID = '{}'".format(opportunity_status_dictionary.get(str(opportunity_status)),opportunity_id))
             
             
             
