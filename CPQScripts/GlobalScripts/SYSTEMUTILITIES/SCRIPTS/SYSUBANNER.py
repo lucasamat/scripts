d02896c106c8036d45673f37e55f13ae158b6b2b
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2021-12-13 16:42:57 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -3177,7 +3177,7 @@ if CurrentTab == 'Quotes':
     if TreeParentParam == "Quote Items" and (subTabName == "Spare Part Details" or subTabName == "Entitlements") and getQuotetype == "ZWK1 - SPARES":
         ObjName = "SAQIFP"
     if (TreeParam.startswith("Sending") or TreeParam.startswith("Receiving")):
-        ObjectName = "SAQSRA"	    
+        ObjectName = "SAQSRA"	     
     # if crnt_Qry is not None:
     # 	if str(ObjName) != "SAQRIB":
     # 		CurrentRecordId = str(crnt_Qry.SAPCPQ_ATTRIBUTE_NAME)
