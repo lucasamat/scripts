4de5ab971a8ba6212ac2c42e671490d355211cc0
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-12-13 14:51:32 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -8409,7 +8409,14 @@ class SYLDRTLIST:
 					else:
 						Action_str += '<li><a class="dropdown-item" href="#" onclick="Commontree_edit_RL(this)">EDIT</a></li>'    
 				if str(Action_permission.get("Delete")).upper() == "TRUE":
-					Action_str += '<li><a class="dropdown-item" data-target="#cont_viewModalDelete" data-toggle="modal" onclick="cont_delete(this)" href="#">DELETE</a></li>'
+					if ObjectName == "SAQDLT":
+						Action_str += (
+									'<li><a class="dropdown-item" href="#" id="deletebtn" onclick="'
+									+ str(onclick)
+									+ '" data-target="#cont_CommonModalDelete" data-toggle="modal">DELETE</a></li>'
+								)
+					else:
+						Action_str += '<li><a class="dropdown-item" data-target="#cont_viewModalDelete" data-toggle="modal" onclick="cont_delete(this)" href="#">DELETE</a></li>'
 				Action_str += "</ul></div></div>"
 				new_dict = {}
 				seg_pric = {}
