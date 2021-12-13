b9ceed9045f252e82303328583c2ee319857b8f0
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2021-12-13 18:07:31 +0530

    Create CQSPLITQTE.py
    A055S000P01-13545

---
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSPLITQTE.py
@@ -0,0 +1,17 @@
+# =========================================================================================================================================
+#   __script_name : CQSPLITQTE.PY
+#   __script_description : THIS SCRIPT IS USED TO SPLIT THE ITEMS BY PRODUCT OFFERINGS
+#   __primary_author__ : WASIM.ABDUL
+#   __create_date :12-13-2021
+#   © BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
+# ==========================================================================================================================================
+import Webcom.Configurator.Scripting.Test.TestProduct
+import clr
+import System.Net
+import sys
+import re
+import datetime
+from System.Net import CookieContainer, NetworkCredential, Mail
+from System.Net.Mail import SmtpClient, MailAddress, Attachment, MailMessage
+from SYDATABASE import SQL
+Sql = SQL()
