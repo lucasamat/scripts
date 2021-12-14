ea1ae64a6a45036e1d3369f7cf92f9803e3ee624
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-12-14 18:43:03 +0530

    Update SYCTLKPPUP.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYCTLKPPUP.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYCTLKPPUP.py
@@ -462,7 +462,7 @@ def GSCONTLOOKUPPOPUP(
                 elif str(TABLEID) == "SYOBJR":
                     VAL_Str = "SELECT top 10 " + str(API_NAME_str) + " FROM " + str(TABLEID)
                     count_query = SqlHelper.GetList("SELECT COUNT(*) as cnt FROM " + str(TABLEID))
-                elif str(tab_Name) == "Quote"  and str(TABLEID) == "SYPFTY" and str(TreeParentParam) == "Sales Team":
+                elif str(tab_Name) == "Quotes"  and str(TABLEID) == "SYPFTY" and str(TreeParentParam) == "Sales Team":
                     Trace.Write("TABLEID====>>>"+str(TABLEID))
                     VAL_Str = (" SELECT top 1000 PARTNERFUNCTION_RECORD_ID,C4C_PARTNER_FUNCTION,CRM_PARTNERFUNCTION FROM SYPFTY WHERE C4C_PARTNER_FUNCTION NOT IN(SELECT C4C_PARTNERFUNCTION_ID FROM SAQDLT WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}')".format(ContractRecordId,quote_revision_record_id))
                     VAL_Obj = Sql.GetList(VAL_Str)    
