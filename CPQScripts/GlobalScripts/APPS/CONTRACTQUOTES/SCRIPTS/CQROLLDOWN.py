5136ed0ca95247911320f736d56e7c40a5a4c4bf
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-12-15 16:12:07 +0530

    Update CQROLLDOWN.py
    removed for testing

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQROLLDOWN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQROLLDOWN.py
@@ -240,26 +240,7 @@ def CoveredObjEntitlement():
 			Quote = QuoteHelper.Edit(quote_obj.QUOTE_ID)	
 	except Exception:
 		Log.Info("Exception in Quote Edit") 
-	# try:
-	# 	Log.Info("Called CQINSQTITM ==>cqroll "+str(Qt_rec_id))
-	# 	# data = ScriptExecutor.ExecuteGlobal("CQINSQTITM",{"ContractQuoteRecordId":Qt_rec_id, "ContractQuoteRevisionRecordId":rev_rec_id, "ServiceId":TreeParam, "ActionType":'INSERT_LINE_ITEMS'})
-	# 	#Log.Info("Called CQINSQTITM ==>cqroll enddddd "+str(Qt_rec_id))
-	# except Exception:
-	# 	Log.Info("Exception in Quote Item insert") 
 	
-	# if ancillary_dict:
-	# 	Log.Info("ancillary_dict--qi-"+str(ancillary_dict)+'--'+str(Qt_rec_id)) 
-	# 	for anc_key,anc_val in ancillary_dict.items():
-	# 		#if anc_val == 'INSERT':
-	# 		try:
-	# 			#temp_val = "SERVICE_ID = '{}'".format(anc_key)
-	# 			#where = re.sub(r'SERVICE_ID\s*\=\s*\'[^>]*?\'', temp_val, where )
-	# 			#where = where.replace('Z0091','{}'.format(anc_key))
-	# 			#Log.Info('where--CQINSQTITM-'+str(where)+str(anc_key))
-	# 			data = ScriptExecutor.ExecuteGlobal("CQINSQTITM",{"ContractQuoteRecordId":Qt_rec_id, "ContractQuoteRevisionRecordId":rev_rec_id, "ServiceId":anc_key, "ActionType":'INSERT_LINE_ITEMS'})
-	# 			#data = ScriptExecutor.ExecuteGlobal("CQINSQTITM",{"WhereString":where, "ActionType":'UPDATE_LINE_ITEMS'})
-	# 		except Exception:
-	# 			Log.Info("Exception in Quote Item insert1111")
 	sendEmail(level)
 
 def CoveredObjItemEntitlement():
