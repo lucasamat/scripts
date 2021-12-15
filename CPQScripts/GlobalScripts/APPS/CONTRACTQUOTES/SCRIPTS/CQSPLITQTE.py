681bcf5b747861cafcfd6546f9d4cd57f25c05ba
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2021-12-15 10:46:04 +0530

    Update CQSPLITQTE.py
    A055S000P01-13545

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSPLITQTE.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSPLITQTE.py
@@ -15,6 +15,51 @@ from System.Net import CookieContainer, NetworkCredential, Mail
 from System.Net.Mail import SmtpClient, MailAddress, Attachment, MailMessage
 from SYDATABASE import SQL
 Sql = SQL()
+
+TestProduct = Webcom.Configurator.Scripting.Test.TestProduct() or "Sales"
+try:
+	contract_quote_record_id = Quote.QuoteId
+except:
+	contract_quote_record_id = ''
+
+try:
+	quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")
+except:
+	quote_revision_record_id = ""
+
+try:
+	current_prod = Product.Name
+	
+except:
+	current_prod = "Sales"
+try:
+	TabName = TestProduct.CurrentTab
+except:
+	TabName = "Quotes"
+
+contract_quote_rec_id = Quote.GetGlobal("contract_quote_record_id")
+quote_revision_rec_id = Quote.GetGlobal("quote_revision_record_id")
 user_id = str(User.Id)
-user_name = str(User.UserName)		
-datetime_value = datetime.datetime.now()
+user_name = str(User.UserName) 
+
+def splitserviceinsert()
+    material_obj = Sql.GetFirst("SELECT MATERIAL_RECORD_ID,SAP_DESCRIPTION,MATERIALCONFIG_TYPE FROM MAMTRL WHERE SAP_PART_NUMBER = 'Z0105'")
+		get_existing_record = Sql.GetFirst("SELECT * FROM SAQIAC WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' AND ENTITLEMENT_NAME ='Split Quote' AND ENTITLEMENT_DISPLAY_VALUE = 'Yes'".format())
+		if get_existing_record:
+			description = material_obj.SAP_DESCRIPTION
+			material_record_id = material_obj.MATERIAL_RECORD_ID
+
+			Sql.RunQuery("""INSERT SAQTSV (QTEREV_RECORD_ID,QTEREV_ID,QUOTE_ID, QUOTE_NAME,UOM_ID,UOM_RECORD_ID, QUOTE_RECORD_ID, SERVICE_DESCRIPTION, SERVICE_ID, PAR_SERVICE_ID,PAR_SERVICE_DESCRIPTION,PAR_SERVICE_RECORD_ID,SERVICE_RECORD_ID, SERVICE_TYPE, CONTRACT_VALID_FROM, CONTRACT_VALID_TO, SALESORG_ID, SALESORG_NAME, SALESORG_RECORD_ID, QUOTE_SERVICE_RECORD_ID, CPQTABLEENTRYADDEDBY, CPQTABLEENTRYDATEADDED, CpqTableEntryModifiedBy, CpqTableEntryDateModified)
+							SELECT A.*, CONVERT(VARCHAR(4000),NEWID()) as QUOTE_SERVICE_RECORD_ID, '{UserName}' as CPQTABLEENTRYADDEDBY, GETDATE() as CPQTABLEENTRYDATEADDED, {UserId} as CpqTableEntryModifiedBy, GETDATE() as CpqTableEntryDateModified FROM (
+							SELECT DISTINCT QTEREV_RECORD_ID, QTEREV_ID,QUOTE_ID, QUOTE_NAME,UOM_ID,UOM_RECORD_ID, QUOTE_RECORD_ID, '{description}' AS SERVICE_DESCRIPTION, '{splitservice_object}' AS SERVICE_ID,SERVICE_ID as PAR_SERVICE_ID,SERVICE_DESCRIPTION AS PAR_SERVICE_DESCRIPTION,QUOTE_SERVICE_RECORD_ID as PAR_SERVICE_RECORD_ID, '{material_record_id}' AS SERVICE_RECORD_ID, '' AS SERVICE_TYPE, CONTRACT_VALID_FROM, CONTRACT_VALID_TO, SALESORG_ID, SALESORG_NAME,SALESORG_RECORD_ID FROM SAQTSV (NOLOCK)
+							WHERE SERVICE_ID = '{service_id}' AND QUOTE_RECORD_ID = '{contract_quote_rec_id}' AND QTEREV_RECORD_ID = '{quote_revision_rec_id}' 
+							) A""".format(description=description, service_id = get_existing_record.SERVICE_ID, material_record_id = material_record_id,contract_quote_rec_id = contract_quote_rec_id , quote_revision_rec_id = quote_revision_rec_id ,UserName = user_name, UserId = user_id, splitservice_object = 'Z0105' ))
+        
+        ###split the items with new insert and updation:
+
+
+
+
+
+
+splitserviceinsert()
