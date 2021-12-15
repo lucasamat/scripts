b5a0c2387c19d631d221afaf3e6f4da919e87bb2
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-12-15 11:13:50 +0530

    Update SYCTLKPPUP.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYCTLKPPUP.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYCTLKPPUP.py
@@ -818,7 +818,7 @@ def GSCONTLOOKUPPOPUPFILTER(
                     Trace.Write("TABLEID====>>>"+str(TABLEID))
                     ContractRecordId = str(Quote.GetGlobal("contract_quote_record_id"))
                     quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")
-                    VAL_Str = (" SELECT top 1000 EMPLOYEE_RECORD_ID,EMPLOYEE_ID,EMPLOYEE_NAME,EMAIL FROM SAEMPL WHERE EMPLOYEE_ID NOT IN(SELECT MEMBER_ID FROM SAQDLT WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}')".format(ContractRecordId,quote_revision_record_id))
+                    VAL_Str = (" SELECT EMPLOYEE_RECORD_ID,EMPLOYEE_ID,EMPLOYEE_NAME,EMAIL FROM SAEMPL WHERE EMPLOYEE_ID NOT IN(SELECT MEMBER_ID FROM SAQDLT WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}')".format(ContractRecordId,quote_revision_record_id))
                     VAL_Obj = Sql.GetList(VAL_Str) 
                 elif str(tab_Name) == "Approval Chain" and str(TABLEID) == "SYOBJD" and str(SegmentsClickParam) == "Approval Chain Status Mappings":
                     Header_Obj = Sql.GetFirst("SELECT OBJECT_NAME FROM SYOBJH WHERE LABEL = '{}'".format(MAPPINGSAPPROVALOBJECT))
