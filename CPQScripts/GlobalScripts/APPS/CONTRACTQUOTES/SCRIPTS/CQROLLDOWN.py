d223203cddbb2c824fbcb5a1c6f433bff9b2a3e8
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-12-15 15:28:42 +0530

    Update CQROLLDOWN.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQROLLDOWN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQROLLDOWN.py
@@ -247,19 +247,19 @@ def CoveredObjEntitlement():
 	except Exception:
 		Log.Info("Exception in Quote Item insert") 
 	
-	if ancillary_dict:
-		Log.Info("ancillary_dict--qi-"+str(ancillary_dict)+'--'+str(Qt_rec_id)) 
-		for anc_key,anc_val in ancillary_dict.items():
-			#if anc_val == 'INSERT':
-			try:
-				#temp_val = "SERVICE_ID = '{}'".format(anc_key)
-				#where = re.sub(r'SERVICE_ID\s*\=\s*\'[^>]*?\'', temp_val, where )
-				#where = where.replace('Z0091','{}'.format(anc_key))
-				#Log.Info('where--CQINSQTITM-'+str(where)+str(anc_key))
-				data = ScriptExecutor.ExecuteGlobal("CQINSQTITM",{"ContractQuoteRecordId":Qt_rec_id, "ContractQuoteRevisionRecordId":rev_rec_id, "ServiceId":anc_key, "ActionType":'INSERT_LINE_ITEMS'})
-				#data = ScriptExecutor.ExecuteGlobal("CQINSQTITM",{"WhereString":where, "ActionType":'UPDATE_LINE_ITEMS'})
-			except Exception:
-				Log.Info("Exception in Quote Item insert1111")
+	# if ancillary_dict:
+	# 	Log.Info("ancillary_dict--qi-"+str(ancillary_dict)+'--'+str(Qt_rec_id)) 
+	# 	for anc_key,anc_val in ancillary_dict.items():
+	# 		#if anc_val == 'INSERT':
+	# 		try:
+	# 			#temp_val = "SERVICE_ID = '{}'".format(anc_key)
+	# 			#where = re.sub(r'SERVICE_ID\s*\=\s*\'[^>]*?\'', temp_val, where )
+	# 			#where = where.replace('Z0091','{}'.format(anc_key))
+	# 			#Log.Info('where--CQINSQTITM-'+str(where)+str(anc_key))
+	# 			data = ScriptExecutor.ExecuteGlobal("CQINSQTITM",{"ContractQuoteRecordId":Qt_rec_id, "ContractQuoteRevisionRecordId":rev_rec_id, "ServiceId":anc_key, "ActionType":'INSERT_LINE_ITEMS'})
+	# 			#data = ScriptExecutor.ExecuteGlobal("CQINSQTITM",{"WhereString":where, "ActionType":'UPDATE_LINE_ITEMS'})
+	# 		except Exception:
+	# 			Log.Info("Exception in Quote Item insert1111")
 	sendEmail(level)
 
 def CoveredObjItemEntitlement():
