3ca70a7525fd7f7825403d60fbfe8ac5fd0f95ac
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-12-16 15:28:50 +0530

    Update CQSPBLKUPL.py
    added pencil icon in popup

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSPBLKUPL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSPBLKUPL.py
@@ -22,18 +22,19 @@ if ACTION == "BULKUPLOADDATA":
                         </div>
                     </div>
                     <div class="col-md-12">
-                            <p class="p-0">Please ensure the file you're uploading exactly matches the format of the downloaded parts list.</p>
+                            <p class="pl-0 pr-0 pt-0">Please ensure the file you're uploading exactly matches the format of the downloaded parts list.</p>
                     </div>
-                    <div class="col-md-12 d-flex align-items-center bortop1">
-                        <div class="partno-lbl col-md-4"><p class="pl-0 pr-0 pt-0">File Name</p></div>
+                    <div class="col-md-12 d-flex align-items-center bortop1 p-0">
+                        <div class="partno-lbl col-md-4"><p class="p-0">File Name</p></div>
                         <div class="col-md-1 text-right p-0"><span class="hint_icon_req"><i class="fa fa-info-circle "></i></span></div>
                         <div class="txt-col-sec col-md-4"><input id="uploadsheet" class="light_yellow" style="width: 100%;" disabled></div>
                         <div class="col-md-4">
                             <label for="file-input" class="m-0">
-                                <img src="https://sandbox.webcomcpq.com/mt/APPLIEDMATERIALS_TST/Additionalfiles/document_upload_icon.svg" class="wid-9per">
+                                <img src="https://sandbox.webcomcpq.com/mt/APPLIEDMATERIALS_TST/Additionalfiles/document_upload_icon.svg" class="wid-6per">
                             </label>
                             <input id="file-input" style="display:none" type="file">
                         </div>
+                        <div class="text-right"><a href="#" onclick="editclick_row(this)" class="editclick"><i class="fa fa-pencil" aria-hidden="true"></i></a></div>
                     </div>
                     <div class="modal-footer col-md-12 pb-10px">
                         <button id="bulkaddpopupcancel" class="btn btn-list-cust" data-dismiss="modal" aria-hidden="true">CANCEL</button>
