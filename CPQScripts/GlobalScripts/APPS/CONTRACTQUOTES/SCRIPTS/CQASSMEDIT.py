a0bf42658b9632e6bf7afac6e9fd12d5dff70de6
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2021-12-16 19:17:02 +0530

    Update CQASSMEDIT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQASSMEDIT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQASSMEDIT.py
@@ -189,7 +189,7 @@ def child_ent_request(tableName,where,serviceId):
 							if row.ENTITLEMENT_TYPE in ('Check Box','CheckBox'):
 								Trace.Write("auto update---"+str(row.ENTITLEMENT_VALUE_CODE)+'---'+str( eval(row.ENTITLEMENT_VALUE_CODE)))
 								#Log.Info('ENTITLEMENT_VALUE_CODE----'+str(row.ENTITLEMENT_VALUE_CODE)+'---'+str(eval(row.ENTITLEMENT_VALUE_CODE)))
-								for code in eval(row.ENTITLEMENT_VALUE_CODE):
+								for code in row.ENTITLEMENT_VALUE_CODE.split(','):
 									requestdata += '{"value":"' + str(code) + '","selected":true}'
 									requestdata +=','
 								requestdata +=']},'	
