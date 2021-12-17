2aae0de5fc6ca3c3774a5ab2cd2e98dbc07ddbdb
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-12-17 11:48:22 +0530

    Update CQEXPIMAIL.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/CQEXPIMAIL.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/CQEXPIMAIL.py
@@ -65,13 +65,13 @@ current_date_obj = str(now).split(" ")[0].strip()
 today_date_obj = datetime.datetime.strptime(str(current_date_obj),"%Y-%m-%d")
 today_date_string = str(today_date_obj).split(" ")[0].strip()
 target_mail_date_obj = today_date_obj + timedelta(days=7)
-target_mail_date_obj= target_mail_date_obj.strftime('%m-%d-%Y')
+target_mail_date_obj= target_mail_date_obj
 target_mail_date = str(target_mail_date_obj).split(" ")[0].strip()
-target_mail_date = target_mail_date.replace("-","/")
+#target_mail_date = target_mail_date.replace("-","/")
 
-expired_quotes_query = Sql.GetList("SELECT QUOTE_ID,QUOTE_EXPIRE_DATE FROM SAQTMT where QUOTE_EXPIRE_DATE = '{target_mail_date}'".format(target_mail_date=target_mail_date))
-###A055S000P01-12558 Chaged logic based on Revision table
-updatesaqtmtexpire = (""" UPDATE A SET A.EXPIRED = '1' FROM SAQTMT A INNER JOIN SAQTRV B ON  A.MASTER_TABLE_QUOTE_RECORD_ID = B.QUOTE_RECORD_ID AND A.QTEREV_RECORD_ID = B.QUOTE_REVISION_RECORD_ID WHERE B.REV_EXPIRE_DATE = '{today_current_date}' AND A.QTEREV_STATUS NOT IN ('SUBMITTED FOR BOOKING') """.format(today_current_date = today_current_date ))
+expired_quotes_query = Sql.GetList("SELECT QUOTE_ID,CONVERT(nvarchar(10),QUOTE_EXPIRE_DATE,120) as QUOTE_EXPIRE_DATE FROM SAQTMT where CONVERT(nvarchar(10),QUOTE_EXPIRE_DATE,120) = '{target_mail_date}'".format(target_mail_date=target_mail_date))
+###A055S000P01-12558
+updatesaqtmtexpire = (""" UPDATE SAQTMT SET EXPIRED = '1' WHERE  QUOTE_EXPIRE_DATE = '{today_current_date}' AND QTEREV_STATUS NOT IN ('SUBMITTED FOR BOOKING') """.format(today_current_date = today_current_date ))
 Sql.RunQuery(updatesaqtmtexpire)
 
 
@@ -79,7 +79,9 @@ expired_quotes = []
 if expired_quotes_query is not None:
     for quotes in expired_quotes_query:   
         expire_date = str(quotes.QUOTE_EXPIRE_DATE).split(" ")[0]     
+        #Trace.Write(" sjdhjksadhjds  "+str(target_mail_date)+ "  -  " +str(expire_date))
         if str(target_mail_date) == str(expire_date):
             expired_quotes.append(quotes.QUOTE_ID)
 if expired_quotes is not None:
     expiration_obj.mailtrigger(expired_quotes)
+
