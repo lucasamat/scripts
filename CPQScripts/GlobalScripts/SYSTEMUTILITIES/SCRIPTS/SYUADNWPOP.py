9e9e3412a7ac2f369b97bceed892723f1226de64
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-12-22 11:09:11 +0530

    Update SYUADNWPOP.py
    pagination records offset change

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
@@ -1754,7 +1754,7 @@ def POPUPLISTVALUEADDNEW(
 				
 
 			pagination_condition = "OFFSET {Offset_Skip_Count} ROWS FETCH NEXT {Fetch_Count} ROWS ONLY".format(
-				Offset_Skip_Count=offset_skip_count, Fetch_Count=fetch_count
+				Offset_Skip_Count=offset_skip_count-1 if offset_skip_count%10==1 else offset_skip_count, Fetch_Count=fetch_count
 			)
 
 			Pagination_M = Sql.GetFirst("select count(MAADPR.CpqTableEntryId) as count from MAADPR WHERE PRDOFR_ID = '"+str(TreeSuperParentParam)+"' AND PRDOFR_DOCTYP = '"+str(getDocType.DOCTYP_ID)+"' AND COMP_PRDOFR_ID NOT IN (SELECT SERVICE_ID FROM SAQSGB where QUOTE_RECORD_ID ='"+str(contract_quote_record_id)+"' AND QTEREV_RECORD_ID ='"+str(quote_revision_record_id)+"' AND GREENBOOK = '"+str(TreeParentParam)+"' ) ")
