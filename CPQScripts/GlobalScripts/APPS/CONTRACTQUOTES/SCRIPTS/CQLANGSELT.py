203ac5525a73fe1faa224cfe5d3285d401f7299b
Aravind Muthiah --CNTR <aravind_muthiah@contractor.amat.com>
2021-12-22 12:58:26 +0530

    Update CQLANGSELT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQLANGSELT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQLANGSELT.py
@@ -137,7 +137,7 @@ def language_select():
 		Trace.Write("If")
 		sec_str += ('<div id="container">')
 		sec_str += (
-				'<div class="dyn_main_head master_manufac glyphicon pointer   glyphicon-chevron-down" onclick="dyn_main_sec_collapse_arrow(this)" data-target=".sec_" data-toggle="collapse"><label class="onlytext"><label class="onlytext"><div><div id="ctr_drop" class="btn-group dropdown"><div class="dropdown"><i data-toggle="dropdown" class="fa fa-sort-desc dropdown-toggle"></i><ul class="dropdown-menu left" aria-labelledby="dropdownMenuButton"><li class="edit_list"> <a class="dropdown-item" href="#" onclick="CommonEDIT(this)">EDIT</a></li></ul></div></div>GENERAL SETTINGS</div></label></div>')
+				'<div class="dyn_main_head master_manufac glyphicon pointer   glyphicon-chevron-down" onclick="dyn_main_sec_collapse_arrow(this)" data-target=".sec_" data-toggle="collapse"><label class="onlytext"><label class="onlytext"><div>GENERAL SETTINGS</div></label></div>')
 
 	
 		sec_str += ('<div id="sec_LANG" class= sec_LANG>')
