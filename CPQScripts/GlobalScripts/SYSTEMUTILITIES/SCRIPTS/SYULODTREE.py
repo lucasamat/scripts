040bc70220d5f65bc5123cd34a92770f704e9030
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-12-22 17:10:13 +0530

    Update SYULODTREE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
@@ -2202,7 +2202,7 @@ class TreeView:
 									# 	Trace.Write("subTabName Green service list---"+str(subTabName))
 									
 									elif (subTabName in ("PM Events","New Parts","Service Parts List","Service Inclusions") and '/>Z' in NodeText ) or subTabName in ('Greenbook Inclusions','Green Parts List') :
-										# Trace.Write("service_id-inclusion-- "+str(NodeText)+'--'+str(subTabName))
+										Trace.Write("service_id-inclusion-- "+str(NodeText)+'--'+str(subTabName))
 										subtab_temp_variable = subTabName 
 										whr_str_greenbook =""
 										ent_table =""
@@ -2244,12 +2244,12 @@ class TreeView:
 												pattern_name = re.compile(r'<ENTITLEMENT_DISPLAY_VALUE>Some Exclusions</ENTITLEMENT_DISPLAY_VALUE>')
 												subtab_temp ="Exclusions"
 											elif subtab_temp_variable in ('Greenbook Inclusions','Service Inclusions') and service_id =='Z0092':
-												# Trace.Write(" Greenbook Inclusions")
+												Trace.Write(" Greenbook Inclusions")
 												pattern_id = re.compile(r'<ENTITLEMENT_ID>AGS_[^>]*?_TSC_CONSUM</ENTITLEMENT_ID>')
 												pattern_name = re.compile(r'<ENTITLEMENT_DISPLAY_VALUE>Some Inclusions</ENTITLEMENT_DISPLAY_VALUE>')
 												subtab_temp ="Inclusions"
 											if pattern_id and pattern_name:
-												# Trace.Write("inside ent tabs"+str(subtab_temp))
+												Trace.Write("inside ent tabs"+str(subtab_temp))
 												updateentXML = get_entitlement_xml.ENTITLEMENT_XML
 												flag_excluse=0
 												
@@ -2261,6 +2261,7 @@ class TreeView:
 														flag_excluse=1
 														break
 												if flag_excluse==1 and subtab_temp:
+													Trace.Write("subtab_temp_j"+str(subtab_temp))
 													subTabName = subtab_temp
 									
 
