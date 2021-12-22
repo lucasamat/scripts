949d99d6f8cb459b7bad1eba310ea8edec39c963
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-12-22 18:39:20 +0530

    Update CQSYNCQTBL.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
@@ -648,6 +648,8 @@ class SyncQuoteAndCustomTables:
 													"QTEREV_ID":quote_rev_id })
 						Quote.GetCustomField('QUOTE_REVISION_ID').Content = quote_revision_id
 						Quote.GetCustomField('QUOTE_REVISION_DESC').Content = salesorg_data.get("REVISION_DESCRIPTION")
+						Quote.GetCustomField('QUOTE_EXCHANGE_RATE').Content = salesorg_data.get("EXCHANGE_RATE")
+						Quote.GetCustomField('QUOTE_PAYMENT_TERM').Content = salesorg_data.get("PAYMENTTERM_NAME")
 						#UPDATE BLUEBOOK TO SAQTRV
 						bluebook_obj = Sql.GetFirst(
 							"SELECT BLUEBOOK,BLUEBOOK_RECORD_ID FROM SASAAC(NOLOCK) WHERE ACCOUNT_ID LIKE '%{}' AND DISTRIBUTIONCHANNEL_ID = '{}' AND DIVISION_ID = '{}' AND SALESORG_ID = '{}'".format(
