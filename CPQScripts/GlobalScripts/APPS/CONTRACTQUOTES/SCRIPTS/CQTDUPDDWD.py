cf2694ce3c7f0ea74b6740ad82c19ee3691b9d1d
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-12-23 11:55:23 +0530

    Update CQTDUPDDWD.py
    Indent resolved

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTDUPDDWD.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTDUPDDWD.py
@@ -16,7 +16,7 @@ from SYDATABASE import SQL
 Sql = SQL()
 
 class ContractQuoteDownloadTableData:
-    def __init__(self, **kwargs):		
+	def __init__(self, **kwargs):		
 		self.user_id = str(User.Id)
 		self.user_name = str(User.UserName)		
 		self.datetime_value = datetime.datetime.now()		
@@ -25,14 +25,14 @@ class ContractQuoteDownloadTableData:
 		self.set_contract_quote_related_details()
 		
 	def set_contract_quote_related_details(self):
-        try:
-            self.contract_quote_record_id = Quote.GetGlobal("contract_quote_record_id")
-        except Exception:
-            self.contract_quote_record_id = ''	
-        try:
-            self.contract_quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")
-        except Exception:
-            self.contract_quote_revision_record_id = ''
+		try:
+			self.contract_quote_record_id = Quote.GetGlobal("contract_quote_record_id")
+		except Exception:
+			self.contract_quote_record_id = ''	
+		try:
+			self.contract_quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")
+		except Exception:
+			self.contract_quote_revision_record_id = ''
 		contract_quote_obj = Sql.GetFirst("SELECT QUOTE_ID, QUOTE_TYPE, SALE_TYPE, C4C_QUOTE_ID, QTEREV_ID, QUOTE_CURRENCY, QUOTE_CURRENCY_RECORD_ID FROM SAQTMT (NOLOCK) WHERE MASTER_TABLE_QUOTE_RECORD_ID = '{QuoteRecordId}'".format(QuoteRecordId=self.contract_quote_record_id))
 		if contract_quote_obj:
 			self.contract_quote_id = contract_quote_obj.QUOTE_ID
