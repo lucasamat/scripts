4686a902e22ad2f749c9938d5334c3fa2b0e8bda
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-12-23 17:58:38 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -2804,12 +2804,12 @@ class SYLDRTLIST:
 								contract_quote_rec_id = Quote.GetGlobal("contract_quote_record_id")
 								quote_revision_rec_id = Quote.GetGlobal("quote_revision_record_id")
 								docnode_action_btn = SqlHelper.GetList("SELECT DATE_SUBMITTED FROM SAQDOC WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' ".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_rec_id))
-								for date_value in docnode_action_btn:
-									if date_value:
-										Action_str += '<li><a id = "" class="dropdown-item" href="#" " onclick="customer_accepted(this)">CUSTOMER ACCEPTED</a></li>'
-										Action_str += '<li><a id = "" class="dropdown-item" href="#" " onclick="customer_rejected(this)">CUSTOMER REJECTED</a></li>'
-									else:										
-										Action_str += '<li><a id = "" class="dropdown-item" href="#" " onclick="submit_to_customer(this)">SUBMITTED TO CUSTOMER</a></li>'     
+								#for date_value in docnode_action_btn:
+								if docnode_action_btn:
+									Action_str += '<li><a id = "" class="dropdown-item" href="#" " onclick="customer_accepted(this)">CUSTOMER ACCEPTED</a></li>'
+									Action_str += '<li><a id = "" class="dropdown-item" href="#" " onclick="customer_rejected(this)">CUSTOMER REJECTED</a></li>'
+								else:										
+									Action_str += '<li><a id = "" class="dropdown-item" href="#" " onclick="submit_to_customer(this)">SUBMITTED TO CUSTOMER</a></li>'     
 							elif ObjectName == "SAQDLT":
 								Action_str += (
 									'<li><a class="dropdown-item" href="#" onclick="replace_cont_manager(this)">REPLACE</a></li>'
