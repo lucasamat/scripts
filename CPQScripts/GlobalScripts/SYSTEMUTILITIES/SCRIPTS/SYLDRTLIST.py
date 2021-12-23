6f6fbb57fcc6e134b1e53425526e914ceffb4f9f
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2021-12-23 18:09:51 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -2803,9 +2803,9 @@ class SYLDRTLIST:
 							elif ObjectName == "SAQDOC":
 								contract_quote_rec_id = Quote.GetGlobal("contract_quote_record_id")
 								quote_revision_rec_id = Quote.GetGlobal("quote_revision_record_id")
-								docnode_action_btn = SqlHelper.GetList("SELECT DATE_SUBMITTED FROM SAQDOC WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' ".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_rec_id))
+								docnode_action_btn = SqlHelper.GetList("SELECT ISNULL(DATE_SUBMITTED,'') FROM SAQDOC WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' ".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_rec_id))
 								for date_value in docnode_action_btn:
-									if date_value.DATE_SUBMITTED:
+									if date_value.DATE_SUBMITTED != "":
 										Action_str += '<li><a id = "" class="dropdown-item" href="#" " onclick="customer_accepted(this)">CUSTOMER ACCEPTED</a></li>'
 										Action_str += '<li><a id = "" class="dropdown-item" href="#" " onclick="customer_rejected(this)">CUSTOMER REJECTED</a></li>'
 									else:										
