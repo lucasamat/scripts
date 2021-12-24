7fa06a2724b191742c608a8cf10d57a27166e7af
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2021-12-24 22:21:22 +0530

    Update CQINSQTITM.py
    If document_type is ZWK1 AUTO LINE INSERT IS 10,20 AND SOON.

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQINSQTITM.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQINSQTITM.py
@@ -1505,7 +1505,10 @@ class ContractQuoteItem:
 			quote_line_item_obj = Sql.GetFirst("SELECT COUNT(LINE) AS LINE FROM SAQRIT (NOLOCK) WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{RevisionRecordId}'".format(QuoteRecordId=self.contract_quote_record_id,RevisionRecordId=self.contract_quote_revision_record_id))
 			equipments_count = int(quote_item_obj.LINE)
 		'''
-		quote_line_item_obj = Sql.GetFirst("SELECT TOP 1 LINE FROM SAQRIT (NOLOCK) WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{RevisionRecordId}' ORDER BY LINE DESC".format(QuoteRecordId=self.contract_quote_record_id,RevisionRecordId=self.contract_quote_revision_record_id))
+		Sql.RunQuery("DELETE FROM SAQRIT WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{RevisionRecordId}' AND SERVICE_ID LIKE '{ServiceId}%'".format(
+			QuoteRecordId=self.contract_quote_record_id,RevisionRecordId=self.contract_quote_revision_record_id,ServiceId=self.service_id))
+		
+  		quote_line_item_obj = Sql.GetFirst("SELECT TOP 1 LINE FROM SAQRIT (NOLOCK) WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{RevisionRecordId}' ORDER BY LINE DESC".format(QuoteRecordId=self.contract_quote_record_id,RevisionRecordId=self.contract_quote_revision_record_id))
 		if quote_line_item_obj:
 			equipments_count = int(quote_line_item_obj.LINE)
 		
@@ -1513,8 +1516,6 @@ class ContractQuoteItem:
 		if doctype_obj:
 			item_number_inc = int(doctype_obj.ITEM_NUMBER_INCREMENT)
 		
-		Sql.RunQuery("DELETE FROM SAQRIT WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{RevisionRecordId}' AND SERVICE_ID LIKE '{ServiceId}%'".format(
-			QuoteRecordId=self.contract_quote_record_id,RevisionRecordId=self.contract_quote_revision_record_id,ServiceId=self.service_id))
 		Log.Info("SAQRIT=== 5")
 		Sql.RunQuery("""INSERT SAQRIT (QUOTE_REVISION_CONTRACT_ITEM_ID, CPQTABLEENTRYADDEDBY, CPQTABLEENTRYDATEADDED, CpqTableEntryModifiedBy, CpqTableEntryDateModified, CONTRACT_VALID_FROM, CONTRACT_VALID_TO, DOC_CURRENCY, DOCURR_RECORD_ID, EXCHANGE_RATE, EXCHANGE_RATE_DATE, EXCHANGE_RATE_RECORD_ID, GL_ACCOUNT_NO, GLOBAL_CURRENCY, GLOBAL_CURRENCY_RECORD_ID, LINE, OBJECT_ID, OBJECT_TYPE, SERVICE_DESCRIPTION, SERVICE_ID, SERVICE_RECORD_ID, PROFIT_CENTER, QUANTITY, QUOTE_ID, QUOTE_RECORD_ID, QTEREV_ID, QTEREV_RECORD_ID, REF_SALESORDER, STATUS, TAXCLASSIFICATION_DESCRIPTION, TAXCLASSIFICATION_ID, TAXCLASSIFICATION_RECORD_ID, GREENBOOK, GREENBOOK_RECORD_ID,NET_VALUE_INGL_CURR, NET_PRICE_INGL_CURR) 
 		SELECT
@@ -1533,9 +1534,9 @@ class ContractQuoteItem:
 			null as GL_ACCOUNT_NO,
 			SAQTRV.GLOBAL_CURRENCY,
 			SAQTRV.GLOBAL_CURRENCY_RECORD_ID,
-			ROW_NUMBER()OVER(ORDER BY(SAQSPT.CpqTableEntryId)) + {EquipmentsCount} as LINE,
+			--ROW_NUMBER()OVER(ORDER BY(SAQSPT.CpqTableEntryId)) + {EquipmentsCount} as LINE,
 			--({EquipmentsCount} + ROW_NUMBER()OVER(ORDER BY(SAQSPT.CpqTableEntryId))) * {itemnumberinc} as LINE,
-			--(ROW_NUMBER()OVER(ORDER BY(SAQSPT.CpqTableEntryId)) * {itemnumberinc}) as LINE,
+			(ROW_NUMBER()OVER(ORDER BY(SAQSPT.CpqTableEntryId)) * {itemnumberinc}) as LINE,
    			null as OBJECT_ID,
 			null as OBJECT_TYPE,
 			SAQSPT.SERVICE_DESCRIPTION,
