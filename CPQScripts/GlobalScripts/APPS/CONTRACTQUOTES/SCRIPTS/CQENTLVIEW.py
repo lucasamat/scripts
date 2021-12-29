e517c71a498ed90eb5eae50c5170ec93b7c6a9b2
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2021-12-29 15:44:58 +0530

    Update CQENTLVIEW.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
@@ -10,7 +10,7 @@ import Webcom.Configurator.Scripting.Test.TestProduct
 import System.Net
 from datetime import datetime
 from SYDATABASE import SQL
-
+import re
 Sql = SQL()
 userId = str(User.Id)
 #userName = str(User.UserName)
