d9dbda1b1e1aa7cc9a0389fb60abefb404b4c71b
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2021-12-29 17:23:42 +0530

    Update SYUADNWPOP.py
    no records to display template

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
@@ -1741,7 +1741,7 @@ def POPUPLISTVALUEADDNEW(
 					+ str(RECORDID)
 					+ "\", 'RECORDFEILD':  \""
 					+ str(RECORDFEILD)
-					+ "\", 'NEWVALUE': '', 'LOOKUPOBJ': '', 'LOOKUPAPI': '','A_Keys':a_list,'A_Values':ATTRIBUTE_VALUEList}, function(data) {  date_field = data[3]; var assoc = data[1]; var api_name = data[2];data4 = data[4];data5 = data[5]; try { if(date_field.length > 0) { $(\""
+					+ "\", 'NEWVALUE': '', 'LOOKUPOBJ': '', 'LOOKUPAPI': '','A_Keys':a_list,'A_Values':ATTRIBUTE_VALUEList}, function(data) {  date_field = data[3]; var assoc = data[1]; var api_name = data[2];data4 = data[4];data5 = data[5]; try { console.log('date-field-->'+date_field);if(date_field.length > 0) { $(\""
 					+ str(table_ids)
 					+ '").bootstrapTable("load", date_field  ); $("#Include_add_on_addnew_footer").html(data[6]);$("button#country_save").attr("disabled",false); $("#noRecDisp").remove() } else{ var date_field = [];$("'
 					+ str(table_ids)
@@ -1749,7 +1749,7 @@ def POPUPLISTVALUEADDNEW(
 					+ str(table_ids)
 					+ '").bootstrapTable("load", date_field  ); $("button#country_save").attr("disabled",false); } else{ $("'
 					+ str(table_ids)
-					+ '").bootstrapTable("load", date_field  ); $("button#country_save").attr("disabled",true); } } ; });  });'
+					+ '").bootstrapTable("load", date_field  );$("#Include_add_on_addnew > tbody").html("<div id=\'noRecDisp\' class=\'noRecord\'>No Records to Display</div>");$("#Include_add_on_addnew_footer").html(data[6]); $("button#country_save").attr("disabled",true); } } ; });  });'
 				)
 				
 
