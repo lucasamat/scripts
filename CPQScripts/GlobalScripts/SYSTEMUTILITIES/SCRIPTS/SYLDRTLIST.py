9f8aae35339f6a59222548f733d1a619b0ce6904
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2021-12-29 14:27:26 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -3136,7 +3136,9 @@ class SYLDRTLIST:
 							else:
 								if not re.match(r'[A-Za-z0-9]',value1234):
 									Trace.Write("encode character")
-									Trace.Write(value1234)
+									Trace.Write(value1234.encode('utf-8'))
+									Trace.Write(value1234.decode('utf-8'))
+									Trace.Write(value1234.encode('utf-8').decode('utf-8'))
 									new_dict[value123] = ('<abbr id ="' + key_value + '" title="' + value1234.encode('utf-8').decode('utf-8') + '">' + value1234.encode('utf-8').decode('utf-8') + "</abbr>")
 								else:
 									new_dict[value123] = ('<abbr id ="' + str(key_value) + '" title="' + str(value1234) + '">' + str(value1234) + "</abbr>")  
