7edc49528ec1f1501d3c4228fe0a0277b446c544
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-12-30 15:47:38 +0530

    Update CQTDUPDDWD.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTDUPDDWD.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTDUPDDWD.py
@@ -146,115 +146,107 @@ class ContractQuoteUploadTableData(ContractQuoteSpareOpertion):
 		datetime_string = self.datetime_value.strftime("%d%m%Y%H%M%S")
 		spare_parts_temp_table_name = "SAQSPT_BKP_{}_{}".format(self.contract_quote_id, datetime_string)
 		Trace.Write("============>> 000 ====>"+str(spare_parts_temp_table_name))
-		
-		spare_parts_temp_table_drop = SqlHelper.GetFirst("sp_executesql @T=N'IF EXISTS (SELECT ''X'' FROM SYS.OBJECTS WHERE NAME= ''"+str(spare_parts_temp_table_name)+"'' ) BEGIN DROP TABLE "+str(spare_parts_temp_table_name)+" END  ' ")
-		Trace.Write("============>> 111 ===> "+str(spare_parts_temp_table_name))
-		query1 = "sp_executesql @T=N'SELECT "+str(self.columns)+" INTO "+str(spare_parts_temp_table_name)+" FROM (SELECT DISTINCT "+str(self.columns)+" FROM (VALUES "+str(self.records)+") AS TEMP("+str(self.columns)+")) OQ ' "
-		Trace.Write("============>> 222 ===> "+str(query1))
-		spare_parts_temp_table_bkp = SqlHelper.GetFirst(query1)
-		Trace.Write("============>> 333 ===> "+str(spare_parts_temp_table_name))
-		Sql.RunQuery("""
-						INSERT SAQSPT (QUOTE_SERVICE_PART_RECORD_ID, BASEUOM_ID, BASEUOM_RECORD_ID, CUSTOMER_PART_NUMBER, CUSTOMER_PART_NUMBER_RECORD_ID, DELIVERY_MODE, EXTENDED_UNIT_PRICE, PART_DESCRIPTION, PART_NUMBER, PART_RECORD_ID, PRDQTYCON_RECORD_ID, CUSTOMER_ANNUAL_QUANTITY, QUOTE_ID, QUOTE_NAME, QUOTE_RECORD_ID,QTEREV_ID,QTEREV_RECORD_ID,SALESORG_ID, SALESORG_RECORD_ID, SALESUOM_CONVERSION_FACTOR, SALESUOM_ID, SALESUOM_RECORD_ID, SCHEDULE_MODE, SERVICE_DESCRIPTION, SERVICE_ID, SERVICE_RECORD_ID, UNIT_PRICE, MATPRIGRP_ID, MATPRIGRP_RECORD_ID, DELIVERY_INTERVAL, VALID_FROM_DATE, VALID_TO_DATE,PAR_SERVICE_DESCRIPTION,PAR_SERVICE_ID,PAR_SERVICE_RECORD_ID, CPQTABLEENTRYADDEDBY, CPQTABLEENTRYDATEADDED)
-						SELECT DISTINCT
-							CONVERT(VARCHAR(4000),NEWID()) as QUOTE_SERVICE_PART_RECORD_ID,
-							BASEUOM_ID,
-							BASEUOM_RECORD_ID,
-							CUSTOMER_PART_NUMBER,
-							CUSTOMER_PART_NUMBER_RECORD_ID,
-							DELIVERY_MODE,
-							EXTENDED_UNIT_PRICE,
-							PART_DESCRIPTION,
-							PART_NUMBER,
-							PART_RECORD_ID,
-							PRDQTYCON_RECORD_ID,
-							QUANTITY,
-							QUOTE_ID,
-							QUOTE_NAME,
-							QUOTE_RECORD_ID,
-							QTEREV_ID,
-							QTEREV_RECORD_ID,
-							SALESORG_ID,
-							SALESORG_RECORD_ID,
-							SALESUOM_CONVERSION_FACTOR,
-							SALESUOM_ID,
-							SALESUOM_RECORD_ID, 
-							SCHEDULE_MODE,
-							SERVICE_DESCRIPTION,
-							SERVICE_ID,
-							SERVICE_RECORD_ID,
-							UNIT_PRICE,
-							MATPRIGRP_ID,
-							MATPRIGRP_RECORD_ID,
-							DELIVERY_INTERVAL,
-							VALID_FROM_DATE, 
-							VALID_TO_DATE,
-							PAR_SERVICE_DESCRIPTION,
-							PAR_SERVICE_ID,
-							PAR_SERVICE_RECORD_ID,
-							{UserId} as CPQTABLEENTRYADDEDBY, 
-							GETDATE() as CPQTABLEENTRYDATEADDED
-						FROM (
-						SELECT 
-							DISTINCT
-							MAMTRL.UNIT_OF_MEASURE as BASEUOM_ID,
-							MAMTRL.UOM_RECORD_ID as BASEUOM_RECORD_ID,
-							MAMTRL.SAP_PART_NUMBER as CUSTOMER_PART_NUMBER,
-							MAMTRL.MATERIAL_RECORD_ID as CUSTOMER_PART_NUMBER_RECORD_ID,
-							'ONSITE' as DELIVERY_MODE,
-							0.00 as EXTENDED_UNIT_PRICE,
-							MAMTRL.SAP_DESCRIPTION as PART_DESCRIPTION,
-							MAMTRL.SAP_PART_NUMBER as PART_NUMBER,
-							MAMTRL.MATERIAL_RECORD_ID as PART_RECORD_ID,
-							'' as PRDQTYCON_RECORD_ID,
-							0 as QUANTITY,
-							SAQTMT.QUOTE_ID as QUOTE_ID,
-							SAQTMT.QUOTE_NAME as QUOTE_NAME,
-							SAQTMT.MASTER_TABLE_QUOTE_RECORD_ID as QUOTE_RECORD_ID,
-							SAQTMT.QTEREV_ID as QTEREV_ID,
-							SAQTMT.QTEREV_RECORD_ID as QTEREV_RECORD_ID,
-							SAQTSV.SALESORG_ID as SALESORG_ID,
-							SAQTSV.SALESORG_RECORD_ID as SALESORG_RECORD_ID,
-							0.00 as SALESUOM_CONVERSION_FACTOR,
-							MAMTRL.UNIT_OF_MEASURE as SALESUOM_ID,
-							MAMTRL.UOM_RECORD_ID as SALESUOM_RECORD_ID, 
-							'SCHEDULED' as SCHEDULE_MODE,
-							SAQTSV.SERVICE_DESCRIPTION as SERVICE_DESCRIPTION,
-							SAQTSV.SERVICE_ID as SERVICE_ID,
-							SAQTSV.SERVICE_RECORD_ID as SERVICE_RECORD_ID,
-							0.00 as UNIT_PRICE,
-							MAMSOP.MATPRIGRP_ID as MATPRIGRP_ID,
-							MAMSOP.MATPRIGRP_RECORD_ID as MATPRIGRP_RECORD_ID,
-							'MONTHLY' as DELIVERY_INTERVAL,
-							SAQTMT.CONTRACT_VALID_FROM as VALID_FROM_DATE, 
-							SAQTMT.CONTRACT_VALID_TO as VALID_TO_DATE,
-							SAQTSV.PAR_SERVICE_DESCRIPTION as PAR_SERVICE_DESCRIPTION,
-							SAQTSV.PAR_SERVICE_ID as PAR_SERVICE_ID,
-							SAQTSV.PAR_SERVICE_RECORD_ID as PAR_SERVICE_RECORD_ID
-						FROM {TempTable} TEMP_TABLE(NOLOCK)
-						JOIN MAMTRL (NOLOCK) ON MAMTRL.SAP_PART_NUMBER = TEMP_TABLE.PART_NUMBER
-						JOIN SAQTMT (NOLOCK) ON SAQTMT.MASTER_TABLE_QUOTE_RECORD_ID = TEMP_TABLE.QUOTE_RECORD_ID
-						JOIN SAQTSV (NOLOCK) ON SAQTSV.QUOTE_RECORD_ID = SAQTMT.MASTER_TABLE_QUOTE_RECORD_ID AND SAQTSV.QTEREV_RECORD_ID = SAQTMT.QTEREV_RECORD_ID AND SAQTSV.SERVICE_ID = '{ServiceId}'
-						JOIN MAMSOP (NOLOCK) ON MAMSOP.MATERIAL_RECORD_ID = MAMTRL.MATERIAL_RECORD_ID AND MAMSOP.SALESORG_RECORD_ID = SAQTSV.SALESORG_RECORD_ID
-						WHERE TEMP_TABLE.QUOTE_RECORD_ID = '{QuoteRecordId}' AND TEMP_TABLE.QTEREV_RECORD_ID = '{RevisionRecordId}' AND MAMTRL.PRODUCT_TYPE IS NULL AND MAMTRL.IS_SPARE_PART = 1 ) IQ
-						""".format(
-									TempTable=spare_parts_temp_table_name,
-									ServiceId=self.tree_param,									
-									QuoteRecordId=self.contract_quote_record_id,
-									RevisionRecordId=self.contract_quote_revision_record_id,
-									UserId=self.user_id
-								)
-		)
-		# except Exception:
-		# 	spare_parts_temp_table_drop = SqlHelper.GetFirst("sp_executesql @T=N'IF EXISTS (SELECT ''X'' FROM SYS.OBJECTS WHERE NAME= ''"+str(spare_parts_temp_table_name)+"'' ) BEGIN DROP TABLE "+str(spare_parts_temp_table_name)+" END  ' ")
-		# Sql.RunQuery("""INSERT SAQSPT ({DynamicColumns}, QUOTE_ID, QUOTE_NAME, QUOTE_RECORD_ID, QTEREV_ID, QTEREV_RECORD_ID, SERVICE_DESCRIPTION, SERVICE_ID, SERVICE_RECORD_ID, QUOTE_SERVICE_PART_RECORD_ID, CPQTABLEENTRYADDEDBY, CPQTABLEENTRYDATEADDED,CpqTableEntryModifiedBy,CpqTableEntryDateModified)
-		# 		SELECT OQ.*, '{QuoteId}' as QUOTE_ID, '{QuoteName}' as QUOTE_NAME, '{QuoteRecordId}' as QUOTE_RECORD_ID, '{QuoteRevisionId}' as QTEREV_ID, '{QuoteRevisionRecordId}' as QTEREV_RECORD_ID, '{ServiceDescription}' as SERVICE_DESCRIPTION, '{ServiceId}' as SERVICE_ID, '{ServiceRecordId}' as SERVICE_RECORD_ID, CONVERT(VARCHAR(4000),NEWID()) as QUOTE_SERVICE_PART_RECORD_ID, '{UserName}' as CPQTABLEENTRYADDEDBY, GETDATE() as CPQTABLEENTRYDATEADDED,{UserId} as CpqTableEntryModifiedBy, GETDATE() as CpqTableEntryDateModified FROM (
-		# 		SELECT DISTINCT					
-		# 				{DynamicColumns}	
-		# 		FROM 
-		# 			(VALUES {Records}) AS Temp({DynamicColumns})					
-		# 		) OQ
-		# 		""".format(UserId=self.user_id, UserName=self.user_name, QuoteId=self.contract_quote_id, QuoteName='', QuoteRecordId=self.contract_quote_record_id, QuoteRevisionId=self.contract_quote_revision_id, QuoteRevisionRecordId=self.contract_quote_revision_record_id, ServiceDescription='', ServiceId=self.tree_param, ServiceRecordId='', DynamicColumns=self.columns, Records=self.records)
-		# 	)
+		try:
+			spare_parts_temp_table_drop = SqlHelper.GetFirst("sp_executesql @T=N'IF EXISTS (SELECT ''X'' FROM SYS.OBJECTS WHERE NAME= ''"+str(spare_parts_temp_table_name)+"'' ) BEGIN DROP TABLE "+str(spare_parts_temp_table_name)+" END  ' ")			
+			
+			spare_parts_temp_table_bkp = SqlHelper.GetFirst("sp_executesql @T=N'SELECT "+str(self.columns)+" INTO "+str(spare_parts_temp_table_name)+" FROM (SELECT DISTINCT "+str(self.columns)+" FROM (VALUES "+str(self.records)+") AS TEMP("+str(self.columns)+")) OQ ' ")
+			
+			spare_parts_existing_records_delete = SqlHelper.GetFirst("sp_executesql @T=N'DELETE FROM SAQSPT WHERE QUOTE_RECORD_ID = ''"+str(self.contract_quote_record_id)+"'' AND QTEREV_RECORD_ID = ''"+str(self.contract_quote_revision_record_id)+"'' ' ")
+
+			Sql.RunQuery("""
+							INSERT SAQSPT (QUOTE_SERVICE_PART_RECORD_ID, BASEUOM_ID, BASEUOM_RECORD_ID, CUSTOMER_PART_NUMBER, CUSTOMER_PART_NUMBER_RECORD_ID, DELIVERY_MODE, EXTENDED_UNIT_PRICE, PART_DESCRIPTION, PART_NUMBER, PART_RECORD_ID, PRDQTYCON_RECORD_ID, CUSTOMER_ANNUAL_QUANTITY, QUOTE_ID, QUOTE_NAME, QUOTE_RECORD_ID,QTEREV_ID,QTEREV_RECORD_ID,SALESORG_ID, SALESORG_RECORD_ID, SALESUOM_CONVERSION_FACTOR, SALESUOM_ID, SALESUOM_RECORD_ID, SCHEDULE_MODE, SERVICE_DESCRIPTION, SERVICE_ID, SERVICE_RECORD_ID, UNIT_PRICE, MATPRIGRP_ID, MATPRIGRP_RECORD_ID, DELIVERY_INTERVAL, VALID_FROM_DATE, VALID_TO_DATE,PAR_SERVICE_DESCRIPTION,PAR_SERVICE_ID,PAR_SERVICE_RECORD_ID, CPQTABLEENTRYADDEDBY, CPQTABLEENTRYDATEADDED)
+							SELECT DISTINCT
+								CONVERT(VARCHAR(4000),NEWID()) as QUOTE_SERVICE_PART_RECORD_ID,
+								BASEUOM_ID,
+								BASEUOM_RECORD_ID,
+								CUSTOMER_PART_NUMBER,
+								CUSTOMER_PART_NUMBER_RECORD_ID,
+								DELIVERY_MODE,
+								EXTENDED_UNIT_PRICE,
+								PART_DESCRIPTION,
+								PART_NUMBER,
+								PART_RECORD_ID,
+								PRDQTYCON_RECORD_ID,
+								QUANTITY,
+								QUOTE_ID,
+								QUOTE_NAME,
+								QUOTE_RECORD_ID,
+								QTEREV_ID,
+								QTEREV_RECORD_ID,
+								SALESORG_ID,
+								SALESORG_RECORD_ID,
+								SALESUOM_CONVERSION_FACTOR,
+								SALESUOM_ID,
+								SALESUOM_RECORD_ID, 
+								SCHEDULE_MODE,
+								SERVICE_DESCRIPTION,
+								SERVICE_ID,
+								SERVICE_RECORD_ID,
+								UNIT_PRICE,
+								MATPRIGRP_ID,
+								MATPRIGRP_RECORD_ID,
+								DELIVERY_INTERVAL,
+								VALID_FROM_DATE, 
+								VALID_TO_DATE,
+								PAR_SERVICE_DESCRIPTION,
+								PAR_SERVICE_ID,
+								PAR_SERVICE_RECORD_ID,
+								{UserId} as CPQTABLEENTRYADDEDBY, 
+								GETDATE() as CPQTABLEENTRYDATEADDED
+							FROM (
+							SELECT 
+								DISTINCT
+								MAMTRL.UNIT_OF_MEASURE as BASEUOM_ID,
+								MAMTRL.UOM_RECORD_ID as BASEUOM_RECORD_ID,
+								MAMTRL.SAP_PART_NUMBER as CUSTOMER_PART_NUMBER,
+								MAMTRL.MATERIAL_RECORD_ID as CUSTOMER_PART_NUMBER_RECORD_ID,
+								'ONSITE' as DELIVERY_MODE,
+								0.00 as EXTENDED_UNIT_PRICE,
+								MAMTRL.SAP_DESCRIPTION as PART_DESCRIPTION,
+								MAMTRL.SAP_PART_NUMBER as PART_NUMBER,
+								MAMTRL.MATERIAL_RECORD_ID as PART_RECORD_ID,
+								'' as PRDQTYCON_RECORD_ID,
+								0 as QUANTITY,
+								SAQTMT.QUOTE_ID as QUOTE_ID,
+								SAQTMT.QUOTE_NAME as QUOTE_NAME,
+								SAQTMT.MASTER_TABLE_QUOTE_RECORD_ID as QUOTE_RECORD_ID,
+								SAQTMT.QTEREV_ID as QTEREV_ID,
+								SAQTMT.QTEREV_RECORD_ID as QTEREV_RECORD_ID,
+								SAQTSV.SALESORG_ID as SALESORG_ID,
+								SAQTSV.SALESORG_RECORD_ID as SALESORG_RECORD_ID,
+								0.00 as SALESUOM_CONVERSION_FACTOR,
+								MAMTRL.UNIT_OF_MEASURE as SALESUOM_ID,
+								MAMTRL.UOM_RECORD_ID as SALESUOM_RECORD_ID, 
+								'SCHEDULED' as SCHEDULE_MODE,
+								SAQTSV.SERVICE_DESCRIPTION as SERVICE_DESCRIPTION,
+								SAQTSV.SERVICE_ID as SERVICE_ID,
+								SAQTSV.SERVICE_RECORD_ID as SERVICE_RECORD_ID,
+								0.00 as UNIT_PRICE,
+								MAMSOP.MATPRIGRP_ID as MATPRIGRP_ID,
+								MAMSOP.MATPRIGRP_RECORD_ID as MATPRIGRP_RECORD_ID,
+								'MONTHLY' as DELIVERY_INTERVAL,
+								SAQTMT.CONTRACT_VALID_FROM as VALID_FROM_DATE, 
+								SAQTMT.CONTRACT_VALID_TO as VALID_TO_DATE,
+								SAQTSV.PAR_SERVICE_DESCRIPTION as PAR_SERVICE_DESCRIPTION,
+								SAQTSV.PAR_SERVICE_ID as PAR_SERVICE_ID,
+								SAQTSV.PAR_SERVICE_RECORD_ID as PAR_SERVICE_RECORD_ID
+							FROM {TempTable} TEMP_TABLE(NOLOCK)
+							JOIN MAMTRL (NOLOCK) ON MAMTRL.SAP_PART_NUMBER = TEMP_TABLE.PART_NUMBER
+							JOIN SAQTMT (NOLOCK) ON SAQTMT.MASTER_TABLE_QUOTE_RECORD_ID = TEMP_TABLE.QUOTE_RECORD_ID
+							JOIN SAQTSV (NOLOCK) ON SAQTSV.QUOTE_RECORD_ID = SAQTMT.MASTER_TABLE_QUOTE_RECORD_ID AND SAQTSV.QTEREV_RECORD_ID = SAQTMT.QTEREV_RECORD_ID AND SAQTSV.SERVICE_ID = '{ServiceId}'
+							JOIN MAMSOP (NOLOCK) ON MAMSOP.MATERIAL_RECORD_ID = MAMTRL.MATERIAL_RECORD_ID AND MAMSOP.SALESORG_RECORD_ID = SAQTSV.SALESORG_RECORD_ID
+							WHERE TEMP_TABLE.QUOTE_RECORD_ID = '{QuoteRecordId}' AND TEMP_TABLE.QTEREV_RECORD_ID = '{RevisionRecordId}' AND MAMTRL.PRODUCT_TYPE IS NULL AND MAMTRL.IS_SPARE_PART = 1 ) IQ
+							""".format(
+										TempTable=spare_parts_temp_table_name,
+										ServiceId=self.tree_param,									
+										QuoteRecordId=self.contract_quote_record_id,
+										RevisionRecordId=self.contract_quote_revision_record_id,
+										UserId=self.user_id
+									)
+			)
+			spare_parts_temp_table_drop = SqlHelper.GetFirst("sp_executesql @T=N'IF EXISTS (SELECT ''X'' FROM SYS.OBJECTS WHERE NAME= ''"+str(spare_parts_temp_table_name)+"'' ) BEGIN DROP TABLE "+str(spare_parts_temp_table_name)+" END  ' ")
+		except Exception:
+			spare_parts_temp_table_drop = SqlHelper.GetFirst("sp_executesql @T=N'IF EXISTS (SELECT ''X'' FROM SYS.OBJECTS WHERE NAME= ''"+str(spare_parts_temp_table_name)+"'' ) BEGIN DROP TABLE "+str(spare_parts_temp_table_name)+" END  ' ")		
 	
 	def _do_opertion(self):
 		for sheet_data in self.upload_data:	
@@ -262,9 +254,7 @@ class ContractQuoteUploadTableData(ContractQuoteSpareOpertion):
 				break	
 			xls_spare_records = list(sheet_data.Value)
 			if xls_spare_records:
-				header = list(xls_spare_records[0]) + ['QUOTE_RECORD_ID','QTEREV_RECORD_ID']				
-				Trace.Write("====> "+str(header))
-				
+				header = list(xls_spare_records[0]) + ['QUOTE_RECORD_ID','QTEREV_RECORD_ID']
 				self.columns = ",".join(header)
 				self.records = ', '.join(map(str, [str(tuple(list(spare_record)+[self.contract_quote_record_id, self.contract_quote_revision_record_id])) for spare_record in xls_spare_records[1:]])).replace("None","null").replace("'","''")
 				
