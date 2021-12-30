d1118c963730e431570d230c558603d795b6cceb
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2021-12-30 13:33:20 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -3074,6 +3074,8 @@ def Related_Sub_Banner(
                                 sec_rel_sub_bnr += (btn)
                     if quote_status.QUOTE_STATUS != 'APPROVED' and 'SPLIT' not in btn and 'EDIT' not in btn:
                         sec_rel_sub_bnr += (btn)
+                    else:
+                        sec_rel_sub_bnr += (btn)
         
         Trace.Write('sec_rel_sub_bnr--2941--'+str(sec_rel_sub_bnr))
     return sec_rel_sub_bnr,recall_edit,buttonvisibility,price_bar
