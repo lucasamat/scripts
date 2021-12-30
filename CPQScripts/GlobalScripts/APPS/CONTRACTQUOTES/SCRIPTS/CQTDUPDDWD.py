b9b7c922f4fd4bca063ae3194c5b588bf7638059
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2021-12-30 14:31:30 +0530

    Update CQTDUPDDWD.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTDUPDDWD.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTDUPDDWD.py
@@ -260,12 +260,11 @@ class ContractQuoteUploadTableData(ContractQuoteSpareOpertion):
 				break	
 			xls_spare_records = list(sheet_data.Value)
 			if xls_spare_records:
-				header = xls_spare_records[0] 
-				header.append(['QUOTE_RECORD_ID','QTEREV_RECORD_ID'])
+				header = list(xls_spare_records[0]) + ['QUOTE_RECORD_ID','QTEREV_RECORD_ID']				
 				Trace.Write("====> "+str(header))
 				
 				self.columns = ",".join(header)
-				self.records = ', '.join(map(str, [str(tuple(spare_record+[self.contract_quote_record_id, self.contract_quote_revision_record_id])) for spare_record in xls_spare_records[1:]])).replace("None","null")
+				self.records = ', '.join(map(str, [str(tuple(list(spare_record)+[self.contract_quote_record_id, self.contract_quote_revision_record_id])) for spare_record in xls_spare_records[1:]])).replace("None","null")
 				
 			# for index, data in enumerate(list(sheet_data.Value)):
 			# 	if index == 0:
