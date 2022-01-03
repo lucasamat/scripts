11b5dad42ab7c86574d391aa29982d6d4e980b07
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2022-01-03 18:01:58 +0530

    Update CQASSMEDIT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQASSMEDIT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQASSMEDIT.py
@@ -400,24 +400,27 @@ def entitlement_update(whereReq=None,add_where=None,AttributeID=None,NewValue=No
 						ent_disp_val = get_display_val.STANDARD_ATTRIBUTE_DISPLAY_VAL 
 				elif PRODUCT_ATTRIBUTES.ATT_DISPLAY_DESC in ('Check Box') and ent_disp_val and ent_val_code:
 					Trace.Write('ent_val_code--'+str(type(ent_val_code))+'---'+str(ent_val_code))
-					#if type(eval(str(ent_val_code))) is list:
-					ent_val = str(tuple(ent_val_code)).replace(',)',')')
-					get_display_val = Sql.GetList("SELECT STANDARD_ATTRIBUTE_DISPLAY_VAL  from STANDARD_ATTRIBUTE_VALUES S INNER JOIN ATTRIBUTE_DEFN (NOLOCK) A ON A.STANDARD_ATTRIBUTE_CODE=S.STANDARD_ATTRIBUTE_CODE WHERE S.STANDARD_ATTRIBUTE_CODE = '{}' AND A.SYSTEM_ID = '{}' AND S.STANDARD_ATTRIBUTE_VALUE in {} ".format(STANDARD_ATTRIBUTE_VALUES.STANDARD_ATTRIBUTE_CODE,attrs,  ent_val ) )
-					if get_display_val:
-						ent_disp_val = [i.STANDARD_ATTRIBUTE_DISPLAY_VAL for i in get_display_val if i.STANDARD_ATTRIBUTE_DISPLAY_VAL]
-						#ent_disp_val = str(ent_disp_val).replace("'", '"')
-						#ent_val_code = str(ent_val_code).replace("'", '"')
-						ent_disp_val = ','.join(ent_disp_val)
-						ent_val_code = ','.join(ent_val_code)
+					if type(eval(str(ent_val_code))) is list:
+						ent_val = str(tuple(ent_val_code)).replace(',)',')')
+						get_display_val = Sql.GetList("SELECT STANDARD_ATTRIBUTE_DISPLAY_VAL  from STANDARD_ATTRIBUTE_VALUES S INNER JOIN ATTRIBUTE_DEFN (NOLOCK) A ON A.STANDARD_ATTRIBUTE_CODE=S.STANDARD_ATTRIBUTE_CODE WHERE S.STANDARD_ATTRIBUTE_CODE = '{}' AND A.SYSTEM_ID = '{}' AND S.STANDARD_ATTRIBUTE_VALUE in {} ".format(STANDARD_ATTRIBUTE_VALUES.STANDARD_ATTRIBUTE_CODE,attrs,  ent_val ) )
+						if get_display_val:
+							ent_disp_val = [i.STANDARD_ATTRIBUTE_DISPLAY_VAL for i in get_display_val if i.STANDARD_ATTRIBUTE_DISPLAY_VAL]
+							#ent_disp_val = str(ent_disp_val).replace("'", '"')
+							#ent_val_code = str(ent_val_code).replace("'", '"')
+							ent_disp_val = ','.join(ent_disp_val)
+							ent_val_code = ','.join(ent_val_code)
+						else:
+							ent_disp_val = ent_val_code =''
+						
 					else:
-						ent_disp_val = ent_val_code =''
-					Trace.Write('ent_val_code-1111-'+str(type(ent_val_code))+'---'+str(ent_val_code))
-					
-					else:
+						
 						get_display_val = Sql.GetFirst("SELECT STANDARD_ATTRIBUTE_DISPLAY_VAL  from STANDARD_ATTRIBUTE_VALUES S INNER JOIN ATTRIBUTE_DEFN (NOLOCK) A ON A.STANDARD_ATTRIBUTE_CODE=S.STANDARD_ATTRIBUTE_CODE WHERE S.STANDARD_ATTRIBUTE_CODE = '{}' AND A.SYSTEM_ID = '{}' AND S.STANDARD_ATTRIBUTE_VALUE = '{}' ".format(STANDARD_ATTRIBUTE_VALUES.STANDARD_ATTRIBUTE_CODE,attrs,  attributevalues[attrs] ) )
 						if get_display_val:
 							ent_disp_val = str(str(get_display_val.STANDARD_ATTRIBUTE_DISPLAY_VAL).split("'") ).replace("'", '"')
 							ent_val_code = str(str(ent_val_code).split(',') ).replace("'", '"')
+							ent_disp_val = ','.join(ent_disp_val)
+							ent_val_code = ','.join(ent_val_code)
+							Trace.Write('ent_val_code--'+str(ent_disp_val)+'---'+str(ent_val_code))
 				else:
 					#Trace.Write(str(AttributeID)+'---369--attrs---'+str(attrs))
 					Trace.Write('369-NewValue-370----'+str(attrs)+'-----'+str(NewValue)+str(ent_val_code))
