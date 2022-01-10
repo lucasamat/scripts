b88028ff2e8f62d87481cf16cca7f5ed6b18d1a0
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2022-01-10 10:59:39 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -2756,7 +2756,7 @@ class SYLDRTLIST:
 						elif ObjectName == "SAQDOC":
 							contract_quote_rec_id = Quote.GetGlobal("contract_quote_record_id")
 							quote_revision_rec_id = Quote.GetGlobal("quote_revision_record_id")
-							docnode_action_btn = Sql.GetFirst("SELECT * FROM SAQDOC WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' AND QUOTE_DOCUMENT_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_rec_id,ik.QUOTE_DOCUMENT_RECORD_ID))
+							docnode_action_btn = Sql.GetFirst("SELECT DOCUMENT_DESCRIPTION FROM SAQDOC WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' AND QUOTE_DOCUMENT_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_rec_id,ik.QUOTE_DOCUMENT_RECORD_ID))
 							if docnode_action_btn:
 								if str(docnode_action_btn.DOCUMENT_DESCRIPTION) == "" or docnode_action_btn.DOCUMENT_DESCRIPTION == "":
 									Trace.Write("edit=====")
@@ -8386,7 +8386,7 @@ class SYLDRTLIST:
 					elif ObjectName == "SAQDOC":
 						contract_quote_rec_id = Quote.GetGlobal("contract_quote_record_id")
 						quote_revision_rec_id = Quote.GetGlobal("quote_revision_record_id")
-						docnode_action_btn = Sql.GetFirst("SELECT * FROM SAQDOC WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' AND QUOTE_DOCUMENT_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_rec_id,ik.QUOTE_DOCUMENT_RECORD_ID))
+						docnode_action_btn = Sql.GetFirst("SELECT DOCUMENT_DESCRIPTION FROM SAQDOC WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' AND QUOTE_DOCUMENT_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),quote_revision_rec_id,ik.QUOTE_DOCUMENT_RECORD_ID))
 						if str(docnode_action_btn.DOCUMENT_DESCRIPTION) == "":
 							Action_str += '<li><a id = "" class="dropdown-item" href="#" " onclick="doc_edit_desc(this)">EDIT DESC</a></li>'						    
 
