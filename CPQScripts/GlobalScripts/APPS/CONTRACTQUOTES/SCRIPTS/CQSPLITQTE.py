61788d6cdc885b7b4ad78b8596223feead1b5380
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-01-11 17:46:41 +0530

    Update CQSPLITQTE.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSPLITQTE.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSPLITQTE.py
@@ -556,3 +556,24 @@ Sql.RunQuery("""UPDATE SAQTRV
 						WHERE SAQTRV.QUOTE_RECORD_ID = '{contract_quote_rec_id}' AND SAQTRV.QUOTE_REVISION_RECORD_ID = '{quote_revision_rec_id}' """.format(contract_quote_rec_id = contract_quote_rec_id,quote_revision_rec_id = quote_revision_rec_id) )
 
 splitserviceinsert()
+
+
+
+LOGIN_CREDENTIALS = SqlHelper.GetFirst("SELECT USER_NAME as Username,Password,Domain FROM SYCONF where Domain='AMAT_TST'")
+if LOGIN_CREDENTIALS is not None:
+	Login_Username = str(LOGIN_CREDENTIALS.Username)
+	Login_Password = str(LOGIN_CREDENTIALS.Password)
+	authorization = Login_Username+":"+Login_Password
+	binaryAuthorization = UTF8.GetBytes(authorization)
+	authorization = Convert.ToBase64String(binaryAuthorization)
+	authorization = "Basic " + authorization
+
+
+	webclient = System.Net.WebClient()
+	webclient.Headers[System.Net.HttpRequestHeader.ContentType] = "application/json"
+	webclient.Headers[System.Net.HttpRequestHeader.Authorization] = authorization;
+	
+	result = '''<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope	xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">	<soapenv:Body><CPQ_Columns>	<QUOTE_ID>{Qt_Id}</QUOTE_ID><REVISION_ID>{Rev_Id}</REVISION_ID></CPQ_Columns></soapenv:Body></soapenv:Envelope>'''.format( Qt_Id= contract_quote_rec_id,Rev_Id = quote_revision_rec_id)
+	
+	LOGIN_CRE = SqlHelper.GetFirst("SELECT URL FROM SYCONF where EXTERNAL_TABLE_NAME ='BILLING_MATRIX_ASYNC'")
+	Async = webclient.UploadString(str(LOGIN_CRE.URL), str(result))
