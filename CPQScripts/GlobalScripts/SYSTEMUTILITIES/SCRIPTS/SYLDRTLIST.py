760de33243d81e09ac27292ea297bda7f4475637
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-01-18 18:07:55 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -4415,16 +4415,16 @@ class SYLDRTLIST:
 		if RECORD_ID == 'SYOBJR-34575':
 			grouping_columns_delivery = ""
 			#if table_group_columns:
-			#Trace.Write('table_group_columns_delivery---'+str(table_group_columns_delivery))
+			Trace.Write('table_group_columns_delivery---'+str(table_group_columns_delivery))
 			
-			grouping_columns_delivery += table_group_columns_delivery
+			#grouping_columns_delivery += table_group_columns_delivery
 			# if table_group_columns_delivery2:
 			# 	Trace.Write('table_group_columns_delivery2---'+str(table_group_columns_delivery2))
 			# 	grouping_columns_delivery += table_group_columns_delivery2
 
 				
-			Trace.Write('grouping_columns_delivery---'+str(grouping_columns_delivery))
-			table_header += "<tr >{}</tr>".format(grouping_columns_delivery)
+			#Trace.Write('grouping_columns_delivery---'+str(grouping_columns_delivery))
+			#table_header += "<tr >{}</tr>".format(grouping_columns_delivery)
 		if RECORD_ID == 'SYOBJR-00009':
 			cls = "eq(3)"
 			table_header += '</thead><tbody onclick="Table_Onclick_Scroll(this)"></tbody></table>'
