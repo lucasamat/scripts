33b782081793ef098e20e9a70428f5d457a7baad
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-01-18 17:56:14 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -4426,7 +4426,7 @@ class SYLDRTLIST:
 
 				
 			Trace.Write('grouping_columns_delivery---'+str(grouping_columns_delivery))
-			table_header += "<tr >{}</tr>".format(grouping_columns_delivery)
+			#table_header += "<tr >{}</tr>".format(grouping_columns_delivery)
 		if RECORD_ID == 'SYOBJR-00009':
 			cls = "eq(3)"
 			table_header += '</thead><tbody onclick="Table_Onclick_Scroll(this)"></tbody></table>'
