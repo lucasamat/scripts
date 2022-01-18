8f1bfc8c52c29cf5a37421acf515a77edef9bb66
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-01-18 20:03:46 +0530

    Update CQINSQTITM.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQINSQTITM.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQINSQTITM.py
@@ -909,7 +909,7 @@ class ContractQuoteItem:
 				""".format(UserId=self.user_id, UserName=self.user_name, ObjectName=self.source_object_name, QuoteRecordId=self.contract_quote_record_id, QuoteRevisionRecordId=self.contract_quote_revision_record_id, ServiceId=self.service_id, JoinConditionString=join_condition_string, dynamic_is_changed_value = dynamic_is_changed_value, dynamic_group_id_value = dynamic_group_id_value))
 		return True
 
-	def _quote_items_fpm_entitlement_insert(self, update=False):		
+    def _quote_items_fpm_entitlement_insert(self, update=False):		
 		join_condition_string = ''
 		dynamic_group_id_value = 'null as ENTITLEMENT_GROUP_ID'
 		dynamic_is_changed_value = 'null as IS_CHANGED'
@@ -945,7 +945,7 @@ class ContractQuoteItem:
 						SAQRIT.QTEREV_RECORD_ID						
 					FROM {ObjectName} (NOLOCK) 
 					JOIN SAQRIT (NOLOCK) ON SAQRIT.QUOTE_RECORD_ID = {ObjectName}.QUOTE_RECORD_ID
-												AND SAQRIT.SERVICE_RECORD_ID = {ObjectName}.SERVICE_RECORD_ID
+												AND SAQRIT.SERVICE_ID = {ObjectName}.SERVICE_ID
 												AND SAQRIT.QTEREV_RECORD_ID = {ObjectName}.QTEREV_RECORD_ID		
 												{JoinConditionString}			
 					WHERE {ObjectName}.QUOTE_RECORD_ID = '{QuoteRecordId}' AND {ObjectName}.QTEREV_RECORD_ID = '{QuoteRevisionRecordId}' AND {ObjectName}.SERVICE_ID = '{ServiceId}' AND ISNULL({ObjectName}.CONFIGURATION_STATUS,'') = 'COMPLETE'			
@@ -976,7 +976,7 @@ class ContractQuoteItem:
 						SAQRIT.QTEREV_RECORD_ID
 					FROM {ObjectName} (NOLOCK) 
 					JOIN SAQRIT (NOLOCK) ON SAQRIT.QUOTE_RECORD_ID = {ObjectName}.QUOTE_RECORD_ID
-												AND SAQRIT.SERVICE_RECORD_ID = {ObjectName}.SERVICE_RECORD_ID
+												AND SAQRIT.SERVICE_ID = {ObjectName}.SERVICE_ID
 												AND SAQRIT.QTEREV_RECORD_ID = {ObjectName}.QTEREV_RECORD_ID	
 												{JoinConditionString}			
 					WHERE {ObjectName}.QUOTE_RECORD_ID = '{QuoteRecordId}' AND {ObjectName}.QTEREV_RECORD_ID = '{QuoteRevisionRecordId}' AND {ObjectName}.SERVICE_ID = '{ServiceId}' AND ISNULL({ObjectName}.CONFIGURATION_STATUS,'') = 'COMPLETE'			
