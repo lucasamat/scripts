11502ea452a707a90022b524b0cd55f755e9d8f6
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2022-01-19 16:36:57 +0530

    Update SYBLKETRLG.py
    traces

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYBLKETRLG.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYBLKETRLG.py
@@ -634,9 +634,10 @@ def RELATEDMULTISELECTONEDIT(TITLE, VALUE, CLICKEDID, RECORDID,SELECTALL):
 		TITLE = "BILLING_DATE"
 	#if str(CLICKEDID) == "SYOBJR_00009_E5504B40_36E7_4EA6_9774_EA686705A63F" and (TreeParentParam != 'Quote Items' and TreeParentParam != ''):
 	#canedit = "FALSE"	
-	Trace.Write("@175----canedit-------->"+str(canedit))
-	if str(Product.GetGlobal("TreeParentLevel0")=="Complementary Products" and TITLE == "CUSTOMER_PART_NUMBER" and VALUE==''):
+	Trace.Write("@175----canedit,value-------->"+str(canedit)+','+str(VALUE))
+	if str(Product.GetGlobal("TreeParentLevel0")=="Complementary Products" and TITLE == "CUSTOMER_PART_NUMBER" and str(VALUE)==''):
 		edt_str = "NO"
+		Trace.Write('###Empty value')
 	elif objh_obj is not None and str(canedit).upper() == "TRUE":
 		obj_obj = str(objh_obj.OBJECT_NAME)
 		Trace.Write("object name--"+str(obj_obj))
