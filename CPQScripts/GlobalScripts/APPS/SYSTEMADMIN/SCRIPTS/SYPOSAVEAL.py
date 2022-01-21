f7bf1619accdf27bc8f02f68cd7530354649f10b
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-01-21 18:12:20 +0530

    Update SYPOSAVEAL.py

--- CPQScripts/GlobalScripts/APPS/SYSTEMADMIN/SCRIPTS/SYPOSAVEAL.py
+++ CPQScripts/GlobalScripts/APPS/SYSTEMADMIN/SCRIPTS/SYPOSAVEAL.py
@@ -199,7 +199,7 @@ def do_process(TABLEID, LABLE, VALUE):
                         # quote_val=Sql.GetFirst("SELECT MASTER_TABLE_QUOTE_RECORD_ID,QUOTE_NAME FROM SAQTMT WHERE QUOTE_ID = '"+row["QUOTE_ID"]+"'")
 
                         quote_val=Sql.GetFirst("SELECT MASTER_TABLE_QUOTE_RECORD_ID,QUOTE_NAME,QTEREV_RECORD_ID,QTEREV_ID FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID = '"+str(ContractRecordId)+"' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id)+"'  ")
-					    Sql.RunQuery("UPDATE SAQTIP SET [PRIMARY] = 'false' WHERE PARTY_ROLE = 'SHIP TO' AND QUOTE_RECORD_ID = '{qte_rec_id}' AND QTEREV_RECORD_ID = '{qte_rev_id}'".format(qte_rec_id=quote_val.MASTER_TABLE_QUOTE_RECORD_ID,qte_rev_id=quote_val.QTEREV_RECORD_ID))
+                        Sql.RunQuery("UPDATE SAQTIP SET [PRIMARY] = 'false' WHERE PARTY_ROLE = 'SHIP TO' AND QUOTE_RECORD_ID = '{qte_rec_id}' AND QTEREV_RECORD_ID = '{qte_rev_id}'".format(qte_rec_id=quote_val.MASTER_TABLE_QUOTE_RECORD_ID,qte_rev_id=quote_val.QTEREV_RECORD_ID))
                         row["QUOTE_RECORD_ID"]=quote_val.MASTER_TABLE_QUOTE_RECORD_ID
                         row["QUOTE_NAME"]=quote_val.QUOTE_NAME
                         row["QTEREV_RECORD_ID"]=quote_val.QTEREV_RECORD_ID
