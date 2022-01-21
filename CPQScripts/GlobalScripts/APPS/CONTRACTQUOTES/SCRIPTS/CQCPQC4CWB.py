c55c3a1cf3aa0ba2c6d30d69cb5ec6462ea59460
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2022-01-21 17:53:20 +0530

    Update CQCPQC4CWB.py
    Removed the ExpectedProcessingEndDate field from the writeback script

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCPQC4CWB.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCPQC4CWB.py
@@ -81,7 +81,7 @@ def writeback_to_c4c(writeback,contract_quote_record_id,quote_revision_record_id
         
         
         ##opportunity header write back details starts...
-        opportunity_header_data = '{\"ExpectedRevenueAmount\":"'+str(net_value)+'", \"ExpectedRevenueAmountCurrencyCode\":"USD", \"ExpectedProcessingStartDate\":"'+str(valid_from)+'",\"ExpectedProcessingEndDate\":"'+str(valid_from)+'", \"ExpectedRevenueStartDate\":"'+str(valid_from)+'", \"ExpectedRevenueEndDate\":"'+str(valid_to)+'", \"ZQuoteRevisionStatus_KUT\":"'+str(revision_status_code.get(revision_obj.REVISION_STATUS))+'"}'
+        opportunity_header_data = '{\"ExpectedRevenueAmount\":"'+str(net_value)+'", \"ExpectedRevenueAmountCurrencyCode\":"USD", \"ExpectedProcessingStartDate\":"'+str(valid_from)+'", \"ExpectedRevenueStartDate\":"'+str(valid_from)+'", \"ExpectedRevenueEndDate\":"'+str(valid_to)+'", \"ZQuoteRevisionStatus_KUT\":"'+str(revision_status_code.get(revision_obj.REVISION_STATUS))+'"}'
         ##opportunity header write back details ends...
         
         requestdata = (
