8fff942fee1c7d04e28109196afde44218cd1f0e
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2022-01-21 16:59:39 +0530

    Update QTPOSTPTPR.py
    added logs

--- CPQScripts/IntegrationScripts/QTPOSTPTPR.py
+++ CPQScripts/IntegrationScripts/QTPOSTPTPR.py
@@ -102,7 +102,7 @@ try:
 			contract_quote_record_id = None				
 
 
-			Log.Info("456 type(price) --->")
+			Log.Info("456 type(price) --->"+str(price))
 			if str(type(price)) == "<type 'Dictionary[str, object]'>":
 				#Log.Info("type condition--->")
 				price = [price]
@@ -172,12 +172,15 @@ try:
 				core_credit_amount = ''
 				try:
 					cust_participate = Sql.GetFirst("SELECT CUSTOMER_PARTICIPATE FROM SAQSPT WHERE QUOTE_RECORD_ID = '"+str(contract_quote_record_id)+"' AND QTEREV_RECORD_ID = '"+str(revision_rec_id)+"' AND PART_NUMBER = '"+str(Itemidinfo[0])+"' ")
+					Log.Info('###At line 175')
 					if cust_participate and str(cust_participate.CUSTOMER_PARTICIPATE).upper() == "TRUE":
+						Log.Info('###At line 177')
 						response1 = response1.replace("null","''")
 						response1 = response1.replace("true","'TRUE'")
 						response1 = response1.replace("false","'FALSE'")
 						response1 = eval(response1)
 						conditions = response1['items'][0]['conditions']
+						Log.Info('###At line 181')
 						for condition in conditions:
 							if condition['conditionType'] == "ZERU":
 								core_credit_amount = condition['conditionValue']
