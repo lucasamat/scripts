1437a0f0324d8a525e572f73e8f2759d5e2df912
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2022-01-24 14:10:30 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -2793,7 +2793,7 @@ def Related_Sub_Banner(
                                         sec_rel_sub_bnr += str(btn)
                         else:##Added the else part for the multi buttons scenario.....
                             for btn in multi_buttons:
-                                if "INLINE EDIT" not in btn:
+                                if "INLINE EDIT" not in btn and Treeparam != "Fab Locations":
                                     Trace.Write("ADD_FAB====")
                                     sec_rel_sub_bnr += str(btn)
                             
