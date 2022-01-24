f27d823da3d671d9d3a342684c3e12964abf9009
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2022-01-24 14:18:37 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -2795,7 +2795,8 @@ def Related_Sub_Banner(
                             for btn in multi_buttons:
                                 #if "INLINE EDIT" not in btn and TreeParam != "Fab Locations":
                                     #Trace.Write("ADD_FAB====")
-                                sec_rel_sub_bnr += str(btn)
+                                if TreeParam != "Fab Loctions":
+                                    sec_rel_sub_bnr += str(btn)
                             
                                 
 
