2e19d0940e778a6300795cfa1577da41670fb156
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2022-01-25 12:19:44 +0530

    Update CQUPDLNITM.py
    Removed Unwanted Libraries and Codes

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQUPDLNITM.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQUPDLNITM.py
@@ -1,27 +1,25 @@
 # __script_name : CQUPDLNITM .PY
-# __script_description : THIS SCRIPT IS used to load additional data for quote line items
+# __script_description : This script is used to load additional data for quote line items
 # __primary_author__ :Dhurga
 # __create_date :7/29/2021
 # © BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
 # ==========================================================================================================================================
-import re
+
 import datetime
+
 gettodaydate = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S %p")
-def getData():
-    Quotelinecommments = Quote.QuoteTables["SAQICD"]
-    data = []
-    sec_list = []
+
+def getData():    
+    data = []    
     counter = 0
     data_info = ''
     tr_class = 'editable'
-    section_row1 = {}
-    CrtId = TagParserProduct.ParseString("<*CTX( Quote.CartId )*>")
-    Ownrid = TagParserProduct.ParseString("<*CTX( Quote.OwnerId )*>")
+    section_row1 = {}    
     quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")
     QuoteNumber=Quote.GetGlobal("contract_quote_record_id")
     getquoteid = SqlHelper.GetFirst("select QUOTE_ID from SAQTMT where MASTER_TABLE_QUOTE_RECORD_ID = '"+str(QuoteNumber)+"' and QTEREV_RECORD_ID ='"+str(quote_revision_record_id)+"' ")
     for item in Quote.Items :
-        rolled_up_id = factor_id =rate = con_type = con_uom = con_rec = ""
+        rolled_up_id = rate = con_type = con_uom = con_rec = ""
         section_row1[str("dyn_" + str(item.Rank))] = ""
         Queryload = SqlHelper.GetList("select ownerId,cartId,CONDITION_COUNTER,CONDITION_CURRENCY,CONDITION_DATA_TYPE,CONDITION_RATE,CONDITION_TYPE,CONDITIONTYPE_NAME,CONDITIONTYPE_RECORD_ID,UOM,CONDITION_VALUE,UOM_RECORD_ID,LINE,QUOTE_ID,QTEITM_RECORD_ID,QUOTE_NAME,SERVICE_DESCRIPTION,SERVICE_ID,STEP_NUMBER,SERVICE_RECORD_ID,QUOTE_RECORD_ID,CONDITION_BASE from QT__SAQICD where QUOTE_ID = '"+str(getquoteid.QUOTE_ID)+"'  and SERVICE_ID= '"+str(item.PartNumber)+"' and QTEREV_RECORD_ID ='"+str(quote_revision_record_id)+"' ")
         if Queryload and counter == 0:
@@ -48,6 +46,7 @@ def getData():
         data.append({rolled_up_id:data_info})
     Trace.Write(str(data))
     return data
+    
 LoadAction = Param.LoadAction
 if LoadAction == "LOADQT":
     ApiResponse = ApiResponseFactory.JsonResponse(getData())
