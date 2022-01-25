cdef4a899a7423e92e669cfb26909228a85eb6a1
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-01-25 12:06:01 +0530

    Create CQWBAPPC4C.py
    NEW SCRIPT FOR APPROVERS

---
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQWBAPPC4C.py
@@ -0,0 +1,29 @@
+# =========================================================================================================================================
+#   __script_name : CQWBAPPC4C.PY
+#   __script_description : TO UPDATE THE OPPORTUNITY STATUS IN  CPQ WHEN THE USER IS CHANGING THE STATUS IN C4C.
+#   __primary_author__ : WASIM
+#   __create_date :25-01-2022
+#   © BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
+# ==========================================================================================================================================
+
+from SYDATABASE import SQL
+Sql = SQL()
+import CQCPQC4CWB
+import sys
+
+if 'Param' in globals():
+    Log.Info("CQWBAPPC4C called......."+str(Param)+"paramsss---"+str(hasattr(Param, 'CPQ_Columns')))	
+    if hasattr(Param, 'CPQ_Columns'):
+        Log.Info("CQWBAPPC4C called.......inside if")    
+        
+        approval_object_data = [str(param_result.Value) for param_result in Param.CPQ_Columns]
+        object_id = approval_object_data[0]
+        quote_id = str(approval_object_data[-1]).upper()
+        approver_id = str(approval_object_data[-2]).upper()
+
+        
+    else:
+        Log.Info("else condition")
+            
+            
+            
