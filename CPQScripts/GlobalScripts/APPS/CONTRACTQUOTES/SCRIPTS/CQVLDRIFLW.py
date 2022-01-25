eb57686f1d9deca8e57f23deee1b5e5fae4751b7
Ayyappan Subramaniyan --CNTR <Ayyappan_Subramaniyan@contractor.amat.com>
2022-01-25 12:08:50 +0530

    Update CQVLDRIFLW.py
    Removed Unwanted Libraries and Codes

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQVLDRIFLW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQVLDRIFLW.py
@@ -9,18 +9,10 @@ import Webcom.Configurator.Scripting.Test.TestProduct
 import clr
 import System.Net
 import sys
-#import sys
-import datetime
-#import clr
-#import System.Net
-from System.Text.Encoding import UTF8
-from System import Convert
-#from SYDATABASE import SQL
 from SYDATABASE import SQL
 
 Sql = SQL()
-def iflow_valuedriver_rolldown(quote,level,ancillary_dict=''):
-    #Trace.Write("1111111111111111     " + str(username))
+def iflow_valuedriver_rolldown(quote,level,ancillary_dict=''):    
     requestdata = (
         '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body><CPQ_Columns><Quote>'
         + str(quote)
@@ -29,8 +21,7 @@ def iflow_valuedriver_rolldown(quote,level,ancillary_dict=''):
         +"</Level><Ancillary_dict>"
 		+str(ancillary_dict)
 		+"</Ancillary_dict></CPQ_Columns></soapenv:Body></soapenv:Envelope>"
-    )
-    Log.Info("2222222222222222     ENT-------------- " + str(requestdata))
+    )    
     LOGIN_CREDENTIALS = SqlHelper.GetFirst("SELECT URL FROM SYCONF where External_Table_Name='SAQTVD'")
     LOGIN_QUERY = SqlHelper.GetFirst("SELECT User_name as Username,Password,Domain,URL FROM SYCONF where Domain='AMAT_TST'")
     if LOGIN_CREDENTIALS is not None:
@@ -44,8 +35,7 @@ def iflow_valuedriver_rolldown(quote,level,ancillary_dict=''):
         from System import Convert
 
         authorization = Convert.ToBase64String(binaryAuthorization)
-        authorization = "Basic " + authorization
-    Log.Info("testing......IFLOW ENT.......")
+        authorization = "Basic " + authorization    
     webclient = System.Net.WebClient()
     webclient.Headers[System.Net.HttpRequestHeader.ContentType] = "application/xml"
     webclient.Headers[System.Net.HttpRequestHeader.Authorization] = authorization
