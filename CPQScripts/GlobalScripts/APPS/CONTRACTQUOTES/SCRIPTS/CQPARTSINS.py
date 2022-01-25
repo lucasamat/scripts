3bbd7121d684e7996b90ac353c97615aabd871c3
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2022-01-25 15:12:29 +0530

    Update CQPARTSINS.py
    commented

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTSINS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTSINS.py
@@ -6,7 +6,7 @@
 #   © BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
 # ==========================================================================================================================================
 #from logging import exception
-from ast import Param
+#from ast import Param
 import Webcom.Configurator.Scripting.Test.TestProduct
 from SYDATABASE import SQL
 import clr
