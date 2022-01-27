4843be1acceffdd4f75b3fc8a8f51e8ce7b7d63a
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2022-01-27 18:58:26 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -546,7 +546,7 @@ class SYLDRTLIST:
 						attr_list[str(attr.API_NAME)] = str(attr.FIELD_LABEL)
 					#Trace.Write("attr_list_j"+str(attr_list))
 					val_type=type(attr.LOOKUP_API_NAME)
-					if (attr.LOOKUP_API_NAME and  str(val_type)!="<type 'DBNull>'" and 
+					if (attr.LOOKUP_API_NAME and str(val_type)!="<type 'DBNull>'" and 
 						str(attr.LOOKUP_API_NAME) != ""
 						and str(attr.LOOKUP_API_NAME) is not None
 						and str(attr.LOOKUP_API_NAME) not in ["CONTROLLING_FIELD", "DEPENDENT_FIELD"]
