1fd1a5dbd4c4b4d6de9e49c39d116da1a97a99da
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-01-27 19:42:56 +0530

    Update CQCRUDOPTN.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -5522,6 +5522,7 @@ class ContractQuoteCoveredObjModel(ContractQuoteCrudOpertion):
 							Trace.Write("555 "+str(self.tree_param)+" 555 "+str(pm_event_attribute_value))
 							if (self.tree_param in ("Z0035","Z0091","Z0009","Z0004") and "Included - All PM" in pm_event_attribute_value):
 								additional_where = "AND MAEAPK.PM_LEVEL == 'Chamber / Module PM' AND MAEAPK.PM_LEVEL = 'Scheduled Maintenance'"
+								Trace.Write("additional_where_chk_1 "+str(additional_where))
 							elif (self.tree_param in ("Z0035","Z0091",) and "Included - Monthly and Above" in pm_event_attribute_value):
 								additional_where = "AND MAEAPK.PM_LEVEL == 'Chamber / Module PM' AND MAEAPK.PM_LEVEL = 'Scheduled Maintenance' AND MAEAPK.PM_ID = 'Monthly' AND MAEAPK.PM_ID = 'Quarterly' AND MAEAPK.PM_ID = 'Semi-Annual' AND MAEAPK.PM_ID = 'Annual'"
 							elif (self.tree_param in ("Z0092","Z0099") and "Included - Quarterly and Above" in pm_event_attribute_value):
