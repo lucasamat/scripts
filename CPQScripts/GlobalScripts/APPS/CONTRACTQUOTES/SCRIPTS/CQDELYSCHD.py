1a06dec5441a1d9e3a3836401a8272b5ef20e518
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-01-27 13:29:24 +0530

    Update CQDELYSCHD.py

---
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQDELYSCHD.py
@@ -0,0 +1,15 @@
+# =========================================================================================================================================
+#   __script_name : CQDELYSCHD.PY
+#   __script_description : THIS SCRIPT IS USED TO  update,delete, insert in delivery schedule based on quantiy and delivery schedule change
+#   __create_date : 27/01/2022
+#   Â© BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
+# ==========================================================================================================================================
+import re
+import Webcom.Configurator.Scripting.Test.TestProduct
+from SYDATABASE import SQL
+import datetime
+import sys
+import System.Net
+
+Param = Param 
+Sql = SQL()
