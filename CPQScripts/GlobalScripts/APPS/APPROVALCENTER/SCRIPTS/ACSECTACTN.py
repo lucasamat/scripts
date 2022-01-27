3e5aa5075df6d3ca2c0d2bcfe4a21f219f763c3c
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-01-27 11:30:19 +0530

    Update ACSECTACTN.py
    Commit as instead , in exception

--- CPQScripts/GlobalScripts/APPS/APPROVALCENTER/SCRIPTS/ACSECTACTN.py
+++ CPQScripts/GlobalScripts/APPS/APPROVALCENTER/SCRIPTS/ACSECTACTN.py
@@ -516,7 +516,7 @@ class approvalCenter:
 				##Calling the iflow script to update the details in c4c..(cpq to c4c write back...)
 				CQCPQC4CWB.writeback_to_c4c("quote_header",Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
 				CQCPQC4CWB.writeback_to_c4c("opportunity_header",Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
-			except Exception, e:
+			except Exception as e:
 				Trace.Write("EXCEPTION: QUOTE WRITE BACK "+str(e))
 			#iflow for approver insert
 			contract_quote_id = Sql.GetFirst("Select QUOTE_ID FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
@@ -1230,7 +1230,7 @@ class approvalCenter:
 					##Calling the iflow script to update the details in c4c..(cpq to c4c write back...)
 					CQCPQC4CWB.writeback_to_c4c("quote_header",Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
 					CQCPQC4CWB.writeback_to_c4c("opportunity_header",Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
-				except Exception, e:
+				except Exception as e:
 					Trace.Write("EXCEPTION: QUOTE WRITE BACK "+str(e))
 				#iflow for approver insert
 				contract_quote_id = Sql.GetFirst("Select QUOTE_ID FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
@@ -1387,7 +1387,7 @@ class approvalCenter:
 				##Calling the iflow script to update the details in c4c..(cpq to c4c write back...)
 				CQCPQC4CWB.writeback_to_c4c("quote_header",Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
 				CQCPQC4CWB.writeback_to_c4c("opportunity_header",Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
-			except Exception, e:
+			except Exception as e:
 				Trace.Write("EXCEPTION: QUOTE WRITE BACK " +str(e))
 			#iflow for approver insert
 			contract_quote_id = Sql.GetFirst("Select QUOTE_ID FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
@@ -1534,7 +1534,7 @@ class approvalCenter:
 							CQCPQC4CWB.writeback_to_c4c("quote_header",Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
 							CQCPQC4CWB.writeback_to_c4c("opportunity_header",Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
 
-						except Exception, e:
+						except Exception as e:
 							Trace.Write("EXCEPTION: QUOTE WRITE BACK "+str(e))
 						#iflow for approver insert
 						contract_quote_id = Sql.GetFirst("Select QUOTE_ID FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID = '{}'".format(Quote.GetGlobal("contract_quote_record_id"),Quote.GetGlobal("quote_revision_record_id"))
