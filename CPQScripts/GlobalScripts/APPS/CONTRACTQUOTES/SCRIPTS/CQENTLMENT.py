64cd37cccc13b45876635d8d6556630f60835b0c
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-01-28 14:12:23 +0530

    Update CQENTLMENT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLMENT.py
@@ -1163,7 +1163,7 @@ class Entitlements:
 									Trace.Write("Exception While running CQCRUDOPTN "+str(e))
 							Trace.Write("script called")
 						if key in ( "AGS_{}_NET_PRMALB".format(serviceId)) and str(tableName) in ('SAQTSE'):
-							Sql.RunQuery("DELETE FROM SAQSAP WHERE QUOTE_RECORD_ID = {} AND QTEREV_RECORD_ID = {} AND SERVICE_ID = {}".format(self.ContractRecordId,self.revision_recordid,serviceId))
+							Sql.RunQuery("DELETE FROM SAQSAP WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' AND SERVICE_ID = '{}'".format(self.ContractRecordId,self.revision_recordid,serviceId))
 							try:
 								ScriptExecutor.ExecuteGlobal(
 										"CQCRUDOPTN",
@@ -1175,7 +1175,7 @@ class Entitlements:
 								)
 							except Exception as e:
 								Trace.Write("Exception While running CQCRUDOPTN "+str(e))
-							Trace.Write("script called")
+							Trace.Write("script called J")
 						elif key == "AGS_{}_TSC_CUOWPN".format(serviceId) and serviceId in ("Z0091",'Z0092','Z0004','Z0009') :
 							#ancillary_object = 'A6200'
 							if entitlement_value.upper() == "YES":
