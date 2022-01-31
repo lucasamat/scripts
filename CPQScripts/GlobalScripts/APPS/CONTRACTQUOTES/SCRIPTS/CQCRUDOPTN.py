13fb8fae012f06538ea3a2b8d3a0fd9ddc767621
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2022-01-31 18:31:41 +0530

    Update CQCRUDOPTN.py
    CREDIT_APPLIED changed to CRTAPP_INGL_CURR in SACRVC update query

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -2488,7 +2488,7 @@ class ContractQuoteFabModel(ContractQuoteCrudOpertion):
 							unapplied = float(credit_details.WRBTR)-int(APPLIED_CREDITS[key]) if APPLIED_CREDITS[key]!='' else float(credit_details.WRBTR)
 						else:
 							unapplied = float(credit_details.UNAPPLIED_BALANCE)-int(APPLIED_CREDITS[key]) if APPLIED_CREDITS[key]!='' else float(credit_details.UNAPPLIED_BALANCE)
-						Sql.RunQuery("UPDATE SACRVC SET CREDIT_APPLIED = '{}', UNAPPLIED_BALANCE = '{}' WHERE CpqTableEntryId = '{}'".format(float(credit_details.CREDIT_APPLIED)- int(APPLIED_CREDITS[key]), unapplied, id))
+						Sql.RunQuery("UPDATE SACRVC SET CRTAPP_INGL_CURR = '{}', UNAPPLIED_BALANCE = '{}' WHERE CpqTableEntryId = '{}'".format(float(credit_details.CRTAPP_INGL_CURR)- int(APPLIED_CREDITS[key]), unapplied, id))
 					except Exception as e:
 						Trace.Write('EXCEPTION: '+str(e))
 						Trace.Write('APPLIED_CREDITS'+str(APPLIED_CREDITS))
