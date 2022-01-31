da658a7809fc95a89112dfbc41e06afb231e626c
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-01-31 14:21:14 +0530

    Create CQCPSTAXRE.py
    New script for CPS TAX CALL

---
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCPSTAXRE.py
@@ -0,0 +1,12 @@
+# =========================================================================================================================================
+#   __script_name : CQCPSTAXRE.PY
+#   __script_description : THIS SCRIPT IS USED TO RETURN TAX FOR PRODUCT OFFERINGS
+#   __primary_author__ : AYYAPPAN SUBRAMANIYAN
+#   __create_date :30-09-2021
+#   © BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED -
+# ==========================================================================================================================================
+import Webcom.Configurator.Scripting.Test.TestProduct
+import System.Net
+import re
+import datetime
+from SYDATABASE import SQL
