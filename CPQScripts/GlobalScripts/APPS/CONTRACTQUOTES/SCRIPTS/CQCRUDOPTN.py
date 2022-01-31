c66ca0d3a21c41e47932079201fedaf95c5c28db
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-01-31 13:21:23 +0530

    Update CQCRUDOPTN.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -5857,6 +5857,7 @@ class ContractQuoteCoveredObjModel(ContractQuoteCrudOpertion):
 											additional_where = " (MAEAPK.PM_LEVEL = 'Chamber / Module PM' OR MAEAPK.PM_LEVEL = 'Scheduled Maintenance') AND"
 											Trace.Write("additional_where_chk_1 "+str(additional_where))
 										self._insert_quote_service_preventive_maintenance_kit_parts(batch_group_record_id=batch_group_record_id,additional_where = additional_where)
+										
 							qte_type_flag=1
 						if self.tree_param == 'Z0009' and qte_type_flag == 1:
 							break
