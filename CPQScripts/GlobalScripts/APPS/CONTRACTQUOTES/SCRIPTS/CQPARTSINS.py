7e9427836ef0ae717c23fe4c0a775b077b21bcf9
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2022-01-31 17:56:45 +0530

    Update CQPARTSINS.py
    log

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTSINS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTSINS.py
@@ -40,6 +40,7 @@ class SyncFPMQuoteAndHanaDatabase:
         self.columns = re.sub(r',,',',',self.columns)
         Log.Info("Columns--->"+str(self.columns))
         Log.Info("Values---->"+str(self.records))
+        Log.Info("TempTableName--->"+str(spare_parts_temp_table_name))
         try:
             spare_parts_temp_table_drop = SqlHelper.GetFirst("sp_executesql @T=N'IF EXISTS (SELECT ''X'' FROM SYS.OBJECTS WHERE NAME= ''"+str(spare_parts_temp_table_name)+"'' ) BEGIN DROP TABLE "+str(spare_parts_temp_table_name)+" END  ' ")			
             spare_parts_temp_table_bkp = SqlHelper.GetFirst("sp_executesql @T=N'SELECT "+str(self.columns)+" INTO "+str(spare_parts_temp_table_name)+" FROM (SELECT DISTINCT "+str(self.columns)+" FROM (VALUES "+str(self.records)+") AS TEMP("+str(self.columns)+")) OQ ' ")
