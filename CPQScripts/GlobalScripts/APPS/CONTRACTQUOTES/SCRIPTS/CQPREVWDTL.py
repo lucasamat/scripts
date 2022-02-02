032e18e08e20344b315e77d4ce227932668caa12
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-02-02 15:18:27 +0530

    Update CQPREVWDTL.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
@@ -750,9 +750,9 @@ def savecbc(Qt_rec_id, Quote, MODE):
 
 def save_annualiziedgrid_inline(Quote,values, MODE):
 	Trace.Write("value===values"+str(values))
-	response =""
-	response = ','.join(str(ele) for ele in values)
-	Trace.Write("response"+str(response))
+	#response =""
+	#response = ','.join(str(ele) for ele in values)
+	#Trace.Write("response"+str(response))
 	for data in values:	
 		Trace.Write("data_values"+str(data))
 	
