795b7667b4536181ffb2a48bebd6f189bb67058e
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2022-02-02 18:38:02 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -4320,7 +4320,7 @@ class SYLDRTLIST:
 							)  
 						if invs == "LINE"and RECORD_ID != 'SYOBJR-98873' :
 							Trace.Write("CHKNG_J_07 "+str(qstring))
-							if RECORD_ID == "SYOBJR-98877" or RECORD_ID == "SYOBJR-98872":
+							if RECORD_ID == "SYOBJR-98877" or RECORD_ID == "SYOBJR-98872" or RECORD_ID == "SYOBJR-00009":
 								table_header += (
 									'<th  data-field="'
 									+ str(invs)
