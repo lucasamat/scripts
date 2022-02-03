d4641f8333e841c029bde41d98a0951ee52b8c8f
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-02-03 18:27:07 +0530

    Update CQCRUDOPTN.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -2481,8 +2481,8 @@ class ContractQuoteFabModel(ContractQuoteCrudOpertion):
 				credit_details = Sql.GetFirst("SELECT * FROM SACRVC WHERE CpqTableEntryId = '"+str(id)+"' ")
 				if APPLIED_CREDITS!='':
 					Trace.Write("crdit amt-"+str(APPLIED_CREDITS[key]))
-					if '-' not in APPLIED_CREDITS[key] :
-						APPLIED_CREDITS[key] = '-'+str(APPLIED_CREDITS[key])
+					# if '-' not in APPLIED_CREDITS[key] :
+					# 	APPLIED_CREDITS[key] = '-'+str(APPLIED_CREDITS[key])
 					try:
 						if float(credit_details.UNBL_INGL_CURR)==0 or float(credit_details.UNBL_INGL_CURR)=='':
 							unapplied = float(credit_details.WRBTR)-int(APPLIED_CREDITS[key]) if APPLIED_CREDITS[key]!='' else float(credit_details.WRBTR)
