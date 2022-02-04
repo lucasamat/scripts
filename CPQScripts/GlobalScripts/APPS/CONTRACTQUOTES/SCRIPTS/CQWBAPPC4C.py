a297445f5fdd1886447489473b175e1e166dc0a5
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-02-04 12:02:47 +0530

    Update CQWBAPPC4C.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQWBAPPC4C.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQWBAPPC4C.py
@@ -28,8 +28,9 @@ if 'Param' in globals():
         Log.Info("approver_approver_step_idid--"+str(approver_step_id)) 
         get_approver_id = Sql.GetFirst("SELECT *FROM SAEMPL(NOLOCK) WHERE C4C_EMPLOYEE_ID = '{approver_id}'".format(approver_id = approver_id))
         Log.Info("get_approver_id"+str(get_approver_id.EMPLOYEE_ID))
+        emp_approver_id = 'USR-'+str(get_approver_id.EMPLOYEE_ID)
         if get_approver_id:
-            update_obj_id="""UPDATE ACAPTX SET OWNER_ID = '{obj_id}' WHERE APRTRXOBJ_ID = '{quote_id}' AND APRCHNSTP_APPROVER_ID ='{emp_approver_id}' AND APRCHNSTP_ID ='{approver_step_id}'""".format(obj_id=obj_id,quote_id =quote_id,emp_approver_id = get_approver_id.EMPLOYEE_ID,approver_step_id =approver_step_id)
+            update_obj_id="""UPDATE ACAPTX SET OWNER_ID = '{obj_id}' WHERE APRTRXOBJ_ID = '{quote_id}' AND APRCHNSTP_APPROVER_ID ='{emp_approver_id}' AND APRCHNSTP_ID ='{approver_step_id}'""".format(obj_id=obj_id,quote_id =quote_id,emp_approver_id = emp_approver_id,approver_step_id =approver_step_id)
             Sql.RunQuery(update_obj_id) 
     else:
         Log.Info("else condition")
