aba88b0ea46b08951cac9a7efde64163b80bfa44
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-02-08 10:59:09 +0530

    Update CQWBAPPC4C.py
    Commit

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQWBAPPC4C.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQWBAPPC4C.py
@@ -28,7 +28,13 @@ if 'Param' in globals():
             Log.Info("object_id--"+str(obj_id))
             Log.Info("quote_id--"+str(quote_id))
             Log.Info("approver_id--"+str(approver_id))
-            emp_approver_id = 'USR-'+str(approver_id)
+            emp_id ="8000"
+            if emp_id in approver_id:
+                get_approver_emp_id = Sql.GetFirst("SELECT *FROM SAEMPL(NOLOCK) WHERE C4C_EMPLOYEE_ID = '{approver_id}'".format(approver_id = approver_id))
+                if get_approver_emp_id:
+                    emp_approver_id = 'USR-'+str(get_approver_emp_id.EMPLOYEE_ID)
+            else:        
+                emp_approver_id = 'USR-'+str(approver_id)
             #Log.Info("approver_approver_step_idid--"+str(approver_step_id)) 
             get_approver_id = Sql.GetFirst("SELECT * FROM ACAPTX(NOLOCK) WHERE APRCHNSTP_APPROVER_ID = '{emp_approver_id}' AND OWNER_ID = '' AND APRTRXOBJ_ID = '{quote_id}' ".format(emp_approver_id = emp_approver_id,quote_id =quote_id))
             # Log.Info("get_approver_id"+str(get_approver_id.EMPLOYEE_ID))
