2cb37f1027d5c68abb3c3971cfec84e25598af93
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-02-08 09:32:02 +0530

    Update CQGNTDOCPF.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQGNTDOCPF.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQGNTDOCPF.py
@@ -1,6 +1,7 @@
 import Webcom.Configurator.Scripting.Test.TestProduct
 #GDLIST = Quote.GetGeneratedDocumentList('AMAT Quote')
 GDLIST = Quote.GetGeneratedDocumentList('AMAT_SUBTOTAL_OFFERING')
+get_delivery_parts = Quote.GetCustomField('ITEM_DELIVERY_SCHEDULE').Content
 
 for gndc in GDLIST:
     docid = gndc.Id
