69b23386e0a5a1b49083ae279c63ad02681069f2
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2022-02-08 12:02:43 +0530

    Update CQCRUDOPTN.py
    Added the break to restrict the multiple calls

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -5763,7 +5763,8 @@ class ContractQuoteCoveredObjModel(ContractQuoteCrudOpertion):
 							quotetype_value_for_offering = str(quotetype_value).upper()
 							Trace.Write("quotetype_value_for_offering -----"+str(quotetype_value_for_offering))
 							if quotetype_value_for_offering != "['EVENT BASED']" and quotetype_value_for_offering != "['FLEX EVENT BASED']":
-								self._insert_quote_service_covered_assembly(batch_group_record_id=batch_group_record_id)				
+								self._insert_quote_service_covered_assembly(batch_group_record_id=batch_group_record_id)
+								break
 				self._insert_quote_service_fab_location(batch_group_record_id=batch_group_record_id)
 				#SAQSCA_end_time = time.time()
 				
