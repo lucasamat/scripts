34af5c64aae3984ad7fa3325b09d8de780a38288
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-02-08 17:45:48 +0530

    Update SYULODTREE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
@@ -2187,7 +2187,7 @@ class TreeView:
 												subtab_temp =""
 												if subtab_temp_variable == 'Events' and ent_table == 'SAQTSE':
 													pattern_id = re.compile(r'<ENTITLEMENT_ID>AGS_[^>]*?_NET_PRMALB</ENTITLEMENT_ID>')
-													pattern_name = re.compile(r'<ENTITLEMENT_DISPLAY_VALUE>(?:Included - All PM|Included - Monthly and Above|Included - Quarterly and Above|Included - All PM (PDC/MPS)|Included - Qtrly and Above|Included - < Quarterly)</ENTITLEMENT_DISPLAY_VALUE>')
+													pattern_name = re.compile(r'<ENTITLEMENT_DISPLAY_VALUE>(?:Included - All PM|Included - Monthly and Above|Included - Quarterly and Above|Included - All PM (PDC/MPS)|Included - Qtrly and Above|Included - &lt; Quarterly)</ENTITLEMENT_DISPLAY_VALUE>')
 													subtab_temp ="Events"
 													
 												elif subtab_temp_variable in ('Service Inclusions','Greenbook Inclusions') :
