5db83b7df1513d85b9b03993b46eaf9e5980e3af
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2022-02-09 19:59:05 +0530

    Update CQINSQTITM.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQINSQTITM.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQINSQTITM.py
@@ -1578,9 +1578,9 @@ class ContractQuoteItem:
 				
 				else:
 					continue
-			if self.service_id == 'Z0046':
-				self.quote_service_entitlement_type = 'OFFERING + FAB + GREENBOOK'
-				self.source_object_name = 'SAQSGE'
+			# if self.service_id == 'Z0046':
+			# 	self.quote_service_entitlement_type = 'OFFERING + FAB + GREENBOOK'
+			# 	self.source_object_name = 'SAQSGE'
 			# if self.service_id == 'Z0101':
 			# 	self.quote_service_entitlement_type = 'OFFERING + GREENBOOK + GR EQUI'
 			Log.Info(str(self.contract_quote_id)+"_set_quote_service_entitlement_type ===> 2"+str(self.quote_service_entitlement_type))
