f2b643980c9514eec1bc845862fb250202a3c3b3
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2022-02-09 13:20:53 +0530

    Update CQNESTGRID.py
    trace

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
@@ -347,7 +347,7 @@ def GetEquipmentMaster(PerPage, PageInform, A_Keys, A_Values):
 				+"'"
 			)
 		elif TreeTopSuperParentParam != "Fab Locations" or TreeParentParam != 'Sending Equipment' or TreeParentParam != 'Receiving Equipment':
-			Trace.Write("else--------")
+			Trace.Write("else-------**")
 			if str(where_string):
 				where_string = " AND "+str(where_string)
 			Qstr = (
