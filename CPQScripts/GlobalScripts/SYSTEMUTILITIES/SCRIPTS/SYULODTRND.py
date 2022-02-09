80ffbea307b88c2fdeb18042b9e417feb8bbf293
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2022-02-09 18:48:14 +0530

    Update SYULODTRND.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTRND.py
@@ -1448,10 +1448,13 @@ and GREENBOOK = '{}' AND FABLOCATION_ID = '{}' AND SERVICE_ID = '{}'""".format(q
 						current_obj_value = str(my_format.format(round(float(current_obj_value), int(Decimal_Value))))
 					if current_obj_value and str(ObjectName) == "SAQICO":
 						Trace.Write("if-----111"+str(current_obj_value))
-						my_format = "{:." + str(Decimal_Values) + "f}"
-						current_obj_value = str(current_obj_value).replace('%','')
-						current_obj_value = str(my_format.format(round(float(current_obj_value), int(Decimal_Values))))
-						current_obj_value = str(current_obj_value) + '%'							
+						try:
+							my_format = "{:." + str(Decimal_Values) + "f}"
+							current_obj_value = str(current_obj_value).replace('%','')
+							current_obj_value = str(my_format.format(round(float(current_obj_value), int(Decimal_Values))))
+							current_obj_value = str(current_obj_value) + '%'
+						except:
+							current_obj_value = str(current_obj_value)													
 					
 
 						#my_format = "{:." + str(Decimal_Values) + "f}"
