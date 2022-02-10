baf89bfb13d2758a33b5297b0f777b62f5e4161c
Aravind <Aravind_Muthiah@contractor.amat.com>
2022-02-10 15:17:21 +0530

    Update SYBLKETRLG.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYBLKETRLG.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYBLKETRLG.py
@@ -1212,7 +1212,7 @@ def RELATEDMULTISELECTONSAVE(TITLE, VALUE, CLICKEDID, RECORDID,selectPN,ALLVALUE
 				#Table.TableActions.Update(obj_name, objh_head, row)
 				##Updating the fabname and fablocation id in bulk edit scenario starts....	
 		if len(DEL_PN)>0:
-			Part_Numbers=str(DEL_PN.replace("[","(").replace("]",")"))
+			Part_Numbers=str(DEL_PN).replace("[","(").replace("]",")")							
 			ScriptExecutor.ExecuteGlobal('CQPARTSINS',{"CPQ_Columns":{"Action": "Delete","QuoteID":Quote.CompositeNumber,"Delete_Partlist":Part_Numbers}})
 
 		if len(ADD_PN)>100:
