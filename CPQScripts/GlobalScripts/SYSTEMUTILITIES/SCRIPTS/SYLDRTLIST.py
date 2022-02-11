ddfc38bb13127751a9911a9a1eea490aed22c7d8
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2022-02-11 16:07:33 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -4189,7 +4189,7 @@ class SYLDRTLIST:
 					align = ''
 					rowspan_level1 = ""
 					if not table_group_columns2:
-						table_header += '<th colspan="5" '+rowspan_level1+'  data-align="right"><div>WTY/SPLIT INFO<button style="border:none;" class="glyphicon glyphicon-minus-sign" id="warranty_info_column_toggle" onclick="quote_items_column_toggle(this)"></button></div></th>'
+						table_header += '<th colspan="5" '+rowspan_level1+'  data-align="right"><div>WTYY/SPLIT INFO<button style="border:none;" class="glyphicon glyphicon-minus-sign" id="warranty_info_column_toggle" onclick="quote_items_column_toggle(this)"></button></div></th>'
 					if str(invs) in right_align_list:
 						align = 'right'
 					elif str(invs) in center_align_list:
