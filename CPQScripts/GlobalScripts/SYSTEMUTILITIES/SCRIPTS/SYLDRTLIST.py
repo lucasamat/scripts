010f1a266e7929ef72e3fac1c255c40fd1276833
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2022-02-14 17:11:33 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -9776,6 +9776,7 @@ class SYLDRTLIST:
 		return table_list, QueryCount, PageInformS,dbl_clk_function,footer_str
 
 	def SPARE_PARTS_ORDERING(self,select_obj_str, Qustr,PerPage, Page_start, Page_End,Wh_API_NAMEs):
+		Trace.Write("WH_API_NAMEs--->"+str(Wh_API_NAMEs))
 		if "CpqTableEntryId" in str(Wh_API_NAMEs):
 			ordered_values = []
 			parent_parts = []
