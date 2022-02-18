bad3d0f68fa7bdcad6979b4e849d71d5a5cd33c1
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2022-02-18 18:50:22 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -297,9 +297,7 @@ class SYLDRTLIST:
 							rem_list_sp = ["QUOTE_ITEM_COVERED_OBJECT_RECORD_ID"]
 							Columns = str([ele for ele in  eval(Columns) if ele not in rem_list_sp])
 						if RECORD_ID == 'SYOBJR-00005' and str(TreeParam)=="Z0110":
-							for column in eval(Columns):
-								if not (column.split('_')[0]=="DELIVERY" and column.split('_')[1].isdigit()):
-									Columns.append(column)
+    						Columns = str([column for column in eval(Columns) if not(column.split('_')[0]=="DELIVERY" and column.split('_')[1].isdigit()]))
 			#Hide columns in Related list based on Quote type End
 			Obj_Name = obj_obj.OBJ_REC_ID            
 			COLUMN_REC_ID = obj_obj.COLUMN_REC_ID            
