ad1573c54e189e377ad5f4052c097e01b22b1bd8
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2022-02-18 11:11:50 +0530

    Update CQCRUDOPTN.py
    Trace

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -5916,6 +5916,7 @@ class ContractQuoteCoveredObjModel(ContractQuoteCrudOpertion):
 							#Trace.Write("quote_type_attribute_value_chk "+str(quote_type_attribute_value)+" - "+str(pm_event_attribute_value))
 							if quote_type_attribute_value != ['Tool based'] and quote_type_attribute_value != "" and quote_type_attribute_value is not None:
 								if self.tree_param == 'Z0010':
+									Trace.Write("self.tools_from_ui----"+str(self.tools_from_ui))
 									self.tools_from_ui = "No"
 									self.applied_preventive_maintainence_quote_type_changed = "Yes"
 								self.applied_preventive_maintainence(batch_group_record_id=batch_group_record_id,quote_type_attribute_value = quote_type_attribute_value,applied_preventive_maintainence_quote_type_changed = self.applied_preventive_maintainence_quote_type_changed)
