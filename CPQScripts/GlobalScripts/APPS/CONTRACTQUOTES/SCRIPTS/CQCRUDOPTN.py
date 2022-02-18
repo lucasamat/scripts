868f0a91515a0755087a292e33e81bdaa6641be4
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2022-02-18 12:43:35 +0530

    Update CQCRUDOPTN.py
    added trace

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -1376,7 +1376,9 @@ class ContractQuoteOfferingsModel(ContractQuoteCrudOpertion):
 			countweeks += 1
 			#Trace.Write('countweeks--'+str(countweeks))
 			billing_date = start_date + datetime.timedelta(days=(7*countweeks))
-			periods_insert = Sql.RunQuery("""INSERT SAQRDS (QUOTE_REV_DELIVERY_SCHEDULE_RECORD_ID,QUOTE_ID,QUOTE_RECORD_ID,QTEREV_ID,QTEREV_RECORD_ID,DELIVERY_DATE,DELIVERY_PERIOD) select CONVERT(VARCHAR(4000),NEWID()) as QUOTE_REV_DELIVERY_SCHEDULE_RECORD_ID,'{quote_id}' as QUOTE_ID,'{contract_rec_id}' as QUOTE_RECORD_ID,'{qt_rev_id}' as QTEREV_ID,'{qt_rev_recid}' as QTEREV_RECORD_ID,'{delivery_date}' as DELIVERY_DATE,'{delivery_period}' as DELIVERY_PERIOD """.format(quote_id=self.contract_quote_id,contract_rec_id= self.contract_quote_record_id,qt_rev_id = self.quote_revision_id,qt_rev_recid = self.quote_revision_record_id,delivery_date =billing_date,delivery_period=index+1))
+			Query = "INSERT SAQRDS (QUOTE_REV_DELIVERY_SCHEDULE_RECORD_ID,QUOTE_ID,QUOTE_RECORD_ID,QTEREV_ID,QTEREV_RECORD_ID,DELIVERY_DATE,DELIVERY_PERIOD) select CONVERT(VARCHAR(4000),NEWID()) as QUOTE_REV_DELIVERY_SCHEDULE_RECORD_ID,'{quote_id}' as QUOTE_ID,'{contract_rec_id}' as QUOTE_RECORD_ID,'{qt_rev_id}' as QTEREV_ID,'{qt_rev_recid}' as QTEREV_RECORD_ID,'{delivery_date}' as DELIVERY_DATE,'{delivery_period}' as DELIVERY_PERIOD ".format(quote_id=self.contract_quote_id,contract_rec_id= self.contract_quote_record_id,qt_rev_id = self.quote_revision_id,qt_rev_recid = self.quote_revision_record_id,delivery_date =billing_date,delivery_period=index+1)
+			Trace.Write(Query)
+			periods_insert = Sql.RunQuery(Query)
 
 	def CreateEntitlements(self,OfferingRow_detail):		
 		Request_URL="https://cpservices-product-configuration.cfapps.us10.hana.ondemand.com/api/v2/configurations?autoCleanup=False"
