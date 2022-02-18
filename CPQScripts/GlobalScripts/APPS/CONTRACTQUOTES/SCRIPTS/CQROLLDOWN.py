c96bbb55e932e33df62d8450dbf09f00b86236bf
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2022-02-18 18:58:37 +0530

    Update CQROLLDOWN.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQROLLDOWN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQROLLDOWN.py
@@ -242,6 +242,8 @@ def CoveredObjEntitlement():
                     RevisionRecordId=rev_rec_id)
                     )
 	#creating backup table for saqgpe table insert...
+	getQuoteId = Sql.GetFirst("SELECT QUOTE_ID FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID = '{}'".format(Qt_rec_id))
+	saqsge_backup_table = "saqsge_backup_table_{}".format(getQuoteId.QUOTE_ID) 
 	saqsge_backup_table = "saqsge_backup_table_{}".format(Qt_rec_id) 
 
 	drop_saqsge_backup_table = SqlHelper.GetFirst("sp_executesql @T=N'IF EXISTS (SELECT ''X'' FROM SYS.OBJECTS WHERE NAME= ''"+str(saqsge_backup_table)+"'' ) BEGIN DROP TABLE "+str(saqsge_backup_table)+" END  ' ")
