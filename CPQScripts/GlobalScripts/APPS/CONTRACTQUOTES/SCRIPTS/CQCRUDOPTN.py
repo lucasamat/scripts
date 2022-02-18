594b2089d96ac41565c9768ed5063dc93481fb25
Viknesh Duraisamy <Gayathri_Amaresan@contractor.amat.com>
2022-02-18 11:17:55 +0530

    Update CQCRUDOPTN.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -1630,10 +1630,7 @@ class ContractQuoteOfferingsModel(ContractQuoteCrudOpertion):
 					account_info = {}
 					for keyobj in get_party_role:
 						account_info[keyobj.CPQ_PARTNER_FUNCTION] = keyobj.PARTY_ID
-					try:
-						contract_quote_id = contract_quote_obj.QUOTE_ID 
-					except:
-						contract_quote_id = ''
+					contract_quote_id = contract_quote_obj.QUOTE_ID 
 					get_sales_ifo = Sql.GetFirst("select SALESORG_ID,CONTRACT_VALID_TO,CONTRACT_VALID_FROM,PRICELIST_ID,PRICEGROUP_ID from SAQTRV where QUOTE_RECORD_ID = '"+str(contract_quote_record_id)+"' AND QUOTE_REVISION_RECORD_ID = '"+str(quote_revision_record_id)+"'")
 					
 					if get_sales_ifo:
