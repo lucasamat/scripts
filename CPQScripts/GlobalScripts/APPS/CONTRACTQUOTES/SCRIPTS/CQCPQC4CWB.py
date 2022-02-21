109494c361c057ad6a217cdf4f588e2375a256f1
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-02-21 16:29:43 +0530

    Update CQCPQC4CWB.py
    Commit

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCPQC4CWB.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCPQC4CWB.py
@@ -96,7 +96,7 @@ def writeback_to_c4c(writeback,contract_quote_record_id,quote_revision_record_id
             )
     elif writeback == "approver_list":
         contract_quote_id = Sql.GetFirst("Select QUOTE_ID FROM SAQTMT WHERE MASTER_TABLE_QUOTE_RECORD_ID ='{}' AND QTEREV_RECORD_ID = '{}'".format(contract_quote_record_id,quote_revision_record_id))
-        approver_list_id=Sql.GetList("Select REPLACE(APRCHNSTP_APPROVER_ID,'USR-','') as APRCHNSTP_APPROVER_ID,APRCHNSTP_ID FROM ACAPTX WHERE APRTRXOBJ_ID = '{}' AND APPROVALSTATUS = 'APPROVAL REQUIRED'".format(contract_quote_id.QUOTE_ID))
+        approver_list_id=Sql.GetList("Select REPLACE(APRCHNSTP_APPROVER_ID,'USR-','') as APRCHNSTP_APPROVER_ID,APRCHNSTP_ID FROM ACAPTX WHERE APRTRXOBJ_ID = '{}'".format(contract_quote_id.QUOTE_ID))
         approver_list = []
         #approver_step_list =[]
         if contract_quote_id and approver_list_id:
@@ -106,7 +106,7 @@ def writeback_to_c4c(writeback,contract_quote_record_id,quote_revision_record_id
                 #approver_list.append(approver)
                 getempid = Sql.GetFirst("Select C4C_EMPLOYEE_ID FROM SAEMPL(NOLOCK) WHERE EMPLOYEE_ID ='{approver}'".format(approver = approver))
                 approver_list.append(getempid.C4C_EMPLOYEE_ID)
-                approver_list = list(dict.fromkeys(approver_list))
+                #approver_list = list(dict.fromkeys(approver_list))
                 #approver_step_list.append(approver_step)
                 role_code_id = "71"
             requestdata = (
