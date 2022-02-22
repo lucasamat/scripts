7c149e2939953da72672d6d489a7102ee7025d52
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-02-22 16:44:24 +0530

    attribute editability code start

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLNVAL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLNVAL.py
@@ -125,6 +125,82 @@ def entitlement_attributes_lvel_request(partnumber,inserted_value_list,ent_level
 	#Trace.Write('get_attr_leve_based_list--type return'+str(type(get_attr_leve_based_list)))
 	return get_attr_leve_based_list
 
+#attribute editablity code start	
+def entitlement_attributes_editability_request(partnumber,inserted_value_list,ent_level_table,where_cond):
+	Trace.Write("ent_level_table--"+str(ent_level_table))
+	level_name = get_clicked_greenbook = get_greenbook_value_itemlevel = ''
+	if ent_level_table == "SAQTSE":
+		level_name = 'OFFERING LEVEL'
+	elif ent_level_table == "SAQSFE":
+		level_name = 'OFFERING FAB LEVEL'
+	elif ent_level_table == "SAQITE":
+		get_entitlement_qt_item_sctructure = Sql.GetFirst("select ENTITLEMENT_XML,SERVICE_ID,GREENBOOK from SAQITE where {where_condition}".format(where_condition= where_cond))
+		#flag_excluse=0
+		get_service_val =get_entitlement_qt_item_sctructure.SERVICE_ID
+		get_greenbook_value_itemlevel = get_entitlement_qt_item_sctructure.GREENBOOK
+		#condition based on quote item strcuture start
+		if get_entitlement_qt_item_sctructure:
+			Trace.Write('get_service_val-32----'+str(get_service_val))
+			get_ent_val = ''
+			updateentXML = get_entitlement_qt_item_sctructure.ENTITLEMENT_XML
+			pattern_tag = re.compile(r'(<QUOTE_ITEM_ENTITLEMENT>[\w\W]*?</QUOTE_ITEM_ENTITLEMENT>)')
+			pattern_id = re.compile(r'<ENTITLEMENT_ID>(AGS_'+str(get_service_val)+'_PQB_QTITST)</ENTITLEMENT_ID>')
+			pattern_name = re.compile(r'<ENTITLEMENT_DISPLAY_VALUE>([^>]*?)</ENTITLEMENT_DISPLAY_VALUE>')
+			for m in re.finditer(pattern_tag, updateentXML):
+				sub_string = m.group(1)
+				get_ent_id = re.findall(pattern_id,sub_string)
+				get_ent_val= re.findall(pattern_name,sub_string)
+				if get_ent_id:
+					Trace.Write(str(sub_string)+'---get_ent_name---'+str(get_ent_val[0]))
+					get_ent_val = str(get_ent_val[0])
+					#flag_excluse=1
+					break
+		Trace.Write('get_ent_val---4750--'+str(get_ent_val))
+		if str(get_ent_val).upper() == "OFFERING + EQUIPMENT":
+			level_name = 'OFFERING FAB GREENBOOK TOOL LEVEL'
+		elif str(get_ent_val).upper() == "OFFERING + GREENBOOK + GR EQUI":
+			level_name = 'OFFERING FAB GREENBOOK LEVEL'
+		else:
+			level_name = 'OFFERING LEVEL'
+		#condition based on quote item strcuture end
+	elif ent_level_table == "SAQSGE":
+		if Product.GetGlobal('TreeParam') == 'Add-On Products' and Product.GetGlobal("TreeParentLevel3") == 'Product Offerings':
+			get_clicked_greenbook = Product.GetGlobal("TreeParentLevel0")
+		else:
+			get_clicked_greenbook = Product.GetGlobal('TreeParam')
+		level_name = 'OFFERING FAB GREENBOOK LEVEL'
+	elif ent_level_table == "SAQSCE":
+		get_clicked_greenbook = Product.GetGlobal('TreeParam')
+		level_name = 'OFFERING FAB GREENBOOK TOOL LEVEL'
+	elif ent_level_table == "SAQGPE":
+		Trace.Write('107-gpe---get_greenbook_value_itemlevel----'+str(get_greenbook_value_itemlevel))
+		get_clicked_greenbook = Product.GetGlobal("TreeParentLevel1")
+		level_name = 'OFFERING FAB GREENBOOK TOOL LEVEL'
+	else:
+		Trace.Write('107-else---get_greenbook_value_itemlevel----'+str(get_greenbook_value_itemlevel))
+		get_clicked_greenbook = Product.GetGlobal('TreeParam')
+		level_name = 'OFFERING FAB GREENBOOK TOOL ASSEMBLY LEVEL'
+	get_attr_leve_based_list =[]
+	if get_clicked_greenbook == "" and ent_level_table == "SAQITE":
+		Trace.Write('107----get_greenbook_value_itemlevel----'+str(get_greenbook_value_itemlevel))
+		get_clicked_greenbook =get_greenbook_value_itemlevel
+	for val in inserted_value_list:
+		#Trace.Write(str(level_name)+'--level_name--value---'+str(val))
+		if level_name in ["OFFERING FAB LEVEL","OFFERING LEVEL"]:
+			get_attr_editable_fields = Sql.GetFirst("select ENTITLEMENTLEVEL_ID from PRENLI where ENTITLEMENT_ID = '"+str(val)+"' and ENTITLEMENTLEVEL_NAME = '"+str(level_name)+"' and EDITABLE = 'True'")
+			get_attr_non_editable_fields = Sql.GetFirst("select ENTITLEMENTLEVEL_ID from PRENLI where ENTITLEMENT_ID = '"+str(val)+"' and ENTITLEMENTLEVEL_NAME = '"+str(level_name)+"' and EDITABLE = 'False'")
+		else:
+			get_attr_editable_fields= Sql.GetFirst("select PRENLI.ENTITLEMENTLEVEL_ID from PRENLI JOIN PRENGB on PRENLI.ENTITLEMENT_ID=PRENGB.ENTITLEMENT_ID where PRENLI.ENTITLEMENT_ID = '"+str(val)+"' and PRENLI.ENTITLEMENTLEVEL_NAME = '"+str(level_name)+"' and PRENGB.GREENBOOK = '"+str(get_clicked_greenbook)+"' and EDITABLE = 'True'")
+			get_attr_non_editable_fields = Sql.GetFirst("select PRENLI.ENTITLEMENTLEVEL_ID from PRENLI JOIN PRENGB on PRENLI.ENTITLEMENT_ID=PRENGB.ENTITLEMENT_ID where PRENLI.ENTITLEMENT_ID = '"+str(val)+"' and PRENLI.ENTITLEMENTLEVEL_NAME = '"+str(level_name)+"' and PRENGB.GREENBOOK = '"+str(get_clicked_greenbook)+"' and EDITABLE = 'False'")
+		if get_attr_editable_fields:
+			get_attr_editable_fields.append(str(val))
+		if get_attr_non_editable_fields:
+			get_attr_non_editable_fields.append(str(val))
+	#Trace.Write('get_attr_leve_based_list--type return'+str(type(get_attr_leve_based_list)))
+	return get_attr_editable_fields,get_attr_non_editable_fields
+
+#attribute editablity code end
+
 def ChildEntRequest(partnumber,tableName,where):	
 	get_c4c_quote_id = Sql.GetFirst("select * from SAQTMT where MASTER_TABLE_QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}'".format(quote_record_id,revision_record_id))
 	#attribute_id,value_code,attr_type,display_name,config_id,cpsmatchID,isdefault	
