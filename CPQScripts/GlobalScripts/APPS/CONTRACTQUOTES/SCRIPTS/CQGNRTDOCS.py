fb5ba3292cfa44c705acdbc9d5120c0a90fef4a3
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-02-24 17:32:28 +0530

    Update CQGNRTDOCS.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQGNRTDOCS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQGNRTDOCS.py
@@ -272,7 +272,7 @@ def DownloadAttachments():
 	Trace.Write('QT_DWD---RECORD_ID-'+str(RECORD_ID))
 	c4c_quote_id = Quote.CompositeNumber
 	cartobj = Sql.GetFirst("select CART_ID, USERID from CART where ExternalId = '{}'".format(c4c_quote_id))
-	get_last_attached_filename = Sql.GetFirst("SELECT id, cart_id, owner_id, user_id, content, fileName, dateCreated, contentType, contentLength, quoteId FROM CartAttachments WHERE owner_id ='"+str(cartobj.USERID)+"' and cart_id ='"+str(cartobj.CART_ID)+"' and  id = ( SELECT MAX(id) FROM CartAttachments )")
+	get_last_attached_filename = Sql.GetFirst("SELECT id, cart_id, owner_id, user_id, content, fileName, dateCreated, contentType, contentLength, quoteId FROM CartAttachments WHERE owner_id ='"+str(cartobj.USERID)+"' and cart_id ='"+str(cartobj.CART_ID)+"' and  fileName = '"+str(RECORD_ID)+"'")
 	if get_last_attached_filename:
 		fileContent =get_last_attached_filename.content
 		fileName = get_last_attached_filename.fileName
