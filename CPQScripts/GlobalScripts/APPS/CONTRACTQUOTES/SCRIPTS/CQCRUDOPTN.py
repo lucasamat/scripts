36a922305d7f56f437d9bcfff27a949df4953bb3
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2022-02-24 17:38:33 +0530

    Trace

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -6710,6 +6710,7 @@ class ContractQuoteNoficationModel(ContractQuoteCrudOpertion):
 
 			fab_location_data = Sql.GetList(" SELECT * FROM SAQFBL (NOLOCK) WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{RevisionRecordId}'".format(QuoteRecordId= self.contract_quote_record_id,RevisionRecordId=self.quote_revision_record_id))
 			if not fab_location_data:
+				Trace.Write("fab_delete###===========")
 				msg_app_txt = (
 					'<div  class="col-md-12" id="dirty-flag-warning"><div class="col-md-12 alert-info"><label> <img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/infor_icon_green.svg" alt="Warning">'
 					+ str(fab_location_msg_notification.MESSAGE_LEVEL)
