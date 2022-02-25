79c75bfe1b66342a8813cd263ddf45f2a8615c49
Krishna Chaitanya --CNTR <Krishna_Chaitanya@contractor.amat.com>
2022-02-25 11:37:38 +0530

    commented

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYDELCNMSG.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYDELCNMSG.py
@@ -414,9 +414,9 @@ class DeleteConfirmPopup:
                     if Table == "SAQFBL":
                         QueryStatement = "DELETE FROM "+str(Table)+" WHERE QUOTE_RECORD_ID ='"+str(contract_quote_record_id)+"' and FABLOCATION_ID = '{fab_id}' and QUOTE_FABLOCATION_RECORD_ID = '{fab_location_rec_id}' AND QTEREV_RECORD_ID = '{quote_revision_record_id}'".format(ObjectName = Table,fab_id = fab_location.FABLOCATION_ID,fab_location_rec_id = fab_location.QUOTE_FABLOCATION_RECORD_ID,quote_revision_record_id=quote_revision_record_id)
                         Sql.RunQuery(QueryStatement)
-                    if Table in ("SAQTSV","SAQTSE","SAQSGB","SAQSAP"):
-                        QueryStatement = "DELETE FROM "+str(Table)+" WHERE QUOTE_RECORD_ID ='"+str(contract_quote_record_id)+"'  AND QTEREV_RECORD_ID = '{quote_revision_record_id}'".format(ObjectName = Table,quote_revision_record_id=quote_revision_record_id)
-                        Sql.RunQuery(QueryStatement)
+                    # if Table in ("SAQSGB","SAQSAP"):
+                    #     QueryStatement = "DELETE FROM "+str(Table)+" WHERE QUOTE_RECORD_ID ='"+str(contract_quote_record_id)+"'  AND QTEREV_RECORD_ID = '{quote_revision_record_id}'".format(ObjectName = Table,quote_revision_record_id=quote_revision_record_id)
+                    #     Sql.RunQuery(QueryStatement)
                     else:
                         QueryStatement = "DELETE FROM "+str(Table)+" WHERE QUOTE_RECORD_ID ='"+str(contract_quote_record_id)+"' and FABLOCATION_ID = '{fab_id}' and FABLOCATION_RECORD_ID = '{fab_location_rec_id}' AND QTEREV_RECORD_ID = '{quote_revision_record_id}'".format(ObjectName = Table,fab_id = fab_location.FABLOCATION_ID,fab_location_rec_id = fab_location.FABLOCATION_RECORD_ID,quote_revision_record_id=quote_revision_record_id)
                         Sql.RunQuery(QueryStatement)
