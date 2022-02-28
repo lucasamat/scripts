39fadbb1611d27de25d21044ebc56ca2fe740af3
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2022-02-28 14:28:06 +0530

    Update CQSDELPGPN.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/CQSDELPGPN.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/CQSDELPGPN.py
@@ -23,37 +23,51 @@ class qt_pricing_review_mail_trigger:
     def mailtrigger(self):
         
         
-        for quotes in pricing_review:
-            pricing_review = Sql.GetFirst("SELECT MEMBER_ID,MEMBER_NAME,EMAIL FROM SAQDLT WHERE QUOTE_ID ='"+str(quotes)+"' AND QUOTE_RECORD_ID = '"+str(quote_record_id)+"' AND C4C_PARTNERFUNCTION_ID = 'PRICING PERSON'")
-            Subject = "Pricing Review Status Notification"
-            mailBody = """
-                        Dear """+str(getting_quotes.OWNER_NAME)+""",<br><br>
-                        This is to notify that the Following Quote Number """+str(getting_quotes.QUOTE_ID)+""" is in Pricing Review Status.
-                        <br><br>
-                        Thank You
-                        """
-            try:
-                LOGIN_CRE = Sql.GetFirst("SELECT USER_NAME,PASSWORD FROM SYCONF (NOLOCK) where Domain ='SUPPORT_MAIL'")
-                mailClient = SmtpClient()
-                mailClient.Host = "smtp.gmail.com"
-                mailClient.Port = 587
-                mailClient.EnableSsl = "true"
-                mailCred = NetworkCredential()
-                mailCred.UserName = str(LOGIN_CRE.USER_NAME)
-                mailCred.Password = str(LOGIN_CRE.PASSWORD)
-                mailClient.Credentials = mailCred
-                toEmail = MailAddress(str(recepient))
-                fromEmail = MailAddress(str(LOGIN_CRE.USER_NAME))
-                msg = MailMessage(fromEmail, toEmail)
-                msg.Subject = Subject
-                msg.IsBodyHtml = True
-                msg.Body = mailBody  
-                mailClient.Send(msg)
-            except Exception as e:
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                Trace.Write("TEST::"+str(exc_type) +str(fname)+str(exc_tb.tb_lineno))
+        #for quotes in pricing_review:
+        pricing_review = Sql.GetFirst("SELECT MEMBER_ID,MEMBER_NAME,EMAIL,QUOTE_ID FROM SAQDLT WHERE  QUOTE_RECORD_ID = '"+str(QUOTE_ID)+"' AND C4C_PARTNERFUNCTION_ID = 'PRICING PERSON'")
+        Subject = "Pricing Review Status Notification"
+        mailBody = """
+                    Dear """+str(pricing_review.MEMBER_NAME)+""",<br><br>
+                    This is to notify that the Following Quote Number """+str(pricing_review.QUOTE_ID)+""" is in Pricing Review Status.
+                    <br><br>
+                    Thank You
+                    """
+        try:
+            LOGIN_CRE = Sql.GetFirst("SELECT USER_NAME,PASSWORD FROM SYCONF (NOLOCK) where Domain ='SUPPORT_MAIL'")
+            mailClient = SmtpClient()
+            mailClient.Host = "smtp.gmail.com"
+            mailClient.Port = 587
+            mailClient.EnableSsl = "true"
+            mailCred = NetworkCredential()
+            mailCred.UserName = str(LOGIN_CRE.USER_NAME)
+            mailCred.Password = str(LOGIN_CRE.PASSWORD)
+            mailClient.Credentials = mailCred
+            toEmail = MailAddress(str(recepient))
+            fromEmail = MailAddress(str(LOGIN_CRE.USER_NAME))
+            msg = MailMessage(fromEmail, toEmail)
+            msg.Subject = Subject
+            msg.IsBodyHtml = True
+            msg.Body = mailBody  
+            mailClient.Send(msg)
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            Trace.Write("TEST::"+str(exc_type) +str(fname)+str(exc_tb.tb_lineno))
 
+
+
+try:
+    QUOTE_ID = Param.QUOTE_ID
+except:
+    QUOTE_ID = ''
+try:
+    QTEREV_ID = Param.QTEREV_ID
+except:
+    QTEREV_ID = ''
+try:
+    ACTION = Param.ACTION
+except:
+    ACTION = ''
 obj = qt_pricing_review_mail_trigger(Quote)   
 
-obj.mailtrigger()
+obj.mailtrigger(QUOTE_ID,QTEREV_ID,ACTION)
