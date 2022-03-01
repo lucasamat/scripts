1ee0c1f69612045600a527cb9c034cf4390e79e3
Aravind <Aravind_Muthiah@contractor.amat.com>
2022-03-01 21:05:05 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -3875,7 +3875,7 @@ class SYLDRTLIST:
 								+ "</th>"
 							)
 						else:							
-							if (str(TreeParam) != 'Quote Preview' and str(TreeParam) != 'Contract Preview' and  str(TreeParam) != 'Billing Matrix' and str(current_tab).upper() != "APP") and RECORD_ID not in ("SYOBJR-98875","SYOBJR-98873","SYOBJR-00005","SYOBJR-00010","SYOBJR-98881"):
+							if (str(TreeParam) != 'Quote Preview' and str(TreeParam) != 'Contract Preview' and  str(TreeParam) != 'Billing Matrix' and str(current_tab).upper() != "APP") and RECORD_ID not in ("SYOBJR-98875","SYOBJR-98873","SYOBJR-00010","SYOBJR-98881"):
 								Trace.Write("CHKNG_J_05 "+str(qstring))
 								table_header += (
 									'<th  data-field="'
