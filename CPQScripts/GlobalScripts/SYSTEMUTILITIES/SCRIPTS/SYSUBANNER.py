5f98eeb28df195eee71d99a8d6b42babd9e7e9cc
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-03-02 14:56:34 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -3166,7 +3166,10 @@ def Related_Sub_Banner(
             if str(subTabName)=="Events" and str(TreeSuperParentParam)!="Z0009":
                 sec_rel_sub_bnr += ('<button id="ADDNEW__SYOBJR_00011_SYOBJ_00974" onclick="PM_FrequencyInlineEdit()" class="btnconfig" >INLINE EDIT</button>')
             elif 'Add-On Products' in str(TreeParam) and ("INCLUDE ADD-ON PRODUCTS" not in sec_rel_sub_bnr) and ("ADD CREDITS" not in sec_rel_sub_bnr):
-                sec_rel_sub_bnr+= str(add_button)
+                if "ADD FROM LIST" in sec_rel_sub_bnr:
+                    sec_rel_sub_bnr+= ""
+                else:
+                    sec_rel_sub_bnr+= str(add_button)
             else:
                 if str(subTabName)!="Exclusions" and str(subTabName)!="New Parts" and str(subTabName)!="Inclusions":
                     sec_rel_sub_bnr += ('<button id="fabcostlocate_save" onclick="fabcostlocatesave(this)" style="display: none;" class="btnconfig">SAVE</button><button id="fabcostlocate_cancel" onclick="fabcostlocatecancel(this)" style="display: none;" class="btnconfig">CANCEL</button>'  )
