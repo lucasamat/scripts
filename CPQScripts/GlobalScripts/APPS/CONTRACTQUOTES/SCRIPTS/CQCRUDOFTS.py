dfca1580804263f748eac1a8dcea511eb7495f61
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2022-03-02 13:11:57 +0530

    Update CQCRUDOFTS.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOFTS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOFTS.py
@@ -302,7 +302,7 @@ def sending_equipment_insert(values,all_values,A_Keys,A_Values):
                                 QuoteRecId=contract_quote_record_id,
                                 RevisionId=quote_revision_id,
                                 RevisionRecordId=quote_revision_record_id,
-                                QuoteName=contract_quote_name,
+                                QuoteName= '',
                                 BatchGroupRecordId=batch_group_record_id,
                             )
                         )
