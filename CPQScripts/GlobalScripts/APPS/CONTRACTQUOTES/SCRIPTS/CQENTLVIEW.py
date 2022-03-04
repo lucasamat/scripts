fba46972665c438f2cd7376c31db28e468f1a274
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-03-04 15:32:37 +0530

    Update CQENTLVIEW.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
@@ -336,13 +336,13 @@ class EntitlementView():
 			Trace.Write('attributedefaultvalue--325----'+str(attributedefaultvalue))
 			#Trace.Write("validation_dict---"+str(validation_dict))
 			#editability contro strat
-			if ProductPartnumber == "Z0091":
-				get_attr_edit_based_list = ScriptExecutor.ExecuteGlobal("CQENTLNVAL", {'where_cond':where,'partnumber':ProductPartnumber,'ent_level_table':ObjectName,'inserted_value_list':overallattributeslist_visible,'action':'get_edit_attr_list'})
-				if get_attr_edit_based_list:
-					attributeEditlst = get_attr_edit_based_list[0]
-					Trace.Write('attributeReadonlylst--Z0091----'+str(attributeReadonlylst))
-					attributeReadonlylst = get_attr_edit_based_list[1]
-					Trace.Write('attributeReadonlylst--Z0091---after----'+str(attributeReadonlylst))
+			
+			get_attr_edit_based_list = ScriptExecutor.ExecuteGlobal("CQENTLNVAL", {'where_cond':where,'partnumber':ProductPartnumber,'ent_level_table':ObjectName,'inserted_value_list':overallattributeslist_visible,'action':'get_edit_attr_list'})
+			if get_attr_edit_based_list:
+				attributeEditlst = get_attr_edit_based_list[0]
+				Trace.Write('attributeReadonlylst--Z0091----'+str(attributeReadonlylst))
+				attributeReadonlylst = get_attr_edit_based_list[1]
+				Trace.Write('attributeReadonlylst--Z0091---after----'+str(attributeReadonlylst))
 			#editability control end
 			product_obj = Sql.GetFirst("""SELECT 
 										MAX(PDS.PRODUCT_ID) AS PRD_ID,PDS.SYSTEM_ID,PDS.PRODUCT_NAME 
