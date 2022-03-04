e6057f188049a273f1cc8be320b6e2cd2bdb3927
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-03-04 12:15:31 +0530

    Update SYUADNWPOP.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
@@ -2487,7 +2487,7 @@ def POPUPLISTVALUEADDNEW(
 			stp_account_id = Product.GetGlobal("stp_account_id")
 			Pagination_M = Sql.GetFirst(
 				"SELECT COUNT(PRLPBE.CpqTableEntryId) as count FROM {} (NOLOCK) WHERE GREENBOOK = '{}' AND POSS_NSO_PART_ID NOT IN (SELECT POSS_NSO_PART_ID FROM SAQSCN (NOLOCK) WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' )".format(
-					ObjectName,TreeParentParam, contract_quote_record_id,quote_revision_record_id
+					ObjectName,Product.GetGlobal("TreeParentLevel0"), contract_quote_record_id,quote_revision_record_id
 				)
 			)
 
@@ -2512,7 +2512,7 @@ def POPUPLISTVALUEADDNEW(
 			if where_string:
 				where_string += " AND"
 			where_string += """ GREENBOOK = '{}' AND  POSS_NSO_PART_ID NOT IN (SELECT POSS_NSO_PART_ID FROM SAQSCN (NOLOCK) WHERE QUOTE_RECORD_ID = '{}' AND QTEREV_RECORD_ID = '{}' )""".format(
-				TreeParentParam, contract_quote_record_id, quote_revision_record_id 
+				Product.GetGlobal("TreeParentLevel0"), contract_quote_record_id, quote_revision_record_id 
 			)
 
 			table_data = Sql.GetList(
