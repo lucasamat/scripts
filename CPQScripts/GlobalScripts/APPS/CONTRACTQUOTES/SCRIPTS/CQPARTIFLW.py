34282e369ce4f3c2c8a6a9e4e1c5bc04a5522c85
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-03-04 12:59:59 +0530

    Update CQPARTIFLW.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTIFLW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTIFLW.py
@@ -45,7 +45,6 @@ def iflow_pricing_call(user,entries,revision):
 
         authorization = Convert.ToBase64String(binaryAuthorization)
         authorization = "Basic " + authorization
-    Log.Info("testing............."+str(requestdata))
     webclient = System.Net.WebClient()
     webclient.Headers[System.Net.HttpRequestHeader.ContentType] = "application/xml"
     webclient.Headers[System.Net.HttpRequestHeader.Authorization] = authorization
