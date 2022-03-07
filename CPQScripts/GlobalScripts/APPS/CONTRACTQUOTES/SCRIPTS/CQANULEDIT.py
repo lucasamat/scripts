d83e41643cfcbc7ce15f039d62c0e3e47fc49ebb
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-03-07 14:58:18 +0530

    Update CQANULEDIT.py
    C

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQANULEDIT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQANULEDIT.py
@@ -10,3 +10,20 @@ import datetime
 from datetime import datetime
 Sql = SQL()
 import SYCNGEGUID as CPQID
+
+def constructcat4editablity(Quote_rec_id,MODE):
+	Trace.Write("INSIDE_constructcat4editablity")
+	return True
+
+
+ACTION = Param.ACTION
+try:
+	values = Param.values
+except:
+	values = ""
+
+
+if ACTION == 'CAT4_ENTITLMENT':
+    MODE="EDIT"
+    Quote_rec_id = Quote.GetGlobal("contract_quote_record_id")
+    ApiResponse = ApiResponseFactory.JsonResponse(constructcat4editablity(Quote_rec_id,MODE))
