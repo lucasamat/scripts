27a0681d50dcb8c176195258f2e769c7245e6369
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-03-07 14:42:06 +0530

    Create CQANULEDIT.py
    Commit new script for annaual edit

---
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQANULEDIT.py
@@ -0,0 +1,12 @@
+# =========================================================================================================================================
+#   __script_name : CQANULEDIT.PY
+#   __script_description : THIS SCRIPT IS USED TO EDIT THE ANNUAL GRID BASED ON ENTITLEMENT PRICING
+#   __primary_author__ : WASIM ABDUL 
+#   © BOSTON HARBOR TECHNOLOGY LLC - ALL RIGHTS RESERVED
+# ==========================================================================================================================================
+import Webcom.Configurator.Scripting.Test.TestProduct
+from SYDATABASE import SQL
+import datetime
+from datetime import datetime
+Sql = SQL()
+import SYCNGEGUID as CPQID
