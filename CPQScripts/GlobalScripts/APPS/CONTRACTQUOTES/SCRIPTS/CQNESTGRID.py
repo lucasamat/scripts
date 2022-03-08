f322ce49c9e050f6d4a24c9cb8a891a02e04d007
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2022-03-08 19:35:25 +0530

    Update CQNESTGRID.py
    A055S000P01-17070

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQNESTGRID.py
@@ -6398,6 +6398,14 @@ def UpdateBreadcrumb():
 			eq_id = str(qry.SNDFBL_ID)
 		else:
 			eq_id = "Sending fab"
+	elif TreeParam == "Customer Information" and TABLENAME == 'SAQFBL':
+    	qry = Sql.GetFirst(
+		"SELECT FABLOCATION_ID FROM SAQFBL (NOLOCK) WHERE QUOTE_FABLOCATION_RECORD_ID = '{recid}'".format(recid=CURR_REC_ID)
+		)
+		if qry:
+			eq_id = str(qry.FABLOCATION_ID)
+		else:
+			eq_id = "Sending fab"
 	elif TreeParam == "Customer Information" and TABLENAME == 'SAQICT':
 		qry = Sql.GetFirst(
 		"SELECT CONTACT_ID FROM SAQICT (NOLOCK) WHERE QUOTE_REV_INVOLVED_PARTY_CONTACT_ID = '{recid}'".format(recid=CURR_REC_ID)
