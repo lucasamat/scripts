0ac62d33cb769cbb6d7fd219cc96a8666cdd99c0
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2022-03-08 16:46:52 +0530

    Update CQPARTSINS.py
    check 6k parts

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTSINS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTSINS.py
@@ -435,7 +435,7 @@ class SyncFPMQuoteAndHanaDatabase:
                     temp_value +=','+ele.group(2) if ele.group(2) !='' else None
                     if ele.group(1) == '"CHILD_PART_NUMBER"':
                         childvalue = str(ele.group(2))
-                        if re.search(r'^6000-',childvalue):
+                        if re.search(r'6000-',childvalue):
                             child_temp_value +=','+'null'
                             child_temp_flag=1
                         else:
