858955d78bf120ff6431655dee77cedb749055fa
Selvirajapriya Krishnan --CNTR <Selvirajapriya_Krishnan@contractor.amat.com>
2022-03-08 19:39:44 +0530

    Update CQENTLNVAL.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLNVAL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLNVAL.py
@@ -457,7 +457,7 @@ def entitlemnt_attr_update(partnumber,entitlement_table, where):
 								"field":["SPQTEV","Split Quote","AGS_{}_PQB_SPLQTE".format(partnumber)]
 								},
 								{
-								"field":["SVSPCT","Service Split %","AGS_Z0105_PQB_SVSPPC".format(partnumber)]
+								"field":["SVSPCT","Service Split %","AGS_Z0105_PQB_SVSPPC"]
 								},
 								{
 								"field":["SPSPCT","Spares Split %","AGS_{}_PQB_SPSPPC".format(partnumber)]
