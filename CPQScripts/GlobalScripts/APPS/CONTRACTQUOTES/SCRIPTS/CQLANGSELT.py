b85f735fce6d6f8e6689a7655334c4325ce2af3d
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-03-09 10:40:19 +0530

    Update CQLANGSELT.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQLANGSELT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQLANGSELT.py
@@ -682,7 +682,7 @@ def _insert_subtotal_by_offerring_quote_table():
 
     get_items_details_obj_insert = Quote.QuoteTables["QT_SAQRIT"]
     get_items_details_obj_insert.Rows.Clear()
-    get_items_details_obj = Sql.GetList("""select SAQRIT.LINE,SAQRIT.FABLOCATION_ID,SAQRIT.OBJECT_ID,SAQRIT.OBJECT_ID as EQUIPMENT_ID,SAQRIT.GREENBOOK,SAQRIT.OFFERING_DESCRIPTION,SAQRIT.SERVICE_RECORD_ID,SAQRIT.SERVICE_ID,SAQRIT.QUOTE_ID,SAQRIT.QUOTE_RECORD_ID,SAQRIT.QTEREV_ID,SAQRIT.QTEREV_RECORD_ID,SAQRIT.TAX_AMOUNT_INGL_CURR,SAQRIT.ESTVAL_INGL_CURR,SAQRIT.NET_VALUE_INGL_CURR,{UserId} as ownerId,{CartId} as cartId from SAQRIT (NOLOCK)  where SAQRIT.QUOTE_RECORD_ID ='{c4c_quote_id}' and  SAQRIT.QTEREV_RECORD_ID= '{rev_rec_id}'""".format(CartId = cartobj.CART_ID,UserId= cartobj.USERID,c4c_quote_id = contract_quote_record_id,rev_rec_id = quote_revision_record_id))
+    get_items_details_obj = Sql.GetList("""select SAQRIT.LINE,SAQRIT.FABLOCATION_ID,SAQRIT.OBJECT_ID,SAQRIT.OBJECT_ID as EQUIPMENT_ID,SAQRIT.GREENBOOK,SAQRIT.OFFERING_DESCRIPTION,SAQRIT.SERVICE_DESCRIPTION,SAQRIT.SERVICE_RECORD_ID,SAQRIT.SERVICE_ID,SAQRIT.QUOTE_ID,SAQRIT.QUOTE_RECORD_ID,SAQRIT.QTEREV_ID,SAQRIT.QTEREV_RECORD_ID,SAQRIT.TAX_AMOUNT_INGL_CURR,SAQRIT.ESTVAL_INGL_CURR,SAQRIT.NET_VALUE_INGL_CURR,{UserId} as ownerId,{CartId} as cartId from SAQRIT (NOLOCK)  where SAQRIT.QUOTE_RECORD_ID ='{c4c_quote_id}' and  SAQRIT.QTEREV_RECORD_ID= '{rev_rec_id}'""".format(CartId = cartobj.CART_ID,UserId= cartobj.USERID,c4c_quote_id = contract_quote_record_id,rev_rec_id = quote_revision_record_id))
     if get_items_details_obj:
         for val in get_items_details_obj:
             newRow = get_items_details_obj_insert.AddNewRow()
@@ -713,7 +713,7 @@ def _insert_subtotal_by_offerring_quote_table():
             if val.OFFERING_DESCRIPTION:
                 newRow['SERVICE_DESCRIPTION'] = val.OFFERING_DESCRIPTION
             else:
-                val.OFFERING_DESCRIPTION =''
+                newRow['SERVICE_DESCRIPTION'] =val.SERVICE_DESCRIPTION
             
             if val.SERVICE_ID:
                 newRow['SERVICE_ID'] = val.SERVICE_ID
