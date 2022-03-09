24b95f06141cd0a7c39c58536ed7b7c3bb346bb3
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2022-03-09 10:24:21 +0530

    Update CQPARTSINS.py
    updated

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTSINS.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTSINS.py
@@ -355,12 +355,12 @@ class SyncFPMQuoteAndHanaDatabase:
             self.quote_revision_id = saqtrv_obj.QUOTE_REVISION_RECORD_ID
             self.quote_record_id = saqtrv_obj.QUOTE_RECORD_ID
             
-        get_party_role = Sql.GetList("SELECT CPQ_PARTNER_FUNCTION, PARTY_ID FROM SAQTIP(NOLOCK) WHERE QUOTE_RECORD_ID = '"+str(self.contract_quote_record_id)+"' AND QTEREV_RECORD_ID = '"+str(self.quote_revision_record_id)+"' and CPQ_PARTNER_FUNCTION in ('SOLD TO')")
+        get_party_role = Sql.GetList("SELECT CPQ_PARTNER_FUNCTION, PARTY_ID FROM SAQTIP(NOLOCK) WHERE QUOTE_RECORD_ID = '"+str(self.quote_record_id)+"' AND QTEREV_RECORD_ID = '"+str(self.quote_revision_record_id)+"' and CPQ_PARTNER_FUNCTION in ('SOLD TO')")
         account_info = {}
         for keyobj in get_party_role:
             account_info[keyobj.CPQ_PARTNER_FUNCTION] = keyobj.PARTY_ID
         
-        get_party_role = Sql.GetList("SELECT CPQ_PARTNER_FUNCTION, PARTY_ID FROM SAQTIP(NOLOCK) WHERE QUOTE_RECORD_ID = '"+str(self.contract_quote_record_id)+"' AND QTEREV_RECORD_ID = '"+str(self.quote_revision_record_id)+"' and CPQ_PARTNER_FUNCTION in ('SHIP TO')")
+        get_party_role = Sql.GetList("SELECT CPQ_PARTNER_FUNCTION, PARTY_ID FROM SAQTIP(NOLOCK) WHERE QUOTE_RECORD_ID = '"+str(self.quote_record_id)+"' AND QTEREV_RECORD_ID = '"+str(self.quote_revision_record_id)+"' and CPQ_PARTNER_FUNCTION in ('SHIP TO')")
         shipto_list=[]
         for keyobj in get_party_role:
             shipto_list.append('00'+str(keyobj.PARTY_ID))
