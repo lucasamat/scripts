fa175018e420ac4d68b04e9c9e832bae049705e3
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2022-03-09 07:06:08 +0530

    Update CQSYNCQTBL.py
    primary ship to handled.

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
@@ -1869,10 +1869,11 @@ class SyncQuoteAndCustomTables:
 										response=eval(response)	
 										auth="Bearer"+' '+str(response['access_token'])
 
-										get_party_role = Sql.GetList("SELECT PARTY_ID,CPQ_PARTNER_FUNCTION FROM SAQTIP(NOLOCK) WHERE QUOTE_RECORD_ID = '"+str(contract_quote_record_id)+"' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id)+"' and CPQ_PARTNER_FUNCTION in ('SOLD TO','SHIP TO')")
+										get_party_role = Sql.GetList("SELECT * FROM SAQTIP(NOLOCK) WHERE QUOTE_RECORD_ID = '"+str(contract_quote_record_id)+"' AND QTEREV_RECORD_ID = '"+str(quote_revision_record_id)+"' and CPQ_PARTNER_FUNCTION in ('SOLD TO','SHIP TO')")
 										account_info = {}
 										for keyobj in get_party_role:
-											account_info[keyobj.CPQ_PARTNER_FUNCTION] = keyobj.PARTY_ID
+											if keyobj.PRIMARY==1:
+												account_info[keyobj.CPQ_PARTNER_FUNCTION] = keyobj.PARTY_ID
 
 										contract_quote_id = contract_quote_obj.QUOTE_ID 
 										get_sales_ifo = Sql.GetFirst("select SALESORG_ID,CONTRACT_VALID_TO,CONTRACT_VALID_FROM,PRICELIST_ID,PRICEGROUP_ID from SAQTRV where QUOTE_RECORD_ID = '"+str(contract_quote_record_id)+"' AND QUOTE_REVISION_RECORD_ID = '"+str(quote_revision_record_id)+"'")
