3035a0da16ebfa6f0e2a18394514dd43f75e04ad
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2022-03-09 19:43:30 +0530

    Update CQPARTIFLW.py
    LOG

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTIFLW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPARTIFLW.py
@@ -30,7 +30,7 @@ def iflow_pricing_call(user,entries,revision):
         +str(revision)
         +"</Revision></CPQ_Columns></soapenv:Body></soapenv:Envelope>"
     )
-    #Log.Info("2222222222222222      " + str(requestdata))
+    Log.Info("2222222222222222      " + str(requestdata))
     LOGIN_CREDENTIALS = SqlHelper.GetFirst("SELECT URL FROM SYCONF where External_Table_Name='SAQIFP'")
     LOGIN_QUERY = SqlHelper.GetFirst("SELECT User_name as Username,Password,Domain,URL FROM SYCONF where Domain='AMAT_TST'")
     if LOGIN_CREDENTIALS is not None:
