ac85c0c973f095175db35adf996eaa9590c11b0d
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-03-10 12:28:39 +0530

    Update CQANULEDIT.py
    Commit

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQANULEDIT.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQANULEDIT.py
@@ -59,7 +59,7 @@ def constructcat4editablity(Quote_rec_id,MODE,values):
 
 		annual_dict[line_values.LINE] = record_list
 	Trace.Write("dictdictdict"+str(annual_dict)) 
-	return str(annual_dict)
+	return annual_dict
 
 
 ACTION = Param.ACTION
