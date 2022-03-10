40f8095c2ec55d119786de167d26d35b9681b280
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-03-10 16:31:51 +0530

    Update CQSYNCQTBL.py
    Commit

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
@@ -2049,27 +2049,28 @@ class SyncQuoteAndCustomTables:
 												records = ','.join(value[previous_index:index])
 												Log.Info("records"+str(records))
 												previous_index = index
-												quote_fab_equipments_obj = Sql.GetList("Select QUOTE_FAB_LOCATION_EQUIPMENTS_RECORD_ID FROM SAQFEQ(NOLOCK) WHERE EQUIPMENT_ID IN ({equipment_ids}) AND QUOTE_RECORD_ID = '{quote_record_id}' AND QTEREV_RECORD_ID = '{quote_revision_record_id}' ".format(equipment_ids = records,quote_record_id = Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")))
-												quote_service_obj = Sql.GetFirst("select SERVICE_TYPE from SAQTSV where SERVICE_ID = '{Service_Id}' AND QUOTE_RECORD_ID = '{quote_record_id}' AND QTEREV_RECORD_ID = '{quote_revision_record_id}'".format(Service_Id = service_id,quote_record_id = Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")))
-												quote_fab_equipments_record_id = [quote_fab_equipment_obj.QUOTE_FAB_LOCATION_EQUIPMENTS_RECORD_ID for quote_fab_equipment_obj in quote_fab_equipments_obj]
-												#Log.Info("quote_fab_equipments_record_id@@@@@@@"+str(len(quote_fab_equipments_record_id)))
-												service_id = service_id
-												service_type = quote_service_obj.SERVICE_TYPE
-												quote_record_id = contract_quote_obj.MASTER_TABLE_QUOTE_RECORD_ID
-												Product.SetGlobal("contract_quote_record_id",str(quote_record_id))
-												ScriptExecutor.ExecuteGlobal(
-																		"CQCRUDOPTN",
-																	{
-																		"NodeType"   : "COVERED OBJ MODEL",
-																		"ActionType" : "ADD_COVERED_OBJ",
-																		"Opertion"    : "ADD",
-																		"AllValues"  : False,
-																		"TriggerFrom"   : "python_script",
-																		"Values"	  : quote_fab_equipments_record_id,
-																		"ServiceId"  : service_id,
-																		"ServiceType" : service_type,
-																	},
-																)
+												if records:
+													quote_fab_equipments_obj = Sql.GetList("Select QUOTE_FAB_LOCATION_EQUIPMENTS_RECORD_ID FROM SAQFEQ(NOLOCK) WHERE EQUIPMENT_ID IN ({equipment_ids}) AND QUOTE_RECORD_ID = '{quote_record_id}' AND QTEREV_RECORD_ID = '{quote_revision_record_id}' ".format(equipment_ids = records,quote_record_id = Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")))
+													quote_service_obj = Sql.GetFirst("select SERVICE_TYPE from SAQTSV where SERVICE_ID = '{Service_Id}' AND QUOTE_RECORD_ID = '{quote_record_id}' AND QTEREV_RECORD_ID = '{quote_revision_record_id}'".format(Service_Id = service_id,quote_record_id = Quote.GetGlobal("contract_quote_record_id"),quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")))
+													quote_fab_equipments_record_id = [quote_fab_equipment_obj.QUOTE_FAB_LOCATION_EQUIPMENTS_RECORD_ID for quote_fab_equipment_obj in quote_fab_equipments_obj]
+													#Log.Info("quote_fab_equipments_record_id@@@@@@@"+str(len(quote_fab_equipments_record_id)))
+													service_id = service_id
+													service_type = quote_service_obj.SERVICE_TYPE
+													quote_record_id = contract_quote_obj.MASTER_TABLE_QUOTE_RECORD_ID
+													Product.SetGlobal("contract_quote_record_id",str(quote_record_id))
+													ScriptExecutor.ExecuteGlobal(
+																			"CQCRUDOPTN",
+																		{
+																			"NodeType"   : "COVERED OBJ MODEL",
+																			"ActionType" : "ADD_COVERED_OBJ",
+																			"Opertion"    : "ADD",
+																			"AllValues"  : False,
+																			"TriggerFrom"   : "python_script",
+																			"Values"	  : quote_fab_equipments_record_id,
+																			"ServiceId"  : service_id,
+																			"ServiceType" : service_type,
+																		},
+																	)
 										else:
 											Log.Info("value_equipmentsinsideelse"+str(len(value)))
 											elements = (','.join(value))
