3045d7a151071ebe3ad997089c3c7ccf8e5190fe
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2022-03-16 15:07:03 +0530

    Update SYULODTREE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
@@ -2322,6 +2322,7 @@ class TreeView:
 									#Trace.Write(str(ObjRecId)+"---SUBTAB_NAMEsss*"+str(subTabName)+'--1947---'+str(NodeText)+'---Node Name---'+str(NodeName)+'--Objname--'+str(ObjName))
 										
 									if subTabName:
+										Trace.Write("Events subtab-----"+str(subTabName))
 										if subTabName=="Events" and Product.GetGlobal("SERVICE")=="Z0009":
 											service_entitlement_object =Sql.GetFirst("""select ENTITLEMENT_XML from SAQTSE (nolock) where QUOTE_RECORD_ID = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{RevisionRecordId}' and SERVICE_ID = '{service_id}' """.format(QuoteRecordId = contract_quote_record_id,RevisionRecordId=quote_revision_record_id,service_id = Product.GetGlobal("SERVICE")))
 											if service_entitlement_object is not None:
