9d8868d679b15707cd9c32ae489ae76c21594fe8
Aravind <Aravind_Muthiah@contractor.amat.com>
2022-03-17 19:48:01 +0530

    Update CQTDUPDDWD.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTDUPDDWD.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTDUPDDWD.py
@@ -587,6 +587,13 @@ class ContractQuoteUploadTableData(ContractQuoteSpareOpertion):
 				
 				self.columns = ",".join(header)
 				Trace.Write("colums"+str(self.columns))
+				col=self.columns
+				table_columns = col.split(",")
+				replace_col ={'CONSUMABLE/NON CONSUMABALE':'MATPRIGRP_ID','CUSTOMER WILL ACCPET W/6K PART':'CUSTOMER_ACCEPT_PART','CUSTOMER ANNUAL COMMIT':'CUSTOMER_ANNUAL_QUANTITY'}
+				xls_col=replace_col.get
+				table_columns = [xls_col(val,val) for val in table_columns]
+				self.columns = ",".join(table_columns)
+				Trace.Write("table_columns---"+str(table_columns))
 				modified_records = []
 				for spare_record in xls_spare_records[1:]:
 					
