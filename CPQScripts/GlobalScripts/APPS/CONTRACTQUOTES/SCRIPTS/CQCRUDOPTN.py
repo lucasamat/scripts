29debf6ee1aab44c2020097a86bd131098635ad7
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2022-03-18 19:14:09 +0530

    Update CQCRUDOPTN.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -6580,7 +6580,7 @@ class ContractQuoteCoveredObjModel(ContractQuoteCrudOpertion):
 
 								Trace.Write("555 "+str(self.tree_param)+" 555 "+str(pm_event_attribute_value))
 								if (self.tree_param in ("Z0035","Z0091","Z0009","Z0004") and "Included - All PM" in pm_event_attribute_value):
-									additional_where = " (MAEAPK.MNTEVT_LEVEL = 'Chamber / Module PM' OR MAEAPK.MNTEVT_LEVEL = 'Scheduled Maintenance') AND"
+									additional_where = " (ISNULL(MAEAPK.MNTEVT_LEVEL,'') IN ('Chamber / Module PM' ,'Scheduled Maintenance' )) AND"
 								elif (self.tree_param in ("Z0035","Z0091",) and "Included - Monthly and Above" in pm_event_attribute_value):
 									additional_where = " (MAEAPK.MNTEVT_LEVEL = 'Chamber / Module PM' OR MAEAPK.MNTEVT_LEVEL = 'Scheduled Maintenance') AND (MAEAPK.PM_ID = 'Monthly' OR MAEAPK.PM_ID = 'Quarterly' OR MAEAPK.PM_ID = 'Semi-Annual' OR MAEAPK.PM_ID = 'Annual') AND"
 								elif (self.tree_param in ("Z0092","Z0099") and ("Included - Quarterly and Above" in pm_event_attribute_value or "Included - Qtrly and Above" in pm_event_attribute_value)):
