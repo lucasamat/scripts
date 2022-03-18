0720b0e74a6f07fd2154a5fbb7fe9b0a0fbd3ebf
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2022-03-18 15:14:28 +0530

    Update SYBLKETRLG.py
    added regex part for single parts

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYBLKETRLG.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYBLKETRLG.py
@@ -1429,6 +1429,7 @@ def RELATEDMULTISELECTONSAVE(TITLE, VALUE, CLICKEDID, RECORDID,selectPN,ALLVALUE
 			part_numbers=re.sub(r"'",'"',part_numbers)
 			part_numbers=re.sub(r"\(",'[',part_numbers)
 			part_numbers=re.sub(r"\)",']',part_numbers)
+			part_numbers=re.sub(r',]',']',part_numbers)
 			Trace.Write('### Part Number for CQIFLSPARE-->'+str(part_numbers))
 			CQIFLSPARE.iflow_pullspareparts_call(str(User.UserName),str(account_info.get('SOLD TO')),str(account_info.get('SHIP TO')),salesorg, pricelist,pricegroup,'Yes','Yes',part_numbers,validfrom,validto,Quote.CompositeNumber,rev_rec_id,auth)
 
