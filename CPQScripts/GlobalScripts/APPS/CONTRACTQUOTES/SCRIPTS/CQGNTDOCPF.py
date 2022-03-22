2e970cf470e2939a24afcd2550eeb5eb8058e051
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-03-22 14:02:05 +0530

    Update CQGNTDOCPF.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQGNTDOCPF.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQGNTDOCPF.py
@@ -4,10 +4,10 @@ GDLIST = Quote.GetGeneratedDocumentList('AMAT_SUBTOTAL_OFFERING')
 get_delivery_parts = Quote.GetCustomField('ITEM_DELIVERY_SCHEDULE').Content
 get_bill_matrix = Quote.GetCustomField('Billing_Matrix').Content
 gets_delivery_parts_z0110 = Quote.GetCustomField('PARTS_DELIVERY_SCHEDULE').Content
-
-if get_delivery_parts == "YES":
+fpm_delivery_schedules = Quote.GetCustomField('QT_OD_DELIVERY_SERVICE').Content
+if get_delivery_parts == "YES" and fpm_delivery_schedules == "YES":
 	GDLIST = Quote.GetGeneratedDocumentList('AMAT_FPM_QUOTE')
-elif gets_delivery_parts_z0110 == "YES":
+elif fpm_delivery_schedules == "NO_GENERTAE_DELIVERY":
 	GDLIST = Quote.GetGeneratedDocumentList('Amat FPM Z0110')
 elif get_bill_matrix == "YES":
 	GDLIST = Quote.GetGeneratedDocumentList('AMAT Total Quote')
