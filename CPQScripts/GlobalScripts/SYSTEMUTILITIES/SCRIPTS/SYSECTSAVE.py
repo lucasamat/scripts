da8f9348114d2b0245a2d6ce124a0793f941c1a4
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-03-24 11:49:33 +0530

    Update SYSECTSAVE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSECTSAVE.py
@@ -748,7 +748,7 @@ def MaterialSave(ObjectName, RECORD, warning_msg, SectionRecId=None,subtab_name=
 						newdict.update(dictc)
 						tableInfo = Sql.GetTable(str(TableName))
 						Trace.Write('subtab_name---'+str(subtab_name))
-						if subtab_name != "Legal SoW":
+						if subtab_name not in  ("Legal SoW","Basic Information"):
 							newdict["SLSDIS_PRICE_INGL_CURR"] = re.sub('USD','',newdict["SLSDIS_PRICE_INGL_CURR"])
 							newdict["BD_PRICE_INGL_CURR"] = re.sub('USD','',newdict["BD_PRICE_INGL_CURR"])
 							newdict["CEILING_PRICE_INGL_CURR"] = re.sub('USD','',newdict["CEILING_PRICE_INGL_CURR"])
