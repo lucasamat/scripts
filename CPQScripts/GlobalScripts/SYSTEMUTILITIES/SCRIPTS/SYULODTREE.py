e26e3505629d0c229fc0602ec7da42adedaa4887
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-03-24 10:38:05 +0530

    Update SYULODTREE.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
@@ -784,7 +784,7 @@ class TreeView:
 		#Trace.Write("returnList----------------> " + str(returnList))
 		cbc_subtab = ""
 		try:
-			Trace.Write("Clean Booking Checklist")
+			#Trace.Write("Clean Booking Checklist")
 			user_id = ScriptExecutor.ExecuteGlobal("SYUSDETAIL", "USERNAME")
 			saqdlt_query = Sql.GetFirst("SELECT MEMBER_ID FROM SAQDLT (NOLOCK) WHERE QUOTE_RECORD_ID = '{qte_rec_id}' AND QTEREV_RECORD_ID = '{revision_rec_id}' AND C4C_PARTNERFUNCTION_ID = 'CONTRACT MANAGER'".format(qte_rec_id = contract_quote_record_id,revision_rec_id = quote_revision_record_id))
 			#A055S000P01-17166 start
