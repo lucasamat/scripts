0cc9befd97ac31346a855198becaeabb221f5241
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-03-25 18:08:18 +0530

    commented cbc completed code for verify

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQPREVWDTL.py
@@ -737,7 +737,7 @@ def countcbc(Qt_rec_id, Quote, MODE):
 	popupquery_value = popupquery.cnt
 	#A055S000P01-17166 start
 	update_rev_status = "UPDATE SAQTRV SET WORKFLOW_STATUS = 'CLEAN BOOKING CHECKLIST',REVISION_STATUS = 'CBC-CBC COMPLETED' where QUOTE_RECORD_ID='{contract_quote_rec_id}' AND QTEREV_RECORD_ID = '{quote_revision_rec_id}'".format(contract_quote_rec_id=Quote,quote_revision_rec_id=quote_revision_record_id)
-	Sql.RunQuery(update_rev_status)
+	#Sql.RunQuery(update_rev_status)
 	#A055S000P01-17166 end
 	CQCPQC4CWB.writeback_to_c4c("quote_header",Quote,quote_revision_record_id)
 	time.sleep(3) #A055S000P01-16535
