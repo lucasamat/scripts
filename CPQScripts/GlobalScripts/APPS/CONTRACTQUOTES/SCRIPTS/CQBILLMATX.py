0bdb884fecdaa54b60b7a3f35182ac4da467cb34
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-03-28 17:38:05 +0530

    Update CQBILLMATX.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQBILLMATX.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQBILLMATX.py
@@ -754,7 +754,7 @@ def insert_items_billing_plan(total_months=1, billing_date='',billing_end_date =
 						
 						COMVAL_INGL_CURR as COMMITTED_VALUE_INGL_CURR,
 						SAQRIT.ESTVAL_INGL_CURR	as 	ESTVAL_INGL_CURR,
-						ISNULL({amount_column}, 0) / {get_val} AS DOC_CURRENCY,
+						SAQRIT.DOC_CURRENCY AS DOC_CURRENCY,
 						0 as ESTVAL_INDT_CURR,	
 						QUOTE_RECORD_ID,
 						QTEREV_ID,
@@ -806,7 +806,7 @@ def insert_items_billing_plan(total_months=1, billing_date='',billing_end_date =
 						
 						COMVAL_INGL_CURR as COMMITTED_VALUE_INGL_CURR,
 						ISNULL({amount_column}, 0) / {get_val}	as 	ESTVAL_INGL_CURR,
-						{amount_column} AS DOC_CURRENCY,
+						DOC_CURRENCY AS DOC_CURRENCY,
 						ISNULL({amount_column}, 0) / {get_val} as ESTVAL_INDT_CURR,	
 						QUOTE_RECORD_ID,
 						QTEREV_ID,
@@ -857,7 +857,7 @@ def insert_items_billing_plan(total_months=1, billing_date='',billing_end_date =
 						
 						COMVAL_INGL_CURR as COMMITTED_VALUE_INGL_CURR,
 						ISNULL({amount_column}, 0) / {get_val}	as 	ESTVAL_INGL_CURR,
-						{amount_column} AS DOC_CURRENCY,
+						DOC_CURRENCY AS DOC_CURRENCY,
 						ISNULL({amount_column}, 0) / {get_val} as ESTVAL_INDT_CURR,	
 						QUOTE_RECORD_ID,
 						QTEREV_ID,
