14df0bcdf7aa8c724c9b6ec2283418689db53491
Srijaydhurga <Srijaydhurga_Subburah@contractor.amat.com>
2022-04-01 11:20:53 +0530

    Update CQIFWUDQTM.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQIFWUDQTM.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQIFWUDQTM.py
@@ -1417,7 +1417,7 @@ def billingmatrix_create():
 	get_ent_val = get_ent_billing_type_value = get_ent_bill_cycle = get_billing_type = ''
 	if contract_start_date and contract_end_date and billing_plan_obj:
 		Sql.RunQuery("""DELETE FROM SAQIBP WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{RevisionRecordId}'""".format(QuoteRecordId=contract_quote_rec_id,RevisionRecordId=quote_revision_rec_id))
-		#Trace.Write('4739---------4744------')
+		Log.Info('4739---------4744------')
 		for val in billing_plan_obj:
 			if billing_plan_obj:				
 				contract_start_date = val.BILLING_START_DATE
@@ -1428,6 +1428,7 @@ def billingmatrix_create():
 				get_service_val = val.PRDOFR_ID
 				get_billing_cycle = Sql.GetFirst("select ENTITLEMENT_XML from SAQITE where QUOTE_RECORD_ID = '{qtid}' AND QTEREV_RECORD_ID = '{qt_rev_id}' and SERVICE_ID = '{get_service}'".format(qtid =contract_quote_rec_id,qt_rev_id=quote_revision_rec_id,get_service = str(get_service_val).strip()))
 				if get_billing_cycle:
+					Log.Info('1431-----')
 					updateentXML = get_billing_cycle.ENTITLEMENT_XML
 					pattern_tag = re.compile(r'(<QUOTE_ITEM_ENTITLEMENT>[\w\W]*?</QUOTE_ITEM_ENTITLEMENT>)')
 					pattern_id = re.compile(r'<ENTITLEMENT_ID>(AGS_'+str(get_service_val)+'_PQB_BILCYC|AGS_'+str(get_service_val)+'_PQB_BILTYP)</ENTITLEMENT_ID>')
@@ -1456,6 +1457,7 @@ def billingmatrix_create():
 				#Log.Info(str(get_billing_type)+'--475--'+str(get_ent_bill_cycle))
 				billing_month_end = 0
 				entitlement_obj = Sql.GetFirst("select convert(xml,replace(replace(replace(replace(replace(replace(ENTITLEMENT_XML,'&',';#38'),'''',';#39'),' < ',' &lt; ' ),' > ',' &gt; ' ),'_>','_&gt;'),'_<','_&lt;')) as ENTITLEMENT_XML,QUOTE_RECORD_ID,SERVICE_ID from SAQTSE (nolock) where QUOTE_RECORD_ID = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{RevisionRecordId}'".format(QuoteRecordId =contract_quote_rec_id,RevisionRecordId=quote_revision_rec_id))
+				Log.Info('1431--get_ent_bill_cycle-----'+str(get_ent_bill_cycle))
 				if str(get_ent_bill_cycle).upper() == "ONE ITEM PER QUOTE":
 					if billing_day in (29,30,31):
 						if start_date.month == 2:
