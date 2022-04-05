cc2655aa5c145fbbd46a3a64787351ed6d080b96
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-04-05 13:13:35 +0530

    Update SYUADNWPOP.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYUADNWPOP.py
@@ -3739,7 +3739,7 @@ def POPUPLISTVALUEADDNEW(
 							
 							# new_value_dict["ZAFNOTE"] = credit_notes_str
 							new_value_dict["ZAFNOTE"] = (
-									<abbr> Joe </abbr>
+									'<abbr> Joe </abbr>'
 								)  
 						else:
 							new_value_dict["ZAFNOTE"] = ""
