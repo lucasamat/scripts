e34d530c08284af2833faaf2e75a076d5358717a
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2022-04-05 17:51:23 +0530

    Update CQCRUDOPTN.py
    syntax error fixed

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQCRUDOPTN.py
@@ -7250,10 +7250,19 @@ class ContractQuoteNoficationModel(ContractQuoteCrudOpertion):
 			if getStatus.REVISION_STATUS == "APPROVED":
 				gettransactionmessage = ""
 			if getStatus:
-				if getStatus.MODVRS_DIRTY_FLAG == 1 or getItemFlag.MODVRS_DIRTY_FLAG == 1 or getFlag.MODVRS_DIRTY_FLAG == 1:
+				if getStatus.MODVRS_DIRTY_FLAG == 1:
 					gettransactionmessage += 'This Quote has the following notifications:'
 					gettransactionmessage += ('<div class="col-md-12" id="dirty-flag-warning"><div class="col-md-12 alert-warning"><label> <img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/warning1.svg" alt="Warning"> 99999 | NEW COST MODEL VERSIONS AVAILABLE | There are new cost model versions available from SSCM. In order to update the costs and prices, please revision the quote.</label></div></div>')
-		#Trace.Write('gettransactionmessage---'+str(gettransactionmessage))
+			elif getItemFlag:
+				if getItemFlag.MODVRS_DIRTY_FLAG == 1:
+					gettransactionmessage += 'This Quote has the following notifications:'
+					gettransactionmessage += ('<div class="col-md-12" id="dirty-flag-warning"><div class="col-md-12 alert-warning"><label> <img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/warning1.svg" alt="Warning"> 99999 | NEW COST MODEL VERSIONS AVAILABLE | There are new cost model versions available from SSCM. In order to update the costs and prices, please revision the quote.</label></div></div>')
+			elif getFlag:
+				if getFlag.MODVRS_DIRTY_FLAG == 1:
+					gettransactionmessage += 'This Quote has the following notifications:'
+					gettransactionmessage += ('<div class="col-md-12" id="dirty-flag-warning"><div class="col-md-12 alert-warning"><label> <img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/warning1.svg" alt="Warning"> 99999 | NEW COST MODEL VERSIONS AVAILABLE | There are new cost model versions available from SSCM. In order to update the costs and prices, please revision the quote.</label></div></div>')
+			
+        #Trace.Write('gettransactionmessage---'+str(gettransactionmessage))
 		# if ent_message_query:
 		# 	#for val in obj_list:
 		# 	tablename = 'SAQTSE'
