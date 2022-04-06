a34eaa8aaf7602a5d20f8c84ed02eae6fd72056d
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-04-06 12:20:29 +0530

    Update SYDELCNMSG.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYDELCNMSG.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYDELCNMSG.py
@@ -439,7 +439,7 @@ class DeleteConfirmPopup:
             sacrcv_rec_query = Sql.GetFirst("SELECT CRTAPP_INGL_CURR,UNBL_INGL_CURR FROM SACRVC (NOLOCK) WHERE CREDITVOUCHER_RECORD_ID = '"+str(deleted_rec_query.CREDITVOUCHER_RECORD_ID)+"'")
             credit_applied = sacrcv_rec_query.CRTAPP_INGL_CURR - deleted_rec_query.CREDIT_APPLIED_INGL_CURR
             unapplied_balance = sacrcv_rec_query.UNBL_INGL_CURR + deleted_rec_query.CREDIT_APPLIED_INGL_CURR
-            update_SAQRCV = 'UPDATE SACRVC SET CRTAPP_INGL_CURR = "'+str(credit_applied)+'",UNBL_INGL_CURR = "'+str(unapplied_balance)+'" WHERE CREDITVOUCHER_RECORD_ID = "'+str(deleted_rec_query.CREDITVOUCHER_RECORD_ID)+'"'
+            update_SAQRCV = "UPDATE SACRVC SET CRTAPP_INGL_CURR = '"+str(credit_applied)+"',UNBL_INGL_CURR = '"+str(unapplied_balance)+"' WHERE CREDITVOUCHER_RECORD_ID = '"+str(deleted_rec_query.CREDITVOUCHER_RECORD_ID)+"'"
             Sql.RunQuery(update_SAQRCV)
             QueryStatement = "DELETE FROM "+str(Table)+" WHERE QUOTE_REV_CREDIT_VOUCHER_RECORD_ID ='"+str(RecordId)+"'"
             Sql.RunQuery(QueryStatement)
