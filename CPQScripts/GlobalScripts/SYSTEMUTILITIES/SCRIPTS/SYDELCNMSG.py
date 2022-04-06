f52a6d3e77ffd0d29503b586827ff66fe6b1ee12
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-04-06 11:13:06 +0530

    Update SYDELCNMSG.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYDELCNMSG.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYDELCNMSG.py
@@ -435,12 +435,12 @@ class DeleteConfirmPopup:
                     update_saqtrv = ("UPDATE SAQTRV SET DIRTY_FLAG = 'TRUE', REVISION_STATUS = 'CFG-CONFIGURING', WORKFLOW_STATUS = 'CONFIGURE' WHERE QUOTE_RECORD_ID = '"+str(contract_quote_record_id)+"' AND QTEREV_RECORD_ID = '" + str(quote_revision_record_id) +"'")
                     Sql.RunQuery(update_saqtrv)
         elif ObjName == "SAQRCV":
-            # deleted_rec_query = Sql.GetFirst("SELECT CREDIT_APPLIED_INGL_CURR,CREDITVOUCHER_RECORD_ID FROM SAQRCV (NOLOCK) WHERE QUOTE_REV_CREDIT_VOUCHER_RECORD_ID = '"+str(RecordId)+"'")
-            # saqrcv_rec_query = Sql.GetFirst("SELECT ")
-            # credit_applied = deleted_rec_query.CREDIT_APPLIED_INGL_CURR - 
-
-            # update_SAQRCV = 'UPDATE SAQRCV SET CREDIT_APPLIED_INGL_CURR = "'+str(credit_applied)+'",UNBL_INGL_CURR = "'+str(unapplied_balance)+'" WHERE CREDITVOUCHER_RECORD_ID = "'+str(deleted_rec_query.CREDITVOUCHER_RECORD_ID)+'"'
-
+            deleted_rec_query = Sql.GetFirst("SELECT CREDIT_APPLIED_INGL_CURR,CREDITVOUCHER_RECORD_ID FROM SAQRCV (NOLOCK) WHERE QUOTE_REV_CREDIT_VOUCHER_RECORD_ID = '"+str(RecordId)+"'")
+            sacrcv_rec_query = Sql.GetFirst("SELECT CRTAPP_INGL_CURR FROM SACRVC (NOLOCK) WHERE CREDITVOUCHER_RECORD_ID = '"+str(deleted_rec_query.CREDIT_APPLIED_INGL_CURR)+"'")
+            credit_applied = sacrcv_rec_query.CRTAPP_INGL_CURR - deleted_rec_query.CREDIT_APPLIED_INGL_CURR
+            unapplied_balance = sacrcv_rec_query.UNBL_INGL_CURR + deleted_rec_query.CREDIT_APPLIED_INGL_CURR
+            update_SAQRCV = 'UPDATE SACRVC SET CRTAPP_INGL_CURR = "'+str(credit_applied)+'",UNBL_INGL_CURR = "'+str(unapplied_balance)+'" WHERE CREDITVOUCHER_RECORD_ID = "'+str(deleted_rec_query.CREDITVOUCHER_RECORD_ID)+'"'
+            Sql.RunQuery(update_SAQRCV)
             QueryStatement = "DELETE FROM "+str(Table)+" WHERE QUOTE_REV_CREDIT_VOUCHER_RECORD_ID ='"+str(RecordId)+"'"
             Sql.RunQuery(QueryStatement)
         
