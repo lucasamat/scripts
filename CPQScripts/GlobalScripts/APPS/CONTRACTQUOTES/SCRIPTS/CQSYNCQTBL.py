fc9f42539950cf5387184884c5b5e4d26d5e125a
Wasim Abdul Kader <wasim.abdul@bostonharborconsulting.com>
2022-04-08 22:54:13 +0530

    Update CQSYNCQTBL.py
    Commit

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQSYNCQTBL.py
@@ -2102,7 +2102,8 @@ class SyncQuoteAndCustomTables:
                                     tableInfo.AddRow(tablerow)
                                     Sql.Upsert(tableInfo)       
                                 if employee_obj:
-                                    contact_master_table = Sql.GetFirst("SELECT CONTACT_RECORD_ID FROM SACONT (NOLOCK) WHERE CONTACT_ID = '{contact_id}'".format(contact_id=employees.get("PRIMARY_CONTACT_ID")))
+                                    employee_obj = Sql.GetFirst("select * from SAEMPL(nolock) where CRM_EMPLOYEE_ID = '{crm_employee_id}'".format(crm_employee_id=employees.get("CRM_EMPLOYEE_ID")))
+                                    contact_master_table = Sql.GetFirst("SELECT * FROM SACONT (NOLOCK) WHERE CONTACT_ID = '{contact_id}'".format(contact_id=employees.get("PRIMARY_CONTACT_ID")))
                                     quote_involved_party_contact_table_info = Sql.GetTable("SAQICT")
                                     contact_info_update = {
                                         "QUOTE_REV_INVOLVED_PARTY_CONTACT_ID": str(Guid.NewGuid()).upper(),
