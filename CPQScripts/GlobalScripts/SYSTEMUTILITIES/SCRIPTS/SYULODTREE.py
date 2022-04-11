e3bfa8ac1ef3121d596a91c7c334227f451285b9
Gayathri Amaresan <gayathri.amaresan@bostonharborconsulting.com>
2022-04-11 18:14:27 +0530

    Update SYULODTREE.py
    To Load the tree view as TKM for Z0128 product offering

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYULODTREE.py
@@ -3404,7 +3404,7 @@ class TreeView:
 
     def PMSATree(self, TreeParam):
         flag = 0
-        if str(Quote.GetGlobal("SERVICE")) == "Z0009" or str(Quote.GetGlobal("SERVICE")) == "Z0010":
+        if str(Quote.GetGlobal("SERVICE")) == "Z0009" or str(Quote.GetGlobal("SERVICE")) == "Z0010" or str(Quote.GetGlobal("SERVICE")) == "Z0128":
             TableName = "SAQTSE"
             contract_quote_record_id = Quote.GetGlobal("contract_quote_record_id")
             quote_revision_record_id = Quote.GetGlobal("quote_revision_record_id")
