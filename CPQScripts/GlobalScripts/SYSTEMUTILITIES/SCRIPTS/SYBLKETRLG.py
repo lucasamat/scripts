01b0b2336687c0a70fdae93ce4d5ed134f9430a4
Joe Ebenezer H <66054352+JoeEbenezerBHC@users.noreply.github.com>
2022-04-18 14:44:57 +0530

    Update SYBLKETRLG.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYBLKETRLG.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYBLKETRLG.py
@@ -1408,7 +1408,7 @@ def RELATEDMULTISELECTONSAVE(TITLE, VALUE, CLICKEDID, RECORDID,selectPN,ALLVALUE
 						Trace.Write("current_credit--- "+str(current_credit)+"value--- "+str(value))
 						try:
 							if float(current_credit) > float(value):
-								credit_applied = float(current_credit) - float(value)
+								credit_applied = sacrcv_rec_query.CRTAPP_INGL_CURR - float(value)
 								unapplied_balance = sacrcv_rec_query.UNBL_INGL_CURR + float(value)
 								Sql.RunQuery("UPDATE SACRVC SET CRTAPP_INGL_CURR = '"+str(credit_applied)+"',UNBL_INGL_CURR = '"+str(unapplied_balance)+"' WHERE CREDITVOUCHER_RECORD_ID = '"+str(current_credit_query.CREDITVOUCHER_RECORD_ID)+"'")
 								Trace.Write("SACRVC UPDATED")
