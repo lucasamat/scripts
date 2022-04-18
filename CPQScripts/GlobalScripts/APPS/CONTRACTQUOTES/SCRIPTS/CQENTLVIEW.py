f6afb364ab626dfa6fd4e089e1cc078bc82cd5ce
Viknesh Duraisamy <Viknesh_Duraisamy@contractor.amat.com>
2022-04-19 00:15:36 +0530

    Update CQENTLVIEW.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQENTLVIEW.py
@@ -767,7 +767,7 @@ class EntitlementView():
 					Trace.Write(str(tbrow))
 					columns = ', '.join("" + str(x) + "" for x in tbrow.keys())
 					for x in tbrow.values():
-						x = x.encode('ascii','ignore').decode('ascii')
+						x = str(x).encode('ascii','ignore').decode('ascii')
 						values = ', '.join("'"+x+"'")
 					#values = ', '.join("'" + x + "'" for x in tbrow.values())
 					insert_qtqtse_query = "INSERT INTO SAQTSE ( %s ) VALUES ( %s );" % (columns, values)				
