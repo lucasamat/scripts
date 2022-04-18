4bcfe5356cdf50e53b1f818eaa2fbdae80f67d1b
Aravind <Aravind_Muthiah@contractor.amat.com>
2022-04-18 14:00:50 +0530

    Update SYLDRTLIST.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYLDRTLIST.py
@@ -3286,7 +3286,6 @@ class SYLDRTLIST:
                             value1234 = value1234.replace("APPROVAL REQUIRED","<img title='Approval Required' src=/mt/APPLIEDMATERIALS_TST/Additionalfiles/clock_exe.svg> APPROVAL REQUIRED")
                         if value1234 == "ACQUIRING":
                             if RECORD_ID == 'SYOBJR-00005':
-
                                 value1234 = value1234.replace("ACQUIRING","<img title='Acquiring' src=/mt/APPLIEDMATERIALS_TST/Additionalfiles/Cloud_Icon.svg>")
                             else:
                                 value1234 = value1234.replace("ACQUIRING","<img title='Acquiring' src=/mt/APPLIEDMATERIALS_TST/Additionalfiles/Cloud_Icon.svg> ACQUIRING")
@@ -3304,9 +3303,8 @@ class SYLDRTLIST:
                         if value1234 ==  "PRR-ON HOLD PRICING":
                             value1234 = value1234.replace("PRR-ON HOLD PRICING","<img title='PRR-ON HOLD PRICING' src=/mt/APPLIEDMATERIALS_TST/Additionalfiles/pricing_on_hold.svg>")
                         if value1234 == "NOT PRICED":
-                            Trace.Write("@@@Price")
                             if RECORD_ID == 'SYOBJR-00005':
-                                value1234 = value1234.replace("NOT PRICING","<img title='NOT PRICING' src=/mt/appliedmaterials_tst/Additionalfiles/no-price.svg>")
+                                value1234 = value1234.replace("NOT PRICED","<img title='NOT PRICED' src=/mt/appliedmaterials_tst/Additionalfiles/no-price.svg>")
                         if value1234 != "ACQUIRED" and value1234 != "APPROVAL REQUIRED" and value1234 != "ERROR" and value1234 != "ASSEMBLY IS MISSING" and value1234 != "CFG-ON HOLD - COSTING" and value1234 != "PARTIALLY PRICED" and value1234 != "ACQUIRING" and value1234 != "PRICED" and value1234 != "PRR-ON HOLD PRICING":                        
                             value1234 = value1234
                         if value123 == objRecName:
