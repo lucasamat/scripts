1747eed93d8682d54186406aa560078439a11b60
Aravind <Aravind_Muthiah@contractor.amat.com>
2022-04-19 19:32:57 +0530

    Update SYSUBANNER.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYSUBANNER.py
@@ -3251,7 +3251,7 @@ def Related_Sub_Banner(
                     #sec_rel_sub_bnr += (btn)
                     dropdown_multi_btn_str += '''</ul></div></div>'''
                     #Trace.Write('3095--dropdown_multi_btn_str--'+str(dropdown_multi_btn_str))
-                    #sec_rel_sub_bnr += (dropdown_multi_btn_str)
+                    sec_rel_sub_bnr += (dropdown_multi_btn_str)
                     #Trace.Write('3095--sec_rel_sub_bnr--'+str(sec_rel_sub_bnr))
                 else:
                     sec_rel_sub_bnr += str(add_button)
