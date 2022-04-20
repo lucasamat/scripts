8487b3150e4848370f4de0e5e63718f27f1ed9f6
Suriyanarayananp01 <79976716+Suriyanarayananp01@users.noreply.github.com>
2022-04-20 17:04:34 +0530

    Update QTPOSTPTPR.py

--- CPQScripts/IntegrationScripts/QTPOSTPTPR.py
+++ CPQScripts/IntegrationScripts/QTPOSTPTPR.py
@@ -225,14 +225,11 @@ try:
                 if len(prefixZero)>0:
                     Itemidinfo[0]=re.sub(prefixZero,'',Itemidinfo[0])
                 try:
-                    Log.Info("NPU-->"+str(i["netPriceUnit"]))
-                    Log.Info("NPUV-->"+str(i["netPriceUnitValue"]))
-                    if str(i["netPriceUnit"]) != str(isocode_salesuom) and int(i["netPriceUnitValue"]) == 1:
+                    if str(i["netPriceUnit"]) != str(isocode_salesuom) and int(float((i["netPriceUnitValue"]))) == 1:
                         i["netPrice"] = i["netPrice"] * numerator
-                    elif str(i["netPriceUnit"]) != str(isocode_salesuom) and int(i["netPriceUnitValue"]) > 1:
-                        i["netPrice"] = (i["netPrice"] / int(i["netPriceUnitValue"])) * numerator
+                    elif str(i["netPriceUnit"]) != str(isocode_salesuom) and int(float(i["netPriceUnitValue"])) > 1:
+                        i["netPrice"] = (i["netPrice"] / int(float((i["netPriceUnitValue"]))) * numerator
                 except:
-                    Log.Info("PN:"+str(Itemidinfo[-5]))
                     Log.Info("Item=>I::"+str(i))    
                 insert_data.append((str(Guid.NewGuid()).upper(), Itemidinfo[0], Itemidinfo[-5], i["netPrice"], 'IN PROGRESS', QUOTE, contract_quote_record_id, batch_group_record_id,str(Taxrate),str(core_credit_amount),i["taxValue"],i["netValue"],i["grossValue"],i["freightValue"],i["netPrice"],i["netPriceUnit"],i["netPriceUnitValue"]))
                 
