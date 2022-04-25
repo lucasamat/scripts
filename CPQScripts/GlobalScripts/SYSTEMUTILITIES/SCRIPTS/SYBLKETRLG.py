540ef9dc4698700c821698342829785a8d4e91db
Aravind <Aravind_Muthiah@contractor.amat.com>
2022-04-25 12:58:30 +0530

    Update SYBLKETRLG.py

--- CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYBLKETRLG.py
+++ CPQScripts/GlobalScripts/SYSTEMUTILITIES/SCRIPTS/SYBLKETRLG.py
@@ -1173,7 +1173,7 @@ def RELATEDMULTISELECTONSAVE(TITLE, VALUE, CLICKEDID, RECORDID,selectPN,ALLVALUE
 					Trace.Write("val1"+str(ALLVALUES1[index]))
 					Trace.Write("val2"+str(ALLVALUES2[index]))
 					Trace.Write("val3"+str(ALLVALUES3[index]))
-					Trace.Write("**"+ str(objh_head)))
+					Trace.Write("**"+ str(objh_head))
 									
 					Sql.RunQuery("""UPDATE SAQSPT SET {column} = {val},{column1} = '{value1}',{column2} = '{value2}',{column3} = '{value3}' WHERE QUOTE_RECORD_ID = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{rev_rec_id}' AND {rec_name} = '{rec_id}' """.format(column=TITLE.split(',')[0],val=value,column1=TITLE.split(',')[1],value1 = ALLVALUES1[index],column2=TITLE.split(',')[2],value2 = ALLVALUES2[index],column3=TITLE.split(',')[3],value3 = ALLVALUES3[index],QuoteRecordId = Qt_rec_id,rev_rec_id = rev_rec_id,rec_name = objh_head,rec_id = sql_obj.QUOTE_SERVICE_PART_RECORD_ID))
 
