39c7fcb772623458cd31cdec1c51c26f03492b86
Aravind <Aravind_Muthiah@contractor.amat.com>
2022-04-25 22:39:44 +0530

    Update CQTDUPDDWD.py

--- CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTDUPDDWD.py
+++ CPQScripts/GlobalScripts/APPS/CONTRACTQUOTES/SCRIPTS/CQTDUPDDWD.py
@@ -13,6 +13,7 @@ from SYDATABASE import SQL
 import time
 from datetime import timedelta , date
 import CQPARTIFLW
+import CQIFLSPARE
 Sql = SQL()
 
 class ContractQuoteSpareOpertion:
@@ -693,7 +694,190 @@ class ContractQuoteUploadTableData(ContractQuoteSpareOpertion):
 		#self.insert_delivery_schedule()
 		return "Import Success"
 
+	def _message_txt(self):
 
+		Trace.Write("@@")
+		msg_txt = (
+					'<div  class="col-md-12" id="dirty-flag-warning"><div class="col-md-12 alert-info"><label> <img src="/mt/APPLIEDMATERIALS_TST/Additionalfiles/infor_icon_green.svg" alt="Warning">'
+					+ "NUMBER OF PART NUMBER IMPORTED SUCCESSFULLY"
+					+ " : "
+					+ str(parts_imprted)
+					+ " : "
+					+ " PART NUMBER FAILED : "
+					+ str(parts_failed)
+					+ "</label></div></div>"
+				)
+		return msg_txt
+
+	def _add_parts(self):
+		batch_group_record_id = str(Guid.NewGuid()).upper()
+	
+
+		spare_parts_details = [(str(Guid.NewGuid()).upper(), spare_part, 1, 'IN PROGRESS', self.contract_quote_id, self.contract_quote_record_id, batch_group_record_id,self.quote_revision_record_id) for spare_part in self.values[0].splitlines()]
+
+		spare_parts_details_joined = ', '.join(map(str, spare_parts_details))
+		self._process_query("""INSERT INTO SYSPBT(BATCH_RECORD_ID, SAP_PART_NUMBER, QUANTITY, BATCH_STATUS, QUOTE_ID, QUOTE_RECORD_ID, BATCH_GROUP_RECORD_ID,QTEREV_RECORD_ID) 
+								SELECT * FROM (VALUES {}) QS (BATCH_RECORD_ID, SAP_PART_NUMBER, QUANTITY, BATCH_STATUS, QUOTE_ID, QUOTE_RECORD_ID, BATCH_GROUP_RECORD_ID,QTEREV_RECORD_ID)""".format(spare_parts_details_joined))
+		self._process_query("""DELETE SYSPBT FROM SYSPBT JOIN SAQSPT ON SYSPBT.SAP_PART_NUMBER = SAQSPT.PART_NUMBER AND SYSPBT.QUOTE_RECORD_ID = SAQSPT.QUOTE_RECORD_ID AND  SYSPBT.QTEREV_RECORD_ID = SAQSPT.QTEREV_RECORD_ID WHERE SYSPBT.QUOTE_RECORD_ID = '{QuoteRecordId}' AND SYSPBT.QTEREV_RECORD_ID = '{RevisionRecordId}'""".format(QuoteRecordId=self.contract_quote_record_id,RevisionRecordId=self.quote_revision_record_id))
+		self._process_query("""DELETE SYSPBT FROM SYSPBT JOIN SAQRSP ON SYSPBT.SAP_PART_NUMBER = SAQRSP.PART_NUMBER AND SYSPBT.QUOTE_RECORD_ID = SAQRSP.QUOTE_RECORD_ID AND  SYSPBT.QTEREV_RECORD_ID = SAQRSP.QTEREV_RECORD_ID WHERE SYSPBT.QUOTE_RECORD_ID = '{QuoteRecordId}' AND SYSPBT.QTEREV_RECORD_ID = '{RevisionRecordId}'""".format(QuoteRecordId=self.contract_quote_record_id,RevisionRecordId=self.quote_revision_record_id))
+		if self.action_type == "ADD_PARTS":
+			parts_value = 0
+			Service_Id = self.tree_param
+			entitlement_obj = Sql.GetFirst("select ENTITLEMENT_XML from SAQTSE (nolock) where QUOTE_RECORD_ID  = '{QuoteRecordId}' AND QTEREV_RECORD_ID = '{RevisionRecordId}'".format(QuoteRecordId=self.contract_quote_record_id,RevisionRecordId=self.quote_revision_record_id))
+			entitlement_xml = entitlement_obj.ENTITLEMENT_XML
+			quote_item_tag = re.compile(r'(<QUOTE_ITEM_ENTITLEMENT>[\w\W]*?</QUOTE_ITEM_ENTITLEMENT>)')
+			entitlement_value_str = re.compile(r'<ENTITLEMENT_ID>AGS_'+str(Service_Id)+'[^>]*?_TSC_SCPT</ENTITLEMENT_ID>')
+			value = re.compile(r'<ENTITLEMENT_DISPLAY_VALUE>([^>]*?)</ENTITLEMENT_DISPLAY_VALUE>')
+			for m in re.finditer(quote_item_tag, entitlement_xml):
+				sub_string = m.group(1)
+				scheduled_parts =re.findall(entitlement_value_str,sub_string)
+				scheduled_value =re.findall(value,sub_string)
+				if scheduled_parts and scheduled_value:
+					parts_value = scheduled_value[0]
+					break
+			if self.tree_param == 'Z0108':
+				delivery_mode = "OFFSITE"
+				schedule_mode= "SCHEDULED" if int(parts_value) > 9 else "UNSCHEDULED"
+			elif self.tree_param == 'Z0110':
+				delivery_mode = " "
+				schedule_mode= " "
+			if self.tree_param in ('Z0108','Z0110'):
+				#iflow for spare parts...
+				requestdata = "client_id=application&grant_type=client_credentials&username=ef66312d-bf20-416d-a902-4c646a554c10&password=Ieo.6c8hkYK9VtFe8HbgTqGev4&scope=fpmxcsafeaccess"
+				webclient.Headers[System.Net.HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded"
+				webclient.Headers[System.Net.HttpRequestHeader.Authorization] = "Basic ZWY2NjMxMmQtYmYyMC00MTZkLWE5MDItNGM2NDZhNTU0YzEwOkllby42Yzhoa1lLOVZ0RmU4SGJnVHFHZXY0"
+				response = webclient.UploadString('https://oauth2.c-1404e87.kyma.shoot.live.k8s-hana.ondemand.com/oauth2/token',str(requestdata))
+				response=response.replace("null",'""')
+				response=eval(response)	
+				auth="Bearer"+' '+str(response['access_token'])
+
+				get_party_role = Sql.GetList("SELECT CPQ_PARTNER_FUNCTION, PARTY_ID FROM SAQTIP(NOLOCK) WHERE QUOTE_RECORD_ID = '"+str(self.contract_quote_record_id)+"' AND QTEREV_RECORD_ID = '"+str(self.quote_revision_record_id)+"' and CPQ_PARTNER_FUNCTION in ('SOLD TO')")
+				account_info = {}
+				for keyobj in get_party_role:
+					account_info[keyobj.CPQ_PARTNER_FUNCTION] = keyobj.PARTY_ID
+				
+				get_party_role = Sql.GetList("SELECT CPQ_PARTNER_FUNCTION, PARTY_ID FROM SAQTIP(NOLOCK) WHERE QUOTE_RECORD_ID = '"+str(self.contract_quote_record_id)+"' AND QTEREV_RECORD_ID = '"+str(self.quote_revision_record_id)+"' and CPQ_PARTNER_FUNCTION in ('SHIP TO')")
+				shipto_list=[]
+				for keyobj in get_party_role:
+					shipto_list.append('00'+str(keyobj.PARTY_ID))
+				shiptostr=str(shipto_list)
+				shiptostr=re.sub(r"'",'"',shiptostr)
+				account_info['SHIP TO']=shiptostr
+				
+
+				get_sales_ifo = Sql.GetFirst("select SALESORG_ID,CONTRACT_VALID_TO,CONTRACT_VALID_FROM,PRICELIST_ID,PRICEGROUP_ID from SAQTRV where QUOTE_RECORD_ID = '"+str(self.contract_quote_record_id)+"' AND QUOTE_REVISION_RECORD_ID = '"+str(self.quote_revision_record_id)+"'")
+				
+				if get_sales_ifo:
+					salesorg = get_sales_ifo.SALESORG_ID
+					pricelist =get_sales_ifo.PRICELIST_ID
+					pricegroup =get_sales_ifo.PRICEGROUP_ID
+					cv=str(get_sales_ifo.CONTRACT_VALID_FROM)
+					(cm,cd,cy)=re.sub(r'\s+([^>]*?)$','',cv).split('/')
+					cd = '0'+str(cd) if len(cd)==1 else cd
+					cm = '0'+str(cm) if len(cm)==1 else cm        
+					validfrom = cy+cm+cd
+					cv=str(get_sales_ifo.CONTRACT_VALID_TO)
+					(cm,cd,cy)=re.sub(r'\s+([^>]*?)$','',cv).split('/')
+					cd = '0'+str(cd) if len(cd)==1 else cd
+					cm = '0'+str(cm) if len(cm)==1 else cm        
+					validto = cy+cm+cd
+				
+				part_numbers=''
+				part_numbers= str([spare_part for spare_part in self.values[0].splitlines()])
+				part_numbers=part_numbers.replace("'",'"')
+				Trace.Write('### Part Number for CQIFLSPARE-->'+str(part_numbers))
+				CQIFLSPARE.iflow_pullspareparts_call(str(User.UserName),str(account_info.get('SOLD TO')),str(account_info.get('SHIP TO')),salesorg, pricelist,pricegroup,'Yes','Yes',part_numbers,validfrom,validto,self.contract_quote_id,self.quote_revision_record_id,auth)
+			else:	
+				self._process_query("""
+							INSERT SAQSPT (QUOTE_SERVICE_PART_RECORD_ID, BASEUOM_ID, BASEUOM_RECORD_ID, CUSTOMER_PART_NUMBER, CUSTOMER_PART_NUMBER_RECORD_ID, DELIVERY_MODE, EXTENDED_UNIT_PRICE, PART_DESCRIPTION, PART_NUMBER, PART_RECORD_ID, PRDQTYCON_RECORD_ID, CUSTOMER_ANNUAL_QUANTITY, QUOTE_ID, QUOTE_NAME, QUOTE_RECORD_ID,QTEREV_ID,QTEREV_RECORD_ID,SALESORG_ID, SALESORG_RECORD_ID, SALESUOM_CONVERSION_FACTOR, SALESUOM_ID, SALESUOM_RECORD_ID, SCHEDULE_MODE, SERVICE_DESCRIPTION, SERVICE_ID, SERVICE_RECORD_ID, UNIT_PRICE, MATPRIGRP_ID, MATPRIGRP_RECORD_ID, DELIVERY_INTERVAL, VALID_FROM_DATE, VALID_TO_DATE,PAR_SERVICE_DESCRIPTION,PAR_SERVICE_ID,PAR_SERVICE_RECORD_ID, CPQTABLEENTRYADDEDBY, CPQTABLEENTRYDATEADDED)
+							SELECT DISTINCT
+								CONVERT(VARCHAR(4000),NEWID()) as QUOTE_SERVICE_PART_RECORD_ID,
+								BASEUOM_ID,
+								BASEUOM_RECORD_ID,
+								CUSTOMER_PART_NUMBER,
+								CUSTOMER_PART_NUMBER_RECORD_ID,
+								DELIVERY_MODE,
+								EXTENDED_UNIT_PRICE,
+								PART_DESCRIPTION,
+								PART_NUMBER,
+								PART_RECORD_ID,
+								PRDQTYCON_RECORD_ID,
+								QUANTITY,
+								QUOTE_ID,
+								QUOTE_NAME,
+								QUOTE_RECORD_ID,
+								QTEREV_ID,
+								QTEREV_RECORD_ID,
+								SALESORG_ID,
+								SALESORG_RECORD_ID,
+								SALESUOM_CONVERSION_FACTOR,
+								SALESUOM_ID,
+								SALESUOM_RECORD_ID, 
+								SCHEDULE_MODE,
+								SERVICE_DESCRIPTION,
+								SERVICE_ID,
+								SERVICE_RECORD_ID,
+								UNIT_PRICE,
+								MATPRIGRP_ID,
+								MATPRIGRP_RECORD_ID,
+								DELIVERY_INTERVAL,
+								VALID_FROM_DATE, 
+								VALID_TO_DATE,
+								PAR_SERVICE_DESCRIPTION,
+								PAR_SERVICE_ID,
+								PAR_SERVICE_RECORD_ID,
+								{UserId} as CPQTABLEENTRYADDEDBY, 
+								GETDATE() as CPQTABLEENTRYDATEADDED
+							FROM (
+							SELECT 
+								DISTINCT
+								MAMTRL.UNIT_OF_MEASURE as BASEUOM_ID,
+								MAMTRL.UOM_RECORD_ID as BASEUOM_RECORD_ID,
+								NULL as CUSTOMER_PART_NUMBER,
+								MAMTRL.MATERIAL_RECORD_ID as CUSTOMER_PART_NUMBER_RECORD_ID,
+								'{delivery_mode}' as DELIVERY_MODE,
+								0.00 as EXTENDED_UNIT_PRICE,
+								MAMTRL.SAP_DESCRIPTION as PART_DESCRIPTION,
+								MAMTRL.SAP_PART_NUMBER as PART_NUMBER,
+								MAMTRL.MATERIAL_RECORD_ID as PART_RECORD_ID,
+								'' as PRDQTYCON_RECORD_ID,
+								null as QUANTITY,
+								SAQTMT.QUOTE_ID as QUOTE_ID,
+								SAQTMT.QUOTE_NAME as QUOTE_NAME,
+								SAQTMT.MASTER_TABLE_QUOTE_RECORD_ID as QUOTE_RECORD_ID,
+								SAQTMT.QTEREV_ID as QTEREV_ID,
+								SAQTMT.QTEREV_RECORD_ID as QTEREV_RECORD_ID,
+								SAQTSV.SALESORG_ID as SALESORG_ID,
+								SAQTSV.SALESORG_RECORD_ID as SALESORG_RECORD_ID,
+								1.00 as SALESUOM_CONVERSION_FACTOR,
+								MAMTRL.UNIT_OF_MEASURE as SALESUOM_ID,
+								MAMTRL.UOM_RECORD_ID as SALESUOM_RECORD_ID, 
+								'{schedule_mode}' as SCHEDULE_MODE,
+								SAQTSV.SERVICE_DESCRIPTION as SERVICE_DESCRIPTION,
+								SAQTSV.SERVICE_ID as SERVICE_ID,
+								SAQTSV.SERVICE_RECORD_ID as SERVICE_RECORD_ID,
+								0.00 as UNIT_PRICE,
+								MAMSOP.MATPRIGRP_ID as MATPRIGRP_ID,
+								MAMSOP.MATPRIGRP_RECORD_ID as MATPRIGRP_RECORD_ID,
+								'MONTHLY' as DELIVERY_INTERVAL,
+								SAQTMT.CONTRACT_VALID_FROM as VALID_FROM_DATE, 
+								SAQTMT.CONTRACT_VALID_TO as VALID_TO_DATE,
+								SAQTSV.PAR_SERVICE_DESCRIPTION as PAR_SERVICE_DESCRIPTION,
+								SAQTSV.PAR_SERVICE_ID as PAR_SERVICE_ID,
+								SAQTSV.PAR_SERVICE_RECORD_ID as PAR_SERVICE_RECORD_ID
+							FROM SYSPBT (NOLOCK)
+							JOIN MAMTRL (NOLOCK) ON MAMTRL.SAP_PART_NUMBER = SYSPBT.SAP_PART_NUMBER
+							JOIN SAQTMT (NOLOCK) ON SAQTMT.MASTER_TABLE_QUOTE_RECORD_ID = SYSPBT.QUOTE_RECORD_ID
+							JOIN SAQTSV (NOLOCK) ON SAQTSV.QUOTE_RECORD_ID = SAQTMT.MASTER_TABLE_QUOTE_RECORD_ID AND SAQTSV.QTEREV_RECORD_ID = SAQTMT.QTEREV_RECORD_ID AND SAQTSV.SERVICE_ID = '{ServiceId}'
+							JOIN MAMSOP (NOLOCK) ON MAMSOP.MATERIAL_RECORD_ID = MAMTRL.MATERIAL_RECORD_ID AND MAMSOP.SALESORG_RECORD_ID = SAQTSV.SALESORG_RECORD_ID
+							WHERE SYSPBT.BATCH_STATUS = 'IN PROGRESS' AND SYSPBT.BATCH_GROUP_RECORD_ID = '{BatchGroupRecordId}' AND SYSPBT.QUOTE_RECORD_ID = '{QuoteRecordId}' AND SYSPBT.QTEREV_RECORD_ID = '{RevisionRecordId}' AND MAMTRL.PRODUCT_TYPE IS NULL AND MAMTRL.IS_SPARE_PART = 1 ) IQ
+							""".format(
+				ServiceId=self.tree_param,
+				BatchGroupRecordId=batch_group_record_id,
+				QuoteRecordId=self.contract_quote_record_id,RevisionRecordId=self.quote_revision_record_id,
+				UserId=self.user_id, delivery_mode= delivery_mode, schedule_mode = schedule_mode
+				)
+				)
 def Factory(node=None):
 	"""Factory Method"""
 	models = {
